3764dc91fb5b3b55dce91ce321e2c1bc1378acef
Alessio Buccino <alejoe9187@gmail.com>
2019-08-01 16:22:56 +0200

    remove flat API

--- spikeinterface/__init__.py
+++ spikeinterface/__init__.py
@@ -1,9 +1,1 @@
 from .version import version as __version__
-
-from .extractors import *
-from .toolkit import *
-from .toolkit import *
-from .sorters import *
-from .widgets import *
-from .comparison import *
-
