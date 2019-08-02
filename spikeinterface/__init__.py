c2dbf4352f40643714ab1e3c17671b5200c1808c
Alessio Buccino <alejoe9187@gmail.com>
2019-08-02 14:03:28 +0200

    comment out imports

--- spikeinterface/__init__.py
+++ spikeinterface/__init__.py
@@ -1,1 +1,8 @@
 from .version import version as __version__
+
+# from .extractors import *
+# from .toolkit import *
+# from .toolkit import *
+# from .sorters import *
+# from .widgets import *
+# from .comparison import *
