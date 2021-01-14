d00f1cbafc1890636916aeb11cda547c4e7c264a
Samuel Garcia <sam.garcia.die@gmail.com>
2021-01-14 12:02:10 +0100

    wip.

--- spikeinterface/core/recording.py
+++ spikeinterface/core/recording.py
@@ -3,29 +3,48 @@ from typing import List, Union
 from .mytypes import ChannelId, ChannelIndex, Order, SamplingFrequencyHz
 from .base import Base
 
+from .core_tools import write_to_binary_dat_format
 
 
-class Recording(Base):
+class Recording(ExtractorBase):
     """
     Abstract class representing several a multichannel timeseries (or block of raw ephys traces).
     Internally handle list of RecordingSegment
     """
+    _main_annotations = ['is_filtered']
+    _main_properties = ['group', 'location']
+    _main_features = [] # recording do not handle features
+    
     def __init__(self, sampling_frequency: SamplingFrequencyHz, channel_ids: List[ChannelId]):
-        self._signal_segments: List[RecordingSegment] = []
-        self._channel_ids = channel_ids
+        ExtractorBase.__init__(self, channel_ids)
+        
+        self._recording_segments: List[RecordingSegment] = []
+        
+        # initialize main annoation and properties
+        self.annotate(is_filtered=False)
+        #~ self.
+    
+    
+    def get_num_segments(self):
+        return len(self._recording_segments)
 
     def add_recording_segment(self, signal_segment: RecordingSegment):
         # todo: check channel count and sampling frequency
-        self._signal_segments.append(signal_segment)
+        self._recording_segments.append(signal_segment)
+
+    def get_sampling_frequency(self):
+        return self._sampling_frequency
+
+    @property
+    def channel_ids(self):
+        return self._main_ids 
 
     def get_channel_ids(self):
-        return self._channel_ids
+        return self._main_ids
     
     def get_num_channels(self):
         return len(self.get_channel_ids())
 
-    def get_num_segments(self):
-        return len(self._signal_segments)
     
     def _check_segment_index(self, segment_index: Union[int, None]) -> int:
         if segment_index is None:
@@ -38,7 +57,7 @@ class Recording(Base):
 
     def get_num_samples(self, segment_index: Union[int, None]):
         segment_index = self._check_segment_index(segment_index)
-        return self._signal_segments[segment_index].get_num_samples()
+        return self._recording_segments[segment_index].get_num_samples()
 
     def get_traces(self,
             segment_index: Union[int, None]=None,
@@ -48,9 +67,82 @@ class Recording(Base):
             order: Order = Order.K
         ):
         segment_index = self._check_segment_index(segment_index)
-        channel_indices = [ChannelIndex(self._channel_ids.index(id)) for id in channel_ids]
-        S = self._signal_segments[segment_index]
-        return S.get_traces(start=start, end=end, channel_indices=channel_indices, order=order)
+        channel_indices = self.ids_to_indices(channel_ids, prefer_silce=True)
+        rs = self._recording_segments[segment_index]
+        return rs.get_traces(start=start, end=end, channel_indices=channel_indices, order=order)
+
+    def is_filtered(self):
+        # the is_filtered is handle with annotation
+        return self._annotations.get('is_filtered', False)
+    
+    def _save_data(self, folder, format='binary', **cache_kargs):
+        """
+        This replace the old CacheRecordingExtractor but enable more engine 
+        for caching a results. at the moment only binaray with memmap is supported.
+        My plan is to add also zarr support.
+        """
+        if format == 'binary':
+            files_path = [ folder / f'traces_{i}.raw' for i in range(self.get_num_segment())]
+            dtype = cache_kargs.get('dtype', 'float32')
+            keys = ['chunk_size', 'chunk_mb', 'n_jobs', 'joblib_backend']
+            job_kwargs = {cache_kargs[k] for k in keys if k in cache_kargs}
+            write_to_binary_dat_format(self, files_path, time_axis=0, dtype=dtype, **job_kwargs)
+            
+            from . binaryrecordingextractor import BinaryRecordingExtractor
+            cached = BinaryRecordingExtractor(files_path, self.get_sampling_frequency(),
+                                self.get_num_channels(), dtype, channel_ids=self._channel_ids, time_axis=0)
+            
+        elif format == 'zarr':
+            # TODO implement a format based on zarr
+            raise NotImplementedError
+        else:
+            raise ValueError(f'format {format} not supported')
+        
+        return cached
+    
+    ## for backward compatibilities
+    
+    def set_channel_property(self, channel_id, property_name, value):
+        print('depreciated please use recording.set_property(..) in the vector way')
+        self.set_property(property_name, [value], ids=[value])
+    
+    def get_channel_property_names(self):
+        print(' get_channel_property_names() depreciated please use get_property_keys')
+        return self.get_property_keys()
+    
+    def set_channel_locations(self, locations, channel_ids=None):
+        self.set_property('location', locations,  ids=channel_ids)
+        
+    def get_channel_locations(self, channel_ids=None, locations_2d=True):
+        return self.get_property('location')
+        
+    def clear_channel_locations(self, channel_ids=None):
+        if channel_ids is None:
+            n = self.get_num_channel()
+        else:
+            n = len(channel_ids)
+        locations = np.zeros((n, 2)) * np.nan
+        self.set_property('location', locations,  ids=channel_ids)
+    
+    def set_channel_groups(self, groups, channel_ids=None):
+        self.set_property('group', groups,  ids=channel_ids)
+        
+    def get_channel_groups(self, channel_ids=None):
+        return self.get_property('group')
+        
+    def clear_channel_groups(self, channel_ids=None):
+        if channel_ids is None:
+            n = self.get_num_channel()
+        else:
+            n = len(channel_ids)
+        groups = np.zeros(n, dtype='int64')
+        self.set_property('group', groups,  ids=channel_ids)
+    
+    def set_channel_gains(self, gains, channel_ids=None):
+        self.set_property('gain', groups,  ids=channel_ids)
+    
+    def get_channel_gains(self, channel_ids=None):
+        return self.get_property('gain')
 
 
 class RecordingSegment(object):
@@ -67,22 +159,7 @@ class RecordingSegment(object):
         Returns:
             SampleIndex: Number of samples in the signal block
         """
-        raise NotImplementedError
-
-    def get_num_channels(self) -> ChannelIndex:
-        """Returns the number of channels in this signal block
-
-        Returns:
-            ChannelIndex: Number of channels in the signal block
-        """
-        raise NotImplementedError
-
-    def get_sampling_frequency(self) -> SamplingFrequencyHz:
-        """Returns the sampling frequency in Hz for this signal block
-
-        Returns:
-            SamplingFrequencyHz: The sampling frequency for this signal block
-        """
+        # must be implemented in subclass
         raise NotImplementedError
 
     def get_traces(self,
@@ -102,4 +179,5 @@ class RecordingSegment(object):
         Returns:
             np.ndarray: Array of traces, num_samples x num_channels
         """
+        # must be implemented in subclass
         raise NotImplementedError
