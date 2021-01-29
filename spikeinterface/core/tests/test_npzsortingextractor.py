57e07a4ca57ed304327b6f7ca7a1e0ce6c41eb81
Samuel Garcia <sam.garcia.die@gmail.com>
2021-01-29 18:03:00 +0100

    More tests for the core.

--- spikeinterface/core/tests/test_npzsortingextractor.py
+++ spikeinterface/core/tests/test_npzsortingextractor.py
@@ -2,27 +2,14 @@ import pytest
 import numpy as np
 
 from spikeinterface.core import NpzSortingExtractor
+from spikeinterface.core.tests.testing_tools import create_sorting_npz
 
 
 def test_NpzSortingExtractor():
     num_seg = 2
     file_path = 'test_NpzSortingExtractor.npz'
     
-    # create a NPZ sorting file
-    d = {}
-    d['unit_ids'] = np.array([0,1,2], dtype='int64')
-    d['num_segment'] = np.array([2], dtype='int64')
-    d['sampling_frequency'] = np.array([30000.], dtype='float64')
-    for seg_index in range(num_seg):
-        spike_indexes = np.arange(0, 1000, 10)
-        spike_labels = np.zeros(spike_indexes.size, dtype='int64')
-        spike_labels[0::3] = 0
-        spike_labels[1::3] = 1
-        spike_labels[2::3] = 2
-        d[f'spike_indexes_seg{seg_index}'] = spike_indexes
-        d[f'spike_labels_seg{seg_index}'] = spike_labels
-    np.savez(file_path, **d)
-    
+    create_sorting_npz(num_seg, file_path)
     
     sorting = NpzSortingExtractor(file_path)
     
