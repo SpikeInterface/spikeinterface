ccc835b1c9490ad0fa39fa78a6868028e4e4e03c
Samuel Garcia <sam.garcia.die@gmail.com>
2021-03-08 13:43:49 +0100

    add some neo recoring extractors

--- spikeinterface/extractors/neoextractors/__init__.py
+++ spikeinterface/extractors/neoextractors/__init__.py
@@ -2,4 +2,10 @@ from .mearec import MEArecRecordingExtractor, MEArecSortingExtractor
 from .spikeglx import SpikeGLXRecordingExtractor
 from .openephys import OpenEphysLegacyRecordingExtractor, OpenEphysBinaryRecordingExtractor
 from .intan import IntanRecordingExtractor
+from .neuroscope import NeuroScopeRecordingExtractor
+from .plexon import PlexonRecordingExtractor
+from .neuralynx import NeuralynxRecordingExtractor
+from .blackrock import BlackrockRecordingExtractor
+from .mscraw import MCSRawRecordingExtractor
+
 
