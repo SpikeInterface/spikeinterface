595c5b877639c6b273e3e46293422a913f69d741
Samuel Garcia <sam.garcia.die@gmail.com>
2021-03-10 17:04:08 +0100

    wip launcher

--- spikeinterface/extractors/neoextractors/kilosort.py
+++ spikeinterface/extractors/neoextractors/kilosort.py
@@ -10,7 +10,7 @@ class KiloSortSortingExtractor(NeoBaseSortingExtractor):
     NeoRawIOClass = 'PhyRawIO'
     handle_raw_spike_directly = True
     
-    def __init__(self, folder_path, use_natural_unit_ids=True):
+    def __init__(self, folder_path, keep_good_only=False, use_natural_unit_ids=True):
         neo_kwargs = {'dirname' : folder_path}
         
         # sample rate is in "params.py"
@@ -19,7 +19,10 @@ class KiloSortSortingExtractor(NeoBaseSortingExtractor):
             exec(f.read(), None, d)
         sampling_frequency = float(d['sample_rate'])
         
-        
+        if keep_good_only:
+            raise NotImplementedError
+            # TODO must implement annotations in neo to retreive this.
+
         NeoBaseSortingExtractor.__init__(self, 
                     sampling_frequency=sampling_frequency, # auto guess is correct here
                     use_natural_unit_ids=use_natural_unit_ids,
