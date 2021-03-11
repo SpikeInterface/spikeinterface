e3a9b28a7fa6ad58cd154687b126d416720a9c1f
Samuel Garcia <sam.garcia.die@gmail.com>
2021-03-11 14:19:08 +0100

    Continue to put everything in classmethod for BasSorter.

--- spikeinterface/sorters/__init__.py
+++ spikeinterface/sorters/__init__.py
@@ -1,5 +1,8 @@
-from .sorterlist import *
 from .basesorter import BaseSorter
+from .sorterlist import *
+from .runsorter import *
+
+
 #~ from .launcher import (run_sorters, 
         #~ collect_sorting_outputs, iter_output_folders, iter_sorting_output)
 
--- spikeinterface/sorters/tests/test_tridesclous.py
+++ spikeinterface/sorters/tests/test_tridesclous.py
@@ -1,7 +1,7 @@
 import unittest
 
 import pytest
-import spikeinterface.extractors as se
+from spikeinterface.extractors import toy_example
 from spikeinterface.sorters import TridesclousSorter, run_tridesclous
 from spikeinterface.sorters.tests.common_tests import SorterCommonTestSuite
 
@@ -12,22 +12,21 @@ class TridesclousCommonTestSuite(SorterCommonTestSuite, unittest.TestCase):
     SorterClass = TridesclousSorter
 
 
-@pytest.mark.skipif(not TridesclousSorter.is_installed(), reason='tridesclous not installed')
-def test_run_tridesclous():
-    recording, sorting_gt = se.example_datasets.toy_example(num_channels=4, duration=30, seed=0)
+#~ @pytest.mark.skipif(not TridesclousSorter.is_installed(), reason='tridesclous not installed')
+#~ def test_run_tridesclous():
+    #~ recording, sorting_gt = toy_example(num_channels=4, duration=30, seed=0, num_segments=1)
 
-    params = TridesclousSorter.default_params()
-    sorting = run_tridesclous(recording, **params)
+    #~ params = TridesclousSorter.default_params()
+    #~ sorting = run_tridesclous(recording, remove_existing_folder=True, **params)
 
-    print(sorting)
-    print(sorting.get_unit_ids())
-    for unit_id in sorting.get_unit_ids():
-        print('unit #', unit_id, 'nb', len(sorting.get_unit_spike_train(unit_id)))
+    #~ print(sorting)
+    #~ print(sorting.get_unit_ids())
+    #~ for unit_id in sorting.get_unit_ids():
+        #~ print('unit #', unit_id, 'nb', len(sorting.get_unit_spike_train(unit_id)))
 
 
 if __name__ == '__main__':
-    #~ test_run_tridesclous()
-    TridesclousCommonTestSuite().test_on_toy()
+    #~ TridesclousCommonTestSuite().test_on_toy()
     #~ TridesclousCommonTestSuite().test_with_BinDatRecordingExtractor()
     #~ TridesclousCommonTestSuite().test_get_version()
-    # ~ unittest.main()
+    unittest.main()
