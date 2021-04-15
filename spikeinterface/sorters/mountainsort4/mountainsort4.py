06f98078d9c9a3af327a10a1fd4d24d1480dbf33
Samuel Garcia <sam.garcia.die@gmail.com>
2021-04-15 17:04:37 +0200

    Make MS4 working.

--- spikeinterface/sorters/mountainsort4/mountainsort4.py
+++ spikeinterface/sorters/mountainsort4/mountainsort4.py
@@ -2,20 +2,17 @@ import copy
 from pathlib import Path
 
 
-# TODO
-#~ from spiketoolkit.preprocessing import bandpass_filter, whiten
+from spikeinterface.toolkit import bandpass_filter, whiten
 
 from ..basesorter import BaseSorter
 from ..utils import RecordingExtractorOldAPI
 from spikeinterface.core import load_extractor
 
 
-# TODO
-# from spikeinterface.extractors import MdaSortingExtractor
-from spikeinterface.extractors import NpzSortingExtractor
+from spikeinterface.extractors import NpzSortingExtractor, NumpySorting
 
 try:
-    import ml_ms4alg
+    import mountainsort4
 
     HAVE_MS4 = True
 except ImportError:
@@ -67,23 +64,20 @@ class Mountainsort4Sorter(BaseSorter):
     method and automatic curation procedures. For more information see https://doi.org/10.1016/j.neuron.2017.08.030"""
 
     installation_mesg = """\nTo use Mountainsort4 run:\n
-       >>> pip install ml_ms4alg
+       >>> pip install mountainsort4
 
     More information on mountainsort at:
       * https://github.com/flatironinstitute/mountainsort
     """
 
-    #~ def __init__(self, **kargs):
-        #~ BaseSorter.__init__(self, **kargs)
-    
     @classmethod
     def is_installed(cls):
         return HAVE_MS4
     
     @staticmethod
     def get_sorter_version():
-        if hasattr(ml_ms4alg, '__version__'):
-            return ml_ms4alg.__version__
+        if hasattr(mountainsort4, '__version__'):
+            return mountainsort4.__version__
         return 'unknown'
 
     @classmethod
@@ -103,21 +97,24 @@ class Mountainsort4Sorter(BaseSorter):
 
         samplerate = recording.get_sampling_frequency()
         
-        # TODO
         # Bandpass filter
-        #~ if p['filter'] and p['freq_min'] is not None and p['freq_max'] is not None:
-            #~ recording = bandpass_filter(recording=recording, freq_min=p['freq_min'], freq_max=p['freq_max'])
+        if p['filter'] and p['freq_min'] is not None and p['freq_max'] is not None:
+            if verbose:
+                print('filtering')
+            recording = bandpass_filter(recording=recording, freq_min=p['freq_min'], freq_max=p['freq_max'])
 
         # Whiten
-        #~ if p['whiten']:
-            #~ recording = whiten(recording=recording)
+        if p['whiten']:
+            if verbose:
+                print('whitenning')
+            recording = whiten(recording=recording)
             
-        print('Mountainsort4 use the OLD spikeextractors: use RecordingExtractorOldAPI')
+        print('Mountainsort4 use the OLD spikeextractors mapped with RecordingExtractorOldAPI')
         old_api_recording = RecordingExtractorOldAPI(recording)
 
 
         # Check location no more needed done in basesorter
-        sorting = ml_ms4alg.mountainsort4(
+        old_api_sorting = mountainsort4.mountainsort4(
             recording=old_api_recording,
             detect_sign=p['detect_sign'],
             adjacency_radius=p['adjacency_radius'],
@@ -129,36 +126,25 @@ class Mountainsort4Sorter(BaseSorter):
         )
 
         # Curate
-        # TODO
-        #~ if p['noise_overlap_threshold'] is not None and p['curation'] is True:
-            #~ if verbose:
-                #~ print('Curating')
-            #~ sorting = ml_ms4alg.mountainsort4_curation(
-                #~ recording=recording,
-                #~ sorting=sorting,
-                #~ noise_overlap_threshold=p['noise_overlap_threshold']
-            #~ )
+        if p['noise_overlap_threshold'] is not None and p['curation'] is True:
+            if verbose:
+                print('Curating')
+            old_api_sorting = mountainsort4.mountainsort4_curation(
+                recording=old_api_recording,
+                sorting=old_api_sorting,
+                noise_overlap_threshold=p['noise_overlap_threshold']
+            )
 
-        # MdaSortingExtractor.write_sorting(sorting, str(output_folder / 'firings.mda'))
-        # samplerate_fname = str(output_folder / 'samplerate.txt')
-        # with open(samplerate_fname, 'w') as f:
-            # f.write('{}'.format(samplerate))
-     
-        NpzSortingExtractor.write_sorting(sorting, str(output_folder / 'firings.npz'))
+        # convert sorting to new API and save it
+        unit_ids = old_api_sorting.get_unit_ids()
+        units_dict_list = [{u: old_api_sorting.get_unit_spike_train(u) for u in unit_ids}]
+        new_api_sorting = NumpySorting.from_dict(units_dict_list, samplerate)
+        NpzSortingExtractor.write_sorting(new_api_sorting, str(output_folder / 'firings.npz'))
 
    
     @classmethod
     def _get_result_from_folder(cls, output_folder):
         output_folder = Path(output_folder)
-        
-        #~ tmpdir = output_folder
-        #~ result_fname = str(tmpdir / 'firings.mda')
-        #~ samplerate_fname = str(tmpdir / 'samplerate.txt')
-        #~ with open(samplerate_fname, 'r') as f:
-            #~ samplerate = float(f.read())
-        #~  sorting = MdaSortingExtractor(file_path=result_fname, sampling_frequency=samplerate)
-        
         result_fname = output_folder/ 'firings.npz'
-        sorting = NpzSortingExtractor.write_sorting(result_fname)
-        
+        sorting = NpzSortingExtractor(result_fname)
         return sorting
