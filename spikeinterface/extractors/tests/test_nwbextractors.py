7b8e49239c84a096aec5e6c69294a7c217116dff
Samuel Garcia <sam.garcia.die@gmail.com>
2021-05-21 14:36:07 +0200

    Add test for NWB file.

---
+++ spikeinterface/extractors/tests/test_nwbextractors.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+import pytest
+import numpy as np
+
+from spikeinterface import download_dataset
+from spikeinterface.extractors import *
+
+from spikeinterface.extractors.tests.common_tests import RecordingCommonTestSuite, SortingCommonTestSuite
+
+class NwbRecordingTest(RecordingCommonTestSuite, unittest.TestCase):
+    ExtractorClass = NwbRecordingExtractor
+    downloads = []
+    entities = []
+
+class NwbSortingTest(RecordingCommonTestSuite, unittest.TestCase):
+    ExtractorClass = NwbSortingExtractor
+    downloads = []
+    entities = []
+
+# TODO : NwbEventTest
+
+if __name__ == '__main__':
+    test = NwbRecordingTest()
+    #~ test = NwbSortingTest()
+
+
+    test.setUp()
+    test.test_open()
+    
+    
