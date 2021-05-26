82728fb886497b72ff455955a3f1069012dad42d
Samuel Garcia <sam.garcia.die@gmail.com>
2021-05-26 22:18:26 +0200

    smater amplitudes widgets

--- spikeinterface/widgets/amplitudes.py
+++ spikeinterface/widgets/amplitudes.py
@@ -4,15 +4,16 @@ from matplotlib import pyplot as plt
 from .basewidget import BaseWidget, BaseMultiWidget
 
 from ..toolkit import get_unit_amplitudes
+from .utils import get_unit_colors
 
 
 
 
 
-class AmplitudeBaseWidget(BaseMultiWidget):
+class AmplitudeBaseWidget(BaseWidget):
     def __init__(self, waveform_extractor, amplitudes=None, peak_sign='neg',
-            figure=None, ax=None, axes=None, **job_kwargs):
-        BaseMultiWidget.__init__(self, figure, ax, axes)
+            unit_colors=None, figure=None, ax=None, **job_kwargs):
+        BaseWidget.__init__(self, figure, ax)
         
         self.we = waveform_extractor
         if amplitudes is not None:
@@ -22,6 +23,10 @@ class AmplitudeBaseWidget(BaseMultiWidget):
             self.amplitudes = amplitudes
         else:
             self.amplitudes = get_unit_amplitudes(self.we,  peak_sign='neg', outputs='by_units', **job_kwargs)
+        
+        if unit_colors is None:
+            unit_colors = get_unit_colors(self.we.sorting)
+        self.unit_colors = unit_colors
     
     def plot(self):
         self._do_plot()
@@ -52,16 +57,14 @@ class AmplitudeTimeseriesWidget(AmplitudeBaseWidget):
         num_seg = sorting.get_num_segments()
         fs = sorting.get_sampling_frequency()
         
-        nrows, ncols = len(unit_ids), num_seg
-        num_ax = 0
+        # TODO handle segment
+        ax = self.ax
         for i, unit_id in enumerate(unit_ids):
             for segment_index in range(num_seg):
-                ax = self.get_tiled_ax(num_ax, nrows, ncols)
                 times = sorting.get_unit_spike_train(unit_id, segment_index=segment_index)
                 times = times / fs
                 amps = self.amplitudes[segment_index][unit_id]
-                ax.scatter(times, amps)
-                num_ax += 1
+                ax.scatter(times, amps, color=self.unit_colors[unit_id], s=3, alpha=1)
                 
                 if i == 0:
                     ax.set_title(f'segment {segment_index}')
@@ -69,7 +72,6 @@ class AmplitudeTimeseriesWidget(AmplitudeBaseWidget):
                     ax.set_xlabel('Times [s]')
                 if segment_index == 0:
                     ax.set_ylabel(f'{unit_id}')
-                
 
 
 class AmplitudeDistributionWidget(AmplitudeBaseWidget):
@@ -96,21 +98,24 @@ class AmplitudeDistributionWidget(AmplitudeBaseWidget):
         unit_ids = sorting.unit_ids
         num_seg = sorting.get_num_segments()
         
-        nrows, ncols = len(unit_ids), num_seg
-        num_ax = 0
-        for segment_index in range(num_seg):
-            for i, unit_id in enumerate(unit_ids):
-                ax = self.get_tiled_ax(num_ax, nrows, ncols)
-                amps = self.amplitudes[segment_index][unit_id]
-                ax.hist(amps, density=True, color='gray', bins=10)
-                num_ax += 1
-                
-                if i == 0:
-                    ax.set_title(f'segment {segment_index}')
-                if i == len(unit_ids) - 1:
-                    ax.set_xlabel('Amplitudes')
-                if segment_index == 0:
-                    ax.set_ylabel(f'{unit_id}')
+        ax = self.ax
+        unit_amps = []
+        for i, unit_id in enumerate(unit_ids):
+            amps = []
+            for segment_index in range(num_seg):
+                amps.append(self.amplitudes[segment_index][unit_id])
+            amps = np.concatenate(amps)
+            unit_amps.append(amps)
+        parts = ax.violinplot(unit_amps, showmeans=False, showmedians=False, showextrema=False)
+
+        for i, pc in enumerate(parts['bodies']):
+            color = self.unit_colors[unit_ids[i]]
+            pc.set_facecolor(color)
+            pc.set_edgecolor('black')
+            pc.set_alpha(1)
+        
+        ax.set_xticks(np.arange(len(unit_ids)) + 1)
+        ax.set_xticklabels([str(unit_id) for unit_id in unit_ids])
 
 
 def plot_amplitudes_timeseries(*args, **kwargs):
