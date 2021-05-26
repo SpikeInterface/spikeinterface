fe8e27234a7842e07753e0fd84b448ba6c8ab4c7
Samuel Garcia <sam.garcia.die@gmail.com>
2021-05-26 12:48:28 +0200

    Add some widgets in gallery

--- examples/modules/widgets/plot_3_waveforms_gallery.py
+++ examples/modules/widgets/plot_3_waveforms_gallery.py
@@ -51,6 +51,13 @@ unit_ids = sorting.unit_ids
 sw.plot_unit_templates(we, unit_ids=unit_ids, ncols=5)
 
 ##############################################################################
+# plot_unit_templates()
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+unit_ids = sorting.unit_ids[:4]
+sw.plot_unit_map(we, unit_ids=unit_ids)
+
+##############################################################################
 # plot_amplitudes_distribution()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
