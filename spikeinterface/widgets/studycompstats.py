1252ed1b1c7a1b0eb12e94c0b0588bd3628e45f3
yger <pierre.yger@gmail.com>
2021-07-07 15:39:03 +0200

    Summary widget

--- spikeinterface/widgets/studycompstats.py
+++ spikeinterface/widgets/studycompstats.py
@@ -1,12 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-from .basewidget import BaseWidget
+from .basewidget import BaseMultiWidget
 from spikeinterface.toolkit import compute_correlograms
 
 
 
-class StudyComparisonStatisticsWidget(BaseWidget):
+class StudyComparisonStatisticsWidget(BaseMultiWidget):
     """
     Plots difference between real CC matrix, and reconstructed one
 
@@ -28,8 +28,19 @@ class StudyComparisonStatisticsWidget(BaseWidget):
     W: ConfusionMatrixWidget
         The output widget
     """
-    def __init__(self, study, exhaustive_gt=False, figure=None, ax=None):
-        BaseWidget.__init__(self, figure, ax)
+    def __init__(self, study, exhaustive_gt=False, figure=None, ax=None, axes=None):
+
+        if exhaustive_gt:
+            self._ncols = 3
+        else:
+            self.n_cols = 2
+
+        self._nrows = 3
+
+        if axes is None and ax is None:
+            figure, axes = plt.subplots(nrows=self._nrows, ncols=self._ncols)
+        
+        BaseMultiWidget.__init__(self, figure, ax, axes)
         self._study = study
         self.sorter_names = study.sorter_names
         self.rec_names = study.rec_names
@@ -78,6 +89,13 @@ class StudyComparisonStatisticsWidget(BaseWidget):
                 res[sorter][key]['std'] = data.loc[:, sorter, :][key].std()
             return res
 
+    def get_all(self):
+        res = self.run_times()
+        for sorter in self._study.sorter_names:
+            res[sorter].update(self.performance_by_units()[sorter])
+            res[sorter].update(self.count_units()[sorter])
+        return res
+
     def get_metrics(self, **kwargs):
         res = {}
         for rec in self._study.rec_names:
@@ -88,6 +106,30 @@ class StudyComparisonStatisticsWidget(BaseWidget):
         self._do_plot()
 
     def _do_plot(self):
-
-        fig = self.figure
         
+        data = self.get_all()
+        if self._exhaustive_gt:
+            to_display = ['run_time', 'num_well_detected', 'num_redundant', 'num_overmerged',
+                        'num_bad', 'num_false_positive', 'accuracy', 'precision', 'recall']
+        else:
+            to_display = ['run_time', 'num_well_detected', 'num_redundant', 'num_overmerged',
+                        'accuracy']
+
+        nb_sorters = len(self._study.sorter_names)
+
+        colors = ['C%d' %i for i in range(nb_sorters)]
+
+        for scount, key in enumerate(to_display):
+
+            ax = self.get_tiled_ax(scount, self._nrows, self._ncols)
+
+            means = [data[sorter][key]['mean'] for sorter in self._study.sorter_names]
+            stds = [data[sorter][key]['std'] for sorter in self._study.sorter_names]
+            ax.bar(np.arange(nb_sorters), np.nan_to_num(means), yerr=np.nan_to_num(stds), color=colors)
+            ax.set_ylabel(key)
+
+            if (scount // self._ncols) == (self._nrows - 1):
+                ax.set_xticks(np.arange(nb_sorters))
+                ax.set_xticklabels(self._study.sorter_names, rotation=45)
+            else:
+                ax.tick_params(labelbottom=False)
