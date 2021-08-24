0f31eea086964256061170b4eb2efee7531c154b
Alessio Buccino <alejoe9187@gmail.com>
2021-08-24 17:09:02 +0200

    Add has_scaled_traces and fix test_utils

--- spikeinterface/toolkit/tests/test_utils.py
+++ spikeinterface/toolkit/tests/test_utils.py
@@ -25,7 +25,12 @@ def test_get_closest_channels():
 def test_get_noise_levels():
     rec = generate_recording(num_channels=2, sampling_frequency=1000., durations=[60.])
 
-    noise_levels = get_noise_levels(rec)
+    noise_levels = get_noise_levels(rec, return_scaled=False)
+    print(noise_levels)
+
+    rec.set_channel_gains(0.1)
+    rec.set_channel_offsets(0)
+    noise_levels = get_noise_levels(rec, return_scaled=True)
     print(noise_levels)
 
 
