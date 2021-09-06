1144308bc5239c254278a8baba057e0bcb779dd7
Saksham Sharda <saksham20.sharda@gmail.com>
2021-09-06 09:59:16 +0530

    added quantile value for waveforms

--- spikeinterface/core/tests/test_waveform_extractor.py
+++ spikeinterface/core/tests/test_waveform_extractor.py
@@ -69,6 +69,11 @@ def test_WaveformExtractor():
     wfs_std = we.get_all_templates(mode='std')
     assert wfs_std.shape == (5, 210, 2)
 
+    wf_qnt = we.get_template(0, mode='quantile', quantile_value = 0.9)
+    assert wf_qnt.shape == (210, 2)
+    wf_qnt = we.get_all_templates(mode='std')
+    assert wf_qnt.shape == (5, 210, 2)
+
 def test_extract_waveforms():
     # 2 segments
 
