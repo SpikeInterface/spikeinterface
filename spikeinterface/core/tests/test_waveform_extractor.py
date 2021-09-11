7c8cc89ddccfdf40a80fafe5d8eacb3bd90b9e24
Saksham Sharda <saksham20.sharda@gmail.com>
2021-09-11 18:08:10 +0530

    Revert "adding segment option for get templates"
    This reverts commit efcbbbc3

--- spikeinterface/core/tests/test_waveform_extractor.py
+++ spikeinterface/core/tests/test_waveform_extractor.py
@@ -64,14 +64,14 @@ def test_WaveformExtractor():
     templates = we.get_all_templates()
     assert templates.shape == (5, 210, 2)
 
-    wf_std = we.get_template(0, mode='std', segment_index=1)
+    wf_std = we.get_template(0, mode='std')
     assert wf_std.shape == (210, 2)
     wfs_std = we.get_all_templates(mode='std')
     assert wfs_std.shape == (5, 210, 2)
 
-    wf_qnt = we.get_template(0, mode='quantile', quantile_value = 0.9, segment_index=0)
+    wf_qnt = we.get_template(0, mode='quantile', quantile_value = 0.9)
     assert wf_qnt.shape == (210, 2)
-    wf_qnt = we.get_all_templates(mode='quantile', segment_index=1)
+    wf_qnt = we.get_all_templates(mode='quantile')
     assert wf_qnt.shape == (5, 210, 2)
 
 def test_extract_waveforms():
