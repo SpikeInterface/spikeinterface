a13fffebcaf23bca32f0e7720ab073db472d1fee
Samuel Garcia <sam.garcia.die@gmail.com>
2021-11-11 12:19:43 +0100

    handle margin for peak localisation

--- spikeinterface/sortingcomponents/peak_localization.py
+++ spikeinterface/sortingcomponents/peak_localization.py
@@ -3,11 +3,12 @@ import numpy as np
 from spikeinterface.core.job_tools import ChunkRecordingExecutor, _shared_job_kwargs_doc
 from spikeinterface.toolkit import get_noise_levels, get_channel_distances
 
-
 import scipy.optimize
 
+from ..toolkit import get_chunk_with_margin
+
 def localize_peaks(recording, peaks, method='center_of_mass',
-                   local_radius_um=150, ms_before=0.3, ms_after=0.6,
+                   local_radius_um=150, ms_before=0.1, ms_after=0.3,
                    **job_kwargs):
     """
     Localize peak (spike) in 2D or 3D depending the probe.ndim of the recording.
@@ -34,7 +35,7 @@ def localize_peaks(recording, peaks, method='center_of_mass',
     peak_locations: np.array
         Array with estimated x-y location for each spike
     """
-    assert method in ('center_of_mass', 'lsq_optimize')
+    assert method in ('center_of_mass', 'monopolar_triangulation')
 
     # find channel neighbours
     assert local_radius_um is not None
@@ -44,7 +45,10 @@ def localize_peaks(recording, peaks, method='center_of_mass',
     nbefore = int(ms_before * recording.get_sampling_frequency() / 1000.)
     nafter = int(ms_after * recording.get_sampling_frequency() / 1000.)
 
-    contact_locations = recording.get_probe().contact_positions
+    contact_locations = recording.get_channel_locations()
+    
+    # margin at border for get_trace
+    margin = max(nbefore, nafter)
 
     # TODO
     #  make a memmap for peaks to avoid serilisation
@@ -52,7 +56,7 @@ def localize_peaks(recording, peaks, method='center_of_mass',
     # and run
     func = _localize_peaks_chunk
     init_func = _init_worker_localize_peaks
-    init_args = (recording.to_dict(), peaks, method, nbefore, nafter, neighbours_mask, contact_locations)
+    init_args = (recording.to_dict(), peaks, method, nbefore, nafter, neighbours_mask, contact_locations, margin)
     processor = ChunkRecordingExecutor(recording, func, init_func, init_args, handle_returns=True,
                                        job_name='localize peaks', **job_kwargs)
     peak_locations = processor.run()
@@ -65,7 +69,7 @@ def localize_peaks(recording, peaks, method='center_of_mass',
 localize_peaks.__doc__ = localize_peaks.__doc__.format(_shared_job_kwargs_doc)
 
 
-def _init_worker_localize_peaks(recording, peaks, method, nbefore, nafter, neighbours_mask, contact_locations):
+def _init_worker_localize_peaks(recording, peaks, method, nbefore, nafter, neighbours_mask, contact_locations, margin):
     # create a local dict per worker
     worker_ctx = {}
     if isinstance(recording, dict):
@@ -78,7 +82,8 @@ def _init_worker_localize_peaks(recording, peaks, method, nbefore, nafter, neigh
     worker_ctx['nafter'] = nafter
     worker_ctx['neighbours_mask'] = neighbours_mask
     worker_ctx['contact_locations'] = contact_locations
-
+    worker_ctx['margin'] = margin
+    
     return worker_ctx
 
 
@@ -91,10 +96,13 @@ def _localize_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
     nafter = worker_ctx['nafter']
     neighbours_mask = worker_ctx['neighbours_mask']
     contact_locations = worker_ctx['contact_locations']
+    margin =  worker_ctx['margin']
 
     # load trace in memory
-    traces = recording.get_traces(start_frame=start_frame, end_frame=end_frame, segment_index=segment_index)
-
+    # traces = recording.get_traces(start_frame=start_frame, end_frame=end_frame, segment_index=segment_index)
+    recording_segment = recording._recording_segments[segment_index]
+    traces, left_margin, right_margin = get_chunk_with_margin(recording_segment, start_frame, end_frame, None, margin, add_zeros=True)
+    
     # get local peaks (sgment + start_frame/end_frame)
     i0 = np.searchsorted(peaks['segment_ind'], segment_index)
     i1 = np.searchsorted(peaks['segment_ind'], segment_index + 1)
@@ -102,15 +110,19 @@ def _localize_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
     i0 = np.searchsorted(peak_in_segment['sample_ind'], start_frame)
     i1 = np.searchsorted(peak_in_segment['sample_ind'], end_frame)
     local_peaks = peak_in_segment[i0:i1]
-
+    
+    print('_localize_peaks_chunk', start_frame, end_frame, local_peaks.size, peaks.size)
+    
     # make sample index local to traces
     local_peaks.copy()
-    local_peaks['sample_ind'] -= start_frame
+    local_peaks['sample_ind'] -= (start_frame - left_margin)
 
+    
+    
     if method == 'center_of_mass':
         peak_locations = localize_peaks_center_of_mass(traces, local_peaks, contact_locations, neighbours_mask)
-    elif method == 'lsq_optimize':
-        peak_locations = localize_peaks_lsq_optimize(traces, local_peaks, contact_locations, neighbours_mask, nbefore, nafter)
+    elif method == 'monopolar_triangulation':
+        peak_locations = localize_peaks_monopolar_triangulation(traces, local_peaks, contact_locations, neighbours_mask, nbefore, nafter)
 
     return peak_locations
 
@@ -134,21 +146,28 @@ def localize_peaks_center_of_mass(traces, local_peak, contact_locations, neighbo
     return peak_locations
 
 
-def _minimize_dist(vec, wf, local_contact_locations):
+def _minimize_dist(vec, wf_ptp, local_contact_locations):
     # vec dims ar (x, z, y, amplitude_factor)
     # given that for contact_location x=dim0 + z=dim1 and y is orthogonal to probe
-    ptp_estimated = vec[3] / (((local_contact_locations - vec[np.newaxis, :2])**2).sum(axis=1) + vec[2]**2)**0.5
-    err = wf.ptp(axis=0) - ptp_estimated
+    dist = np.sqrt(((local_contact_locations - vec[np.newaxis, :2])**2).sum(axis=1) + vec[2]**2)
+    ptp_estimated = vec[3] / dist
+    err = wf_ptp  - ptp_estimated
     return err
 
 
-def localize_peaks_lsq_optimize(traces, local_peak, contact_locations, neighbours_mask, nbefore, nafter):
+def localize_peaks_monopolar_triangulation(traces, local_peak, contact_locations, neighbours_mask, nbefore, nafter):
+    """
+    This method is from Julien Boussard
+    https://www.biorxiv.org/content/10.1101/2021.11.05.467503v1
+    But without denoise of the spike waveform.
+    """
     #ndim = contact_locations.shape[1]
     ndim = 3
     peak_locations = np.zeros((local_peak.size, ndim), dtype='float64')
 
     # TODO find something faster
     for i, peak in enumerate(local_peak):
+        #~ print('i', i)
         chan_mask = neighbours_mask[peak['channel_ind'], :]
         chan_inds, = np.nonzero(chan_mask)
 
@@ -156,10 +175,10 @@ def localize_peaks_lsq_optimize(traces, local_peak, contact_locations, neighbour
 
         # wf is (nsample, nchan) - chann is only nieghboor
         wf = traces[peak['sample_ind']-nbefore:peak['sample_ind']+nafter, :][:, chan_inds]
+        wf_ptp = wf.ptp(axis=0)
 
         # initial guess is the center of mass
-        amps = wf.ptp(axis=0)
-        com = np.sum(amps[:, np.newaxis] * local_contact_locations, axis=0) / np.sum(amps)
+        com = np.sum(wf_ptp[:, np.newaxis] * local_contact_locations, axis=0) / np.sum(wf_ptp)
         x0 = np.zeros(4, dtype='float32')
         x0[:2] = com
         x0[2] = 20
@@ -167,15 +186,20 @@ def localize_peaks_lsq_optimize(traces, local_peak, contact_locations, neighbour
         
 
         # bounds depend on geometry
-        bounds = ([x0[0] - 100, x0[1]-100, 1, 1], x0  + [x0[0] + 100,  x0[1] + 100, 500, 10000])
+        max_border = 300
+        max_distance = 1000
+        max_alpha = max(wf_ptp) * max_distance
+        bounds = ([x0[0] - max_border, x0[1] - max_border, 1, 0], x0  + [x0[0] + max_border,  x0[1] + max_border, 500, max_alpha])
         # print('x0', x0)
         # print('bounds',bounds)
 
 
         # 
         # print('z_initial', z_initial)
-        args = (wf, local_contact_locations)
+        
+        args = (wf_ptp, local_contact_locations)
         output = scipy.optimize.least_squares(_minimize_dist, x0=x0, bounds=bounds, args = args)
+        #~ print(output['x'][3],  max(wf_ptp) * max_distance)
         # print('i', com, output['x'][:2])
         # print('yep')
         # print('output', output)
