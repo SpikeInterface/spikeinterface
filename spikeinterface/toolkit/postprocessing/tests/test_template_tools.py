9ebc44706f2ce7bcd905669d4732a9df18e44521
Samuel Garcia <sam.garcia.die@gmail.com>
2021-11-19 10:25:12 +0100

    * Refactoring unit_localization * Use peak to peak for center of mass

--- spikeinterface/toolkit/postprocessing/tests/test_template_tools.py
+++ spikeinterface/toolkit/postprocessing/tests/test_template_tools.py
@@ -10,7 +10,7 @@ from spikeinterface.toolkit.postprocessing import (get_template_amplitudes,
                                                    get_template_extremum_channel,
                                                    get_template_extremum_channel_peak_shift,
                                                    get_template_extremum_amplitude,
-                                                   compute_unit_centers_of_mass, get_template_channel_sparsity)
+                                                   get_template_channel_sparsity)
 
 
 def setup_module():
