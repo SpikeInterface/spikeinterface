1f1140d81ab7a477d95a6d399d0adae550851f5e
Kyu Hyun Lee <kyuhyun9056@gmail.com>
2021-11-23 02:17:11 -0800

    Update metrics list

--- spikeinterface/toolkit/qualitymetrics/quality_metric_list.py
+++ spikeinterface/toolkit/qualitymetrics/quality_metric_list.py
@@ -7,13 +7,13 @@ from .misc_metrics import (
     compute_amplitudes_cutoff,
 )
 
-from .pca_metrics import calculate_pc_metrics, _possible_pc_metric_names
+from .pca_metrics import (mahalanobis_metrics,
+                          lda_metrics,
+                          nearest_neighbors_metrics,
+                          nearest_neighbors_isolation,
+                          nearest_neighbors_noise_overlap)
 
-# based on PCA
-# "isolation_distance", "l_ratio", "d_prime",  "nn_hit_rate", "nn_miss_rate",
-
-
-# in misc_metrics.py
+# list of all available metrics and mapping to function
 _metric_name_to_func = {
     "num_spikes": compute_num_spikes,
     "firing_rate": compute_firing_rate,
@@ -21,6 +21,13 @@ _metric_name_to_func = {
     "snr": compute_snrs,
     "isi_violation": compute_isi_violations,
     "amplitude_cutoff": compute_amplitudes_cutoff,
+    'isolation_distance': mahalanobis_metrics,
+    'l_ratio': mahalanobis_metrics,
+    'd_prime': lda_metrics,
+    'nn_hit_rate': nearest_neighbors_metrics,
+    'nn_miss_rate': nearest_neighbors_metrics,
+    'nn_isolation': nearest_neighbors_isolation,
+    'nn_noise_overlap': nearest_neighbors_noise_overlap
 }
 
 # TODO
