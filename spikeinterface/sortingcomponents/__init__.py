be83f5ed03a743214c922e647efbd3c4b1fbeacd
Samuel Garcia <sam.garcia.die@gmail.com>
2021-11-30 13:37:16 +0100

    Start motion estimation.

--- spikeinterface/sortingcomponents/__init__.py
+++ spikeinterface/sortingcomponents/__init__.py
@@ -1,3 +1,4 @@
 from .peak_detection import detect_peaks
 from .peak_localization import localize_peaks
+from .motion_estimation import estimate_motion, make_motion_histogram
 from .template_matching import find_spike_from_templates
