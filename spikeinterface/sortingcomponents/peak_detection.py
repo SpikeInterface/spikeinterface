7a06eb6cc361926e361a92c10e114f9ac6c292b3
Alessio Buccino <alejoe9187@gmail.com>
2021-12-01 09:31:12 +0100

    Remove {} from docs

--- spikeinterface/sortingcomponents/peak_detection.py
+++ spikeinterface/sortingcomponents/peak_detection.py
@@ -2,6 +2,14 @@
 
 import numpy as np
 
+from spikeinterface.core.job_tools import ChunkRecordingExecutor, _shared_job_kwargs_doc
+from spikeinterface.toolkit import get_noise_levels, get_channel_distances
+
+from ..toolkit import get_chunk_with_margin
+
+from .peak_localization import (dtype_localize_by_method, init_kwargs_dict,
+                                localize_peaks_center_of_mass, localize_peaks_monopolar_triangulation)
+
 try:
     import numba
 
@@ -9,16 +17,8 @@ try:
 except ImportError:
     HAVE_NUMBA = False
 
-from spikeinterface.core.job_tools import ChunkRecordingExecutor
-from spikeinterface.toolkit import get_noise_levels, get_channel_distances
-
 base_peak_dtype = [('sample_ind', 'int64'), ('channel_ind', 'int64'),
                    ('amplitude', 'float64'), ('segment_ind', 'int64')]
-
-from ..toolkit import get_chunk_with_margin
-
-from .peak_localization import (dtype_localize_by_method, init_kwargs_dict,
-    localize_peaks_center_of_mass, localize_peaks_monopolar_triangulation)
 
 
 def detect_peaks(recording, method='by_channel', peak_sign='neg', detect_threshold=5, n_shifts=2,
@@ -30,11 +30,11 @@ def detect_peaks(recording, method='by_channel', peak_sign='neg', detect_thresho
     ----------
     recording: RecordingExtractor
         The recording extractor object.
-    method: {'by_channel', 'locally_exclusive'}
+    method: 'by_channel', 'locally_exclusive'
         Method to use. Options:
             * 'by_channel' : peak are detected in each channel independently
             * 'locally_exclusive' : a single best peak is taken from a set of neighboring channels
-    peak_sign: {'neg', 'pos', 'both'}
+    peak_sign: 'neg', 'pos', 'both'
         Sign of the peak.
     detect_threshold: float
         Threshold, in median absolute deviations (MAD), to use to detect peaks.
@@ -50,13 +50,12 @@ def detect_peaks(recording, method='by_channel', peak_sign='neg', detect_thresho
     random_chunk_kwargs: dict, optional
         A dict that contain option to randomize chunk for get_noise_levels().
         Only used if noise_levels is None.
-    outputs: {'numpy_compact', 'numpy_split', 'sorting'}
+    outputs: 'numpy_compact', 'numpy_split', 'sorting'
         The type of the output. By default, "numpy_compact" returns an array with complex dtype.
     localization_dict : dict, optional
         Can optionally do peak localization at the same time as detection.
         This avoids running `localize_peaks` separately and re-reading the entire dataset.
-    job_kwargs: dict
-        Parameters for ChunkRecordingExecutor.
+    {}
 
     Returns
     -------
@@ -117,6 +116,9 @@ def detect_peaks(recording, method='by_channel', peak_sign='neg', detect_thresho
         raise NotImplementedError
 
 
+detect_peaks.__doc__ = detect_peaks.__doc__.format(_shared_job_kwargs_doc)
+
+
 def _init_worker_detect_peaks(recording, method, peak_sign, abs_threholds, n_shifts,
                               neighbours_mask, extra_margin, localization_dict):
     """Initialize a worker for detecting peaks."""
@@ -146,7 +148,8 @@ def _init_worker_detect_peaks(recording, method, peak_sign, abs_threholds, n_shi
             int(ms_before * recording.get_sampling_frequency() / 1000.)
         worker_ctx['localization_dict']['nafter'] = \
             int(ms_after * recording.get_sampling_frequency() / 1000.)
-        # channel sparsity
+
+        # channel sparsity
         channel_distance = get_channel_distances(recording)
         neighbours_mask = channel_distance < localization_dict['local_radius_um']
         worker_ctx['localization_dict']['neighbours_mask'] = neighbours_mask
@@ -155,7 +158,6 @@ def _init_worker_detect_peaks(recording, method, peak_sign, abs_threholds, n_shi
 
 
 def _detect_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
-    """   """
 
     # recover variables of the worker
     recording = worker_ctx['recording']
@@ -163,17 +165,15 @@ def _detect_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
     abs_threholds = worker_ctx['abs_threholds']
     n_shifts = worker_ctx['n_shifts']
     method = worker_ctx['method']
-    extra_margin =  worker_ctx['extra_margin']
+    extra_margin = worker_ctx['extra_margin']
     localization_dict = worker_ctx['localization_dict']
 
     margin = n_shifts + extra_margin
 
     # load trace in memory
-    #~ traces = recording.get_traces(start_frame=start_frame, end_frame=end_frame,
-    #                                segment_index=segment_index)
     recording_segment = recording._recording_segments[segment_index]
-    traces, left_margin, right_margin = get_chunk_with_margin(\
-        recording_segment, start_frame, end_frame, None, margin, add_zeros=True)
+    traces, left_margin, right_margin = get_chunk_with_margin(recording_segment, start_frame, end_frame, 
+                                                              None, margin, add_zeros=True)
 
     if extra_margin > 0:
         # remove extra margin for detection step
@@ -182,12 +182,10 @@ def _detect_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
         trace_detection = traces
 
     if method == 'by_channel':
-        peak_sample_ind, peak_chan_ind = detect_peaks_by_channel(\
-            trace_detection, peak_sign, abs_threholds, n_shifts)
+        peak_sample_ind, peak_chan_ind = detect_peaks_by_channel(trace_detection, peak_sign, abs_threholds, n_shifts)
     elif method == 'locally_exclusive':
-        peak_sample_ind, peak_chan_ind = detect_peak_locally_exclusive(\
-            trace_detection, peak_sign, abs_threholds, n_shifts,
-            worker_ctx['neighbours_mask'])
+        peak_sample_ind, peak_chan_ind = detect_peak_locally_exclusive(trace_detection, peak_sign, abs_threholds, 
+                                                                       n_shifts, worker_ctx['neighbours_mask'])
 
     if extra_margin > 0:
         peak_sample_ind += extra_margin
@@ -197,7 +195,7 @@ def _detect_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
         peak_dtype = base_peak_dtype
     else:
         method = localization_dict['method']
-        peak_dtype =  base_peak_dtype + dtype_localize_by_method[method]
+        peak_dtype = base_peak_dtype + dtype_localize_by_method[method]
 
     peak_amplitude = traces[peak_sample_ind, peak_chan_ind]
 
@@ -215,13 +213,14 @@ def _detect_peaks_chunk(segment_index, start_frame, end_frame, worker_ctx):
 
         # TO BE CONTINUED here
         if localization_dict['method'] == 'center_of_mass':
-            peak_locations = localize_peaks_center_of_mass(\
-                traces, peaks, contact_locations, neighbours_mask_for_loc, nbefore, nafter)
+            peak_locations = localize_peaks_center_of_mass(traces, peaks, contact_locations,
+                                                           neighbours_mask_for_loc, nbefore, nafter)
 
         elif localization_dict['method'] == 'monopolar_triangulation':
             max_distance_um = worker_ctx['localization_dict']['max_distance_um']
-            peak_locations = localize_peaks_monopolar_triangulation(\
-                traces, peaks, contact_locations, neighbours_mask_for_loc, nbefore, nafter, max_distance_um)
+            peak_locations = localize_peaks_monopolar_triangulation(traces, peaks, contact_locations,
+                                                                    neighbours_mask_for_loc, nbefore, nafter,
+                                                                    max_distance_um)
 
         for k in peak_locations.dtype.fields:
             peaks[k] = peak_locations[k]
@@ -269,7 +268,6 @@ def detect_peak_locally_exclusive(traces, peak_sign, abs_threholds, n_shifts, ne
 
     assert HAVE_NUMBA, 'You need to install numba'
     traces_center = traces[n_shifts:-n_shifts, :]
-    length = traces_center.shape[0]
 
     if peak_sign in ('pos', 'both'):
         peak_mask = traces_center > abs_threholds[None, :]
