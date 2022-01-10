20117b9414455669a7be8c961bd212675fd75817
sprenger <julia.sprenger@univ-amu.fr>
2022-01-10 19:12:49 +0100

    improve git-annex repo handling and unlock files for usage in containers

--- spikeinterface/core/datasets.py
+++ spikeinterface/core/datasets.py
@@ -6,13 +6,15 @@ from .default_folders import get_global_dataset_folder, is_set_global_dataset_fo
 
 try:
     import datalad.api
-
+    from datalad.support.gitrepo import GitRepo
     HAVE_DATALAD = True
 except:
     HAVE_DATALAD = False
 
 
 def download_dataset(repo=None, remote_path=None, local_folder=None, update_if_exists=False):
+    assert HAVE_DATALAD, 'You need to install datalad'
+
     if repo is None:
         #  print('Use gin NeuralEnsemble/ephy_testing_data')
         repo = 'https://gin.g-node.org/NeuralEnsemble/ephy_testing_data'
@@ -24,20 +26,25 @@ def download_dataset(repo=None, remote_path=None, local_folder=None, update_if_e
         #  print(f'Local folder is {base_local_folder}, Use set_global_dataset_folder() to set it globally')
         local_folder = base_local_folder / repo.split('/')[-1]
 
-    if local_folder.exists():
+    if local_folder.exists() and GitRepo.is_valid_repo(local_folder):
         dataset = datalad.api.Dataset(path=local_folder)
-        if update_if_exists:
-            dataset.update(merge=True)
+        # make sure git repo is in clean state
+        repo = dataset.repo
+        repo.call_git(['checkout', '--force', 'master'])
+        dataset.update(merge=True)
     else:
         dataset = datalad.api.install(path=local_folder,
-                                      source='https://gin.g-node.org/NeuralEnsemble/ephy_testing_data')
+                                      source=repo)
 
     if remote_path is None:
         print('Bad boy: you have to provide "remote_path"')
         return
 
+    local_path = local_folder / remote_path
+
     dataset.get(remote_path)
 
-    local_path = local_folder / remote_path
+    # unlocking is necessary for binding volume to containers
+    dataset.unlock(remote_path, recursive=True)
 
     return local_path
