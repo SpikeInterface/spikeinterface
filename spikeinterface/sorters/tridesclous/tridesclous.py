e6d17867d855ea9cd8a19744152581f1ba7be0ac
Samuel Garcia <sam.garcia.die@gmail.com>
2022-01-10 11:55:15 +0100

    avoid import error when checking tridesclous installations

--- spikeinterface/sorters/tridesclous/tridesclous.py
+++ spikeinterface/sorters/tridesclous/tridesclous.py
@@ -66,6 +66,9 @@ class TridesclousSorter(BaseSorter):
             HAVE_TDC = True
         except ImportError:
             HAVE_TDC = False
+        except:
+            print('tridesclous is installed but have some depency problems, check numba or hdbscan')
+            HAVE_TDC = False
         return HAVE_TDC
 
     @classmethod
