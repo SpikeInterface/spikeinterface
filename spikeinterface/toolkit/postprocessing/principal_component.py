8c6b0c5349a42b9d86900e124ab40a3610ce822c
Alessio Buccino <alejoe9187@gmail.com>
2022-01-11 15:02:30 +0100

    Copy model files (pkl)

--- spikeinterface/toolkit/postprocessing/principal_component.py
+++ spikeinterface/toolkit/postprocessing/principal_component.py
@@ -70,11 +70,17 @@ class WaveformPrincipalComponent(BaseWaveformExtractorExtension):
         # populate folder
         pca_files = [f for f in (
             self.extension_folder).iterdir() if f.suffix == ".npy"]
+        pca_model_files = [f for f in (
+            self.extension_folder).iterdir() if f.suffix == ".pkl"]
         for unit in unit_ids:
             for pca_file in pca_files:
                 if f"{unit}" in pca_file.name:
                     shutil.copyfile(pca_file, new_waveforms_folder / 
                                     self.extension_name / pca_file.name)
+        for pca_model_file in pca_model_files:
+            shutil.copyfile(pca_model_file, new_waveforms_folder /
+                            self.extension_name / pca_model_file.name)
+                
 
     def get_projections(self, unit_id):
         """
