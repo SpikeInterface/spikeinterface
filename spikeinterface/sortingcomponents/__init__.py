b50bc32f29ed08a7db76bab4b7975d86ee4e9957
yger <pierre.yger@gmail.com>
2022-01-14 13:42:13 +0100

    More typos and tests

--- spikeinterface/sortingcomponents/__init__.py
+++ spikeinterface/sortingcomponents/__init__.py
@@ -4,4 +4,4 @@ from .peak_localization import localize_peaks
 from .motion_estimation import (estimate_motion,
     make_motion_histogram, compute_pairwise_displacement, compute_global_displacement)
 from .motion_correction import correct_motion_on_peaks, correct_motion_on_traces
-from .template_matching import find_spike_from_templates
+from .template_matching import find_spikes_from_templates, template_matching_methods
