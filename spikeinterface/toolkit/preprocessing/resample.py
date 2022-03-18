de8f0ef59939c4fae5b693fa26db4fed7fe2fcc4
mauro.toro.e <mauro.toro.e@gmail.com>
2022-03-18 11:35:21 +0000

    A resample preprocessor that seems to be working, with test.

---
+++ spikeinterface/toolkit/preprocessing/resample.py
@@ -0,0 +1,126 @@
+import numpy as np
+import scipy.signal
+
+from spikeinterface.toolkit.preprocessing.basepreprocessor import (
+    BasePreprocessor,
+    BasePreprocessorSegment,
+)
+from spikeinterface.toolkit.preprocessing.filter import fix_dtype
+from warnings import warn
+
+try:
+    from scipy import special, signal
+
+    HAVE_RR = True
+except ImportError:
+    HAVE_RR = False
+
+
+class ResampleRecording(BasePreprocessor):
+    """
+    Generic resample class based the previous SpikeToolkits API.
+
+    If the resampling rate is multiple of the sampling rate, the faster
+    scipy decimate function is used.
+
+    Parameters
+    ----------
+    recording: Recording
+        The recording extractor to be re-referenced
+    resample_rate: float or list
+        The resampling frequency
+    dtype: dtype or None
+        The dtype of the returned traces. If None, the dtype of the parent recording is used
+
+    Returns
+    -------
+    resample_recording: ResampleRecording
+        The resampled recording extractor object
+
+    """
+
+    name = "resample"
+    installed = HAVE_RR  # check at class level if installed or not
+    installation_mesg = (
+        "To use the ResampleRecording, install scipy: \n\n pip install scipy\n\n"  # err
+    )
+
+    def __init__(
+        self,
+        recording,
+        resample_rate,
+        dtype=None,
+    ):
+        # Check that scipy is present
+        assert self.installed, self.installation_mesg
+        # Original sampling frequency
+        self._orig_samp_freq = recording.get_sampling_frequency()
+        self._resample_rate = resample_rate
+        self._sampling_frequency = resample_rate
+        dtype = fix_dtype(recording, dtype)
+        self.dtype = dtype
+
+        BasePreprocessor.__init__(
+            self, recording, sampling_frequency=resample_rate, dtype=dtype
+        )
+
+        for parent_segment in recording._recording_segments:
+            self.add_recording_segment(
+                ResampleRecordingSegment(
+                    parent_segment,
+                    resample_rate,
+                    dtype,
+                )
+            )
+
+        self._kwargs = dict(
+            recording=recording.to_dict(),
+            resample_rate=resample_rate,
+            dtype=dtype,
+        )
+
+
+class ResampleRecordingSegment(BasePreprocessorSegment):
+    def __init__(self, parent_recording_segment, resample_rate, dtype):
+        # After setting the preprocessor update the sampling rate
+        BasePreprocessorSegment.__init__(self, parent_recording_segment)
+        self.sampling_frequency = resample_rate
+        self._resample_rate = resample_rate
+        self._dtype = dtype
+
+    def get_num_samples(self):
+        return int(
+            self.parent_recording_segment.get_num_samples()
+            / self.parent_recording_segment.sampling_frequency
+            * self._resample_rate
+        )
+
+    def get_times(self):
+        return super().get_times()
+
+    def get_traces(self, start_frame, end_frame, channel_indices):
+
+        # parent_frame = (curr_frame_n / current_samp_rate) * parent_samp_rate
+        parent_start_frame, parent_end_frame = [
+            int(
+                (frame / self.sampling_frequency)
+                * self.parent_recording_segment.sampling_frequency
+            )
+            for frame in [start_frame, end_frame]
+        ]
+
+        parent_traces = self.parent_recording_segment.get_traces(
+            parent_start_frame, parent_end_frame, slice(None)
+        )
+        resampled_traces = signal.resample(
+            parent_traces, int(end_frame - start_frame), axis=0
+        )
+        return resampled_traces.astype(self._dtype)
+
+
+# functions for API
+def resample(*arg, **kwargs):
+    return ResampleRecording(*arg, **kwargs)
+
+
+resample.__doc__ = ResampleRecording.__doc__
---
+++ spikeinterface/toolkit/preprocessing/tests/test_resample.py
@@ -0,0 +1,38 @@
+from spikeinterface.core.testing_tools import generate_recording
+
+# from spikeinterface.toolkit.preprocessing import resample
+
+import numpy as np
+
+if hasattr(pytest, "global_test_folder"):
+    cache_folder = pytest.global_test_folder / "toolkit"
+else:
+    cache_folder = Path("cache_folder") / "toolkit"
+
+set_global_tmp_folder(cache_folder)
+
+
+def test_resample():
+    """simple tests for resample preprocessor.
+
+    - Check that total number of frames make sense
+    - Check that resample durations are the same
+
+    """
+    parent_sf = 30000
+    resamp_sf = 1000
+    parent_rec = generate_recording(
+        num_channels=4, sampling_frequency=parent_sf, durations=[1], set_probe=True
+    )
+    resamp_rec = resample(parent_rec, resamp_sf)
+    # check that the number of samples makes sense
+    assert np.isclose(
+        (parent_sf / resamp_sf),
+        (parent_rec.get_num_samples() / resamp_rec.get_num_samples()),
+         )
+    # check that durations are the same
+    assert (parent_rec.get_total_duration() == resamp_rec.get_total_duration())
+
+
+if __name__ == '__main__':
+    test_resample()
