efb24e7f47d0b57b69c2d8e6d402f1e986c0a97d
mauro.toro.e <mauro.toro.e@gmail.com>
2022-03-18 15:13:47 +0000

    update the relevant issues, carefuly add decimate.

--- spikeinterface/toolkit/preprocessing/resample.py
+++ spikeinterface/toolkit/preprocessing/resample.py
@@ -7,17 +7,12 @@ from .basepreprocessor import (
 )
 from .filter import fix_dtype
 
-try:
-    from scipy import special, signal
-
-    HAVE_RR = True
-except ImportError:
-    HAVE_RR = False
+from scipy import signal
 
 
 class ResampleRecording(BasePreprocessor):
     """
-    Resamples the recording extractor traces.
+    Resample the recording extractor traces.
 
     If the resampling rate is multiple of the sampling rate, the faster
     scipy decimate function is used.
@@ -56,8 +51,9 @@ class ResampleRecording(BasePreprocessor):
         BasePreprocessor.__init__(
             self, recording, sampling_frequency=resample_rate, dtype=dtype
         )
-
+        # in case there was a time_vector, it will be dropped for sanity.
         for parent_segment in recording._recording_segments:
+            parent_segment.time_vector = None
             self.add_recording_segment(
                 ResampleRecordingSegment(
                     parent_segment,
@@ -92,22 +88,37 @@ class ResampleRecordingSegment(BasePreprocessorSegment):
         return super().get_times()
 
     def get_traces(self, start_frame, end_frame, channel_indices):
-
-        # parent_frame = (curr_frame_n / current_samp_rate) * parent_samp_rate
+        parent_samp_fr = self.parent_recording_segment.sampling_frequency
+        current_samp_fr = self.sampling_frequency
+        # to get the start and end frames from the parent:
+        # parent_frame = (frame / current_samp_fr) * parent_samp_fr
+        # One could add a margin
+        # beware of start_ or end_frame as none:
+        if start_frame == None:
+            start_frame = 0
+        if end_frame == None:
+            end_frame = self.get_num_samples()
         parent_start_frame, parent_end_frame = [
-            int(
-                (frame / self.sampling_frequency)
-                * self.parent_recording_segment.sampling_frequency
-            )
+            int((frame / current_samp_fr) * parent_samp_fr)
             for frame in [start_frame, end_frame]
         ]
-
+        #  get traces and resample
         parent_traces = self.parent_recording_segment.get_traces(
-            parent_start_frame, parent_end_frame, slice(None)
+            parent_start_frame,
+            parent_end_frame,
+            channel_indices
         )
-        resampled_traces = signal.resample(
-            parent_traces, int(end_frame - start_frame), axis=0
-        )
+        # Check which method to use:
+        if np.mod(parent_samp_fr, current_samp_fr) == 0:
+            # the
+            q = parent_samp_fr // current_samp_fr
+            # Decimate can have issues for some cases, returning NaNs
+            resampled_traces = signal.decimate(parent_traces, q=q, axis=0)
+            # If that's the case, use signal.resample
+            if np.any(np.isnan(resampled_traces)):
+                resampled_traces = signal.resample(parent_traces, int(end_frame - start_frame), axis=0)
+        else:
+            resampled_traces = signal.resample(parent_traces, int(end_frame - start_frame), axis=0)
         return resampled_traces.astype(self._dtype)
 
 
