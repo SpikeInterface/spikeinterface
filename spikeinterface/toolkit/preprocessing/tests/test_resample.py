633f8f3e22770426943f4b0180a3ea58c2801406
mauro.toro.e <mauro.toro.e@gmail.com>
2022-03-18 15:14:35 +0000

    more tests, now with different resamples.

--- spikeinterface/toolkit/preprocessing/tests/test_resample.py
+++ spikeinterface/toolkit/preprocessing/tests/test_resample.py
@@ -1,7 +1,8 @@
-from spikeinterface.core.testing_tools import generate_recording
-
 import pytest
+from pathlib import Path
 
+from spikeinterface import set_global_tmp_folder
+from spikeinterface.core.testing_tools import generate_recording
 from spikeinterface.toolkit.preprocessing import resample
 
 import numpy as np
@@ -11,7 +12,6 @@ if hasattr(pytest, "global_test_folder"):
 else:
     cache_folder = Path("cache_folder") / "toolkit"
 
-set_global_tmp_folder(cache_folder)
 
 
 def test_resample():
@@ -23,8 +23,9 @@ def test_resample():
     """
     parent_sf = 30000
     resamp_sf = 1000
+    duration = [1.5]
     parent_rec = generate_recording(
-        num_channels=4, sampling_frequency=parent_sf, durations=[1], set_probe=True
+        num_channels=4, sampling_frequency=parent_sf, durations=duration, set_probe=True
     )
     resamp_rec = resample(parent_rec, resamp_sf)
     # check that the number of samples makes sense
@@ -34,6 +35,34 @@ def test_resample():
          )
     # check that durations are the same
     assert (parent_rec.get_total_duration() == resamp_rec.get_total_duration())
+    # check that first and last times are similar enough, with tolerance resampling rate
+    assert np.all(
+        np.isclose(
+            parent_rec.get_times()[[0, -1]],
+            resamp_rec.get_times()[[0, -1]],
+            atol=1/resamp_sf
+        )
+    )
+    # check that for different resamp_rates, it still the same
+    resamp_fss = (np.linspace(0.1, 1, 10) * parent_sf).astype(int)
+    resamp_recs = [resample(parent_rec, resamp_fs) for resamp_fs in resamp_fss]
+    # that they all have the correct number of frames:
+    assert np.allclose([resamp_rec.get_num_frames() for resamp_rec in resamp_recs], resamp_fss * duration)
+    # They all last 1 second
+    assert np.allclose([resamp_rec.get_total_duration() for resamp_rec in resamp_recs], duration)
+    # check that the first and last time points are similar with tolerance
+    assert np.all(
+        [
+            np.isclose(
+                parent_rec.get_times()[[0, -1]],
+                resamp_rec.get_times()[[0, -1]],
+                atol=1/resamp_fs,
+            )
+            for resamp_fs, resamp_rec in zip(resamp_fss, resamp_recs)
+        ]
+    )
+
+
 
 
 if __name__ == '__main__':
