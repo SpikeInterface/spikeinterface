023a5466c15500e61cde28589b6b6ad7fc41e911
Samuel Garcia <sam.garcia.die@gmail.com>
2022-04-11 13:10:14 +0200

    oups

--- spikeinterface/extractors/tests/test_cbin_ibl.py
+++ spikeinterface/extractors/tests/test_cbin_ibl.py
@@ -17,10 +17,10 @@ class CompressedBinaryIblExtractorTest(RecordingCommonTestSuite, unittest.TestCa
 
 
 
-def test_read_cbin_ibl():
-    base_folder = '/media/samuel/dataspikesorting/DataSpikeSorting/olivier_destripe/'
-    data_folder = base_folder + '4c04120d-523a-4795-ba8f-49dbb8d9f63a'
-    rec = read_cbin_ibl(data_folder)
+#~ def test_read_cbin_ibl():
+    #~ base_folder = '/media/samuel/dataspikesorting/DataSpikeSorting/olivier_destripe/'
+    #~ data_folder = base_folder + '4c04120d-523a-4795-ba8f-49dbb8d9f63a'
+    #~ rec = read_cbin_ibl(data_folder)
     
     #~ import matplotlib.pyplot as plt
     #~ import spikeinterface.widgets as sw
@@ -31,9 +31,8 @@ def test_read_cbin_ibl():
 
 
 if __name__ == '__main__':
-    test_read_cbin_ibl()
+    #~ test_read_cbin_ibl()
     
-    #~ test = CompressedBinaryIblExtractorTest()
-    #~ test.setUp()
-    #~ test.test_open()
-
+    test = CompressedBinaryIblExtractorTest()
+    test.setUp()
+    test.test_open()
