ebcc3e38d1b4e7e40c97d5770f331dba747acd1e
Alessio Buccino <alejoe9187@gmail.com>
2022-05-03 11:18:23 +0200

    Enforce spike trains at least 1d

--- spikeinterface/core/basesorting.py
+++ spikeinterface/core/basesorting.py
@@ -64,9 +64,9 @@ class BaseSorting(BaseExtractor):
                 times = self.get_times(segment_index=segment_index)
                 return times[spike_train]
             else:
-                return spike_train / self.get_sampling_frequency()
+                return np.atleast_1d(spike_train / self.get_sampling_frequency())
         else:
-            return spike_train
+            return np.atleast_1d(spike_train)
 
     def register_recording(self, recording):
         assert np.isclose(self.get_sampling_frequency(),
