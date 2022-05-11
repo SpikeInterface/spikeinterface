ec1a451470c67d2b94b77c739967788568db437e
Samuel Garcia <sam.garcia.die@gmail.com>
2022-05-11 18:18:05 +0200

    STart propagate annotations from neo into SI object.

--- spikeinterface/extractors/neoextractors/blackrock.py
+++ spikeinterface/extractors/neoextractors/blackrock.py
@@ -17,7 +17,7 @@ class BlackrockRecordingExtractor(NeoBaseRecordingExtractor):
     NeoRawIOClass = 'BlackrockRawIO'
 
     def __init__(self, file_path, stream_id=None):
-        neo_kwargs = {'filename': str(file_path)}
+        neo_kwargs = {'filename': str(file_path), 'all_annotations': True}
         NeoBaseRecordingExtractor.__init__(self, stream_id=stream_id, **neo_kwargs)
 
         self._kwargs = dict(file_path=str(file_path), stream_id=stream_id)
