471dca29497bad966871225031ea369012f60093
Chuang Yu Min <chg.yumin@gmail.com>
2022-05-17 07:20:03 -0300

    Made ks3 matlab script static and _run_from_folder shell script dynamic

--- spikeinterface/sorters/kilosort3/kilosort3_master.m
+++ spikeinterface/sorters/kilosort3/kilosort3_master.m
@@ -1,43 +1,45 @@
-try
-    % prepare for kilosort execution
-    addpath(genpath('{kilosort3_path}'));
+function kilosort3_master(output_folder, kilosort_path)
+    try
+        % prepare for kilosort execution
+        addpath(genpath(kilosort_path));
 
-    % set file path
-    fpath = '{output_folder}';
+        % set file path
+        fpath = output_folder;
 
-    % add npy-matlab functions (copied in the output folder)
-    addpath(genpath(fpath));
+        % add npy-matlab functions (copied in the output folder)
+        addpath(genpath(fpath));
 
-    % Load channel map file
-    load(fullfile('{output_folder}', 'chanMap.mat'));
+        % Load channel map file
+        load(fullfile(fpath, 'chanMap.mat'));
 
-    % Load the configuration file, it builds the structure of options (ops)
-    ops = load(fullfile('{output_folder}', 'ops.mat'));
+        % Load the configuration file, it builds the structure of options (ops)
+        ops = load(fullfile(fpath, 'ops.mat'));
 
-    % preprocess data to create temp_wh.dat
-    rez = preprocessDataSub(ops);
+        % preprocess data to create temp_wh.dat
+        rez = preprocessDataSub(ops);
 
-    % run data registration
-    rez = datashift2(rez, 1); % last input is for shifting data
+        % run data registration
+        rez = datashift2(rez, 1); % last input is for shifting data
 
-    [rez, st3, tF] = extract_spikes(rez);
+        [rez, st3, tF] = extract_spikes(rez);
 
-    rez = template_learning(rez, tF, st3);
+        rez = template_learning(rez, tF, st3);
 
-    [rez, st3, tF] = trackAndSort(rez);
+        [rez, st3, tF] = trackAndSort(rez);
 
-    rez = final_clustering(rez, tF, st3);
+        rez = final_clustering(rez, tF, st3);
 
-    % final merges
-    rez = find_merges(rez, 1);
+        % final merges
+        rez = find_merges(rez, 1);
 
-    % output to phy
-    fprintf('Saving results to Phy\n')
-    rezToPhy2(rez, fpath);
+        % output to phy
+        fprintf('Saving results to Phy\n')
+        rezToPhy2(rez, fpath);
 
-catch
-    fprintf('----------------------------------------');
-    fprintf(lasterr());
-    quit(1);
+    catch
+        fprintf('----------------------------------------');
+        fprintf(lasterr());
+        quit(1);
+    end
+    quit(0);
 end
-quit(0);
