e4446930531f1b55b9b090a7f742e9c3e80c09d7
Fernando J. Chaure <ferchaure@users.noreply.github.com>
2022-05-18 13:42:42 -0300

    updated codes

--- spikeinterface/toolkit/postprocessing/curationsorting.py
+++ spikeinterface/toolkit/postprocessing/curationsorting.py
@@ -1,8 +1,8 @@
-from .mergesorting import MergeSorting
-from .splitsortingunit import SplitSortingUnit
+from .mergeunitssorting import MergeUnitsSorting
+from .splitunitsorting import SplitUnitSorting
 from collections import namedtuple
 import numpy as np
-node_t = namedtuple('node_t', 'id stage')
+node_t = namedtuple('node_t', 'unit_id stage_id')
 
 class CurationSorting():
     """
@@ -12,6 +12,10 @@ class CurationSorting():
     ----------
     parent_sorting: Recording
         The recording object
+    properties_policy: str
+        Policy used to propagate propierties after split and merge operation. If 'keep' the properties will be
+        pass to the new units (if the original units have the same value). If 'remove' the new units will have 
+        an empty value for all the properties. Default: 'keep'
     make_graph: bool
         True to keep a networksx graph with the curation history
     Returns
@@ -21,10 +25,11 @@ class CurationSorting():
     """
 
 
-    def __init__(self, parent_sorting, make_graph=False):
+    def __init__(self, parent_sorting, make_graph=False, properties_policy='keep'):
         #to allow undo and redo a list of sortingextractors is keep
         self._sorting_stages = [parent_sorting]
         self._sorting_stages_i = 0
+        self._properties_policy = properties_policy
         parent_units = parent_sorting.get_unit_ids()
         self._make_graph = make_graph
         if make_graph:
@@ -39,7 +44,7 @@ class CurationSorting():
         else:
             self.max_used_id = max(parent_units)
         
-        self._kwargs = dict(parent_sorting=parent_sorting.to_dict())
+        self._kwargs = dict(parent_sorting=parent_sorting.to_dict(), make_graph=make_graph, properties_policy=properties_policy)
 
     def _get_unused_id(self, n=1):
         #check units in the graph to the next unused unit id
@@ -48,31 +53,31 @@ class CurationSorting():
             ids = [str(i) for i in ids]
         return ids
 
-    def split(self,unit2split, indices_list):
+    def split(self,split_unit_id, indices_list):
         current_sorting = self._sorting_stages[self._sorting_stages_i]
-        new_units_ids = self._get_unused_id(2)
-        new_sorting = SplitSortingUnit(current_sorting, unit2split=unit2split, indices_list=indices_list, new_units_ids = new_units_ids)
+        new_unit_ids = self._get_unused_id(2)
+        new_sorting = SplitUnitSorting(current_sorting, split_unit_id=split_unit_id, indices_list=indices_list, new_unit_ids=new_unit_ids,properties_policy=self._properties_policy)
 
         if self._make_graph:
             units = current_sorting.get_unit_ids()
             i = self._sorting_stages_i
-            edges = [(node_t(u,i),node_t(u,i+1)) for u in units if u !=unit2split]
-            edges = edges + [(node_t(unit2split,i), node_t(u,i+1)) for u in new_units_ids]
+            edges = [(node_t(u,i),node_t(u,i+1)) for u in units if u !=split_unit_id]
+            edges = edges + [(node_t(split_unit_id,i), node_t(u,i+1)) for u in new_unit_ids]
         else:
             edges = None
         self.max_used_id = self.max_used_id+2
         self._add_new_stage(new_sorting, edges)
 
-    def merge(self,units2merge, delta_time=0.4, cache=False):
+    def merge(self,units_to_merge, delta_time_ms=0.4):
         current_sorting = self._sorting_stages[self._sorting_stages_i]
-        merge_unit_id = self._get_unused_id()[0]
-        new_sorting = MergeSorting(parent_sorting=current_sorting, units2merge=units2merge, 
-                merge_unit_id=merge_unit_id, delta_time=delta_time, cache=cache)
+        new_unit_id = self._get_unused_id()[0]
+        new_sorting = MergeUnitsSorting(parent_sorting=current_sorting, units_to_merge=units_to_merge, 
+                new_unit_id=new_unit_id, delta_time_ms=delta_time_ms, properties_policy=self._properties_policy)
         if self._make_graph:
             units = current_sorting.get_unit_ids()
             i = self._sorting_stages_i
-            edges = [(node_t(u,i),node_t(u,i+1)) for u in units if u not in units2merge]
-            edges = edges + [(node_t(u,i),node_t(merge_unit_id,i+1)) for u in units2merge]
+            edges = [(node_t(u,i),node_t(u,i+1)) for u in units if u not in units_to_merge]
+            edges = edges + [(node_t(u,i),node_t(new_unit_id,i+1)) for u in units_to_merge]
         else:
             edges = None
         self.max_used_id = self.max_used_id + 1
@@ -92,17 +97,6 @@ class CurationSorting():
     def remove_unit(self, unit_id):
         self.remove_units([unit_id])
 
-    def _add_new_stage(self, new_sorting, edges):
-        #adds the stage to the stage list and creates the associated new graph
-        self._sorting_stages = self._sorting_stages[0:self._sorting_stages_i+1]
-        self._sorting_stages.append(new_sorting)
-        if self._make_graph:
-            self._graphs = self._graphs[0:self._sorting_stages_i+1]
-            new_graph = self._graphs[self._sorting_stages_i].copy()
-            new_graph.add_edges_from(edges)
-            self._graphs.append(new_graph)
-        self._sorting_stages_i += 1
-
     def select_units(self, unit_ids, renamed_unit_ids=None):
         new_sorting = self._sorting_stages[self._sorting_stages_i].select_units(unit_ids, renamed_unit_ids)
         if self._make_graph:
@@ -114,6 +108,20 @@ class CurationSorting():
         else:
             edges = None        
         self._add_new_stage(new_sorting, edges)
+    
+    def rename(self, renamed_unit_ids):
+        self.select_units(self.current_sorting.unit_ids, renamed_unit_ids=renamed_unit_ids)
+
+    def _add_new_stage(self, new_sorting, edges):
+        #adds the stage to the stage list and creates the associated new graph
+        self._sorting_stages = self._sorting_stages[0:self._sorting_stages_i+1]
+        self._sorting_stages.append(new_sorting)
+        if self._make_graph:
+            self._graphs = self._graphs[0:self._sorting_stages_i+1]
+            new_graph = self._graphs[self._sorting_stages_i].copy()
+            new_graph.add_edges_from(edges)
+            self._graphs.append(new_graph)
+        self._sorting_stages_i += 1
 
     def remove_empty_units(self):
         i = self._sorting_stages_i
@@ -144,9 +152,9 @@ class CurationSorting():
     def draw_graph(self, **kwargs):
         assert self._make_graph, 'to make graph make_graph=True'
         graph = self.graph
-        ids = [c.id for c in graph.nodes]
-        pos = {n:(n.stage, -ids.index(n.id)) for n in graph.nodes}
-        labels = {n:str(n.id) for n in graph.nodes}
+        ids = [c.unit_id for c in graph.nodes]
+        pos = {n:(n.stage_id, -ids.index(n.unit_id)) for n in graph.nodes}
+        labels = {n:str(n.unit_id) for n in graph.nodes}
         self._nx.draw(graph, pos=pos, labels=labels, **kwargs)
 
     @property
@@ -154,15 +162,18 @@ class CurationSorting():
         assert self._make_graph, 'to have a graph make_graph=True'
         return self._graphs[self._sorting_stages_i]
 
-
     @property
     def sorting(self):
         return self._sorting_stages[self._sorting_stages_i]
 
-    def __getattr__(self,name):
-        #any method not define for this class will try to use the current 
-        #  sorting stage. In that whay this class will behave as a sortingextractor
-        current_sorting = self._sorting_stages[self._sorting_stages_i]
+    @property
+    def current_sorting(self):
+        return self._sorting_stages[self._sorting_stages_i]
 
-        attr = object.__getattribute__(current_sorting, name)
-        return attr
+    # def __getattr__(self,name):
+    #     #any method not define for this class will try to use the current 
+    #     #  sorting stage. In that whay this class will behave as a sortingextractor
+    #     current_sorting = self._sorting_stages[self._sorting_stages_i]
+
+    #     attr = object.__getattribute__(current_sorting, name)
+    #     return attr
