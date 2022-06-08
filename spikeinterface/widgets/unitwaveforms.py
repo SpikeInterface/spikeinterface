f5808c21e328352b01132bbbeb28faecc497e8f5
yger <pierre.yger@gmail.com>
2022-06-08 15:22:26 +0200

    Allow selection of waveforms

--- spikeinterface/widgets/unitwaveforms.py
+++ spikeinterface/widgets/unitwaveforms.py
@@ -25,19 +25,20 @@ class UnitWaveformsWidget(BaseWidget):
     max_channels : None or int
         If not None only max_channels are displayed per units.
         Incompatible with with `radius_um`
-        
-    
     set_title: bool
         Create a plot title with the unit number if True.
     plot_channels: bool
         Plot channel locations below traces.
     axis_equal: bool
-        Equal aspext ratio for x and y axis, to visualise the array geometry to scale
+        Equal aspect ratio for x and y axis, to visualize the array geometry to scale.
     lw: float
         Line width for the traces.
     unit_colors: None or dict
         A dict key is unit_id and value is any color format handled by matplotlib.
         If None, then the get_unit_colors() is internally used.
+    unit_selected_waveforms: None or dict
+        A dict key is unit_id and value is the subset of waveforms indices that should be 
+        be displayed
     show_all_channels: bool
         Show the whole probe if True, or only selected channels if False
         The axis to be used. If not given an axis is created
@@ -49,7 +50,7 @@ class UnitWaveformsWidget(BaseWidget):
     def __init__(self, waveform_extractor, channel_ids=None, unit_ids=None,
                  plot_waveforms=True, plot_templates=True, plot_channels=False,
                  unit_colors=None, max_channels=None, radius_um=None,
-                 ncols=5, axes=None, lw=2, axis_equal=False,
+                 ncols=5, axes=None, lw=2, axis_equal=False, unit_selected_waveforms=None,
                  set_title=True
                  ):
 
@@ -75,12 +76,13 @@ class UnitWaveformsWidget(BaseWidget):
         self._plot_channels = plot_channels
 
         if radius_um is not None:
-            assert max_channels is None, 'radius_um and max_channels are mutually exclussive'
+            assert max_channels is None, 'radius_um and max_channels are mutually exclusive'
         if max_channels is not None:
-            assert radius_um is None, 'radius_um and max_channels are mutually exclussive'
+            assert radius_um is None, 'radius_um and max_channels are mutually exclusive'
 
         self.radius_um = radius_um
         self.max_channels = max_channels
+        self.unit_selected_waveforms = unit_selected_waveforms
 
         # TODO
         self._lw = lw
@@ -122,7 +124,7 @@ class UnitWaveformsWidget(BaseWidget):
             channel_inds = {unit_id: slice(None) for unit_id in unit_ids}
 
         for i, unit_id in enumerate(unit_ids):
-            
+
             ax = self.axes.flatten()[i]
             color = self.unit_colors[unit_id]
 
@@ -132,7 +134,10 @@ class UnitWaveformsWidget(BaseWidget):
             # plot waveforms
             if self._plot_waveforms:
                 wfs = we.get_waveforms(unit_id)
-                wfs = wfs[:, :, chan_inds]
+                if self.unit_selected_waveforms is not None:
+                    wfs = wfs[self.unit_selected_waveforms[unit_id], :, chan_inds]
+                else:
+                    wfs = wfs[:, :, chan_inds]
                 wfs = wfs * y_scale + y_offset[None, :, chan_inds]
                 wfs_flat = wfs.swapaxes(1, 2).reshape(wfs.shape[0], -1).T
                 ax.plot(xvectors_flat, wfs_flat, lw=1, alpha=0.3, color=color)
@@ -145,7 +150,7 @@ class UnitWaveformsWidget(BaseWidget):
                 ax.plot(xvectors_flat, template.T.flatten(), lw=1, color=color)
                 template_label = unit_ids[i]
                 ax.set_title(f'template {template_label}')
-            
+
             # plot channels
             if self._plot_channels:
                 # TODO enhance this
