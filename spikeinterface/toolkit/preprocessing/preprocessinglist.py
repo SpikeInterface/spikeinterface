bd7617a1b8ca372380085c8d98e0acff5a8bcc05
Alessio Buccino <alejoe9187@gmail.com>
2022-06-08 17:53:10 -0400

    zscore preprocessing

--- spikeinterface/toolkit/preprocessing/preprocessinglist.py
+++ spikeinterface/toolkit/preprocessing/preprocessinglist.py
@@ -5,6 +5,7 @@ from .filter import (FilterRecording, filter,
 from .normalize_scale import (
     NormalizeByQuantileRecording, normalize_by_quantile,
     ScaleRecording, scale,
+    ZScoreRecording, scale_zscore,
     CenterRecording, center)
 from .whiten import WhitenRecording, whiten
 from .rectify import RectifyRecording, rectify
@@ -26,6 +27,7 @@ preprocessers_full_list = [
     NormalizeByQuantileRecording,
     ScaleRecording,
     CenterRecording,
+    ZScoreRecording,
 
     # decorrelation stuff
     WhitenRecording,
