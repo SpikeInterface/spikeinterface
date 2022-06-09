cce75955a907dfae4d9d593ccbb4c9fdb8f31b5f
colehurwitz <colehurwitz@gmail.com>
2022-06-09 15:36:08 -0400

    updated waveform features

--- spikeinterface/sortingcomponents/peak_waveform_features.py
+++ spikeinterface/sortingcomponents/peak_waveform_features.py
@@ -1,18 +1,17 @@
 """Sorting components: peak waveform features."""
 
+from lib2to3.pgen2.token import AMPER
 import numpy as np
 
 from spikeinterface.core.job_tools import ChunkRecordingExecutor, _shared_job_kwargs_doc
-from spikeinterface.toolkit import get_channel_distances
-
 from ..toolkit import get_chunk_with_margin
 
 
 def compute_waveform_features_peaks(
     recording,
     peaks,
-    ms_before=1,
-    ms_after=1.5,
+    peak_sign="neg",
+    time_range_list=[(0.2, 0.2), (1, 1.5)],
     feature_list=["amplitude", "ptps"],
     **job_kwargs,
 ):
@@ -29,10 +28,10 @@ def compute_waveform_features_peaks(
         The recording extractor object.
     peaks: array
         Peaks array, as returned by detect_peaks() in "compact_numpy" way.
-    ms_before: float
-        The left window, before a peak, in milliseconds.
-    ms_after: float
-        The right window, after a peak, in milliseconds.
+    peak_sign: str
+        Sign of the template to compute best channels ('neg', 'pos', 'both')
+    time_range_list: List
+        List of time ranges around the peak to compute each feature.
     feature_list: List
         List of features to be computed
     {}
@@ -42,11 +41,29 @@ def compute_waveform_features_peaks(
     waveform_features: ndarray (NxCxF)
         Array with waveform features for each spike.
     """
-    nbefore = int(ms_before * recording.get_sampling_frequency() / 1000.0)
-    nafter = int(ms_after * recording.get_sampling_frequency() / 1000.0)
+    if not "amplitude" in feature_list and not "ptps" in feature_list:
+        raise ValueError("features must be either amplitude or ptps")
+
+    nbefore_max = 0
+    nafter_max = 0
+    if len(time_range_list) == 0 or len(feature_list) == 0:
+        raise ValueError("You must pass in a non-empty feature_list or time_range_list")
+    if len(time_range_list) != len(feature_list):
+        raise ValueError("feature_list and time_range_list must be the same length")
+
+    for i in range(len(time_range_list)):
+        ms_before = time_range_list[i][0]
+        ms_after = time_range_list[i][1]
+        nbefore = int(ms_before * recording.get_sampling_frequency() / 1000.0)
+        nafter = int(ms_after * recording.get_sampling_frequency() / 1000.0)
+        if nbefore > nbefore_max:
+            nbefore_max = nbefore
+        if nafter > nafter_max:
+            nafter_max = nafter
+        time_range_list[i] = (nbefore, nafter)
 
     # margin at border for get_trace
-    margin = max(nbefore, nafter)
+    margin = max(nbefore_max, nafter_max)
 
     # TODO
     #  make a memmap for peaks to avoid serialization
@@ -57,9 +74,9 @@ def compute_waveform_features_peaks(
     init_args = (
         recording.to_dict(),
         peaks,
-        nbefore,
-        nafter,
         margin,
+        peak_sign,
+        time_range_list,
         feature_list,
     )
     processor = ChunkRecordingExecutor(
@@ -85,9 +102,9 @@ compute_waveform_features_peaks.__doc__ = (
 def _init_worker_compute_waveform_features_peaks(
     recording,
     peaks,
-    nbefore,
-    nafter,
     margin,
+    peak_sign,
+    time_range_list,
     feature_list,
 ):
     """Initialize worker for localizing peaks."""
@@ -101,10 +118,10 @@ def _init_worker_compute_waveform_features_peaks(
     worker_ctx = {}
     worker_ctx["recording"] = recording
     worker_ctx["peaks"] = peaks
-    worker_ctx["nbefore"] = nbefore
-    worker_ctx["nafter"] = nafter
     worker_ctx["margin"] = margin
     worker_ctx["feature_list"] = feature_list
+    worker_ctx["time_range_list"] = time_range_list
+    worker_ctx["peak_sign"] = peak_sign
     return worker_ctx
 
 
@@ -114,10 +131,10 @@ def _compute_waveform_features_chunk(segment_index, start_frame, end_frame, work
     # recover variables of the worker
     recording = worker_ctx["recording"]
     peaks = worker_ctx["peaks"]
-    nbefore = worker_ctx["nbefore"]
-    nafter = worker_ctx["nafter"]
     margin = worker_ctx["margin"]
     feature_list = worker_ctx["feature_list"]
+    peak_sign = worker_ctx["peak_sign"]
+    time_range_list = worker_ctx["time_range_list"]
 
     # load trace in memory
     recording_segment = recording._recording_segments[segment_index]
@@ -140,15 +157,17 @@ def _compute_waveform_features_chunk(segment_index, start_frame, end_frame, work
     peak_waveform_features = compute_waveform_features(
         traces,
         local_peaks,
-        nbefore,
-        nafter,
+        peak_sign,
+        time_range_list,
         feature_list,
     )
 
     return peak_waveform_features
 
 
-def compute_waveform_features(traces, local_peak, nbefore, nafter, feature_list):
+def compute_waveform_features(
+    traces, local_peak, peak_sign, time_range_list, feature_list
+):
     """Localize peaks using the center of mass method."""
     peak_waveform_features = np.empty(
         (local_peak.size, traces.shape[1], len(feature_list))
@@ -157,21 +176,32 @@ def compute_waveform_features(traces, local_peak, nbefore, nafter, feature_list)
     do_amplitudes = False
     do_ptps = False
     idx = 0
-    if "amplitude" in feature_list:
-        do_amplitudes = True
-        amplitude_idx = idx
-        idx += 1
-    if "ptps" in feature_list:
-        do_ptps = True
-        ptp_idx = idx
+    for feature in feature_list:
+        if feature == "amplitude":
+            do_amplitudes = True
+            amplitude_idx = idx
+            idx += 1
+        if feature == "ptps":
+            do_ptps = True
+            ptp_idx = idx
+            idx += 1
 
     for i, peak in enumerate(local_peak):
-
-        wf = traces[peak["sample_ind"] - nbefore : peak["sample_ind"] + nafter, :]
         if do_amplitudes:
-            wf_amplitude = wf.min(axis=0)
+            nbefore = time_range_list[amplitude_idx][0]
+            nafter = time_range_list[amplitude_idx][1]
+            wf = traces[peak["sample_ind"] - nbefore : peak["sample_ind"] + nafter, :]
+            if peak_sign == "both":
+                wf_amplitude = np.max(np.abs(wf, axis=0))
+            elif peak_sign == "neg":
+                wf_amplitude = np.min(wf, axis=0)
+            elif peak_sign == "pos":
+                wf_amplitude = np.max(wf, axis=0)
             peak_waveform_features[i, :, amplitude_idx] = wf_amplitude
         if do_ptps:
+            nbefore = time_range_list[ptp_idx][0]
+            nafter = time_range_list[ptp_idx][1]
+            wf = traces[peak["sample_ind"] - nbefore : peak["sample_ind"] + nafter, :]
             wf_ptp = wf.ptp(axis=0)
             peak_waveform_features[i, :, ptp_idx] = wf_ptp
     return peak_waveform_features
