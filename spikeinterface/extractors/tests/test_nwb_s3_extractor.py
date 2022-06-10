5fd161d5d47e5267b5bad9ac3cc7db7410d73471
Alessio Buccino <alejoe9187@gmail.com>
2022-06-10 09:57:01 -0400

    simplify test for s3 nwb

--- spikeinterface/extractors/tests/test_nwb_s3_extractor.py
+++ spikeinterface/extractors/tests/test_nwb_s3_extractor.py
@@ -10,7 +10,7 @@ def test_s3_nwb():
     file_path = "https://dandi-api-staging-dandisets.s3.amazonaws.com/blobs/5f4/b7a/5f4b7a1f-7b95-4ad8-9579-4df6025371cc"
     rec = NwbRecordingExtractor(file_path, driver="ros3")
     
-    start_frame = 100
+    start_frame = 0
     end_frame = 300
     num_frames = end_frame - start_frame
 
@@ -25,15 +25,6 @@ def test_s3_nwb():
                                      end_frame=end_frame)
         assert full_traces.shape == (num_frames, num_chans)
         assert full_traces.dtype == dtype
-
-        traces_sample_first = rec.get_traces(segment_index=segment_index, start_frame=0, end_frame=1)
-        assert traces_sample_first.shape == (1, num_chans)
-        assert np.all(full_traces[0, :] == traces_sample_first[0, :])
-
-        traces_sample_last = rec.get_traces(segment_index=segment_index, start_frame=num_samples - 1,
-                                            end_frame=num_samples)
-        assert traces_sample_last.shape == (1, num_chans)
-        assert np.all(full_traces[-1, :] == traces_sample_last[0, :])
 
     if rec.has_scaled_traces():
         trace_scaled = rec.get_traces(segment_index=segment_index, return_scaled=True, end_frame=2)
