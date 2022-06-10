710780fa2c4f5f6deef892ad7544b7660e78cdf0
Alessio Buccino <alejoe9187@gmail.com>
2022-06-10 09:52:39 -0400

    skip s3 test if ros3 not installed

--- spikeinterface/extractors/tests/test_nwb_s3_extractor.py
+++ spikeinterface/extractors/tests/test_nwb_s3_extractor.py
@@ -1,21 +1,40 @@
-import unittest
+import pytest
+import numpy as np
+import h5py
 
 from spikeinterface.extractors import NwbRecordingExtractor
 
-from spikeinterface.extractors.tests.common_tests import RecordingCommonTestSuite
 
+@pytest.mark.skipif("ros3" not in h5py.registered_drivers(), reason="ROS3 driver not installed")
+def test_s3_nwb():
+    file_path = "https://dandi-api-staging-dandisets.s3.amazonaws.com/blobs/5f4/b7a/5f4b7a1f-7b95-4ad8-9579-4df6025371cc"
+    rec = NwbRecordingExtractor(file_path, driver="ros3")
+    
+    start_frame = 100
+    end_frame = 300
+    num_frames = end_frame - start_frame
 
-class S3NwbRecordingTest(RecordingCommonTestSuite, unittest.TestCase):
-    ExtractorClass = NwbRecordingExtractor
-    downloads = []
-    entities = [(
-        "https://dandi-api-staging-dandisets.s3.amazonaws.com/blobs/5f4/b7a/5f4b7a1f-7b95-4ad8-9579-4df6025371cc",
-        dict(driver="ros3")
-    )]
+    num_seg = rec.get_num_segments()
+    num_chans = rec.get_num_channels()
+    dtype = rec.get_dtype()
 
-    @staticmethod
-    def get_full_path(path):
-        return path
+    for segment_index in range(num_seg):
+        num_samples = rec.get_num_samples(segment_index=segment_index)
 
-    def setUp(self):
-        pass
+        full_traces = rec.get_traces(segment_index=segment_index, start_frame=start_frame,
+                                     end_frame=end_frame)
+        assert full_traces.shape == (num_frames, num_chans)
+        assert full_traces.dtype == dtype
+
+        traces_sample_first = rec.get_traces(segment_index=segment_index, start_frame=0, end_frame=1)
+        assert traces_sample_first.shape == (1, num_chans)
+        assert np.all(full_traces[0, :] == traces_sample_first[0, :])
+
+        traces_sample_last = rec.get_traces(segment_index=segment_index, start_frame=num_samples - 1,
+                                            end_frame=num_samples)
+        assert traces_sample_last.shape == (1, num_chans)
+        assert np.all(full_traces[-1, :] == traces_sample_last[0, :])
+
+    if rec.has_scaled_traces():
+        trace_scaled = rec.get_traces(segment_index=segment_index, return_scaled=True, end_frame=2)
+        assert trace_scaled.dtype == 'float32'
