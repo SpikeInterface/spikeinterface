ff8518c2805cbd530d5921ed93b2ac73959bc4d9
Samuel Garcia <sam.garcia.die@gmail.com>
2022-06-29 16:49:59 +0200

    wip on widgets

--- spikeinterface/widgets/ipywidgets/timeseries.py
+++ spikeinterface/widgets/ipywidgets/timeseries.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
-from ipywidgets import AppLayout, HBox, FloatSlider, Dropdown, BoundedFloatText
+from ipywidgets import AppLayout, Layout, Output,  HBox, FloatSlider, Dropdown, BoundedFloatText, VBox
 
 
 from .base_ipywidgets import IpywidgetsPlotter
@@ -16,9 +16,17 @@ class TimeseriesPlotter(IpywidgetsPlotter):
     def do_plot(self, data_plot, **backend_kwargs):
         rec = data_plot['recording']
         
-        plt.ioff()
-        fig, ax = plt.subplots()
+        cm = 1/2.54
+        # width in cm
+        width = 25
+        hight = 15
         
+        with plt.ioff():
+            output = Output(layout=Layout(width=f'{width}cm'))
+            with output:
+                fig, ax = plt.subplots(figsize=(width * cm, hight * cm))
+                plt.show()
+
         t_start = 0. 
         t_stop = rec.get_num_samples(segment_index=0) / rec.get_sampling_frequency()
         
@@ -26,10 +34,11 @@ class TimeseriesPlotter(IpywidgetsPlotter):
         time_slider = FloatSlider(
             orientation='horizontal',
             description='time:',
-            value=t_start,
+            value=data_plot['time_range'][0],
             min=t_start,
             max=t_stop,
             continuous_update=False,
+            layout=Layout(width=f'{width}cm')
         )
         
         
@@ -43,17 +52,20 @@ class TimeseriesPlotter(IpywidgetsPlotter):
         
         widgets = (time_slider, seg_selector, win_sizer, mode_selector)
         
-        updater = PlotUpdater(rec, ax, *widgets)
+        updater = PlotUpdater(rec, ax,  *widgets)
         for w in widgets:
             w.observe(updater)
         
         
         app = AppLayout(
-            center=fig.canvas,
-            footer=HBox([time_slider, seg_selector, win_sizer, mode_selector]),
+            center=output,
+            footer=VBox([time_slider,
+                        HBox([seg_selector, win_sizer, mode_selector]),
+                        ]),
             pane_heights=[0, 6, 1]
         )
         
+        # a first update
         updater(None)
         
         display(app)
@@ -63,7 +75,7 @@ TimeseriesPlotter.register(TimeseriesWidget)
 
 
 class PlotUpdater:
-    def __init__(self, rec, ax, time_slider, seg_selector, win_sizer, mode_selector):
+    def __init__(self, rec, ax,  time_slider, seg_selector, win_sizer, mode_selector):
         self.rec = rec
         self.ax = ax
         self.time_slider = time_slider
@@ -73,15 +85,11 @@ class PlotUpdater:
         
     
     def __call__(self, change):
-        # print(change)
-        # t = change.new
-        
         t = self.time_slider.value
         d = self.win_sizer.value
         time_range = (t, t+d)
         
         self.ax.clear()
-        
         
         plot_timeseries(self.rec,
                 segment_index=self.seg_selector.value,
