fe6a99324ba4f9e67736fdf326ba62e3c0962632
yger <pierre.yger@gmail.com>
2022-06-29 10:39:07 +0200

    Tests and optimizations

--- spikeinterface/sortingcomponents/tests/test_features_from_peaks.py
+++ spikeinterface/sortingcomponents/tests/test_features_from_peaks.py
@@ -26,9 +26,7 @@ def test_features_from_peaks():
                          chunk_size=10000, verbose=1, progress_bar=False, noise_levels=noise_levels)
 
     # locally_exclusive
-    features = compute_features_from_peaks(recording, peaks, ['ptp', 'energy', 'com', 'dist_com_vs_max_ptp_channel', 'dist_com_vs_max_std_channel'])
-
-    print(features)
+    features = compute_features_from_peaks(recording, peaks, ['amplitude', 'ptp', 'energy', 'com', 'dist_com_vs_max_ptp_channel', 'dist_com_vs_max_std_channel'])
 
 if __name__ == '__main__':
     test_features_from_peaks()
