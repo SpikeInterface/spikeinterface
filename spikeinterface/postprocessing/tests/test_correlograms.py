b468fdea9c1a4960ce230e4c6d6365ece988e795
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-07-01 11:30:53 +0200

    Added ISI numpy and unit test

--- spikeinterface/postprocessing/tests/test_correlograms.py
+++ spikeinterface/postprocessing/tests/test_correlograms.py
@@ -12,7 +12,7 @@ except ModuleNotFoundError as err:
     HAVE_NUMBA = False
 
 
-def _test_correlograms(recording, sorting, window_ms: float, bin_ms: float, methods: List[str]):
+def _test_correlograms(sorting, window_ms: float, bin_ms: float, methods: List[str]):
     for method in methods:
         correlograms, bins = compute_correlograms(sorting, window_ms=window_ms, bin_ms=bin_ms, symmetrize=True, 
                                                   method=method)
@@ -25,15 +25,14 @@ def _test_correlograms(recording, sorting, window_ms: float, bin_ms: float, meth
             assert np.allclose(bins, ref_bins, atol=1e-10), f"Failed with method={method}"
 
 def test_compute_correlograms():
-    methods = ["numpy"]
+    methods = ["numpy", "auto"]
     if HAVE_NUMBA:
         methods.append("numba")
 
-    recording, sorting = se.toy_example(num_segments=2, num_units=4, duration=100)
+    recording, sorting = se.toy_example(num_segments=2, num_units=10, duration=100)
 
-    _test_correlograms(recording, sorting, window_ms=60.0, bin_ms=2.0, methods=methods)
-    _test_correlograms(recording, sorting, window_ms=43.57, bin_ms=1.6421, methods=methods)
-        
+    _test_correlograms(sorting, window_ms=60.0, bin_ms=2.0, methods=methods)
+    _test_correlograms(sorting, window_ms=43.57, bin_ms=1.6421, methods=methods)
 
 
 if __name__ == '__main__':
