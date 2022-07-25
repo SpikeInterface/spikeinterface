ccd3ab30485c974a9186688c61b4e25b7442854b
Pierre Yger <pierre.yger@gmail.com>
2022-07-25 16:03:53 +0200

    Update spikeinterface/comparison/groundtruthstudy.py
    Co-authored-by: Alessio Buccino <alejoe9187@gmail.com>

--- spikeinterface/comparison/groundtruthstudy.py
+++ spikeinterface/comparison/groundtruthstudy.py
@@ -289,7 +289,7 @@ class GroundTruthStudy:
         for rec_name in self.rec_names:
             df = self.get_metrics(rec_name)
             df = df.reset_index()
-            snr.append(df)
-        snr = pd.concat(snr)
-        snr = snr.set_index(['rec_name', 'unit_id'])
+            metrics.append(df)
+        metrics = pd.concat(metrics)
+        metrics = metrics.set_index(['rec_name', 'unit_id'])
         return snr['snr']
