ea5cbed70fb0ef4f1e90feb7db79279c292f591a
Chuang Yu Min <chg.yumin@gmail.com>
2022-09-02 09:14:45 -0300

    remove compiled_name from waveclus_snippets

--- spikeinterface/sorters/waveclus/waveclus_snippets.py
+++ spikeinterface/sorters/waveclus/waveclus_snippets.py
@@ -34,7 +34,6 @@ class WaveClusSnippetsSorter(BaseSorter):
     """WaveClus Sorter object."""
 
     sorter_name: str = 'waveclus_snippets'
-    compiled_name: str = 'waveclus_snippets_compiled'
     waveclus_path: Union[str, None] = os.getenv('WAVECLUS_PATH', None)
     requires_locations = False
 
