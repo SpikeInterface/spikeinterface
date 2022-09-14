2ad460083742c719a30b1df62c6bdc99f23aa3aa
Alessio Buccino <alejoe9187@gmail.com>
2022-09-14 17:52:39 +0200

    Plot timeseries without locations + improve timeseries widget (zoom, all layers)

--- spikeinterface/widgets/ipywidgets/timeseries.py
+++ spikeinterface/widgets/ipywidgets/timeseries.py
@@ -5,7 +5,7 @@ import ipywidgets.widgets as widgets
 
 
 from .base_ipywidgets import IpywidgetsPlotter
-from .utils import make_timeseries_controller, make_channel_controller
+from .utils import make_timeseries_controller, make_channel_controller, make_scale_controller
 
 from ..timeseries import TimeseriesWidget, _get_trace_list
 from ..utils import order_channels_by_depth
@@ -27,12 +27,12 @@ class TimeseriesPlotter(IpywidgetsPlotter):
         backend_kwargs = self.update_backend_kwargs(**backend_kwargs)
         width_cm = backend_kwargs["width_cm"]
         height_cm = backend_kwargs["height_cm"]
-        ratios = [0.8, 0.2]
+        ratios = [0.1, 0.8, 0.2]
 
         with plt.ioff():
             output = widgets.Output()
             with output:
-                fig, ax = plt.subplots(figsize=(ratios[0] * width_cm * cm, height_cm * cm))
+                fig, ax = plt.subplots(figsize=(ratios[1] * width_cm * cm, height_cm * cm))
                 plt.show()
 
         t_start = 0.
@@ -40,26 +40,34 @@ class TimeseriesPlotter(IpywidgetsPlotter):
 
         ts_widget, ts_controller = make_timeseries_controller(t_start, t_stop, data_plot['layer_keys'],
                                                               rec0.get_num_segments(), data_plot['time_range'],
-                                                              data_plot['mode'], width_cm)
+                                                              data_plot['mode'], False, width_cm)
 
-        ch_widget, ch_controller = make_channel_controller(rec0, width_cm=ratios[1] * width_cm,
+        ch_widget, ch_controller = make_channel_controller(rec0, width_cm=ratios[2] * width_cm,
                                                            height_cm=height_cm)
+
+        scale_widget, scale_controller = make_scale_controller(width_cm=ratios[0] * width_cm,
+                                                               height_cm=height_cm)
 
         self.controller = ts_controller
         self.controller.update(ch_controller)
+        self.controller.update(scale_controller)
 
         mpl_plotter = MplTimeseriesPlotter()
 
         self.updater = PlotUpdater(data_plot, mpl_plotter, ax, self.controller)
         for w in self.controller.values():
-            w.observe(self.updater)
+            if isinstance(w, widgets.Button):
+                w.on_click(self.updater)
+            else:
+                w.observe(self.updater)
 
         self.widget = widgets.AppLayout(
             center=fig.canvas,
             footer=ts_widget,
+            left_sidebar=scale_widget,
             right_sidebar=ch_widget,
             pane_heights=[0, 6, 1],
-            pane_widths=[0] + ratios
+            pane_widths=ratios
         )
 
         # a first update
@@ -95,10 +103,18 @@ class PlotUpdater:
         
         # if changing the layer_key, no need to retrieve and process traces
         retrieve_traces = True
+        scale_up = False
+        scale_down = False
         if change is not None:
             for cname, c in self.controller.items():
-                if change["owner"] is c and cname == "layer_key":
-                    retrieve_traces = False
+                if isinstance(change, dict):
+                    if change["owner"] is c and cname == "layer_key":
+                        retrieve_traces = False
+                elif isinstance(change, widgets.Button):
+                    if change is c and cname == "plus":
+                        scale_up = True
+                    if change is c and cname == "minus":
+                        scale_down = True
 
         t_start = self.controller["t_start"].value
         window = self.controller["window"].value
@@ -106,6 +122,18 @@ class PlotUpdater:
         segment_index = self.controller["segment_index"].value
         mode = self.controller["mode"].value
         chan_start, chan_stop = self.controller["channel_inds"].value
+
+        if mode == "line":
+            self.controller["all_layers"].layout.visibility = 'visible'
+            all_layers = self.controller["all_layers"].value
+        elif mode == "map":
+            self.controller["all_layers"].layout.visibility = 'hidden'
+            all_layers = False
+
+        if all_layers:
+            self.controller["layer_key"].layout.visibility = 'hidden'
+        else:
+            self.controller["layer_key"].layout.visibility = 'visible'
 
         if chan_start == chan_stop:
             chan_stop += 1
@@ -138,11 +166,32 @@ class PlotUpdater:
             list_traces = data_plot['list_traces']
             frame_range = data_plot['frame_range']
             channel_ids = data_plot['channel_ids']
+
+        if all_layers:
+            layer_keys = self.data_plot['layer_keys']
+            recordings = self.recordings
+            list_traces_plot = self.list_traces
+        else:
+            layer_keys = [layer_key]
+            recordings = {layer_key: self.recordings[layer_key]}
+            list_traces_plot = [self.list_traces[list(self.recordings.keys()).index(layer_key)]]
             
-        layer_keys = [layer_key]
-        recordings = {layer_key: self.recordings[layer_key]}
-        list_traces_plot = [self.list_traces[list(self.recordings.keys()).index(layer_key)]]
-        
+        if scale_up:
+            if mode == 'line':
+                data_plot["vspacing"] *= 0.8
+            elif mode == 'map':
+                data_plot["clims"] = {layer: (1.2 * val[0], 1.2 * val[1])
+                                      for layer, val in self.data_plot["clims"].items()}
+        if scale_down:
+            if mode == 'line':
+                data_plot["vspacing"] *= 1.2
+            elif mode == 'map':
+                data_plot["clims"] = {layer: (0.8 * val[0], 0.8 * val[1])
+                                      for layer, val in self.data_plot["clims"].items()}
+
+        self.next_data_plot["vspacing"] = data_plot["vspacing"]
+        self.next_data_plot["clims"] = data_plot["clims"]
+
         if mode == 'line':
             clims = None
         elif mode == 'map':
