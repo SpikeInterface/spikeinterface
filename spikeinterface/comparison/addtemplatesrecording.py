546486a217e0349d0666454cddb7ab3bd56007bb
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-09-20 12:17:12 +0200

    Fixed get_traces

--- spikeinterface/comparison/addtemplatesrecording.py
+++ spikeinterface/comparison/addtemplatesrecording.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import List, Union
 import numpy as np
 from spikeinterface.core import BaseRecording, BaseRecordingSegment, BaseSorting, BaseSortingSegment
@@ -18,7 +17,7 @@ class AddTemplatesRecording(BaseRecording):
     nbefore: list[int] | None
         Where is the center of the template for each unit?
         If None, will default to the highest peak.
-    amplitude_factor: list[list[float]] | None
+    amplitude_factor: list[list[float]] | int | None
         The amplitude of each spike for each unit (1.0=default).
         If None, will default to 1.0 everywhere.
     target_recording: BaseRecording | None
@@ -28,7 +27,7 @@ class AddTemplatesRecording(BaseRecording):
         The number of frames in the recording.
     """
 
-    def __init__(self, sorting: BaseSorting, templates: np.ndarray, nbefore: Union[List[int], None] = None,
+    def __init__(self, sorting: BaseSorting, templates: np.ndarray, nbefore: Union[List[int], int, None] = None,
                  amplitude_factor: Union[List[List[float]], None] = None,
                  target_recording: Union[BaseRecording, None] = None, t_max: Union[List[int], None] = None) -> None:
         channel_ids = target_recording.channel_ids if target_recording is not None else list(range(templates.shape[2]))
@@ -40,6 +39,8 @@ class AddTemplatesRecording(BaseRecording):
 
         if nbefore is None:
             nbefore = np.argmax(np.max(np.abs(templates), axis=2), axis=1)
+        elif isnumeric(nbefore):
+            nbefore = [nbefore]*n_units 
         else:
             assert len(nbefore) == n_units
 
@@ -60,67 +61,71 @@ class AddTemplatesRecording(BaseRecording):
         for segment_index in range(sorting.get_num_segments()):
             spikes = self.spike_vector[self.spike_vector['segment_ind'] == segment_index]
             target_recording_segment = None if target_recording is None else target_recording._recording_segments[segment_index]
-            recording_segment = AddTemplatesRecordingSegment(spikes, templates, nbefore, amplitude_factor,
-                                                             target_recording_segment, t_max[segment_index])
+            recording_segment = AddTemplatesRecordingSegment(self.sampling_frequency, 0, self.dtype, spikes, templates, nbefore,
+                                                             amplitude_factor, target_recording_segment, t_max[segment_index]) # TODO: t_start should not necessarily be 0.
             self.add_recording_segment(recording_segment)
 
 
 
-@dataclass
 class AddTemplatesRecordingSegment(BaseRecordingSegment):
     """
     TODO
     """
 
-    spike_vector: np.ndarray
-    templates: np.ndarray
-    nbefore: List[int]
-    amplitude_factor: List[List[float]]
-    t_max: int
-    target_recording: Union[BaseRecordingSegment, None] = None
+    def __init__(self, sampling_frequency: float, t_start: int, dtype, spike_vector: np.ndarray, templates: np.ndarray, nbefore: List[int],
+                 amplitude_factor: List[List[float]], target_recording: Union[BaseRecordingSegment, None] = None, t_max: Union[int, None] = None) -> None:
 
-    # def __init__(self, spike_vector: np.ndarray, templates: np.ndarray, nbefore: List[int],
-    #              amplitude_factor: List[List[float]], target_recording: Union[BaseRecordingSegment, None] = None) -> None:
+        BaseRecordingSegment.__init__(self, sampling_frequency, t_start=t_start)
+        assert not (target_recording is None and t_max is None)
 
-    #     self.spike_vector = spike_vector
-    #     self.templates = templates
-    #     self.nbefore = nbefore
-    #     self.amplitude_factor = amplitude_factor
-    #     self.target_recording = target_recording
+        self.dtype = dtype
+        self.spike_vector = spike_vector
+        self.templates = templates
+        self.nbefore = nbefore
+        self.amplitude_factor = amplitude_factor
+        self.parent_recording = target_recording
+        self.t_max = target_recording.get_num_frames() if t_max is None else t_max
 
     def get_traces(self, start_frame: Union[int, None] = None, end_frame: Union[int, None] = None,
                    channel_indices: Union[List, None] = None) -> np.ndarray:
-        n_channels = self.templates.shape[1]
+        n_channels = self.templates.shape[2]
         start_frame = 0 if start_frame is None else start_frame
         end_frame = self.t_max if end_frame is None else end_frame
 
         if self.parent_recording is not None:
             traces = self.parent_recording.get_traces(start_frame, end_frame)
         else:
-            traces = np.zeros([t_max, n_channels], dtype=np.int16) # TODO: dtype
+            traces = np.zeros([self.t_max, n_channels], dtype=self.dtype)
         
-        channel_indices = list(range(n_channels)) if channel_indices is None else channels_indices
+        channel_indices = list(range(n_channels)) if channel_indices is None else channel_indices
 
-        for spike in self.spike_vector:
+        mask = (self.spike_vector['sample_ind'] > start_frame - self.templates.shape[1]) & \
+               (self.spike_vector['sample_ind'] < end_frame + self.templates.shape[1])
+        spike_vector = self.spike_vector[mask]
+
+        for spike in spike_vector:
             t = spike['sample_ind']
             unit_ind = spike['unit_ind']
 
-            template = self.templates[unit_ind, :, channels_indices]
+            template = self.templates[unit_ind, :, channel_indices]
             m = self.nbefore[unit_ind]
 
             # Add template to traces
             start_traces = t - m - start_frame
-            end_traces = start_traces + templates_t
+            end_traces = start_traces + self.templates.shape[1]
+            if start_traces >= end_frame-start_frame or end_traces <= 0:
+                continue
+
             start_template = 0
-            end_teplate = 0
+            end_template = self.templates.shape[1]
 
             if start_traces < 0:
                 start_template = -start_traces
                 start_traces = 0
             if end_traces > end_frame - start_frame:
-                end_template = end_frame - start_frame - end_traces
+                end_template = self.templates.shape[1] + end_frame - start_frame - end_traces
                 end_traces = end_frame - start_frame
 
-            traces[start_traces : end_traces] = template[start_template : end_teplate] * self.amplitude_factor[unit_ind]
+            traces[start_traces : end_traces] += template[start_template : end_template] # TODO: * self.amplitude_factor[unit_ind]
 
         return traces
