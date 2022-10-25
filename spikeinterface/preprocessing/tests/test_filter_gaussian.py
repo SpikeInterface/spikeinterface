c8479639f9f3b9023cea086c3d40f6fcc39e4e3b
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-10-25 16:09:14 +0200

    More tests to check for dumpability

--- spikeinterface/preprocessing/tests/test_filter_gaussian.py
+++ spikeinterface/preprocessing/tests/test_filter_gaussian.py
@@ -1,11 +1,36 @@
+import pytest
+from pathlib import Path
+from spikeinterface.core import load_extractor, set_global_tmp_folder
+from spikeinterface.core.testing import check_recordings_equal
 from spikeinterface.core.testing_tools import generate_recording
 from spikeinterface.preprocessing import gaussian_filter
 
 
-def test_filter_gaussian():
-	rec = generate_recording(num_channels=3)
-	rec = gaussian_filter(rec)
+if hasattr(pytest, "global_test_folder"):
+	cache_folder = pytest.global_test_folder / "preprocessing" / "gaussian_filter"
+else:
+	cache_folder = Path("cache_folder") / "preprocessing" / "gaussian_filter"
 
-	assert rec.get_traces(segment_index=0, end_frame=600).shape == (600, 3)
-	assert rec.get_traces(segment_index=0, start_frame=100, end_frame=600).shape == (500, 3)
-	assert rec.get_traces(segment_index=1, start_frame=rec.get_num_frames(1) - 200).shape == (200, 3)
+set_global_tmp_folder(cache_folder)
+cache_folder.mkdir(parents=True, exist_ok=True)
+
+
+def test_filter_gaussian():
+	recording = generate_recording(num_channels=3)
+	recording.annotate(is_filtered=True)
+	recording = recording.save(folder=cache_folder / "recording")
+
+	rec_filtered = gaussian_filter(recording)
+
+	assert rec_filtered.get_traces(segment_index=0, end_frame=600).shape == (600, 3)
+	assert rec_filtered.get_traces(segment_index=0, start_frame=100, end_frame=600).shape == (500, 3)
+	assert rec_filtered.get_traces(segment_index=1, start_frame=rec_filtered.get_num_frames(1) - 200).shape == (200, 3)
+
+	# Check dumpability
+	saved_loaded = load_extractor(rec_filtered.to_dict())
+	check_recordings_equal(rec_filtered, saved_loaded, return_scaled=False)
+
+	saved_1job = rec_filtered.save(folder=cache_folder / "1job")
+	saved_2job = rec_filtered.save(folder=cache_folder / "2job", n_jobs=2, chunk_duration='1s')
+	check_recordings_equal(rec_filtered, saved_1job, return_scaled=False)
+	check_recordings_equal(rec_filtered, saved_2job, return_scaled=False)
