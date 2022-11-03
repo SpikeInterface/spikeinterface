7cc0f1f26d13e1fa0b5df02950abfce1758a8b50
h-mayorquin <h.mayorquin@gmail.com>
2022-11-03 14:12:49 -0600

    quick fix for nwb recording

--- spikeinterface/extractors/nwbextractors.py
+++ spikeinterface/extractors/nwbextractors.py
@@ -168,7 +168,7 @@ class NwbRecordingExtractor(BaseRecording):
         # Set offsets
         offset = self._es.offset if hasattr(self._es, "offset") else 0
         if offset == 0 and "offset" in self._nwbfile.electrodes:
-            offset = self._nwbfile.electrodes["offset"]
+            offset = self._nwbfile.electrodes["offset"][:]
 
         self.set_channel_offsets(offset * 1e6)
 
