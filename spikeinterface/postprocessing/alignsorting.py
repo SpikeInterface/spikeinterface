7cefcfffe81970c8f41ce66fa9610ca7dfabe38a
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2022-11-09 15:15:05 +0100

    Added metadata copy from parent sortings

--- spikeinterface/postprocessing/alignsorting.py
+++ spikeinterface/postprocessing/alignsorting.py
@@ -17,6 +17,10 @@ class AlignSortingExtractor(BaseSorting):
         for segment in sorting._sorting_segments:
             self.add_sorting_segment(AlignSortingSegment(segment, unit_peak_shifts))
 
+        sorting.copy_metadata(self, only_main=False)
+        if sorting.has_recording():
+            self.register_recording(sorting._recording)
+
 
 class AlignSortingSegment(BaseSortingSegment):
     def __init__(self, parent_segment, unit_peak_shifts):
