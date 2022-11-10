aa0dc6e6c29df9ae1a5801051a487101f4bfabce
yger <pierre.yger@gmail.com>
2022-11-10 11:27:38 +0100

    Fix for the correcMotionRectording

--- spikeinterface/sortingcomponents/motion_correction.py
+++ spikeinterface/sortingcomponents/motion_correction.py
@@ -289,7 +289,7 @@ class CorrectMotionRecordingSegment(BasePreprocessorSegment):
             #     t0 = t0 + self.t_start
             times += t0
 
-        traces = self.parent_recording_segment.get_traces(start_frame, end_frame, channel_indices=None)
+        traces = self.parent_recording_segment.get_traces(start_frame, end_frame, channel_indices=slice(None))
 
         # print(traces.shape, times.shape, self.channel_locations, self.motion, self.temporal_bins, self.spatial_bins)
         trace2 = correct_motion_on_traces(traces, times, self.channel_locations, self.motion,
