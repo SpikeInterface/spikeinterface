01c4a915edb58742f67248ac9a4cf480524dacb1
yger <pierre.yger@gmail.com>
2022-12-01 20:41:04 +0100

    WIP

--- spikeinterface/sortingcomponents/benchmark/benchmark_motion_estimation.py
+++ spikeinterface/sortingcomponents/benchmark/benchmark_motion_estimation.py
@@ -159,6 +159,98 @@ class BenchmarkMotionEstimationMearec:
         ax.plot([xmin, xmax], [probe_y_max, probe_y_max], 'k--', alpha=0.5)
 
 
+    def plot_drift(self, scaling_probe=1.5):
+                
+        fig = plt.figure(figsize=(15, 10))
+        gs = fig.add_gridspec(1, 3)
+
+
+        ax = fig.add_subplot(gs[0])
+        plot_probe_map(self.recording, ax=ax)
+        _simpleaxis(ax)
+
+        mr_recording = mr.load_recordings(self.mearec_filename)
+            
+        for loc in mr_recording.template_locations:
+            if len(mr_recording.template_locations.shape) == 3:
+                ax.plot([loc[0, 1], loc[-1, 1]], [loc[0, 2], loc[-1, 2]], alpha=0.7, lw=2)
+            else:
+                ax.scatter([loc[1]], [loc[2]], alpha=0.7, s=100)
+    
+        ymin, ymax = ax.get_ylim()
+        ax.set_ylabel('depth (um)')
+        ax.set_xlabel('depth (um)')
+
+        channel_positions = self.recording.get_channel_locations()
+        probe_y_min, probe_y_max = channel_positions[:, 1].min(), channel_positions[:, 1].max()
+
+        ax.set_ylim(scaling_probe*probe_y_min, scaling_probe*probe_y_max)
+
+        ax = fig.add_subplot(gs[1:3])
+        for i in range(self.gt_unit_positions.shape[1]):
+            ax.plot(self.temporal_bins, self.gt_unit_positions[:, i], alpha=0.5, ls='--')
+        
+        if self.spatial_bins is None:
+            center = (probe_y_min + probe_y_max)//2
+            ax.plot(self.temporal_bins, self.gt_motion[:, 0] + center, color='red', lw=2)
+        else:
+            for i in range(self.gt_motion.shape[1]):
+                depth = self.spatial_bins[i]
+                ax.plot(self.temporal_bins, self.gt_motion[:, i] + depth, color='red', lw=2)
+
+
+        ax.set_ylim(ymin, ymax)
+        ax.set_xlabel('time (s)')
+        _simpleaxis(ax)
+        ax.set_yticks([])
+        ax.spines['left'].set_visible(False)
+        ax.set_ylim(scaling_probe*probe_y_min, scaling_probe*probe_y_max)
+        xmin, xmax = ax.get_xlim()
+        ax.plot([xmin, xmax], [probe_y_min, probe_y_min], 'k--', alpha=0.5)
+        ax.plot([xmin, xmax], [probe_y_max, probe_y_max], 'k--', alpha=0.5)
+
+
+    def plot_peaks(self, scaling_probe=1.5, show_drift=True):
+
+        fig = plt.figure(figsize=(15, 10))
+        gs = fig.add_gridspec(1, 3)
+        # Create the Axes.
+
+        ax = fig.add_subplot(gs[0])
+        plot_probe_map(self.recording, ax=ax)
+        _simpleaxis(ax)
+
+        ymin, ymax = ax.get_ylim()
+        ax.set_ylabel('depth (um)')
+        ax.set_xlabel('depth (um)')
+
+        channel_positions = self.recording.get_channel_locations()
+        probe_y_min, probe_y_max = channel_positions[:, 1].min(), channel_positions[:, 1].max()
+
+        ax.set_ylim(scaling_probe*probe_y_min, scaling_probe*probe_y_max)
+
+        ax = fig.add_subplot(gs[1:3])
+        x = self.selected_peaks['sample_ind']/self.recording.get_sampling_frequency()
+        y = self.peak_locations['y']
+        ax.scatter(x, y, s=1, color='k', alpha=0.05)
+        
+        xmin, xmax = ax.get_xlim()
+        ax.plot([xmin, xmax], [probe_y_min, probe_y_min], 'k--', alpha=0.5)
+        ax.plot([xmin, xmax], [probe_y_max, probe_y_max], 'k--', alpha=0.5)
+
+        _simpleaxis(ax)
+        ax.set_yticks([])
+        ax.set_ylim(scaling_probe*probe_y_min, scaling_probe*probe_y_max)
+
+        if show_drift:
+            if self.spatial_bins is None:
+                center = (probe_y_min + probe_y_max)//2
+                ax.plot(self.temporal_bins, self.gt_motion[:, 0] + center, color='red', lw=2)
+            else:
+                for i in range(self.gt_motion.shape[1]):
+                    depth = self.spatial_bins[i]
+                    ax.plot(self.temporal_bins, self.gt_motion[:, i] + depth, color='red', lw=2)
+
 
 def _simpleaxis(ax):
     ax.spines['top'].set_visible(False)
