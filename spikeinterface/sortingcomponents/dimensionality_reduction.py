0f9e25ba90e9c67696b7e3adc672d6e8e1dea286
Heberto <h.mayorquin@gmail.com>
2023-01-06 03:05:21 +0100

    added docstrings

--- spikeinterface/sortingcomponents/dimensionality_reduction.py
+++ spikeinterface/sortingcomponents/dimensionality_reduction.py
@@ -1,19 +1,46 @@
 import pickle
 
 import numpy as np
+from sklearn.decomposition import PCA
 
-from spikeinterface.sortingcomponents.peak_pipeline import run_peak_pipeline, PeakPipelineStep
+from spikeinterface.sortingcomponents.peak_pipeline import  PeakPipelineStep
 from spikeinterface.sortingcomponents.peak_detection import detect_peaks
 from spikeinterface.sortingcomponents.peak_selection import select_peaks
 from spikeinterface.postprocessing import compute_principal_components
+from spikeinterface.core import BaseRecording
 from spikeinterface.core.sparsity import ChannelSparsity
 from spikeinterface import extract_waveforms, NumpySorting
+from spikeinterface.core.job_tools import _shared_job_kwargs_doc
 
 
 class TemporalPCA(PeakPipelineStep):
     need_waveforms = True
 
-    def __init__(self, recording, ms_before=1., ms_after=1.,  peak_sign='neg', all_channels=True, model_path=None, local_radius_um=None):
+    def __init__(self, recording: BaseRecording, ms_before: float = 1., ms_after: float = 1., peak_sign: str = 'neg', 
+                all_channels: bool = True, model_path: str = None, local_radius_um: float = None):
+    
+        """
+        A step that performs a PCA projection on the waveforms extracted by a a peak_detection steps. Note that this 
+        class should be either received the path of a previously trained model with the `model_path` argument or the 
+        model can be trained with the fit method after instantiating the class.
+
+        Parameters
+        ----------
+        recording : BaseRecording
+            The recording object.
+        ms_before : float, optional
+            The number of milliseconds to include before the peak of the spike, by default 1.
+        ms_after : float, optional
+            The number of milliseconds to include after the peak of the spike, by default 1.
+        peak_sign : str, optional
+            The sign of the peak, either 'neg' or 'pos', by default 'neg'.
+        all_channels : bool, optional
+            Whether to extract spikes from all channels or only the channels with spikes, by default True.
+        model_path : str, optional
+            The path to the pca model, by default None.
+        local_radius_um : float, optional
+            The radius (in micrometers) to use for definint sparsity, by default None.
+        """
         PeakPipelineStep.__init__(self, recording, ms_before=ms_before, ms_after=ms_after, local_radius_um=local_radius_um)
         self.all_channels = all_channels
         self.peak_sign = peak_sign
@@ -23,7 +50,33 @@ class TemporalPCA(PeakPipelineStep):
     def get_dtype(self):
         return self._dtype
 
-    def fit(self, recording, n_components, detect_peaks_params, peak_selection_params, whiten=True, **job_kwargs):
+    def fit(self, recording: BaseRecording, n_components: int, detect_peaks_params: dict, 
+            peak_selection_params: dict, whiten: bool = True, **job_kwargs) -> PCA:
+        """
+        Train a pca model using the data in the recording object and the parameters provided.
+        Note that this model returns the pca model from scikit-learn but the model is also saved in the path provided
+        when instantiating the class.
+        
+        Parameters
+        ----------
+        recording : BaseRecording
+            The recording object.
+        n_components : int
+            The number of components to use for the PCA model.
+        detect_peaks_params : dict
+            The parameters for peak detection.
+        peak_selection_params : dict
+            The parameters for peak selection.
+        whiten : bool, optional
+            Whether to whiten the data, by default True.
+
+        {}
+        
+        Returns
+        -------
+        PCA: An estimator from scikit-learn.
+            The pca model
+        """
         
         # Detect peaks and sub-sample them
         peaks = detect_peaks(recording, **detect_peaks_params, **job_kwargs)
@@ -79,3 +132,5 @@ class TemporalPCA(PeakPipelineStep):
 
         return projected_waveforms
         
+
+TemporalPCA.fit.__doc__ = TemporalPCA.fit.__doc__.format(_shared_job_kwargs_doc)
