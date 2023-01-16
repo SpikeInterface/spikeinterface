5e95c152f23c1814292642d4c786cc593f6f994c
Heberto <h.mayorquin@gmail.com>
2023-01-16 09:11:56 -0700

    added parallel case test

--- spikeinterface/sortingcomponents/tests/test_dimensionality_reduction.py
+++ spikeinterface/sortingcomponents/tests/test_dimensionality_reduction.py
@@ -10,7 +10,7 @@ from spikeinterface.sortingcomponents.peak_pipeline import run_peak_pipeline
 from spikeinterface.sortingcomponents.peak_detection import detect_peaks
 
 
-def test_dimensionality_reduction(tmp_path):
+def test_pca_dimensionality_reduction(tmp_path):
     local_path = si.download_dataset(remote_path='mearec/mearec_test_10s.h5')
     recording, sorting = se.read_mearec(local_path)
 
@@ -28,7 +28,7 @@ def test_dimensionality_reduction(tmp_path):
 
     steps = [temporal_pca]
     
-    peaks, projected_waveforms = detect_peaks(recording, pipeline_steps=steps)
+    peaks, projected_waveforms = detect_peaks(recording, pipeline_steps=steps, **job_kwargs)
     extracted_n_peaks, extracted_n_components, extracted_n_channels =  projected_waveforms.shape
     
     n_peaks = peaks.shape[0]
@@ -36,7 +36,34 @@ def test_dimensionality_reduction(tmp_path):
     assert extracted_n_components == n_components
     assert extracted_n_channels == recording.get_num_channels()
     
-def test_dimensionality_reduction_sparsity(tmp_path):
+def test_pca_dimensionality_reduction_parallel(tmp_path):
+    local_path = si.download_dataset(remote_path='mearec/mearec_test_10s.h5')
+    recording, sorting = se.read_mearec(local_path)
+
+    local_radius_um = 100
+
+    model_path = tmp_path / "buffer_pca.pkl"
+    temporal_pca = TemporalPCA(recording, model_path=model_path, local_radius_um=local_radius_um)
+
+    n_components = 3
+    job_kwargs = dict(n_jobs=4, chunk_size=10000, progress_bar=True)
+    n_peaks = recording.get_num_channels() * 1e3 # Heuristic for extracting around 1k waveforms per channel
+    peak_selection_params = dict(method="uniform", select_per_channel=True,  n_peaks=n_peaks)
+    detect_peaks_params = dict(method='by_channel', peak_sign='neg', detect_threshold=5, exclude_sweep_ms=0.1)
+    temporal_pca.fit(recording, n_components, detect_peaks_params, peak_selection_params, job_kwargs)
+
+    steps = [temporal_pca]
+    
+    peaks, projected_waveforms = detect_peaks(recording, pipeline_steps=steps, **job_kwargs)
+    extracted_n_peaks, extracted_n_components, extracted_n_channels =  projected_waveforms.shape
+    
+    n_peaks = peaks.shape[0]
+    assert extracted_n_peaks == n_peaks
+    assert extracted_n_components == n_components
+    assert extracted_n_channels == recording.get_num_channels()
+    
+    
+def test_pca_dimensionality_reduction_sparsity(tmp_path):
     local_path = si.download_dataset(remote_path='mearec/mearec_test_10s.h5')
     recording, sorting = se.read_mearec(local_path)
 
@@ -54,7 +81,7 @@ def test_dimensionality_reduction_sparsity(tmp_path):
 
     steps = [temporal_pca]
     
-    peaks, projected_waveforms = detect_peaks(recording, pipeline_steps=steps)
+    peaks, projected_waveforms = detect_peaks(recording, pipeline_steps=steps, **job_kwargs)
     extracted_n_peaks, extracted_n_components, extracted_n_channels =  projected_waveforms.shape
     
     # We represent sparsity with a dict where the key is the channel and the value is a list of `valid_channels`
@@ -74,5 +101,3 @@ def test_dimensionality_reduction_sparsity(tmp_path):
         np.testing.assert_array_equal(waveform_sparsity, expected_sparsity)
 
 
-if __name__ == '__main__':
-    test_dimensionality_reduction()
