04421e11ce53ba01aeb2f1189010be18d127062d
Samuel Garcia <sam.garcia.die@gmail.com>
2023-01-19 16:30:10 +0100

    Initial refactroing for node pipeline

--- spikeinterface/sortingcomponents/peak_pipeline.py
+++ spikeinterface/sortingcomponents/peak_pipeline.py
@@ -15,45 +15,20 @@ from spikeinterface.core import get_chunk_with_margin, get_channel_distances
 from spikeinterface.core.job_tools import ChunkRecordingExecutor, fix_job_kwargs, _shared_job_kwargs_doc
 
 
-class PeakPipelineStep:
-    """
-    A PeakPipelineStep does a computation on local traces, peaks and optionaly waveforms.
-    It must return an array with shape[0] == peaks.shape[0]
-
-    It can be used to compute on-the-fly and in parralel:
-       * peak location
-       * pca (with pretrain model)
-       * some features : ptp, ...
-    """
-    need_waveforms = False
-
-    def __init__(self, recording, ms_before=None, ms_after=None, local_radius_um=None):
-        self._kwargs = dict()
-
-        if self.need_waveforms:
-            assert ms_before is not None
-            assert ms_after is not None
-
-        self.nbefore = None
-        self.nafter = None
-
-        if ms_before is not None:
-            self.nbefore = int(
-                ms_before * recording.get_sampling_frequency() / 1000.)
-            self._kwargs['ms_before'] = float(ms_before)
-
-        if ms_after is not None:
-            self.nafter = int(
-                ms_after * recording.get_sampling_frequency() / 1000.)
-            self._kwargs['ms_after'] = float(ms_after)
-
-        if local_radius_um is not None:
-            # some steps need sparsity mask
-            self._kwargs['local_radius_um'] = float(local_radius_um)
-            self.local_radius_um = local_radius_um
-            self.contact_locations = recording.get_channel_locations()
-            self.channel_distance = get_channel_distances(recording)
-            self.neighbours_mask = self.channel_distance < local_radius_um
+class PipelineNode:
+    def __init__(self, recording, name, have_global_output, parents=None):
+        self.recording = recording
+        self.name = name
+        self.have_global_output = have_global_output
+        self.parents = parents
+        
+        self._kwargs = dict(
+            name=name,
+            have_global_output=have_global_output,
+        )
+        if parents is not None:
+            self._kwargs['parents'] = parents
+            
 
     @classmethod
     def from_dict(cls, recording, kwargs):
@@ -64,35 +39,60 @@ class PeakPipelineStep:
 
     def get_trace_margin(self):
         # can optionaly be overwritten
-        if self.need_waveforms:
-            return max(self.nbefore, self.nafter)
-        elif self.nbefore is not None:
-            return max(self.nbefore, self.nafter)
-        else:
-            return 0
+        return 0
 
     def get_dtype(self):
         raise NotImplementedError
 
-    def compute_buffer(self, traces, peaks, waveforms=None):
-        raise NotImplementedError
+
+class ExtractDenseWaveforms(PipelineNode):
+    
+    def __init__(self, recording, name='extract_dense_waveforms', have_global_output=False,
+                         ms_before=None, ms_after=None):
+        PipelineNode.__init__(self, recording, name=name, have_global_output=have_global_output)
+
+        self.nbefore = int(ms_before * recording.get_sampling_frequency() / 1000.)
+        self.nafter = int(ms_after * recording.get_sampling_frequency() / 1000.)
+        
+        self._kwargs['ms_before'] = float(ms_before)
+        self._kwargs['ms_after'] = float(ms_after)
+
+    def get_trace_margin(self):
+        return max(self.nbefore, self.nafter)
+    
+    def compute(self, traces, peaks):
+        waveforms = traces[peaks['sample_ind'][:, None] + np.arange(-self.nbefore, self.nafter)]
+        return waveforms
 
 
-def run_peak_pipeline(recording, peaks, steps, job_kwargs, job_name='peak_pipeline', squeeze_output=True):
+
+def sort_nodes(nodes):
+    """
+    Resolve the order of nodes for computing
+    """
+    # TODO check that the parent are before!!!
+    
+    return nodes
+
+
+def run_peak_pipeline(recording, peaks, nodes, job_kwargs, job_name='peak_pipeline', squeeze_output=True):
     """
     Run one or several PeakPipelineStep on already detected peaks.
     """
     job_kwargs = fix_job_kwargs(job_kwargs)
-    assert all(isinstance(step, PeakPipelineStep) for step in steps)
+    assert all(isinstance(node, PipelineNode) for node in nodes)
+    
+    nodes = sort_nodes(nodes)
+    
 
-    if job_kwargs.get('n_jobs', 1) > 1:
+    if job_kwargs['n_jobs'] > 1:
         init_args = (
             recording.to_dict(),
             peaks,  # TODO peaks as shared mem to avoid copy
-            [(step.__class__, step.to_dict()) for step in steps],
+            [(node.__class__, node.to_dict()) for node in nodes],
         )
     else:
-        init_args = (recording, peaks, steps)
+        init_args = (recording, peaks, nodes)
     
     processor = ChunkRecordingExecutor(recording, _compute_peak_step_chunk, _init_worker_peak_pipeline,
                                        init_args, handle_returns=True, job_name=job_name, **job_kwargs)
@@ -105,7 +105,7 @@ def run_peak_pipeline(recording, peaks, steps, job_kwargs, job_name='peak_pipeli
     for output_step in zip(*outputs):
         outs_concat += (np.concatenate(output_step, axis=0), )
 
-    if len(steps) == 1 and squeeze_output:
+    if len(outs_concat) == 1 and squeeze_output:
         # when tuple size ==1  then remove the tuple
         return outs_concat[0]
     else:
@@ -113,28 +113,28 @@ def run_peak_pipeline(recording, peaks, steps, job_kwargs, job_name='peak_pipeli
         return outs_concat
 
 
-def _init_worker_peak_pipeline(recording, peaks, steps):
+def _init_worker_peak_pipeline(recording, peaks, nodes):
     """Initialize worker for localizing peaks."""
 
     if isinstance(recording, dict):
         from spikeinterface.core import load_extractor
         recording = load_extractor(recording)
 
-        steps = [cls.from_dict(recording, kwargs) for cls, kwargs in steps]
+        nodes = [cls.from_dict(recording, kwargs) for cls, kwargs in nodes]
 
-    max_margin = max(step.get_trace_margin() for step in steps)
+    max_margin = max(node.get_trace_margin() for node in nodes)
 
     # create a local dict per worker
     worker_ctx = {}
     worker_ctx['recording'] = recording
     worker_ctx['peaks'] = peaks
-    worker_ctx['steps'] = steps
+    worker_ctx['nodes'] = nodes
     worker_ctx['max_margin'] = max_margin
 
     # check if any waveforms is needed
-    worker_ctx['need_waveform'] = any(step.need_waveforms for step in steps)
-    if worker_ctx['need_waveform']:
-        worker_ctx['nbefore'], worker_ctx['nafter'] = get_nbefore_nafter_from_steps(steps)
+    #~ worker_ctx['need_waveform'] = any(step.need_waveforms for step in steps)
+    #~ if worker_ctx['need_waveform']:
+        #~ worker_ctx['nbefore'], worker_ctx['nafter'] = get_nbefore_nafter_from_steps(steps)
 
     return worker_ctx
 
@@ -149,6 +149,7 @@ def _compute_peak_step_chunk(segment_index, start_frame, end_frame, worker_ctx):
                                                               None, margin, add_zeros=True)
 
     # get local peaks (sgment + start_frame/end_frame)
+    # TODO precompute segment boundaries in the initialize!!!!!
     i0 = np.searchsorted(peaks['segment_ind'], segment_index)
     i1 = np.searchsorted(peaks['segment_ind'], segment_index + 1)
     peaks_in_segment = peaks[i0:i1]
@@ -159,30 +160,39 @@ def _compute_peak_step_chunk(segment_index, start_frame, end_frame, worker_ctx):
     # make sample index local to traces
     local_peaks = local_peaks.copy()
     local_peaks['sample_ind'] -= (start_frame - left_margin)
-
-    # @pierre @alessio
-    # we extract the waveforms once for all the step!!!!
-    # this avoid every step to do it, we should gain in perfs with this
-    if worker_ctx['need_waveform']:
-        waveforms = traces[local_peaks['sample_ind'][:, None] +
-                           np.arange(-worker_ctx['nbefore'], worker_ctx['nafter'])]
-    else:
-        waveforms = None
-
-    #import scipy
-    #waveforms = scipy.signal.savgol_filter(waveforms, 11, 3 , axis=1)
-
-    outs = tuple()
-    for step in worker_ctx['steps']:
-        if step.need_waveforms:
-            # give the waveforms pre extracted when needed
-            out = step.compute_buffer(traces, local_peaks, waveforms=waveforms)
+    
+    
+    nodes = worker_ctx['nodes']
+    
+    # compute the graph
+    outputs = {}
+    for node in nodes:
+        
+        if node.parents is None:
+            # no other input than traces
+            out = node.compute(traces, local_peaks)
         else:
-            out = step.compute_buffer(traces, local_peaks)
-        outs += (out, )
+            # the node need imputs from other nodes
+            inputs = tuple()
+            for parent_name in node.parents:
+                other_out = outputs[parent_name]
+                if not isinstance(other_out, tuple):
+                    other_out = (other_out, )
+                inputs += other_out
 
+            out = node.compute(traces, local_peaks, *inputs)
+
+        outputs[node.name] = out
+    
+    # propagate the output
+    outs = tuple()
+    for node in nodes:
+        if node.have_global_output:
+            out = outputs[node.name]
+            outs += (out, )
+    
     return outs
-
+    
 
 def get_nbefore_nafter_from_steps(steps):
     # check that all step have the same waveform size
--- spikeinterface/sortingcomponents/tests/test_peak_pipeline.py
+++ spikeinterface/sortingcomponents/tests/test_peak_pipeline.py
@@ -5,14 +5,79 @@ from spikeinterface import download_dataset, BaseSorting
 from spikeinterface.extractors import MEArecRecordingExtractor
 
 from spikeinterface.sortingcomponents.peak_detection import detect_peaks
-from spikeinterface.sortingcomponents.peak_pipeline import run_peak_pipeline, PeakPipelineStep
+#~ from spikeinterface.sortingcomponents.peak_pipeline import run_peak_pipeline, PeakPipelineStep
+
+from spikeinterface.sortingcomponents.peak_pipeline import run_peak_pipeline, PipelineNode, ExtractDenseWaveforms
 
 
 
 
-class MyStep(PeakPipelineStep):
-    def __init__(self, recording, param0=5.5):
-        PeakPipelineStep.__init__(self, recording)
+#~ class MyStep(PeakPipelineStep):
+    #~ def __init__(self, recording, param0=5.5):
+        #~ PeakPipelineStep.__init__(self, recording)
+        #~ self.param0 = param0
+        
+        #~ self._kwargs.update(dict(param0=param0))
+        #~ self._dtype = np.dtype([('abs_amplitude', recording.get_dtype())])
+
+    #~ def get_dtype(self):
+        #~ return self._dtype
+    
+    #~ def compute_buffer(self, traces, peaks):
+        #~ amps = np.zeros(peaks.size, dtype=self._dtype)
+        #~ amps['abs_amplitude'] = np.abs(peaks['amplitude'])
+        #~ return amps
+    
+    #~ def get_trace_margin(self):
+        #~ return 5
+
+
+#~ class MyStepWithWaveforms(PeakPipelineStep):
+    #~ need_waveforms = True
+    #~ def __init__(self, recording, ms_before=1., ms_after=1.):
+        #~ PeakPipelineStep.__init__(self, recording, ms_before=ms_before, ms_after=ms_after)
+
+    #~ def get_dtype(self):
+        #~ return np.dtype('float32')
+    
+    #~ def compute_buffer(self, traces, peaks, waveforms):
+        #~ rms_by_channels = np.sum(waveforms ** 2, axis=1)
+        #~ return rms_by_channels
+
+    #~ def get_trace_margin(self):
+        #~ return max(self.nbefore, self.nafter)
+
+
+#~ def test_run_peak_pipeline():
+
+    #~ repo = 'https://gin.g-node.org/NeuralEnsemble/ephy_testing_data'
+    #~ remote_path = 'mearec/mearec_test_10s.h5'
+    #~ local_path = download_dataset(
+        #~ repo=repo, remote_path=remote_path, local_folder=None)
+    #~ recording = MEArecRecordingExtractor(local_path)
+
+    
+    #~ job_kwargs = dict(chunk_duration='0.5s', n_jobs=2, progress_bar=False)
+    
+    #~ peaks = detect_peaks(recording, method='locally_exclusive',
+                         #~ peak_sign='neg', detect_threshold=5, exclude_sweep_ms=0.1,
+                         #~ **job_kwargs)
+    
+    #~ # one step only : squeeze output
+    #~ steps = [MyStep(recording, param0=6.6)]
+    #~ step_one = run_peak_pipeline(recording, peaks, steps, job_kwargs, squeeze_output=True)
+    #~ assert np.allclose(np.abs(peaks['amplitude']), step_one['abs_amplitude'])
+    
+    #~ # two step
+    #~ steps = [MyStep(recording, param0=5.5), MyStepWithWaveforms(recording, ms_before=.5, ms_after=1.)]
+    #~ step_one, step_two = run_peak_pipeline(recording, peaks, steps, job_kwargs)
+    #~ assert np.allclose(np.abs(peaks['amplitude']), step_one['abs_amplitude'])
+    #~ assert step_two.shape[0] == peaks.shape[0]
+    #~ assert step_two.shape[1] == recording.get_num_channels()
+
+class MyStep(PipelineNode):
+    def __init__(self, recording, name='my_step', have_global_output=True, param0=5.5):
+        PipelineNode.__init__(self, recording, name, have_global_output)
         self.param0 = param0
         
         self._kwargs.update(dict(param0=param0))
@@ -21,7 +86,7 @@ class MyStep(PeakPipelineStep):
     def get_dtype(self):
         return self._dtype
     
-    def compute_buffer(self, traces, peaks):
+    def compute(self, traces, peaks):
         amps = np.zeros(peaks.size, dtype=self._dtype)
         amps['abs_amplitude'] = np.abs(peaks['amplitude'])
         return amps
@@ -30,20 +95,18 @@ class MyStep(PeakPipelineStep):
         return 5
 
 
-class MyStepWithWaveforms(PeakPipelineStep):
+class MyStepWithWaveforms(PipelineNode):
     need_waveforms = True
-    def __init__(self, recording, ms_before=1., ms_after=1.):
-        PeakPipelineStep.__init__(self, recording, ms_before=ms_before, ms_after=ms_after)
+    def __init__(self, recording, name='my_step_with_waveforms', have_global_output=True, parents=[]):
+        PipelineNode.__init__(self, recording, name, have_global_output, parents=parents)
 
     def get_dtype(self):
         return np.dtype('float32')
     
-    def compute_buffer(self, traces, peaks, waveforms):
+    def compute(self, traces, peaks, waveforms):
+        print('waveforms.shape', waveforms.shape)
         rms_by_channels = np.sum(waveforms ** 2, axis=1)
         return rms_by_channels
-
-    def get_trace_margin(self):
-        return max(self.nbefore, self.nafter)
 
 
 def test_run_peak_pipeline():
@@ -55,24 +118,37 @@ def test_run_peak_pipeline():
     recording = MEArecRecordingExtractor(local_path)
 
     
-    job_kwargs = dict(chunk_duration='0.5s', n_jobs=2, progress_bar=False)
+    #~ job_kwargs = dict(chunk_duration='0.5s', n_jobs=2, progress_bar=False)
+    job_kwargs = dict(chunk_duration='0.5s', n_jobs=1, progress_bar=False)
     
     peaks = detect_peaks(recording, method='locally_exclusive',
                          peak_sign='neg', detect_threshold=5, exclude_sweep_ms=0.1,
                          **job_kwargs)
     
     # one step only : squeeze output
-    steps = [MyStep(recording, param0=6.6)]
-    step_one = run_peak_pipeline(recording, peaks, steps, job_kwargs, squeeze_output=True)
-    assert np.allclose(np.abs(peaks['amplitude']), step_one['abs_amplitude'])
+    #~ nodes = [
+        #~ MyStep(recording, param0=6.6),
+    #~ ]
+    #~ step_one = run_peak_pipeline(recording, peaks, nodes, job_kwargs, squeeze_output=True)
+    #~ assert np.allclose(np.abs(peaks['amplitude']), step_one['abs_amplitude'])
     
-    # two step
-    steps = [MyStep(recording, param0=5.5), MyStepWithWaveforms(recording, ms_before=.5, ms_after=1.)]
-    step_one, step_two = run_peak_pipeline(recording, peaks, steps, job_kwargs)
+    # 3 nodes two have outputs
+    nodes = [
+        ExtractDenseWaveforms(recording, name='extract_dense_waveforms', ms_before=.5, ms_after=1.),
+        MyStep(recording, param0=5.5),
+        MyStepWithWaveforms(recording, parents=['extract_dense_waveforms'])
+    ]
+    for node in nodes:
+        print(node.to_dict())
+        cls, kwargs = node.__class__, node.to_dict()
+        print(cls, kwargs)
+        node2 = cls.from_dict(recording, kwargs)
+        
+    
+    step_one, step_two = run_peak_pipeline(recording, peaks, nodes, job_kwargs)
     assert np.allclose(np.abs(peaks['amplitude']), step_one['abs_amplitude'])
     assert step_two.shape[0] == peaks.shape[0]
     assert step_two.shape[1] == recording.get_num_channels()
-
 
 
 if __name__ == '__main__':
