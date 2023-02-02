127d6d5c40b159e7458a6fcdff786db21fd9f44b
yger <pierre.yger@gmail.com>
2023-02-02 10:32:43 +0100

    WIP

--- spikeinterface/preprocessing/tests/test_silence.py
+++ spikeinterface/preprocessing/tests/test_silence.py
@@ -7,6 +7,9 @@ from spikeinterface.core import generate_recording
 
 from spikeinterface.preprocessing import silence_periods
 
+
+from spikeinterface.core import get_noise_levels
+
 import numpy as np
 
 
@@ -23,10 +26,16 @@ def test_silence():
     rec0 = silence_periods(rec, list_periods=[[[0, 1000], [5000, 6000]], []], mode="zeros")
     rec0.save(verbose=False)
 
-    traces0 = rec0.get_traces(segment_index=0, channel_ids=[1])
+    traces0 = rec0.get_traces(segment_index=0, start_frame=0, end_frame=1000)
+    traces1 = rec0.get_traces(segment_index=0, start_frame=5000, end_frame=6000)
+    assert np.all(traces0 == 0) and np.all(traces1 == 0)
 
     rec1 = silence_periods(rec, list_periods=[[[0, 1000], [5000, 6000]], []], mode="noise")
     rec1.save(verbose=False)
+    noise_levels = get_noise_levels(rec, return_scaled=False)
+    traces0 = rec1.get_traces(segment_index=0, start_frame=0, end_frame=1000)
+    traces1 = rec1.get_traces(segment_index=0, start_frame=5000, end_frame=6000)
+    assert np.abs((np.std(traces0, axis=0) - noise_levels) < 0.1).sum() and np.abs((np.std(traces1, axis=0) - noise_levels)).sum() < 0.1
 
 
 
