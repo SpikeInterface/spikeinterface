c8c20e9ca29d58a7db86c7712605ff9c5694bc31
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-02-09 13:00:25 +0100

    produce timing for core tests

---
+++ .github/build_job_summary.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import pandas as pd
+
+file_path = Path("test_output.txt")
+assert file_path.is_file(), "file not found"
+
+all_text = file_path.read_text()
+all_lines = all_text.splitlines()
+
+# find the line with the line with slowest in text
+start_line = next(line for line in all_lines if "slowest" in line)
+start_index = all_lines.index(start_line) + 1
+# last_index = all_lines.index(next(line for line in all_lines if line in ['\n', '\r\n']))
+# last_index = next(index for index, line in enumerate(all_lines[start_index:]) if len(line)==0) + start_index
+
+last_index = next(index for index, line in enumerate(all_lines[start_index:]) if "===" in line) + start_index
+#last_index = all_lines.index(last_line)
+
+timing_info = all_lines[start_index:last_index]
+timing_column = [float(line.split("s")[0].rstrip()) for line in timing_info]
+short_name_column = [line.rpartition('::')[2] for line in timing_info]
+
+data_frame = pd.DataFrame(dict(time=timing_column, test_name=short_name_column))
+data_frame["%time"] = 100 * data_frame["time"] / data_frame["time"].sum()
+data_frame["%cum_total_time"] = 100 *  data_frame["time"].cumsum() / data_frame["time"].sum()
+data_frame[["time", "test_name", "%time", "%cum_total_time"]].to_markdown("test_markdown.md")
