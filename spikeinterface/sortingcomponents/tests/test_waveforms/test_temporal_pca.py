d5803d23fc03d147ee2f3517988c1e648c7b9d6f
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-02-17 10:39:22 -0300

    added checks for temporal parameters mismatch

--- spikeinterface/sortingcomponents/tests/test_waveforms/test_temporal_pca.py
+++ spikeinterface/sortingcomponents/tests/test_waveforms/test_temporal_pca.py
@@ -1,84 +1,139 @@
-
 import pytest
 
 import numpy as np
 
 import spikeinterface as si
 import spikeinterface.extractors as se
-from spikeinterface.sortingcomponents.waveforms.temporal_pca import TemporalPCAProjection 
+from spikeinterface.sortingcomponents.waveforms.temporal_pca import TemporalPCAProjection
 from spikeinterface.sortingcomponents.peak_pipeline import ExtractDenseWaveforms, ExtractSparseWaveforms
 from spikeinterface.sortingcomponents.peak_detection import detect_peaks
 
 
-
-    
 def test_pca_projection(tmp_path):
-    local_path = si.download_dataset(remote_path='mearec/mearec_test_10s.h5')
+    local_path = si.download_dataset(remote_path="mearec/mearec_test_10s.h5")
     recording, sorting = se.read_mearec(local_path)
 
     # Parameters
     ms_before = 1.0
     ms_after = 1.0
     job_kwargs = dict(n_jobs=4, chunk_size=10000, progress_bar=True)
-    model_path = tmp_path / "buffer_pca.pkl"
-
-    # Node initialization
-    extract_waveforms = ExtractDenseWaveforms(recording=recording, ms_before=ms_before,
-                                              ms_after=ms_after, return_ouput=False)
-    temporal_pca = TemporalPCAProjection(recording=recording, model_path=model_path, parents=[extract_waveforms])
-    pipeline_nodes = [extract_waveforms, temporal_pca]
-
+    model_folder_path = tmp_path
     # Fit the model
     n_components = 3
-    n_peaks = recording.get_num_channels() * 1e3 # Heuristic for extracting around 1k waveforms per channel
-    peak_selection_params = dict(method="uniform", select_per_channel=True,  n_peaks=n_peaks)
-    detect_peaks_params = dict(method='by_channel', peak_sign='neg', detect_threshold=5, exclude_sweep_ms=0.1)
-    temporal_pca.fit(recording, n_components=n_components, ms_before=ms_before, ms_after=ms_after, 
-                     detect_peaks_params=detect_peaks_params, peak_selection_params=peak_selection_params, 
-                     job_kwargs=job_kwargs)
+    n_peaks = recording.get_num_channels() * 1e3  # Heuristic for extracting around 1k waveforms per channel
+    peak_selection_params = dict(method="uniform", select_per_channel=True, n_peaks=n_peaks)
+    detect_peaks_params = dict(method="by_channel", peak_sign="neg", detect_threshold=5, exclude_sweep_ms=0.1)
+    TemporalPCAProjection.fit(
+        recording=recording,
+        model_folder_path=model_folder_path,
+        n_components=n_components,
+        ms_before=ms_before,
+        ms_after=ms_after,
+        detect_peaks_params=detect_peaks_params,
+        peak_selection_params=peak_selection_params,
+        job_kwargs=job_kwargs,
+    )
+
+    # Node initialization
+    extract_waveforms = ExtractDenseWaveforms(
+        recording=recording, ms_before=ms_before, ms_after=ms_after, return_ouput=False
+    )
+    temporal_pca = TemporalPCAProjection(
+        recording=recording, model_folder_path=model_folder_path, parents=[extract_waveforms]
+    )
+    pipeline_nodes = [extract_waveforms, temporal_pca]
 
     # Extract features and compare
     peaks, projected_waveforms = detect_peaks(recording, pipeline_nodes=pipeline_nodes, **job_kwargs)
-    extracted_n_peaks, extracted_n_components, extracted_n_channels =  projected_waveforms.shape    
+    extracted_n_peaks, extracted_n_components, extracted_n_channels = projected_waveforms.shape
     n_peaks = peaks.shape[0]
     assert extracted_n_peaks == n_peaks
     assert extracted_n_components == n_components
     assert extracted_n_channels == recording.get_num_channels()
     assert extracted_n_channels == recording.get_num_channels()
-    
-    
-def test_pca_projection_sparsity(tmp_path):
-    local_path = si.download_dataset(remote_path='mearec/mearec_test_10s.h5')
+
+def test_pca_projection_waveform_extract_and_model_mismatch(tmp_path):
+    local_path = si.download_dataset(remote_path="mearec/mearec_test_10s.h5")
     recording, sorting = se.read_mearec(local_path)
     
+    # Parameters
+    ms_before = 1.0
+    ms_after = 1.0
+    job_kwargs = dict(n_jobs=4, chunk_size=10000, progress_bar=True)
+    model_folder_path = tmp_path
+    # Fit the model
+    n_components = 3
+    n_peaks = recording.get_num_channels() * 1e3  # Heuristic for extracting around 1k waveforms per channel
+    peak_selection_params = dict(method="uniform", select_per_channel=True, n_peaks=n_peaks)
+    detect_peaks_params = dict(method="by_channel", peak_sign="neg", detect_threshold=5, exclude_sweep_ms=0.1)
+    TemporalPCAProjection.fit(
+        recording=recording,
+        model_folder_path=model_folder_path,
+        n_components=n_components,
+        ms_before=ms_before,
+        ms_after=ms_after,
+        detect_peaks_params=detect_peaks_params,
+        peak_selection_params=peak_selection_params,
+        job_kwargs=job_kwargs,
+    )
+    
+    # Node initialization
+    ms_before = 0.5
+    ms_after = 1.5
+    extract_waveforms = ExtractDenseWaveforms(
+        recording=recording, ms_before=ms_before, ms_after=ms_after, return_ouput=False
+    )
+    # Pytest raise assertion
+    with pytest.raises(AttributeError):
+        TemporalPCAProjection(
+            recording=recording, model_folder_path=model_folder_path, parents=[extract_waveforms]
+        )
+
+    
+def test_pca_projection_sparsity(tmp_path):
+    local_path = si.download_dataset(remote_path="mearec/mearec_test_10s.h5")
+    recording, sorting = se.read_mearec(local_path)
+
     # Parameters
     local_radius_um = 40
     ms_before = 1.0
     ms_after = 1.0
     job_kwargs = dict(n_jobs=1, chunk_size=10000, progress_bar=True)
-    model_path = tmp_path / "buffer_pca.pkl"
-
-    # Node initialization
-    extract_waveforms = ExtractSparseWaveforms(recording=recording, ms_before=ms_before,
-                                              ms_after=ms_after, local_radius_um=local_radius_um, return_ouput=True)
-    temporal_pca = TemporalPCAProjection(recording=recording, model_path=model_path, parents=[extract_waveforms])
-    pipeline_nodes = [extract_waveforms, temporal_pca]   
-
+    model_folder_path = tmp_path
+    
     # Fit the model
     n_components = 8
     n_peaks = 100
-    peak_selection_params = dict(method="uniform", select_per_channel=True,  n_peaks=n_peaks)
-    detect_peaks_params = dict(method='by_channel', peak_sign='neg', detect_threshold=5, exclude_sweep_ms=0.1)
-    temporal_pca.fit(recording, n_components=n_components, ms_before=ms_before, ms_after=ms_after, 
-                     detect_peaks_params=detect_peaks_params, peak_selection_params=peak_selection_params, 
-                     job_kwargs=job_kwargs)
+    peak_selection_params = dict(method="uniform", select_per_channel=True, n_peaks=n_peaks)
+    detect_peaks_params = dict(method="by_channel", peak_sign="neg", detect_threshold=5, exclude_sweep_ms=0.1)
+    TemporalPCAProjection.fit(
+        recording=recording,
+        model_folder_path=model_folder_path,
+        n_components=n_components,
+        ms_before=ms_before,
+        ms_after=ms_after,
+        detect_peaks_params=detect_peaks_params,
+        peak_selection_params=peak_selection_params,
+        job_kwargs=job_kwargs,
+    )
+
+    # Node initialization
+    extract_waveforms = ExtractSparseWaveforms(
+        recording=recording, ms_before=ms_before, ms_after=ms_after, local_radius_um=local_radius_um, return_ouput=True
+    )
+    temporal_pca = TemporalPCAProjection(
+        recording=recording, model_folder_path=model_folder_path, parents=[extract_waveforms]
+    )
+    pipeline_nodes = [extract_waveforms, temporal_pca]
+
 
     # Extract features and compare
     peaks, sparse_waveforms, projected_waveforms = detect_peaks(recording, pipeline_nodes=pipeline_nodes, **job_kwargs)
-    extracted_n_peaks, extracted_n_components, extracted_n_channels =  projected_waveforms.shape    
+    extracted_n_peaks, extracted_n_components, extracted_n_channels = projected_waveforms.shape
     n_peaks = peaks.shape[0]
     max_n_channels = sparse_waveforms.shape[2]
-    
+
     assert extracted_n_peaks == n_peaks
     assert extracted_n_components == n_components
     assert extracted_n_channels == max_n_channels
+    
--- spikeinterface/sortingcomponents/waveforms/temporal_pca.py
+++ spikeinterface/sortingcomponents/waveforms/temporal_pca.py
@@ -1,10 +1,11 @@
 import pickle
+import json
 from pathlib import Path
 
 import numpy as np
 from sklearn.decomposition import IncrementalPCA
 
-from spikeinterface.sortingcomponents.peak_pipeline import  PipelineNode
+from spikeinterface.sortingcomponents.peak_pipeline import PipelineNode, WaveformExtractorNode
 from spikeinterface.sortingcomponents.peak_detection import detect_peaks
 from spikeinterface.sortingcomponents.peak_selection import select_peaks
 from spikeinterface.postprocessing import compute_principal_components
@@ -17,31 +18,87 @@ from .waveform_utils import to_temporal_representation, from_temporal_representa
 
 
 class TemporalPCBaseNode(PipelineNode):
-    
-    def __init__(self, recording: BaseRecording, parents: list = list[PipelineNode], model_path: str = None, return_ouput=True):
+    def __init__(
+        self, recording: BaseRecording, parents: list[PipelineNode], model_folder_path: str, return_ouput=True
+    ):
         """
-        Base class for PCA projection nodes. Contains the logic of the fit method that should be inherited by all the 
-        child classess. The child should implement a compute method that does a specific operation 
+        Base class for PCA projection nodes. Contains the logic of the fit method that should be inherited by all the
+        child classess. The child should implement a compute method that does a specific operation
         (e.g. project, denoise, etc)
         """
-        
+
         PipelineNode.__init__(self, recording=recording, parents=parents, return_ouput=return_ouput)
 
-        self.model_path = model_path
+        self.model_folder_path = model_folder_path
         self._dtype = recording.get_dtype()
 
-        if model_path is not None and Path(model_path).is_file():
-            with open(model_path, "rb") as f:
-                self.pca_model = pickle.load(f)
+        if not Path(model_folder_path).is_dir():
+            exception_string = (
+                f"Pca folder not a dir, "
+                f"Train the pca model before running compute_buffer using {self.__name__}.fit(kwargs)"
+            )
+            raise AttributeError(exception_string)
 
-    def fit(self, recording: BaseRecording, n_components: int, detect_peaks_params: dict, peak_selection_params: dict, 
-            job_kwargs: dict = None, ms_before: float = 1.0, ms_after: float =1.0, whiten: bool = True,
-            local_radius_um: float = None) -> IncrementalPCA:
+        # Load the model and the time interval dict from the model_folder
+        model_path = Path(model_folder_path) / "pca_model.pkl"
+        with open(model_path, "rb") as f:
+            self.pca_model = pickle.load(f)
+        model_time_interval_path = Path(model_folder_path) / "time_interval.json"
+        with open(model_time_interval_path, "rb") as f:
+            self.waveform_time_interval_dict = json.load(f)
+
+        # Check that the parents contain a waveform extractor
+        waveform_extractor_in_parrents = any([isinstance(parent, WaveformExtractorNode) for parent in self.parents])
+        if not waveform_extractor_in_parrents:
+            exception_string = f"TemporalPCBaseNode should have a WaveformExtractorNode as parent, "
+            raise AttributeError(exception_string)
+
+        self.assert_model_and_waveform_temporal_match()
+    
+    def assert_model_and_waveform_temporal_match(self):
+        """
+        Asserts that the model and the waveform extractor have the same temporal parameters
+        """        
+        # Extract the first waveform extractor in the parents
+        waveform_extractor = next(parent for parent in self.parents if isinstance(parent, WaveformExtractorNode))
+        waveforms_ms_before = waveform_extractor.ms_before
+        waveforms_ms_after = waveform_extractor.ms_after
+        waveforms_sampling_frequency = waveform_extractor.recording.get_sampling_frequency()
+
+        model_ms_before = self.waveform_time_interval_dict["ms_before"]
+        model_ms_after= self.waveform_time_interval_dict["ms_after"]
+        model_sampling_frequency = self.waveform_time_interval_dict["sampling_frequency"]
+        
+        ms_before_mismatch = waveforms_ms_before != model_ms_before
+        ms_after_missmatch = waveforms_ms_after != model_ms_after
+        sampling_frequency_mismatch = waveforms_sampling_frequency != model_sampling_frequency
+        if ms_before_mismatch or ms_after_missmatch or sampling_frequency_mismatch:
+            exception_string = (
+                "Time interval mistamch between waveform extractor and the time interval used to train the model, \n"
+                f"{model_ms_before=} and {waveforms_ms_after=} \n"
+                f"{model_ms_after=} and {waveforms_ms_after=} \n"
+                f"{model_sampling_frequency=} and {waveforms_sampling_frequency=} \n"
+            )
+            raise AttributeError(exception_string)
+    
+    @staticmethod
+    def fit(
+        recording: BaseRecording,
+        n_components: int,
+        model_folder_path: str,
+        detect_peaks_params: dict,
+        peak_selection_params: dict,
+        job_kwargs: dict = None,
+        ms_before: float = 1.0,
+        ms_after: float = 1.0,
+        whiten: bool = True,
+        local_radius_um: float = None,
+    ) -> IncrementalPCA:
         """
         Train a pca model using the data in the recording object and the parameters provided.
         Note that this model returns the pca model from scikit-learn but the model is also saved in the path provided
         when instantiating the class.
-        
+
         Parameters
         ----------
         recording : BaseRecording
@@ -62,51 +119,67 @@ class TemporalPCBaseNode(PipelineNode):
             The number of milliseconds to include after the peak of the spike, by default 1.
 
         {}
-        
+
         Returns
         -------
         IncrementalPCA: An incremental PCA estimator from scikit-learn.
             The pca model
         """
-        
+
         # Detect peaks and sub-sample them
         peaks = detect_peaks(recording, **detect_peaks_params, **job_kwargs)
-        peaks = select_peaks(peaks, **peak_selection_params) # How to select n_peaks
+        peaks = select_peaks(peaks, **peak_selection_params)  # How to select n_peaks
 
         # Creates a numpy sorting object where the spike times are the peak times and the unit ids are the peak channel
-        sorting = NumpySorting.from_peaks(peaks, sampling_frequency=recording.sampling_frequency) 
+        sorting = NumpySorting.from_peaks(peaks, sampling_frequency=recording.sampling_frequency)
         # Create a waveform extractor
-        we = extract_waveforms(recording, sorting, ms_before=ms_before, ms_after=ms_after, folder=None, 
-                               mode="memory", max_spikes_per_unit=None, **job_kwargs)
+        we = extract_waveforms(
+            recording,
+            sorting,
+            ms_before=ms_before,
+            ms_after=ms_after,
+            folder=None,
+            mode="memory",
+            max_spikes_per_unit=None,
+            **job_kwargs,
+        )
 
         # compute PCA by_channel_global (with sparsity)
-        if local_radius_um is None:
-            sparsity = None
-        else:
-            sparsity = ChannelSparsity.from_radius(we, radius_um=local_radius_um)
-        pc = compute_principal_components(we, n_components=n_components,  mode='by_channel_global',
-                                        sparsity=sparsity, whiten=whiten)
-        
-        self.pca_model  = pc.get_pca_model()
+        sparsity = ChannelSparsity.from_radius(we, radius_um=local_radius_um) if local_radius_um else None
+        pc = compute_principal_components(
+            we, n_components=n_components, mode="by_channel_global", sparsity=sparsity, whiten=whiten
+        )
+
+        pca_model = pc.get_pca_model()
+        waveform_time_interval_dict = {
+            "ms_before": ms_before,
+            "ms_after": ms_after,
+            "sampling_frequency": recording.get_sampling_frequency(),
+        }
+
+        # Load the model and the time interval dict from the model_folder
+        if model_folder_path is not None and Path(model_folder_path).is_dir():
+            model_path = Path(model_folder_path) / "pca_model.pkl"
+            with open(model_path, "wb") as f:
+                pickle.dump(pca_model, f)
+            model_time_interval_path = Path(model_folder_path) / "time_interval.json"
+            with open(model_time_interval_path, "w") as f:
+                json.dump(waveform_time_interval_dict, f)
+
+        return model_folder_path
 
 
-        with open(self.model_path, "wb") as f:
-            pickle.dump(self.pca_model, f)
-            
-        return self.pca_model
-    
-    
 TemporalPCBaseNode.fit.__doc__ = TemporalPCBaseNode.fit.__doc__.format(_shared_job_kwargs_doc)
 
 
 class TemporalPCAProjection(TemporalPCBaseNode):
     """
     A step that performs a PCA projection on the waveforms extracted by a peak_detection function.
-        
+
     This class can work in two ways:
-    1) The class can receive the path of a previously trained model with the `model_path` argument.
-    2) The class can be trained with the fit method after instantiating the class. The model will be stored in 
-    `model_path`.
+    1) The class can receive the path of a previously trained model with the `model_folder_path` argument.
+    2) The class can be trained with the fit method after instantiating the class. The model will be stored in
+    `model_folder_path`.
 
 
     Parameters
@@ -115,22 +188,23 @@ class TemporalPCAProjection(TemporalPCBaseNode):
         The recording object.
     parents: list
         The parent nodes of this node. This should contain a mechanism to extract waveforms.
-    model_path : str, optional
-        The path to the pca model, by default None.
-    local_radius_um : float, optional
-        The radius (in micrometers) to use for definint sparsity, by default None.
-        
-    """    
+    model_folder_path : str, optional
+        The path to the folder containing the pca model and the training metadata, by default None.
+    return_output: bool, optional, true by default
+        use false to suppress the output of this node in the pipeline
 
+    """
 
-    def __init__(self, recording: BaseRecording, parents: list = list[PipelineNode], model_path: str = None, return_ouput=True):
+    def __init__(
+        self, recording: BaseRecording, parents: list[PipelineNode], model_folder_path: str, return_ouput=True
+    ):
+        TemporalPCBaseNode.__init__(
+            self, recording=recording, parents=parents, return_ouput=return_ouput, model_folder_path=model_folder_path
+        )
 
-        
-        TemporalPCBaseNode.__init__(self, recording=recording, parents=parents, return_ouput=return_ouput, model_path=model_path)
-            
     def compute(self, traces: np.ndarray, peaks: np.ndarray, waveforms: np.ndarray) -> np.ndarray:
         """
-        Projects the waveforms using the PCA model trained in the fit method or loaded from the model_path.
+        Projects the waveforms using the PCA model trained in the fit method or loaded from the model_folder_path.
 
         Parameters
         ----------
@@ -148,15 +222,8 @@ class TemporalPCAProjection(TemporalPCBaseNode):
 
         """
 
-        if self.pca_model == None:
-            exception_string = (
-                f"Pca model not found, "
-                f"Train the pca model before running compute_buffer using {self.__name__}.fit(kwargs)"
-            )
-            raise AttributeError(exception_string)
-        
-        num_waveforms, num_samples , num_channels = waveforms.shape
-        
+        num_waveforms, num_samples, num_channels = waveforms.shape
+
         channeless_waveform = to_temporal_representation(waveforms)
         projected_chaneless_waveforms = self.pca_model.transform(channeless_waveform)
         projected_waveforms = from_temporal_representation(projected_chaneless_waveforms, num_channels)
