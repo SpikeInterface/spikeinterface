02a36b8a4b580081c133c7fd269da4038642deba
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-03-07 13:57:43 -0800

    Moved MyObjective to a new file, added testing file

---
+++ spikeinterface/sortingcomponents/matching/my_objective.py
@@ -0,0 +1,139 @@
+import numpy as np
+from spike_psvae.deconvolve import MatchPursuitObjectiveUpsample as Objective
+from dataclasses import dataclass
+
+@dataclass
+class ObjectiveParameters:
+    lambd: float = 0
+    allowed_scale: float = np.inf
+    save_residual: bool = False
+    t_start: int = 0
+    t_end: int = None
+    n_sec_chunk: int = 1
+    sampling_rate: int = 30_000
+    max_iter: int = 1_000
+    upsample: int = 8
+    threshold: float = 30
+    conv_approx_rank: int = 5
+    n_processors: int = 1
+    multi_processing: bool = False
+    vis_su: float = 1
+    verbose: bool = False
+    template_index_to_unit_id: np.ndarray = None
+    refractory_period_frames: int = 10
+
+
+class MyObjective(Objective):
+    """Wrapper for MatchPursuitObjectiveUpsample"""
+
+    def __init__(self, templates, **params):
+        # Unpack params
+        self.params = ObjectiveParameters(**params)
+        self.verbose = self.params.verbose
+        self.lambd = self.params.lambd
+        self.max_iter = self.params.max_iter
+        self.n_processors = self.params.n_processors
+        self.multi_processing = self.params.multi_processing
+        self.threshold = self.params.threshold
+        self.approx_rank = self.params.conv_approx_rank
+        self.vis_su_threshold = self.params.vis_su
+
+        # templates --> self.temps
+        assert templates.dtype == np.float32, "templates must have dtype np.float32"
+        self.temps = templates.transpose(1, 2, 0)
+        self.n_time, self.n_chan, self.n_unit = self.temps.shape
+
+        # handle grouped templates, as in the superresolution case
+        # TODO: refactor grouped_index mapping
+        self.grouped_temps = False
+        if self.params.template_index_to_unit_id is not None:
+            self.grouped_temps = True
+            assert self.params.template_index_to_unit_id.shape == (self.n_unit,)
+            group_index = [
+                np.flatnonzero(self.params.template_index_to_unit_id == u)
+                for u in self.params.template_index_to_unit_id
+            ]
+            self.max_group_size = max(map(len, group_index))
+
+            # like a channel index, sort of
+            # this is a n_templates x group_size array that maps each
+            # template index to the set of other template indices that
+            # are part of its group. so that the array is not ragged,
+            # we pad rows with -1s when their group is smaller than the
+            # largest group.
+            self.group_index = np.full((self.n_unit, self.max_group_size), -1)
+            for j, row in enumerate(group_index):
+                self.group_index[j, : len(row)] = row
+
+                # variance parameter for the amplitude scaling prior
+        assert self.lambd is None or self.lambd >= 0, "lambd must be a non-negative scalar"
+        self.no_amplitude_scaling = self.lambd is None or self.lambd == 0
+        self.scale_min = 1 / (1 + self.params.allowed_scale)
+        self.scale_max = 1 + self.params.allowed_scale
+
+        if self.verbose:
+            print("expected shape of templates loaded (n_times, n_chan, n_units):", self.temps.shape)
+            print(f"Instantiating MatchPursuitObjectiveUpsample on {self.params.t_end - self.params.t_start}",
+                  f"seconds long recording with threshold {self.threshold}")
+
+        self.start_sample = self.params.t_start * self.params.sampling_rate
+        self.end_sample = self.params.t_end * self.params.sampling_rate
+
+        # Upsample and downsample time shifted versions
+        # Dynamic Upsampling Setup; function for upsampling based on PTP
+        # Cat: TODO find better ptp-> upsample function
+        self.upsample_templates_mp(int(self.params.upsample))
+
+        self.visible_chans()
+        self.template_overlaps()
+        self.spatially_mask_templates()
+        # Upsample the templates
+        # Index of the original templates prior to
+        # upsampling them.
+        self.orig_n_unit = self.n_unit
+        self.n_unit = self.orig_n_unit * self.up_factor
+
+        # Computing SVD for each template.
+        self.compress_templates()
+
+        # Compute pairwise convolution of filters
+        self.pairwise_filter_conv()
+
+        # compute squared norm of templates
+        self.norm = np.zeros(self.orig_n_unit, dtype=np.float32)
+        for i in range(self.orig_n_unit):
+            self.norm[i] = np.sum(
+                np.square(self.temps[:, self.vis_chan[:, i], i])
+            )
+
+            # Initialize outputs
+        self.dec_spike_train = np.zeros((0, 2), dtype=np.int32)
+        self.dec_scalings = np.zeros((0,), dtype=np.float32)
+        self.dist_metric = np.array([])
+
+        # Single time preperation for high resolution matches
+        # matching indeces of peaks to indices of upsampled templates
+        radius = (self.up_factor // 2) + (self.up_factor % 2)
+        self.up_window = np.arange(-radius, radius + 1)[:, np.newaxis]
+        self.up_window_len = len(self.up_window)
+
+        # Indices of single time window the window around peak after upsampling
+        self.zoom_index = radius * self.up_factor + np.arange(-radius, radius + 1)
+        self.peak_to_template_idx = np.concatenate(
+            (np.arange(radius, -1, -1), (self.up_factor - 1) - np.arange(radius))
+        )
+        self.peak_time_jitter = np.concatenate(
+            ([0], np.array([0, 1]).repeat(radius))
+        )
+
+        # Refractory Period Setup.
+        # DO NOT MAKE IT SMALLER THAN self.n_time - 1 !!!
+        # (This is not actually the refractory condition we enforce.
+        #  It is a radius to make sure the algorithm does not subtract
+        #  two overlapping spikes at the same time, which would confuse it.)
+        self.refrac_radius = self.n_time - 1
+
+        # Account for upsampling window so that np.inf does not fall into the
+        # window around peak for valid spikes.
+        # (This is the actual refractory condition we enforce.)
+        self.adjusted_refrac_radius = self.params.refractory_period_frames
---
+++ spikeinterface/sortingcomponents/tests/test_matching_psvae.py
@@ -0,0 +1,166 @@
+# Standard Libraries
+from pathlib import Path
+import numpy as np
+
+# Spikeinterface
+import spikeinterface.core as sc
+import spikeinterface.extractors as se
+import spikeinterface.preprocessing as spre
+from spikeinterface.sortingcomponents.matching import find_spikes_from_templates
+
+
+def test_matching_psvae():
+    verbose = False
+    filepaths = generate_filepaths(project_name="test_matching_psvae", verbose=verbose)
+    job_kwargs = dict(n_jobs=4, chunk_duration='1s', progress_bar=True, verbose=verbose)
+    recording, gt_sorting = generate_gt_recording(job_kwargs, filepaths, verbose=verbose)
+    gt_templates, we = generate_templates(recording, gt_sorting, job_kwargs, filepaths, verbose=verbose)
+    duplicated_templates = np.concatenate((gt_templates, gt_templates[ [0] ]))
+    template_index_to_unit_id = list(range(7))
+    template_index_to_unit_id.append(0)
+    template_index_to_unit_id = np.array(template_index_to_unit_id)
+    param_sets = {
+        0 : dict(lambd=0, n_jobs=1, template_index_to_unit_id=None),
+        1 : dict(lambd=1, n_jobs=1, template_index_to_unit_id=None),
+        2 : dict(lambd=0, n_jobs=2, template_index_to_unit_id=None),
+        3 : dict(lambd=0, n_jobs=1, template_index_to_unit_id=template_index_to_unit_id)
+    }
+    for params in param_sets.values():
+        print(f"{params = }")
+        job_kwargs['n_jobs'] = params.pop('n_jobs')
+        if params['template_index_to_unit_id'] is None:
+            templates = gt_templates
+        else:
+            templates = duplicated_templates
+
+        method_kwargs = generate_method_kwargs(recording, templates, we, verbose=verbose)
+        method_kwargs['objective_kwargs'].update(params)
+        recording, gt_sorting = shorten_recording(recording, gt_sorting, method_kwargs, filepaths, verbose=verbose)
+        spikes = run_matching(recording, method_kwargs, job_kwargs, verbose=verbose)
+        hit_rate, misclass_rate, miss_rate = evaluate_performance(recording, gt_sorting, we, spikes, verbose=verbose)
+
+        print("Performance:")
+        print(f"{hit_rate = }")
+        print(f"{misclass_rate = }")
+        print(f"{miss_rate = }")
+        assert hit_rate == 1, "Missed Spikes"
+
+
+def generate_filepaths(project_name, verbose=False):
+    if verbose:
+        print("...Generating Filepaths...")
+    base_path = Path("/Volumes/T7/CatalystNeuro")
+    mearec_path = base_path / "MEArecDatasets/recordings/toy_recording_30min.h5"
+    project_path = base_path / project_name
+    rec_path = project_path / "recording"
+    rec_short_path = project_path / "rec_short"
+    we_path = project_path / "waveforms"
+    filepaths = dict(
+        base_path = base_path,
+        mearec_path = mearec_path,
+        project_path = project_path,
+        rec_path = rec_path,
+        rec_short_path = rec_short_path,
+        we_path = we_path,
+    )
+    if not filepaths['project_path'].exists():
+        filepaths['project_path'].mkdir()
+    return filepaths
+
+
+def generate_gt_recording(job_kwargs, filepaths, verbose=False):
+    if verbose:
+        print("...Generating Recording...")
+    mearec_path, rec_path = filepaths['mearec_path'], filepaths['rec_path']
+    if not rec_path.exists():
+        recording, _ = se.read_mearec(mearec_path)
+        recording = spre.bandpass_filter(recording, dtype='float32')
+        recording = spre.common_reference(recording)
+        #recording = spre.whiten(recording) # whitening induces an error for some reason
+        recording.save(folder=rec_path, **job_kwargs)
+    recording = sc.load_extractor(rec_path)
+    _, gt_sorting = se.read_mearec(mearec_path)
+    return recording, gt_sorting
+
+
+def generate_templates(recording, sorting, job_kwargs, filepaths, duplicate_templates=False, verbose=False):
+    if verbose:
+        print("...Generating Templates...")
+    we_kwargs = dict(ms_before=2.5, ms_after=3.5, max_spikes_per_unit=500, load_if_exists=True)
+    we = sc.extract_waveforms(recording, sorting, filepaths['we_path'], **we_kwargs, **job_kwargs)
+    templates = we.get_all_templates(we.unit_ids)
+    return templates, we
+
+
+def generate_method_kwargs(recording, templates, we, lambd=0, verbose=False):
+    if verbose:
+        print("...Generating Kwargs...")
+    refractory_period_s = 10 / 30_000
+    objective_kwargs = {
+        'sampling_rate': int(recording.sampling_frequency),  # sampling rate must be integer
+        't_start': 0,
+        't_end': int(recording.get_num_samples() / recording.sampling_frequency),  # t_end must be an integer
+        'n_sec_chunk': 1,
+        'refractory_period_frames': int(refractory_period_s * recording.sampling_frequency),
+        'upsample': 8,
+        'threshold': 50,
+        'lambd' : lambd,
+        'verbose': verbose,
+        'templates': templates,
+    }
+    method_kwargs = {
+        'objective_kwargs': objective_kwargs,
+        'nbefore': we.nbefore,
+        'nafter': we.nafter,
+    }
+    return method_kwargs
+
+
+def shorten_recording(recording, sorting, method_kwargs, filepaths, time_range_sec=(0, 10), verbose=False):
+    if verbose:
+        print("...Shortening Recording...")
+    rec_short_path = filepaths['rec_short_path']
+    time_range_sample = np.array(time_range_sec) * recording.sampling_frequency
+    rec_short = recording.frame_slice(start_frame=time_range_sample[0], end_frame=time_range_sample[1])
+    if not rec_short_path.exists():
+        rec_short.save(folder=rec_short_path)
+    method_kwargs['objective_kwargs']['t_end'] = time_range_sec[1]
+    sort_short = sorting.frame_slice(start_frame=time_range_sample[0], end_frame=time_range_sample[1])
+    return rec_short, sort_short
+
+
+def run_matching(recording, method_kwargs, job_kwargs, verbose=False):
+    if verbose:
+        print("...Running Matching...")
+    job_kwargs['chunk_duration'] = float(method_kwargs['objective_kwargs']['n_sec_chunk'])  # these must be the same
+    spikes = find_spikes_from_templates(recording, method='spike-psvae', method_kwargs=method_kwargs, **job_kwargs)
+    return spikes
+
+
+def evaluate_performance(recording, gt_sorting, we, spikes, hit_threshold=0.4e-3, verbose=False):
+    if verbose:
+        print("...Evaluating Performance...")
+    gt_spike_times, gt_spike_ids = gt_sorting.get_all_spike_trains()[0]
+    hits, misses, misclasses = 0, 0, 0
+    for gt_spike_idx, gt_unit_id in zip(gt_spike_times, gt_spike_ids):
+        idx = np.argmin(np.abs(spikes['sample_ind'] - gt_spike_idx))
+        spike_idx, unit_id = spikes['sample_ind'][idx], we.unit_ids[spikes['cluster_ind'][idx]]
+        time_diff = np.abs(spike_idx - gt_spike_idx) / recording.sampling_frequency
+        if time_diff < hit_threshold:
+            if gt_unit_id == unit_id:
+                hits += 1
+            else:
+                print(f"{time_diff = }")
+                print(f"{gt_unit_id = }")
+                print(f"{unit_id = }")
+                misclasses += 1
+        else:
+            misses += 1
+            print(f"gt_spiketime = {gt_spike_idx / recording.sampling_frequency}")
+    hit_rate = hits / len(gt_spike_times)
+    misclass_rate = misclasses / len(gt_spike_times)
+    miss_rate = misses / len(gt_spike_times)
+    return hit_rate, misclass_rate, miss_rate
+
+if __name__ == '__main__':
+    test_matching_psvae()
