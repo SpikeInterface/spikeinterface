c37b76472d6b5df61628b63d2e15df93623633dc
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-07 12:05:13 -0300

    added failing test

--- spikeinterface/core/tests/test_heavy_memory.py
+++ spikeinterface/core/tests/test_heavy_memory.py
@@ -9,6 +9,7 @@ from spikeinterface.core.generate import generate_lazy_random_recording
 def test_writing_overflow():
     
     # Convert the total memory to GB
+    total_memory = psutil.virtual_memory().total
     total_memory_GiB = total_memory / (1024 * 1024 * 1024)
 
     # Get the model name of the processor
@@ -21,7 +22,7 @@ def test_writing_overflow():
     print("Running tests")
     print("=================")
     
-    full_traces_size_GiB = 15.0  # 15 GiB
+    full_traces_size_GiB = 8.0
     job_kwargs = dict(n_jobs=-1, total_memory="1G", verbose=True, progress_bar=True)
     # job_kwargs = dict(n_jobs=-1, chunk_memory="1G", verbose=True, progress_bar=True)
     large_recording = generate_lazy_random_recording(full_traces_size_GiB=full_traces_size_GiB)
@@ -29,7 +30,6 @@ def test_writing_overflow():
     binary_recoder_cache = large_recording.save_to_folder(**job_kwargs)
     print(binary_recoder_cache)
 
-    total_memory = psutil.virtual_memory().total
 
     
     print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_SELF).ru_maxrss / (1024 * 1024))
@@ -37,6 +37,37 @@ def test_writing_overflow():
     print("children")
     print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
     print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
+
+def test_writing_overflow_failing_non_parallel():
+    # Convert the total memory to GB
+    total_memory = psutil.virtual_memory().total
+    total_memory_GiB = total_memory / (1024 * 1024 * 1024)
+
+    # Get the model name of the processor
+    processor_name = psutil.cpu_info().current_processor
+
+    # Print the RAM and processor information
+    print(f"{total_memory_GiB=}")
+    print(f"{processor_name=}")
+    print("=================")
+    print("Running tests")
+    print("=================")
+    
+    full_traces_size_GiB = 8.0
+    job_kwargs = dict(n_jobs=1, total_memory="8G", verbose=True, progress_bar=True)
+    # job_kwargs = dict(n_jobs=-1, chunk_memory="1G", verbose=True, progress_bar=True)
+    large_recording = generate_lazy_random_recording(full_traces_size_GiB=full_traces_size_GiB)
+    
+    binary_recoder_cache = large_recording.save_to_folder(**job_kwargs)
+    print(binary_recoder_cache)
+    
+    
+    print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_SELF).ru_maxrss / (1024 * 1024))
+    print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_SELF).ru_ixrss / (1024 * 1024))
+    print("children")
+    print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
+    print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
+
 
 if __name__ == '__main__':
     test_writing_overflow()
