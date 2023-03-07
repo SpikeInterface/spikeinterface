86b2616a36a6395b85dfa75d520ef5c607490d6e
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-07 11:57:12 -0300

    add verbose to test

--- spikeinterface/core/tests/test_heavy_memory.py
+++ spikeinterface/core/tests/test_heavy_memory.py
@@ -1,10 +1,25 @@
 import pytest 
 from resource import getrusage, RUSAGE_SELF, RUSAGE_CHILDREN
 
+import psutil
+
 from spikeinterface.core.generate import generate_lazy_random_recording
 
 
 def test_writing_overflow():
+    
+    # Convert the total memory to GB
+    total_memory_GiB = total_memory / (1024 * 1024 * 1024)
+
+    # Get the model name of the processor
+    processor_name = psutil.cpu_info().current_processor
+
+    # Print the RAM and processor information
+    print(f"{total_memory_GiB=}")
+    print(f"{processor_name=}")
+    print("=================")
+    print("Running tests")
+    print("=================")
     
     full_traces_size_GiB = 15.0  # 15 GiB
     job_kwargs = dict(n_jobs=-1, total_memory="1G", verbose=True, progress_bar=True)
@@ -14,6 +29,9 @@ def test_writing_overflow():
     binary_recoder_cache = large_recording.save_to_folder(**job_kwargs)
     print(binary_recoder_cache)
 
+    total_memory = psutil.virtual_memory().total
+
+    
     print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_SELF).ru_maxrss / (1024 * 1024))
     print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_SELF).ru_ixrss / (1024 * 1024))
     print("children")
