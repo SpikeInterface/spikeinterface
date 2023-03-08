13c96d79012e8dd46205521af0c994d9159ff39f
Pierre Yger <pierre.yger@gmail.com>
2023-03-08 14:45:52 +0100

    Update spikeinterface/widgets/matplotlib/spikes_on_traces.py
    Co-authored-by: Alessio Buccino <alejoe9187@gmail.com>

--- spikeinterface/widgets/matplotlib/spikes_on_traces.py
+++ spikeinterface/widgets/matplotlib/spikes_on_traces.py
@@ -82,7 +82,7 @@ class SpikesOnTracesPlotter(MplPlotter):
                     
                     for i, chan_id in enumerate(dp.timeseries["channel_ids"]):
                         offset = vspacing * (n - 1 - i)
-                        if chan_id in chan_inds:
+                        if chan_id in chan_ids:
                             l = ax.plot(times_r, offset + waveforms_r[:, i], color=dp.unit_colors[unit])
                             if not label_set:
                                 handles.append(l[0])
