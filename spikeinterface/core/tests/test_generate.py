c2fff814eff7c3faddcd41724e0931080f7da888
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-10 13:36:40 -0300

    fix imports and refactor tests

--- spikeinterface/core/tests/test_generate.py
+++ spikeinterface/core/tests/test_generate.py
@@ -1,11 +1,32 @@
 import pytest
 import psutil
 
-
-
 import numpy as np
-from spikeinterface.core.generate import LazyRandomRecording, generate_lazy_random_recording
 
+from spikeinterface.core.generate import GeneratorRecording, generate_lazy_recording
+
+def measure_memory_allocation(measure_in_process: bool=True) -> float:
+    """
+    A local utility to measure memory allocation at a specific point in time.
+    Can measure either the process resident memory or system wide memory available
+    
+    Uses psutil package.
+    
+    Parameters
+    ----------
+    measure_in_process : bool, True by default
+        Mesure memory allocation in the current process only, if false then measures at the system
+        level.
+    """
+    
+    if measure_in_process:
+        process = psutil.Process()
+        memory = process.memory_info().rss
+    else:
+        mem_info = psutil.virtual_memory()
+        memory = mem_info.total - mem_info.available
+        
+    return memory
 
 def test_lazy_random_recording():
     bytes_to_MiB_factor = 1024 ** 2
@@ -21,20 +42,14 @@ def test_lazy_random_recording():
     # Around 50 MiB  4 bytes per sample * 384 channels * 30000 samples
     expected_trace_size_MiB = dtype.itemsize * num_channels * num_samples / bytes_to_MiB_factor
     
-    process = psutil.Process()
-    initial_memory_MiB = process.memory_info().rss / bytes_to_MiB_factor
 
-    # mem_info = psutil.virtual_memory()
-    # initial_memory_MiB = (mem_info.total - mem_info.available) / bytes_to_MiB_factor
-
-    lazy_recording = LazyRandomRecording(
+    initial_memory_MiB = measure_memory_allocation() / bytes_to_MiB_factor
+    lazy_recording = GeneratorRecording(
         durations=durations, sampling_frequency=sampling_frequency, num_channels=num_channels, dtype=dtype, seed=seed
     )
     
-    process = psutil.Process()
-    memory_after_instanciation_MiB = process.memory_info().rss / bytes_to_MiB_factor
-    # mem_info = psutil.virtual_memory()
-    # memory_after_instanciation_MiB = (mem_info.total - mem_info.available) / bytes_to_MiB_factor
+    memory_after_instanciation_MiB = measure_memory_allocation() / bytes_to_MiB_factor
+
     excess_memory = memory_after_instanciation_MiB / initial_memory_MiB
     assert excess_memory == pytest.approx(1.0, rel=rel)  
 
@@ -45,50 +60,45 @@ def test_lazy_random_recording():
     assert traces_size_MiB == expected_trace_size_MiB
     assert traces.shape == expected_traces_shape
 
-    process = psutil.Process()
-    memory_after_traces_MiB = process.memory_info().rss / bytes_to_MiB_factor    
-    # mem_info = psutil.virtual_memory()
-    # memory_after_traces_MiB = (mem_info.total - mem_info.available) / bytes_to_MiB_factor
+    memory_after_traces_MiB = measure_memory_allocation() / bytes_to_MiB_factor    
     excess_memory = memory_after_traces_MiB / (memory_after_instanciation_MiB + traces_size_MiB)
 
-    print("Memory usage")
-    print(f"{initial_memory_MiB=} - {memory_after_instanciation_MiB=} - {traces_size_MiB=} - {memory_after_traces_MiB=}")
-
+    print("Memory footprint")
+    print(f"Initial memory = {initial_memory_MiB}")
+    print(f"Memory after instantiate class {memory_after_instanciation_MiB}")
+    print(f"Memory after traces {memory_after_traces_MiB}")
+    print(f"Traces size {traces_size_MiB}")
+    print(f"Difference between the last two", {(memory_after_traces_MiB - traces_size_MiB)})
+    
     assert excess_memory == pytest.approx(1.0, rel=rel)
     
 
-def test_generate_large_recording():
-    bytes_to_GiB_factor = 1024 ** 3
+def test_generate_lazy_recording():
+    
+    bytes_to_MiB_factor = 1024 ** 2
     full_traces_size_GiB = 1.0
     rel = 0.05  # relative tolerance
 
-    process = psutil.Process()
-    initial_memory_GiB = process.memory_info().rss / bytes_to_GiB_factor
+    initial_memory_MiB = measure_memory_allocation() / bytes_to_MiB_factor
 
-    # mem_info = psutil.virtual_memory()
-    # initial_memory_GiB = (mem_info.total - mem_info.available) / bytes_to_GiB_factor 
-    recording = generate_lazy_random_recording(full_traces_size_GiB = full_traces_size_GiB)
-
-    process = psutil.Process()
-    memory_after_instanciation_GiB = process.memory_info().rss / bytes_to_GiB_factor
-    
-    # mem_info = psutil.virtual_memory()
-    # memory_after_instanciation_GiB = (mem_info.total - mem_info.available) / bytes_to_GiB_factor
-    
-    excess_memory = memory_after_instanciation_GiB / initial_memory_GiB
+    lazy_recording = generate_lazy_recording(full_traces_size_GiB = full_traces_size_GiB)
+    memory_after_instanciation_MiB = measure_memory_allocation() / bytes_to_MiB_factor
+        
+    excess_memory = memory_after_instanciation_MiB / initial_memory_MiB
     assert excess_memory == pytest.approx(1.0, rel=rel) 
 
-    traces = recording.get_traces()
-    assert full_traces_size_GiB == round(traces.nbytes / bytes_to_GiB_factor) 
+    traces = lazy_recording.get_traces()
+    traces_size_MiB = traces.nbytes / bytes_to_MiB_factor
+    assert full_traces_size_GiB * 1024 == traces_size_MiB
 
-    process = psutil.Process()
-    memory_after_traces_GiB = process.memory_info().rss / bytes_to_GiB_factor
+    memory_after_traces_MiB = measure_memory_allocation() / bytes_to_MiB_factor
         
-    # mem_info = psutil.virtual_memory()
-    # memory_after_traces_GiB = (mem_info.total - mem_info.available) / bytes_to_GiB_factor
-    excess_memory = memory_after_traces_GiB  / (memory_after_instanciation_GiB + full_traces_size_GiB)
+    excess_memory = memory_after_traces_MiB  / (memory_after_instanciation_MiB + traces_size_MiB)
 
 
-    print("Memory usage")
-    print(f"{initial_memory_GiB=} - {memory_after_instanciation_GiB=} - {full_traces_size_GiB=} - {memory_after_traces_GiB=}")
-    assert excess_memory == pytest.approx(1.0, rel=rel)  
+    print("Memory footprint")
+    print(f"Initial memory = {initial_memory_MiB}")
+    print(f"Memory after instantiate class {memory_after_instanciation_MiB}")
+    print(f"Memory after traces {memory_after_traces_MiB}")
+    print(f"Traces size {traces_size_MiB}")
+    print(f"Difference between the last two", {(memory_after_traces_MiB - traces_size_MiB)})
