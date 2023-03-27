963e4b52166f3d2f4261b7d17bda025fa34c0c8f
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-03-27 20:20:50 +0200

    fire test

--- spikeinterface/core/tests/test_heavy_memory.py
+++ spikeinterface/core/tests/test_heavy_memory.py
@@ -64,42 +64,39 @@ def test_writing_overflow():
         print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
         print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
 
-def test_writing_overflow_failing_non_parallel():
+# def test_writing_overflow_failing_non_parallel():
 
     
-    # Convert the total memory to GB
-    total_memory = psutil.virtual_memory().total
-    total_memory_GiB_total = total_memory / (1024 * 1024 * 1024)
+#     # Convert the total memory to GB
+#     total_memory = psutil.virtual_memory().total
+#     total_memory_GiB_total = total_memory / (1024 * 1024 * 1024)
 
-    total_memory = psutil.virtual_memory().available
-    total_memory_GiB_availalble = total_memory / (1024 * 1024 * 1024)
-    # Get the model name of the processor
+#     total_memory = psutil.virtual_memory().available
+#     total_memory_GiB_availalble = total_memory / (1024 * 1024 * 1024)
+#     # Get the model name of the processor
 
-    # Print the RAM and processor information
-    print(f"{total_memory_GiB_total=}")
-    print(f"{total_memory_GiB_availalble=}")
+#     # Print the RAM and processor information
+#     print(f"{total_memory_GiB_total=}")
+#     print(f"{total_memory_GiB_availalble=}")
 
-    print("=================")
-    print("Running tests")
-    print("=================")
+#     print("=================")
+#     print("Running tests")
+#     print("=================")
 
-    full_traces_size_GiB = 8.0
-    job_kwargs = dict(n_jobs=1, total_memory="8G", verbose=True, progress_bar=True)
-    # job_kwargs = dict(n_jobs=-1, chunk_memory="1G", verbose=True, progress_bar=True)
-    large_recording = generate_lazy_recording(full_traces_size_GiB=full_traces_size_GiB)
+#     platform_system = platform.system()
+#     if platform_system == "Linux":
+#         from resource import getrusage, RUSAGE_SELF, RUSAGE_CHILDREN
+#         print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_SELF).ru_maxrss / (1024 * 1024))
+#         print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_SELF).ru_ixrss / (1024 * 1024))
+#         print("children")
+#         print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
+#         print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
 
-    binary_recoder_cache = large_recording.save_to_folder(**job_kwargs)
-    print(binary_recoder_cache)
-
-
-    platform_system = platform.system()
-    if platform_system == "Linux":
-        from resource import getrusage, RUSAGE_SELF, RUSAGE_CHILDREN
-        print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_SELF).ru_maxrss / (1024 * 1024))
-        print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_SELF).ru_ixrss / (1024 * 1024))
-        print("children")
-        print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
-        print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
+#     full_traces_size_GiB = 8.0
+#     job_kwargs = dict(n_jobs=1, total_memory="8G", verbose=True, progress_bar=True)
+#     # job_kwargs = dict(n_jobs=-1, chunk_memory="1G", verbose=True, progress_bar=True)
+#     large_recording = generate_lazy_recording(full_traces_size_GiB=full_traces_size_GiB)
+#     pytest.raises(MemoryError, large_recording.save_to_folder, **job_kwargs)
 
 
 if __name__ == '__main__':
