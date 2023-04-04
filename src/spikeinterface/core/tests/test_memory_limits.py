1370db3781ebee18fad93d62b434487d32058927
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-04-04 18:23:48 +0200

    tests out

--- src/spikeinterface/core/tests/test_memory_limits.py
+++ src/spikeinterface/core/tests/test_memory_limits.py
@@ -64,16 +64,26 @@ from spikeinterface.core.generate import generate_lazy_recording
 #         print("Peak resident memory ru_maxrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_maxrss / (1024 * 1024))
 #         print("Peak virtual memory ru_ru_ixrss (GiB):", getrusage(RUSAGE_CHILDREN).ru_ixrss / (1024 * 1024))
 
+def  _dummy_chunk_operation(segment_index, start_frame, end_frame, worker_ctx):
+    # recover variables of the worker
+    recording = worker_ctx["recording"]
+
+    # apply function
+    traces = recording.get_traces(
+        start_frame=start_frame,
+        end_frame=end_frame,
+        segment_index=segment_index,
+    )
+    traces[:]
+    
+def _init_dummy_worker(recording):
+    # create a local dict per worker
+    worker_ctx = {}
+    worker_ctx["recording"] = recording
+
+    return worker_ctx
+
 def test_reading_from_memamp_overflow(tmp_path):
-    platform_system = platform.system()
-    partition_path = "C:" if platform_system == "Windows" else "/" 
-    hdd = psutil.disk_usage(partition_path)
-
-
-    available_hard_drive_GiB = hdd.free / (2**30)
-    total_memory = psutil.virtual_memory().total
-    total_memory_available_GiB = total_memory / (1024 * 1024 * 1024)
-
     # Convert the total memory to GB
     full_traces_size_GiB = 10.0  #  Both linux and windows have 7 GiB of ram available
 
@@ -86,25 +96,6 @@ def test_reading_from_memamp_overflow(tmp_path):
 
     from spikeinterface.core.base import load_extractor
     recording = load_extractor(folder)
-
-    def  _dummy_chunk_operation(segment_index, start_frame, end_frame, worker_ctx):
-        # recover variables of the worker
-        recording = worker_ctx["recording"]
-
-        # apply function
-        traces = recording.get_traces(
-            start_frame=start_frame,
-            end_frame=end_frame,
-            segment_index=segment_index,
-        )
-        traces[:]
-        
-    def _init_dummy_worker(recording):
-        # create a local dict per worker
-        worker_ctx = {}
-        worker_ctx["recording"] = recording
-
-        return worker_ctx
 
     func = _dummy_chunk_operation
     init_func = _init_dummy_worker
@@ -119,4 +110,5 @@ def test_reading_from_memamp_overflow(tmp_path):
     )
     executor.run()
 if __name__ == '__main__':
-    test_writing_overflow()
+    pass 
+    # test_writing_overflow()
