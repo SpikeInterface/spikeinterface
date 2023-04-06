5060a8099dc951e6d7a0e32cc19bcc38652e6db2
yger <pierre.yger@gmail.com>
2023-04-06 13:54:49 +0200

    More corrections

--- src/spikeinterface/sortingcomponents/tests/test_waveforms/test_waveform_thresholder.py
+++ src/spikeinterface/sortingcomponents/tests/test_waveforms/test_waveform_thresholder.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import operator
 
 
 from spikeinterface.sortingcomponents.waveforms.waveform_thresholder import WaveformThresholder
@@ -12,44 +13,64 @@ from spikeinterface.sortingcomponents.peak_pipeline import (
 )
 
 
-def test_waveform_thresholder(mearec_recording, detected_peaks, chunk_executor_kwargs):
-    recording = mearec_recording
-    peaks = detected_peaks
-
+@pytest.fixture(scope="module")
+def extract_waveforms(mearec_recording):
     # Parameters
     ms_before = 1.0
     ms_after = 1.0
 
     # Node initialization
-    extract_waveforms = ExtractDenseWaveforms(
-        recording=recording, ms_before=ms_before, ms_after=ms_after, return_output=True
+    return ExtractDenseWaveforms(
+        recording=mearec_recording, ms_before=ms_before, ms_after=ms_after, return_output=True
     )
 
+def test_waveform_thresholder_ptp(extract_waveforms, mearec_recording, detected_peaks, chunk_executor_kwargs):
+    recording = mearec_recording
+    peaks = detected_peaks
+
     tresholded_waveforms_ptp = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='ptp', threshold=3, return_output=True)
-    tresholded_waveforms_mean = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='mean', threshold=3, return_output=True)
-    tresholded_waveforms_energy = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='energy', threshold=3, return_output=True)
-    tresholded_waveforms_peak  = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='v_peak', threshold=-5, return_output=True)
-    
+
     pipeline_nodes = [extract_waveforms, tresholded_waveforms_ptp]
     # Extract projected waveforms and compare
     waveforms, tresholded_waveforms = run_peak_pipeline(
         recording, peaks, nodes=pipeline_nodes, job_kwargs=chunk_executor_kwargs
     )
+
     assert np.all(tresholded_waveforms.ptp(axis=1) >= 3)
+
+def test_waveform_thresholder_mean(extract_waveforms, mearec_recording, detected_peaks, chunk_executor_kwargs):
+    recording = mearec_recording
+    peaks = detected_peaks
+
+    tresholded_waveforms_mean = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='mean', threshold=3, return_output=True)
 
     pipeline_nodes = [extract_waveforms, tresholded_waveforms_mean]
     # Extract projected waveforms and compare
     waveforms, tresholded_waveforms = run_peak_pipeline(
         recording, peaks, nodes=pipeline_nodes, job_kwargs=chunk_executor_kwargs
     )
+
     assert np.all(tresholded_waveforms.mean(axis=1) >= 0)
+
+def test_waveform_thresholder_energy(extract_waveforms, mearec_recording, detected_peaks, chunk_executor_kwargs):
+    recording = mearec_recording
+    peaks = detected_peaks
+
+    tresholded_waveforms_energy = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='energy', threshold=3, return_output=True)
 
     pipeline_nodes = [extract_waveforms, tresholded_waveforms_energy]
     # Extract projected waveforms and compare
     waveforms, tresholded_waveforms = run_peak_pipeline(
         recording, peaks, nodes=pipeline_nodes, job_kwargs=chunk_executor_kwargs
     )
+
     assert np.all(np.linalg.norm(tresholded_waveforms, axis=1) >= 3)
+
+def test_waveform_thresholder_peak(extract_waveforms, mearec_recording, detected_peaks, chunk_executor_kwargs):
+    recording = mearec_recording
+    peaks = detected_peaks
+
+    tresholded_waveforms_peak = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='v_peak', threshold=-5, return_output=True)
 
     pipeline_nodes = [extract_waveforms, tresholded_waveforms_peak]
     # Extract projected waveforms and compare
@@ -58,3 +79,18 @@ def test_waveform_thresholder(mearec_recording, detected_peaks, chunk_executor_k
     )
 
     assert np.all(tresholded_waveforms[:, extract_waveforms.nbefore, :] >= -5)
+
+def test_waveform_thresholder_operator(extract_waveforms, mearec_recording, detected_peaks, chunk_executor_kwargs):
+    recording = mearec_recording
+    peaks = detected_peaks
+
+    import operator
+    tresholded_waveforms_peak = WaveformThresholder(recording=recording, parents=[extract_waveforms], feature='ptp', threshold=3, operator = operator.ge, return_output=True)
+
+    pipeline_nodes = [extract_waveforms, tresholded_waveforms_peak]
+    # Extract projected waveforms and compare
+    waveforms, tresholded_waveforms = run_peak_pipeline(
+        recording, peaks, nodes=pipeline_nodes, job_kwargs=chunk_executor_kwargs
+    )
+
+    assert np.all(tresholded_waveforms[:, extract_waveforms.nbefore, :] <= 3)
--- src/spikeinterface/sortingcomponents/waveforms/waveform_thresholder.py
+++ src/spikeinterface/sortingcomponents/waveforms/waveform_thresholder.py
@@ -3,6 +3,8 @@ import json
 from typing import List, Optional
 import scipy.signal
 import numpy as np
+import operator
+from typing import Literal
 
 from spikeinterface.core import BaseRecording
 from spikeinterface.sortingcomponents.peak_pipeline import (
@@ -13,23 +15,35 @@ from spikeinterface.sortingcomponents.peak_pipeline import (
 
 class WaveformThresholder(WaveformExtractorNode):
     """
-    Waveform Thresholder based on selected feature
-    This allow to perform an adaptive masking, by setting to 0 channels
-    that have a given feature below a given threshold
+    A node that performs waveform thresholding based on a selected feature.
+
+    This node allows you to perform adaptive masking by setting channels to 0
+    that have a given feature below a certain threshold. The available features
+    to consider are peak-to-peak amplitude ('ptp'), mean amplitude ('mean'),
+    energy ('energy'), and voltage peak ('v_peak').
 
     Parameters
     ----------
-    feature: string in ['ptp', 'mean', 'energy', 'v_peak']
-        the feature that should be considered
-    threshold: float
-        the threshold below which channels are set to 0
+    recording: BaseRecording
+        The recording extractor object.
+    return_output: bool, optional
+        Whether to return output from this node (default True).
+    parents: list of PipelineNodes, optional
+        The parent nodes of this node (default None).
+    feature: {'ptp', 'mean', 'energy', 'v_peak'}, optional
+        The feature to be considered for thresholding (default 'ptp').
+    threshold: float, optional
+        The threshold value for the selected feature (default 2).
+    operator: function, callable (default is operator.le)
+        Comparison to flag values that should be set to 0 (default less or equal)
     """
 
     def __init__(
         self, recording: BaseRecording, return_output: bool = True, 
         parents: Optional[List[PipelineNode]] = None,
-        feature = 'ptp',
-        threshold = 2
+        feature: Literal['ptp', 'mean', 'energy', 'v_peak'] = 'ptp',
+        threshold: float = 2,
+        operator: callable = operator.le
     ):
         try:
             waveform_extractor = next(parent for parent in parents if isinstance(parent, WaveformExtractorNode))
@@ -39,9 +53,11 @@ class WaveformThresholder(WaveformExtractorNode):
 
         super().__init__(recording, waveform_extractor.ms_before, waveform_extractor.ms_after,
             return_output=return_output, parents=parents)
+        assert feature in ['ptp', 'mean', 'energy', 'v_peak'], f'{feature} is not a valid feature'
 
         self.threshold = threshold
         self.feature = feature
+        self.operator = operator
         
     def compute(self, traces, peaks, waveforms):
         
@@ -54,8 +70,8 @@ class WaveformThresholder(WaveformExtractorNode):
         elif self.feature == 'v_peak':
             wf_data = waveforms[:, self.nbefore, :]
 
-        mask = wf_data < self.threshold
-        mask = np.repeat(mask[:, np.newaxis, :], waveforms.shape[1], axis=1)
+        mask = self.operator(wf_data, self.threshold)
+        mask = np.broadcast_to(mask[:, np.newaxis, :], waveforms.shape)
         waveforms[mask] = 0
 
         return waveforms
