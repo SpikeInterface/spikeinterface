91a6fe9ec5c1d924e663686b3dcefe69b75a4852
pauladkisson <paul.wesley.adkisson@gmail.com>
2023-04-11 15:16:18 -0700

    Added test for compress_templates

--- src/spikeinterface/sortingcomponents/tests/test_wobble.py
+++ src/spikeinterface/sortingcomponents/tests/test_wobble.py
@@ -3,6 +3,80 @@ import numpy as np
 
 from spikeinterface.sortingcomponents.matching import wobble
 
+
+def test_compute_template_norm():
+    # Arrange: generate random 'data' and edge cases of visible_channels (all True and all False)
+    seed = 0
+    rng = np.random.default_rng(seed)
+    num_templates = rng.integers(1, 100)
+    num_channels = rng.integers(1, 100)
+    num_samples = rng.integers(1, 100)
+    rand_visible_channels = rng.choice(a=[True, False], size=(num_templates, num_channels), p=[0.5, 0.5])
+    true_visible_channels = np.ones((num_templates, num_channels), dtype=bool)
+    false_visible_channels = np.zeros((num_templates, num_channels), dtype=bool)
+    templates = rng.random((num_templates, num_samples, num_channels))
+
+    # Act: run compute_template_norm
+    rand_norm = wobble.compute_template_norm(rand_visible_channels, templates)
+    true_norm = wobble.compute_template_norm(true_visible_channels, templates)
+    false_norm = wobble.compute_template_norm(false_visible_channels, templates)
+
+    # Assert: check shape and sign
+    assert rand_norm.shape == true_norm.shape == false_norm.shape == (num_templates,)
+    assert np.all(rand_norm >= 0)
+    assert np.all(true_norm >= 0)
+    assert np.all(false_norm >= 0)
+
+
+def test_compress_templates():
+    seed = 0
+    rng = np.random.default_rng(seed)
+    num_templates = rng.integers(1, 100)
+    test_cases = ["num_channels > num_samples", "num_samples > num_channels", "num_channels == num_samples"]
+    for test_case in test_cases:
+        # Arrange: generate random 'data'
+        if test_case == "num_channels > num_samples":
+            num_channels = rng.integers(1, 100)
+            num_samples = rng.integers(1, num_channels - 1)
+        elif test_case == "num_samples > num_channels":
+            num_samples = rng.integers(1, 100)
+            num_channels = rng.integers(1, num_samples - 1)
+        elif test_case == "num_channels == num_samples":
+            num_channels = rng.integers(1, 100)
+            num_samples = num_channels
+        templates = rng.random((num_templates, num_samples, num_channels))
+        full_rank = np.minimum(num_samples, num_channels)
+        approx_rank = rng.integers(1, full_rank)
+
+        # Act: run compress_templates
+        temporal, singular, spatial = wobble.compress_templates(templates, approx_rank)
+        temporal_full, singular_full, spatial_full = wobble.compress_templates(templates, full_rank)
+        # temporal_full = np.flip(temporal_full, axis=1)
+
+        # Assert: check shapes, SVD properties, and equivalence(s) for full rank
+        # check that the shapes are correct
+        assert temporal.shape == (num_templates, num_samples, approx_rank)
+        assert singular.shape == (num_templates, approx_rank)
+        assert spatial.shape == (num_templates, approx_rank, num_channels)
+        assert temporal_full.shape == (num_templates, num_samples, full_rank)
+        assert singular_full.shape == (num_templates, full_rank)
+        assert spatial_full.shape == (num_templates, full_rank, num_channels)
+        # check that singular values are non-negative
+        assert np.all(singular >= 0)
+        assert np.all(singular_full >= 0)
+        # check that svd matrices are orthonormal if applicable
+        if num_channels > num_samples:
+            assert np.allclose(np.matmul(temporal_full, temporal_full.transpose(0, 2, 1)), np.eye(num_samples))
+        elif num_samples > num_channels:
+            assert np.allclose(np.matmul(spatial_full, spatial_full.transpose(0, 2, 1)), np.eye(num_channels))
+        elif num_channels == num_samples:
+            assert np.allclose(np.matmul(temporal_full, temporal_full.transpose(0, 2, 1)), np.eye(num_samples))
+            assert np.allclose(np.matmul(spatial_full, spatial_full.transpose(0, 2, 1)), np.eye(num_channels))
+            # check that the full rank svd matrices reconstruct the original templates
+            reconstructed_templates = np.matmul(temporal_full * singular_full[:, np.newaxis, :], spatial_full)
+            assert np.allclose(reconstructed_templates, templates)
+
+
 def compute_objective_loopy(traces, template_data, approx_rank):
     """Compute objective by convolving templates with voltage traces.
 
@@ -70,30 +144,8 @@ def test_compute_objective():
     assert objective.shape == (num_templates, chunk_len+num_samples-1)
     assert np.allclose(objective, expected_objective)
 
-def test_compute_template_norm():
-    # Arrange: generate random 'data' and edge cases of visible_channels (all True and all False)
-    seed = 0
-    rng = np.random.default_rng(seed)
-    num_templates = rng.integers(1, 100)
-    num_channels = rng.integers(1, 100)
-    num_samples = rng.integers(1, 100)
-    rand_visible_channels = rng.choice(a=[True, False], size=(num_templates, num_channels), p=[0.5, 0.5])
-    true_visible_channels = np.ones((num_templates, num_channels), dtype=bool)
-    false_visible_channels = np.zeros((num_templates, num_channels), dtype=bool)
-    templates = rng.random((num_templates, num_samples, num_channels))
-
-    # Act: run compute_template_norm
-    rand_norm = wobble.compute_template_norm(rand_visible_channels, templates)
-    true_norm = wobble.compute_template_norm(true_visible_channels, templates)
-    false_norm = wobble.compute_template_norm(false_visible_channels, templates)
-
-    # Assert: check shape and sign
-    assert rand_norm.shape == true_norm.shape == false_norm.shape == (num_templates,)
-    assert np.all(rand_norm >= 0)
-    assert np.all(true_norm >= 0)
-    assert np.all(false_norm >= 0)
-
 
 if __name__ == '__main__':
-    test_compute_objective()
     test_compute_template_norm()
+    test_compute_objective()
+    test_compress_templates()
