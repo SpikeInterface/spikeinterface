6f1002eeba2af09185ad3ec50646dfe2ee450a37
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-04-18 15:43:18 +0200

    update mereac recording and sorters to ommit unused information

--- src/spikeinterface/extractors/neoextractors/mearec.py
+++ src/spikeinterface/extractors/neoextractors/mearec.py
@@ -39,8 +39,8 @@ class MEArecRecordingExtractor(NeoBaseRecordingExtractor):
         self._kwargs.update({'file_path': str(file_path)})
 
     @classmethod
-    def map_to_neo_kwargs(cls, file_path):
-        neo_kwargs = {'filename': str(file_path)}
+    def map_to_neo_kwargs(cls, file_path, ):
+        neo_kwargs = {'filename': str(file_path), "load_spiketrains": False, "load_recordings": True}
         return neo_kwargs
 
 
@@ -61,7 +61,7 @@ class MEArecSortingExtractor(NeoBaseSortingExtractor):
 
     @classmethod
     def map_to_neo_kwargs(cls, file_path):
-        neo_kwargs = {'filename': str(file_path)}
+        neo_kwargs = {'filename': str(file_path), "load_spiketrains": True, "load_recordings": False}
         return neo_kwargs
 
 
