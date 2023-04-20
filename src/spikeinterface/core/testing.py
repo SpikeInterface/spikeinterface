2df0e4c03405fd521c067e4be199cc07df03d6f0
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2023-04-20 11:10:36 +0200

    Alessio's suggestions

--- src/spikeinterface/core/testing.py
+++ src/spikeinterface/core/testing.py
@@ -11,7 +11,8 @@ def check_sorted_arrays_equal(a1, a2):
     assert_array_equal(a1, a2)
 
 
-def check_recordings_equal(RX1: BaseRecording, RX2: BaseRecording, return_scaled=True, force_dtype=None, check_properties: bool = False) -> None:
+def check_recordings_equal(RX1: BaseRecording, RX2: BaseRecording, return_scaled=True, force_dtype=None,
+                           check_annotations: bool = False, check_properties: bool = False) -> None:
     assert RX1.get_num_segments() == RX2.get_num_segments()
 
     for segment_idx in range(RX1.get_num_segments()):
@@ -51,11 +52,13 @@ def check_recordings_equal(RX1: BaseRecording, RX2: BaseRecording, return_scaled
                                RX2.get_traces(segment_index=segment_idx, channel_ids=ch, start_frame=sf, end_frame=ef,
                                               return_scaled=return_scaled).astype(force_dtype))
 
+    if check_annotations:
+        check_extractor_annotations_equal(RX1, RX2)
     if check_properties:
         check_extractor_properties_equal(RX1, RX2)
 
 
-def check_sortings_equal(SX1: BaseSorting, SX2: BaseSorting, check_properties: bool = False) -> None:
+def check_sortings_equal(SX1: BaseSorting, SX2: BaseSorting, check_annotations: bool = False, check_properties: bool = False) -> None:
     assert SX1.get_num_segments() == SX2.get_num_segments()
 
     for segment_idx in range(SX1.get_num_segments()):
@@ -68,15 +71,21 @@ def check_sortings_equal(SX1: BaseSorting, SX2: BaseSorting, check_properties: b
             train2 = np.sort(SX2.get_unit_spike_train(id, segment_index=segment_idx))
             assert np.array_equal(train1, train2)
 
+    if check_annotations:
+        check_extractor_annotations_equal(SX1, SX2)
     if check_properties:
-        check_extractor_properties_equal(RX1, RX2)
+        check_extractor_properties_equal(SX1, SX2)
 
 
-def check_extractor_properties_equal(EX1, EX2) -> None:
+def check_extractor_annotations_equal(EX1, EX2) -> None:
     assert np.array_equal(sorted(EX1.get_annotation_keys()), sorted(EX2.get_annotation_keys()))
-    assert np.array_equal(sorted(EX1.get_property_keys()), sorted(EX2.get_property_keys()))
 
     for annotation_name in EX1.get_annotation_keys():
         assert EX1.get_annotation(annotation_name) == EX2.get_annotation(annotation_name)
+
+
+def check_extractor_properties_equal(EX1, EX2) -> None:
+    assert np.array_equal(sorted(EX1.get_property_keys()), sorted(EX2.get_property_keys()))
+
     for property_name in EX1.get_property_keys():
         np.assert_array_equal(EX1.get_property(property_name), EX2.get_property(property_name))
