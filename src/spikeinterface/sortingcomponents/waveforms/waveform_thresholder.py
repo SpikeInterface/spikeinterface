79924db456f406ea2c55b804bb4d41c758d17b7c
yger <pierre.yger@gmail.com>
2023-04-26 11:57:37 +0200

    Change v_peak name and thresholder behavior

--- src/spikeinterface/sortingcomponents/waveforms/waveform_thresholder.py
+++ src/spikeinterface/sortingcomponents/waveforms/waveform_thresholder.py
@@ -6,7 +6,7 @@ import numpy as np
 import operator
 from typing import Literal
 
-from spikeinterface.core import BaseRecording
+from spikeinterface.core import BaseRecording, get_noise_levels
 from spikeinterface.sortingcomponents.peak_pipeline import (
     PipelineNode,
     WaveformsNode,
@@ -21,7 +21,7 @@ class WaveformThresholder(WaveformsNode):
     This node allows you to perform adaptive masking by setting channels to 0
     that have a given feature below a certain threshold. The available features
     to consider are peak-to-peak amplitude ('ptp'), mean amplitude ('mean'),
-    energy ('energy'), and voltage peak ('v_peak').
+    energy ('energy'), and peak voltage ('peak_voltage').
 
     Parameters
     ----------
@@ -31,10 +31,14 @@ class WaveformThresholder(WaveformsNode):
         Whether to return output from this node (default True).
     parents: list of PipelineNodes, optional
         The parent nodes of this node (default None).
-    feature: {'ptp', 'mean', 'energy', 'v_peak'}, optional
+    feature: {'ptp', 'mean', 'energy', 'peak_voltage'}, optional
         The feature to be considered for thresholding (default 'ptp').
     threshold: float, optional
         The threshold value for the selected feature (default 2).
+    noise_levels: array, optional
+        The noise levels to determine the thresholds
+    random_chunk_kwargs: dict
+        Parameters for computing noise levels, if not provided (sub optimal)
     operator: callable, optional
         Comparator to flag values that should be set to 0 (default less or equal)
     """
@@ -42,8 +46,10 @@ class WaveformThresholder(WaveformsNode):
     def __init__(
         self, recording: BaseRecording, return_output: bool = True, 
         parents: Optional[List[PipelineNode]] = None,
-        feature: Literal['ptp', 'mean', 'energy', 'v_peak'] = 'ptp',
+        feature: Literal['ptp', 'mean', 'energy', 'peak_voltage'] = 'ptp',
         threshold: float = 2,
+        noise_levels: Optional[np.array] = None,
+        random_chunk_kwargs: dict = {},
         operator: callable = operator.le
     ):
         waveform_extractor = find_parent_of_type(parents, WaveformsNode)
@@ -52,26 +58,31 @@ class WaveformThresholder(WaveformsNode):
 
         super().__init__(recording, waveform_extractor.ms_before, waveform_extractor.ms_after,
             return_output=return_output, parents=parents)
-        assert feature in ['ptp', 'mean', 'energy', 'v_peak'], f'{feature} is not a valid feature'
+        assert feature in ['ptp', 'mean', 'energy', 'peak_voltage'], f'{feature} is not a valid feature'
 
         self.threshold = threshold
         self.feature = feature
         self.operator = operator
 
+        self.noise_levels = noise_levels
+        if self.noise_levels is None:
+            self.noise_levels = get_noise_levels(self.recording, **random_chunk_kwargs, return_scaled=False)
+
         self._kwargs.update(dict(feature=feature,
                                  threshold=threshold,
-                                 operator=operator))
+                                 operator=operator, 
+                                 noise_levels=self.noise_levels))
         
     def compute(self, traces, peaks, waveforms):
         
         if self.feature == 'ptp':
-            wf_data = waveforms.ptp(axis=1)
+            wf_data = waveforms.ptp(axis=1) / self.noise_levels
         elif self.feature == 'mean':
-            wf_data = waveforms.mean(axis=1)
+            wf_data = waveforms.mean(axis=1) / self.noise_levels
         elif self.feature == 'energy':
-            wf_data = np.linalg.norm(waveforms, axis=1)
-        elif self.feature == 'v_peak':
-            wf_data = waveforms[:, self.nbefore, :]
+            wf_data = np.linalg.norm(waveforms, axis=1) / self.noise_levels
+        elif self.feature == 'peak_voltage':
+            wf_data = waveforms[:, self.nbefore, :] / self.noise_levels
 
         mask = self.operator(wf_data, self.threshold)
         mask = np.broadcast_to(mask[:, np.newaxis, :], waveforms.shape)
