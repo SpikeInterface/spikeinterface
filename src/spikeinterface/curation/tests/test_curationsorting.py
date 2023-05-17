5add2b843a425c18171a9679ab8224b316c635a3
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2023-05-17 12:53:53 +0200

    More testing for `CurationSorting.merge`

--- src/spikeinterface/curation/tests/test_curationsorting.py
+++ src/spikeinterface/curation/tests/test_curationsorting.py
@@ -59,12 +59,14 @@ def test_curation():
     cs = CurationSorting(parent_sort, properties_policy="remove")
     # %%
     cs.merge(["a", "c"])
+    assert cs.sorting.get_num_units() == len(spikestimes[0]) - 1
     split_index = [v["b"] < 6 for v in spikestimes]  # spit class 4 in even and odds
     cs.split("b", split_index)
     after_split = cs.sorting
+    assert cs.sorting.get_num_units() == len(spikestimes[0])
 
     all_units = cs.sorting.get_unit_ids()
-    cs.merge(all_units)
+    cs.merge(all_units, new_unit_id=all_units[0])
     assert len(cs.sorting.get_unit_ids()) == 1, "error merging units"
     cs.undo()
 
