41f04e38b57897a6cbb95f3468fe4d3c9b611bf8
Alessio Buccino <alejoe9187@gmail.com>
2023-05-17 10:38:45 -0700

    Leave collision handling for follow-up

--- src/spikeinterface/postprocessing/tests/test_amplitude_scalings.py
+++ src/spikeinterface/postprocessing/tests/test_amplitude_scalings.py
@@ -18,7 +18,6 @@ class AmplitudeScalingsExtensionTest(WaveformExtensionCommonTestSuite, unittest.
         dict(outputs="by_unit", chunk_size=10000, n_jobs=1),
         dict(outputs="concatenated", chunk_size=10000, n_jobs=-1),
         dict(outputs="concatenated", chunk_size=10000, n_jobs=2, ms_before=0.5, ms_after=0.5),
-        dict(outputs="by_unit", handle_collisions=True, n_jobs=1, ms_before=0.5, ms_after=0.5)
     ]
 
     def test_scaling_parallel(self):
@@ -49,41 +48,6 @@ class AmplitudeScalingsExtensionTest(WaveformExtensionCommonTestSuite, unittest.
             print(u, median_scaling)
             np.testing.assert_array_equal(np.round(median_scaling), 1)
 
-    def test_collisions(self):
-        scalings1 = self.extension_class.get_extension_function()(
-            self.we1,
-            outputs="concatenated",
-            chunk_size=10000,
-            handle_collisions=True,
-            ms_before=0.5,
-            ms_after=0.5,
-            n_jobs=1
-        )
-        scalings2 = self.extension_class.get_extension_function()(
-            self.we1,
-            outputs="concatenated",
-            chunk_size=10000,
-            handle_collisions=True,
-            ms_before=0.5,
-            ms_after=0.5,
-            n_jobs=2
-        )
-        np.testing.assert_array_equal(scalings1, scalings2)
-
-        # test higher collision_ms
-        sparsity = compute_sparsity(self.we1, method="radius", radius_um=30)
-        scalings2 = self.extension_class.get_extension_function()(
-            self.we1,
-            outputs="concatenated",
-            chunk_size=10000,
-            sparsity=sparsity,
-            handle_collisions=True,
-            collision_ms=2,
-            ms_before=0.5,
-            ms_after=0.5,
-            n_jobs=2
-        )
-
 
 if __name__ == "__main__":
     test = AmplitudeScalingsExtensionTest()
@@ -91,4 +55,3 @@ if __name__ == "__main__":
     test.test_extension()
     test.test_scaling_values()
     test.test_scaling_parallel()
-    test.test_collisions()
