bfee5a1786dc62ac3aec7447e75921524faae542
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-06-07 09:57:07 +0200

    added first failing test

--- src/spikeinterface/preprocessing/tests/test_zero_padding.py
+++ src/spikeinterface/preprocessing/tests/test_zero_padding.py
@@ -4,8 +4,10 @@ import numpy as np
 
 from spikeinterface import set_global_tmp_folder
 from spikeinterface.core import generate_recording
+from spikeinterface.core.numpyextractors import NumpyRecording
 
 from spikeinterface.preprocessing import zero_channel_pad
+from spikeinterface.preprocessing.zero_channel_pad import ZeroTracePaddedRecording
 
 if hasattr(pytest, "global_test_folder"):
     cache_folder = pytest.global_test_folder / "preprocessing"
@@ -15,7 +17,7 @@ else:
 set_global_tmp_folder(cache_folder)
 
 
-def test_zero_padding():
+def test_zero_paddin_channel():
     num_original_channels = 4
     num_padded_channels = num_original_channels + 8
     rec = generate_recording(num_channels=num_original_channels, durations=[10])
@@ -34,5 +36,25 @@ def test_zero_padding():
     assert np.allclose(tr[:, :num_original_channels], rec.get_traces())
 
 
+def test_zero_padding_trace():
+    num_channels = 4
+    num_samples = 10
+    traces = np.ones((num_samples, num_channels))
+    traces_list = [traces]
+    recording = NumpyRecording(traces_list=traces_list, sampling_frequency=30_000)
+
+    padding_left = 5
+    padding_right = 5
+    padded_recording = ZeroTracePaddedRecording(
+        parent_recording=recording, padding_left=padding_left, padding_right=padding_right
+    )
+    padded_traces = padded_recording.get_traces()
+
+    # Padd traces with zeros on the left and right
+    assert np.allclose(padded_traces[:padding_left, :], np.zeros((padding_left, num_channels)))
+    assert np.allclose(padded_traces[padding_left:-padding_right, :], traces)
+    assert np.allclose(padded_traces[-padding_right:, :], np.zeros((padding_right, num_channels)))
+
+
 if __name__ == "__main__":
-    test_zero_padding()
+    test_zero_paddin_channel()
