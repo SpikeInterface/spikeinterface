b65667b9fa3dad9c34a6781d37806852e2d67f92
Samuel Garcia <sam.garcia.die@gmail.com>
2023-06-07 18:01:18 +0200

    Good trick suggested by Alessio

--- src/spikeinterface/core/numpyextractors.py
+++ src/spikeinterface/core/numpyextractors.py
@@ -245,6 +245,9 @@ class NumpySorting(BaseSorting):
 
         sorting = NumpySorting(spikes, sampling_frequency, unit_ids)
 
+        # Trick : pupulate the cache with dict that already exists
+        sorting._cached_spike_trains = {seg_ind:d for seg_ind, d in enumerate(units_dict_list)}
+
         return sorting
 
     @staticmethod
