312a8e1d66b067d3da9e96bf3d7284b5835126eb
Aurélien WYNGAARD <aurelien.wyngaard@gmail.com>
2023-06-21 17:14:25 +0200

    oops

--- src/spikeinterface/exporters/to_phy.py
+++ src/spikeinterface/exporters/to_phy.py
@@ -1,6 +1,6 @@
+from __future__ import annotations
 from pathlib import Path
 from typing import Literal, Optional
-from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt
