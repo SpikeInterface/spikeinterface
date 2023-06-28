07009e05219e837b9c21229326fd4e569be3be91
Alessio Buccino <alejoe9187@gmail.com>
2023-06-28 11:57:20 +0200

    Make relative_to output_folder default for BaseSorter

--- src/spikeinterface/sorters/basesorter.py
+++ src/spikeinterface/sorters/basesorter.py
@@ -141,7 +141,7 @@ class BaseSorter:
 
         rec_file = output_folder / "spikeinterface_recording.json"
         if recording.is_dumpable:
-            recording.dump_to_json(rec_file)
+            recording.dump_to_json(rec_file, relative_to=output_folder)
         else:
             d = {"warning": "The recording is not dumpable"}
             rec_file.write_text(json.dumps(d, indent=4), encoding="utf8")
@@ -221,9 +221,6 @@ class BaseSorter:
 
         SorterClass = sorter_dict[sorter_name]
 
-        # not needed normally
-        #  recording = load_extractor(output_folder / 'spikeinterface_recording.json')
-
         now = datetime.datetime.now()
         log = {
             "sorter_name": str(SorterClass.sorter_name),
@@ -300,7 +297,7 @@ class BaseSorter:
             sorting = cls._get_result_from_folder(output_folder)
 
         # register recording to Sorting object
-        recording = load_extractor(output_folder / "spikeinterface_recording.json")
+        recording = load_extractor(output_folder / "spikeinterface_recording.json", base_folder=output_folder)
         if recording is not None:
             # can be None when not dumpable
             sorting.register_recording(recording)
