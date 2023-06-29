077a7fe28932be5d8dbf81bb946529c4ca6e90f9
Matthias H Hennig <mhhennig@gmail.com>
2023-06-29 12:45:48 +0100

    Fix problem with non-numeric unit IDs.

--- src/spikeinterface/extractors/mdaextractors.py
+++ src/spikeinterface/extractors/mdaextractors.py
@@ -197,10 +197,14 @@ class MdaSortingExtractor(BaseSorting):
         times_list = []
         labels_list = []
         primary_channels_list = []
-        for unit_id in unit_ids:
+        for unit_id_i, unit_id in enumerate(unit_ids):
             times = sorting.get_unit_spike_train(unit_id=unit_id)
             times_list.append(times)
-            labels_list.append(np.ones(times.shape) * unit_id)
+            # unit id may not be numeric
+            if unit_id.dtype.kind in 'biufc':
+                labels_list.append(np.ones(times.shape) * unit_id)
+            else:
+                labels_list.append(np.ones(times.shape) * unit_id_i)
             if write_primary_channels:
                 if 'max_channel' in sorting.get_unit_property_names(unit_id):
                     primary_channels_list.append([sorting.get_unit_property(unit_id, 'max_channel')] * times.shape[0])
