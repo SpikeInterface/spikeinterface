fb4e3d903a588695ceb750c25f29a856eaef0e5d
Alessio Buccino <alejoe9187@gmail.com>
2023-07-24 19:39:48 +0200

    line breaks

--- src/spikeinterface/qualitymetrics/pca_metrics.py
+++ src/spikeinterface/qualitymetrics/pca_metrics.py
@@ -466,12 +466,14 @@ def nearest_neighbors_isolation(
     # if target unit has fewer than `min_spikes` spikes, print out a warning and return NaN
     if n_spikes_all_units[this_unit_id] < min_spikes:
         warnings.warn(
-            f"Warning: unit {this_unit_id} has fewer spikes than specified by `min_spikes` ({min_spikes}); returning NaN as the quality metric..."
+            f"Warning: unit {this_unit_id} has fewer spikes than specified by `min_spikes` "
+            f"({min_spikes}); returning NaN as the quality metric..."
         )
         return np.nan, np.nan
     elif fr_all_units[this_unit_id] < min_fr:
         warnings.warn(
-            f"Warning: unit {this_uit_id} has a firing rate below the specified `min_fr` ({min_fr}Hz); returning NaN as the quality metric..."
+            f"Warning: unit {this_uit_id} has a firing rate below the specified `min_fr` "
+            f"({min_fr}Hz); returning NaN as the quality metric..."
         )
         return np.nan, np.nan
     else:
@@ -649,12 +651,14 @@ def nearest_neighbors_noise_overlap(
     # if target unit has fewer than `min_spikes` spikes, print out a warning and return NaN
     if n_spikes_all_units[this_unit_id] < min_spikes:
         warnings.warn(
-            f"Warning: unit {this_unit_id} has fewer spikes than specified by `min_spikes` ({min_spikes}); returning NaN as the quality metric..."
+            f"Warning: unit {this_unit_id} has fewer spikes than specified by `min_spikes` "
+            f"({min_spikes}); returning NaN as the quality metric..."
         )
         return np.nan
     elif fr_all_units[this_unit_id] < min_fr:
         warnings.warn(
-            f"Warning: unit {this_unit_id} has a firing rate below the specified `min_fr` ({min_fr}Hz); returning NaN as the quality metric...",
+            f"Warning: unit {this_unit_id} has a firing rate below the specified `min_fr` "
+            f"({min_fr}Hz); returning NaN as the quality metric...",
         )
         return np.nan
     else:
