ed63c949d16f9d9b02a96454a09e5932b7deb94d
Alessio Buccino <alejoe9187@gmail.com>
2023-08-03 12:39:03 +0200

    Restore npzfolder.py file to load previously saved sorting objects

---
+++ src/spikeinterface/core/npzfolder.py
@@ -0,0 +1,7 @@
+"""
+This file is for backwards compatibility with the old npz folder structure.
+"""
+
+from .sortingfolder import NpzFolderSorting as NewNpzFolderSorting
+
+NpzFolderSorting = NewNpzFolderSorting
