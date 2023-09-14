8349b90593622af022fa6b80ede0bc021296e5d6
Samuel Garcia <sam.garcia.die@gmail.com>
2023-09-14 21:07:04 +0200

    implement proof of concept merge_clusters/split_clusters from tridesclous and columbia codes.

---
+++ src/spikeinterface/sortingcomponents/tests/test_split.py
@@ -0,0 +1,12 @@
+import pytest
+import numpy as np
+
+from spikeinterface.sortingcomponents.clustering.split import split_clusters
+
+
+def test_split():
+    pass
+
+
+if __name__ == "__main__":
+    test_split()
