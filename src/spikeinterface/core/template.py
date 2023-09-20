d2c6ec727c01c47ec9635a5279eb3d432ce370ce
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-09-20 22:39:01 +0200

    alessio and samuel requests

--- src/spikeinterface/core/template.py
+++ src/spikeinterface/core/template.py
@@ -1,21 +1,75 @@
+import numpy as np
 import json
 from dataclasses import dataclass, field
-
-import numpy as np
-
-from spikeinterface.core.sparsity import ChannelSparsity
+from .sparsity import ChannelSparsity
 
 
-@dataclass
+@dataclass(kw_only=True)
 class Templates:
     templates_array: np.ndarray
-    sparsity: ChannelSparsity = None
+    sampling_frequency: float
+    nbefore: int
+
+    sparsity_mask: np.ndarray = None
+    channel_ids: np.ndarray = None
+    unit_ids: np.ndarray = None
+
     num_units: int = field(init=False)
     num_samples: int = field(init=False)
     num_channels: int = field(init=False)
 
+    nafter: int = field(init=False)
+    ms_before: float = field(init=False)
+    ms_after: float = field(init=False)
+    sparsity: ChannelSparsity = field(init=False)
+
     def __post_init__(self):
-        self.num_units, self.num_samples, self.num_channels = self.templates_array.shape
+        self.num_units, self.num_samples = self.templates_array.shape[:2]
+        if self.sparsity_mask is None:
+            self.num_channels = self.templates_array.shape[2]
+        else:
+            self.num_channels = self.sparsity_mask.shape[1]
+        self.nafter = self.num_samples - self.nbefore - 1
+        self.ms_before = self.nbefore / self.sampling_frequency * 1000
+        self.ms_after = self.nafter / self.sampling_frequency * 1000
+        if self.channel_ids is None:
+            self.channel_ids = np.arange(self.num_channels)
+        if self.unit_ids is None:
+            self.unit_ids = np.arange(self.num_units)
+        if self.sparsity_mask is not None:
+            self.sparsity = ChannelSparsity(
+                mask=self.sparsity_mask,
+                unit_ids=self.unit_ids,
+                channel_ids=self.channel_ids,
+            )
+
+    def to_dict(self):
+        return {
+            "templates_array": self.templates_array.tolist(),
+            "sparsity_mask": None if self.sparsity_mask is None else self.sparsity_mask.tolist(),
+            "channel_ids": self.channel_ids.tolist(),
+            "unit_ids": self.unit_ids.tolist(),
+            "sampling_frequency": self.sampling_frequency,
+            "nbefore": self.nbefore,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            templates_array=np.array(data["templates_array"]),
+            sparsity_mask=None if data["sparsity_mask"] is None else np.array(data["sparsity_mask"]),
+            channel_ids=np.array(data["channel_ids"]),
+            unit_ids=np.array(data["unit_ids"]),
+            sampling_frequency=data["sampling_frequency"],
+            nbefore=data["nbefore"],
+        )
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
+    @classmethod
+    def from_json(cls, json_str):
+        return cls.from_dict(json.loads(json_str))
 
     # Implementing the slicing/indexing behavior as numpy
     def __getitem__(self, index):
@@ -32,29 +86,3 @@ class Templates:
         result = getattr(ufunc, method)(*inputs, **kwargs)
 
         return result
-
-    def to_dict(self):
-        sparsity = self.sparsity.to_dict() if self.sparsity is not None else None
-        return {
-            "templates_array": self.templates_array.tolist(),
-            "sparsity": sparsity,
-            "num_units": self.num_units,
-            "num_samples": self.num_samples,
-            "num_channels": self.num_channels,
-        }
-
-    # Construct the object from a dictionary
-    @classmethod
-    def from_dict(cls, data):
-        sparsity = ChannelSparsity.from_dict(data["sparsity"]) if data["sparsity"] is not None else None
-        return cls(
-            templates_array=np.array(data["templates_array"]),
-            sparsity=sparsity,
-        )
-
-    def to_json(self):
-        return json.dumps(self.to_dict())
-
-    @classmethod
-    def from_json(cls, json_str):
-        return cls.from_dict(json.loads(json_str))
--- src/spikeinterface/core/tests/test_template_class.py
+++ src/spikeinterface/core/tests/test_template_class.py
@@ -1,102 +1,94 @@
-from pathlib import Path
-import pickle
-import json
-
-import numpy as np
 import pytest
-
+import numpy as np
+import pickle
 from spikeinterface.core.template import Templates
-from spikeinterface.core.sparsity import ChannelSparsity
 
 
-@pytest.fixture
-def dense_templates():
+@pytest.mark.parametrize("template_obj", ["dense", "sparse"])
+def get_template_object(template_obj):
     num_units = 2
-    num_samples = 4
+    num_samples = 5
     num_channels = 3
     templates_shape = (num_units, num_samples, num_channels)
     templates_array = np.arange(num_units * num_samples * num_channels).reshape(templates_shape)
 
-    return Templates(templates_array=templates_array)
+    sampling_frequency = 30_000
+    nbefore = 2
+
+    if template_obj == "dense":
+        return Templates(templates_array=templates_array, sampling_frequency=sampling_frequency, nbefore=nbefore)
+    else:  # sparse
+        sparsity_mask = np.array([[True, False, True], [False, True, False]])
+        return Templates(
+            templates_array=templates_array,
+            sparsity_mask=sparsity_mask,
+            sampling_frequency=sampling_frequency,
+            nbefore=nbefore,
+        )
 
 
-def test_dense_template_instance(dense_templates):
-    templates = dense_templates
-    templates_array = templates.templates_array
-    num_units, num_samples, num_channels = templates_array.shape
+@pytest.mark.parametrize("template_obj", ["dense", "sparse"])
+def test_pickle_serialization(template_obj, tmp_path):
+    obj = get_template_object(template_obj)
 
-    assert np.array_equal(templates.templates_array, templates_array)
-    assert templates.sparsity is None
-    assert templates.num_units == num_units
-    assert templates.num_samples == num_samples
-    assert templates.num_channels == num_channels
+    # Dump to pickle
+    pkl_path = tmp_path / "templates.pkl"
+    with open(pkl_path, "wb") as f:
+        pickle.dump(obj, f)
 
+    # Load from pickle
+    with open(pkl_path, "rb") as f:
+        loaded_obj = pickle.load(f)
 
-def test_numpy_like_behavior(dense_templates):
-    templates = dense_templates
-    templates_array = templates.templates_array
-
-    # Test that slicing works as in numpy
-    assert np.array_equal(templates[:], templates_array[:])
-    assert np.array_equal(templates[0], templates_array[0])
-    assert np.array_equal(templates[0, :], templates_array[0, :])
-    assert np.array_equal(templates[0, :, :], templates_array[0, :, :])
-    assert np.array_equal(templates[3:5, :, 2], templates_array[3:5, :, 2])
-    # Test fancy indexing
-    indices = np.array([0, 1])
-    assert np.array_equal(templates[indices], templates_array[indices])
-    row_indices = np.array([0, 1])
-    col_indices = np.array([2, 3])
-    assert np.array_equal(templates[row_indices, col_indices], templates_array[row_indices, col_indices])
-    mask = templates_array > 0.5
-    assert np.array_equal(templates[mask], templates_array[mask])
-
-    # Test unary ufuncs
-    assert np.array_equal(np.sqrt(templates), np.sqrt(templates_array))
-    assert np.array_equal(np.abs(templates), np.abs(templates_array))
-    assert np.array_equal(np.mean(templates, axis=0), np.mean(templates_array, axis=0))
-
-    # Test binary ufuncs
-    other_array = np.random.rand(*templates_array.shape)
-    other_template = Templates(templates_array=other_array)
-    assert np.array_equal(np.add(templates, other_template), np.add(templates_array, other_array))
-    assert np.array_equal(np.multiply(templates, other_template), np.multiply(templates_array, other_array))
-
-    # Test chaining of operations
-    chained_result = np.mean(np.multiply(templates, other_template), axis=0)
-    chained_expected = np.mean(np.multiply(templates_array, other_array), axis=0)
-    assert np.array_equal(chained_result, chained_expected)
-
-    # Test ufuncs that return non-ndarray results
-    assert np.all(np.greater(templates, -1))
-    assert not np.any(np.less(templates, 0))
+    assert np.array_equal(obj.templates_array, loaded_obj.templates_array)
 
 
-def test_pickle(dense_templates):
-    templates = dense_templates
+@pytest.mark.parametrize("template_obj", ["dense", "sparse"])
+def test_json_serialization(template_obj):
+    obj = get_template_object(template_obj)
 
-    # Serialize and deserialize the object
-    serialized = pickle.dumps(templates)
-    deserialized_templates = pickle.loads(serialized)
+    json_str = obj.to_json()
+    loaded_obj_from_json = Templates.from_json(json_str)
 
-    assert np.array_equal(templates.templates_array, deserialized_templates.templates_array)
-    assert templates.sparsity == deserialized_templates.sparsity
-    assert templates.num_units == deserialized_templates.num_units
-    assert templates.num_samples == deserialized_templates.num_samples
-    assert templates.num_channels == deserialized_templates.num_channels
+    assert np.array_equal(obj.templates_array, loaded_obj_from_json.templates_array)
 
 
-def test_jsonification(dense_templates):
-    templates = dense_templates
-    # Serialize to JSON string
-    serialized = templates.to_json()
+# @pytest.fixture
+# def dense_templates():
+#     num_units = 2
+#     num_samples = 4
+#     num_channels = 3
+#     templates_shape = (num_units, num_samples, num_channels)
+#     templates_array = np.arange(num_units * num_samples * num_channels).reshape(templates_shape)
 
-    # Deserialize back to object
-    deserialized = Templates.from_json(serialized)
+#     return Templates(templates_array=templates_array)
 
-    # Check if deserialized object matches original
-    assert np.array_equal(templates.templates_array, deserialized.templates_array)
-    assert templates.sparsity == deserialized.sparsity
-    assert templates.num_units == deserialized.num_units
-    assert templates.num_samples == deserialized.num_samples
-    assert templates.num_channels == deserialized.num_channels
+
+# def test_pickle(dense_templates):
+#     templates = dense_templates
+
+#     # Serialize and deserialize the object
+#     serialized = pickle.dumps(templates)
+#     deserialized_templates = pickle.loads(serialized)
+
+#     assert np.array_equal(templates.templates_array, deserialized_templates.templates_array)
+#     assert templates.sparsity == deserialized_templates.sparsity
+#     assert templates.num_units == deserialized_templates.num_units
+#     assert templates.num_samples == deserialized_templates.num_samples
+#     assert templates.num_channels == deserialized_templates.num_channels
+
+
+# def test_jsonification(dense_templates):
+#     templates = dense_templates
+#     # Serialize to JSON string
+#     serialized = templates.to_json()
+
+#     # Deserialize back to object
+#     deserialized = Templates.from_json(serialized)
+
+#     # Check if deserialized object matches original
+#     assert np.array_equal(templates.templates_array, deserialized.templates_array)
+#     assert templates.sparsity == deserialized.sparsity
+#     assert templates.num_units == deserialized.num_units
+#     assert templates.num_samples == deserialized.num_samples
+#     assert templates.num_channels == deserialized.num_channels
