961d26979cf339b8799d9f045f20f477589171c4
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-09-20 22:39:38 +0200

    remove slicing

--- src/spikeinterface/core/template.py
+++ src/spikeinterface/core/template.py
@@ -66,23 +66,3 @@ class Templates:
 
     def to_json(self):
         return json.dumps(self.to_dict())
-
-    @classmethod
-    def from_json(cls, json_str):
-        return cls.from_dict(json.loads(json_str))
-
-    # Implementing the slicing/indexing behavior as numpy
-    def __getitem__(self, index):
-        return self.templates_array[index]
-
-    def __array__(self):
-        return self.templates_array
-
-    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs) -> np.ndarray:
-        # Replace any Templates instances with their ndarray representation
-        inputs = tuple(inp.templates_array if isinstance(inp, Templates) else inp for inp in inputs)
-
-        # Apply the ufunc on the transformed inputs
-        result = getattr(ufunc, method)(*inputs, **kwargs)
-
-        return result
