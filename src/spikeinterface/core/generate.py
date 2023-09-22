f2188266647d7faf721d89089b6f9c0bd1d9e637
Samuel Garcia <sam.garcia.die@gmail.com>
2023-09-22 16:22:01 +0200

    feedback from Ramon

--- src/spikeinterface/core/generate.py
+++ src/spikeinterface/core/generate.py
@@ -1433,7 +1433,7 @@ def generate_ground_truth_recording(
     )
     recording.annotate(is_filtered=True)
     recording.set_probe(probe, in_place=True)
-    recording.set_property("gain_to_uV", np.ones(num_channels))
-    recording.set_property("offset_to_uV", np.zeros(num_channels))
+    recording.set_channel_gains(1.)
+    recording.set_channel_offsets(0.)
 
     return recording, sorting
--- src/spikeinterface/core/tests/test_waveform_extractor.py
+++ src/spikeinterface/core/tests/test_waveform_extractor.py
@@ -517,8 +517,6 @@ def test_non_json_object():
         num_units=5,
         )
     
-
-    print(recording.check_serializablility("pickle"))
     # recording is not save to keep it in memory
     sorting = sorting.save()
 
