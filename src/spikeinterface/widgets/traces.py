c0d4c60095f9704f9b27adfb5fa0f4867adfaf10
Samuel Garcia <sam.garcia.die@gmail.com>
2023-10-06 11:38:15 +0200

    oups

--- src/spikeinterface/widgets/traces.py
+++ src/spikeinterface/widgets/traces.py
@@ -88,7 +88,7 @@ class TracesWidget(BaseWidget):
         else:
             raise ValueError("plot_traces recording must be recording or dict or list")
 
-        if rec0.has_channel_locations():
+        if rec0.has_channel_location():
             channel_locations = rec0.get_channel_locations()
         else:
             channel_locations = None
