4e843cb09ac9a9217af0f2a9476514609400d789
Alessio Buccino <alejoe9187@gmail.com>
2023-10-26 12:01:19 +0200

    Update src/spikeinterface/sortingcomponents/tools.py

--- src/spikeinterface/sortingcomponents/tools.py
+++ src/spikeinterface/sortingcomponents/tools.py
@@ -19,9 +19,8 @@ def make_multi_method_doc(methods, ident="    "):
 
 
 def get_prototype_spike(recording, peaks, job_kwargs, nb_peaks=1000, ms_before=0.5, ms_after=0.5):
-    # TODO for Pierre: this function is really unefficient because it runa full pipeline only for a few
-    # spikes, which leans that traces are entirally computed!!!!!
-    # Please find a better way
+    # TODO for Pierre: this function is really inefficient because it runs a full pipeline only for a few
+    # spikes, which means that all traces need to be accesses! Please find a better way
     nb_peaks = min(len(peaks), nb_peaks)
     idx = np.sort(np.random.choice(len(peaks), nb_peaks, replace=False))
     peak_retriever = PeakRetriever(recording, peaks[idx])
