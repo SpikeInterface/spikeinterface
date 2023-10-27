7b506bfdf0718a2a624f283a4b611a7876826028
Alessio Buccino <alejoe9187@gmail.com>
2023-10-27 11:35:43 +0200

    Update src/spikeinterface/extractors/neoextractors/tdt.py
    Co-authored-by: Zach McKenzie <92116279+zm711@users.noreply.github.com>

--- src/spikeinterface/extractors/neoextractors/tdt.py
+++ src/spikeinterface/extractors/neoextractors/tdt.py
@@ -15,7 +15,7 @@ class TdtRecordingExtractor(NeoBaseRecordingExtractor):
     ----------
     folder_path: str
         The folder path to the tdt folder.
-    stream_id: str, default: None
+    stream_id: str or None, default: None
         If there are several streams, specify the stream id you want to load.
     stream_name: str, default: None
         If there are several streams, specify the stream name you want to load.
