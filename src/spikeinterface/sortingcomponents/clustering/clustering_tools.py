9054f7b79ea7cd742bd383f9921ecafe58fd02f5
Pierre Yger <pierre.yger@gmail.com>
2023-11-16 14:46:01 +0100

    Speeding up merging

--- src/spikeinterface/sortingcomponents/clustering/clustering_tools.py
+++ src/spikeinterface/sortingcomponents/clustering/clustering_tools.py
@@ -593,7 +593,7 @@ def remove_duplicates_via_matching(
 
     local_params = method_kwargs.copy()
 
-    local_params.update({"waveform_extractor": waveform_extractor, "amplitudes": [0.975, 1.025]})
+    local_params.update({"waveform_extractor": waveform_extractor, "amplitudes": [0.975, 1.025], "omp_min_sps" : 1e-3})
 
     spikes_per_units, counts = np.unique(waveform_extractor.sorting.to_spike_vector()["unit_index"], return_counts=True)
     indices = np.argsort(counts)
