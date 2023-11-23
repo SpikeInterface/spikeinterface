4b10e3764e21dadbf0a953851f7154adb3cd1151
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-11-23 11:54:48 +0100

    Patch for neuroconv (#2245)
    * sorting tools imports
    
    * patch for neuroconv

--- src/spikeinterface/core/sorting_tools.py
+++ src/spikeinterface/core/sorting_tools.py
@@ -1,7 +1,8 @@
+from __future__ import annotations
 import numpy as np
 
 
-def spike_vector_to_spike_trains(spike_vector: list[np.array], unit_ids: np.array) -> dict[dict]:
+def spike_vector_to_spike_trains(spike_vector: list[np.array], unit_ids: np.array) -> dict[dict[str, np.array]]:
     """
     Computes all spike trains for all units/segments from a spike vector list.
 
