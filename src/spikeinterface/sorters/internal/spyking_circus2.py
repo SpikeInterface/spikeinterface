87e88d0de087030aeb8e83ea1b1fec83412a9618
Sebastien <pierre.yger@gmail.com>
2023-11-30 09:17:07 +0100

    Addition of a multi units mode to bypass clustering/fitting

--- src/spikeinterface/sorters/internal/spyking_circus2.py
+++ src/spikeinterface/sorters/internal/spyking_circus2.py
@@ -55,6 +55,7 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
             },
         "apply_preprocessing": True,
         "shared_memory": True,
+        "multi_units_only" : False,
         "job_kwargs": {"n_jobs": 0.8},
         "debug": False,
     }
@@ -69,13 +70,14 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
                            "detection" : "A dictionnary for the peak detection node (locally_exclusive)",
                            "selection" : "A dictionnary for the peak selection node. Default is to use smart_sampling_amplitudes, with a minimum of 20000 peak_sign\
                                          and 5000 peaks per electrode on average.",
-                            "clustering" : "A dictionnary to be provided to the clustering method. By default, we used random_projections, but if legcacy is set to\
-                            True, one other clustering called circus will be used, similar to the one used in Spyking circus 1",
+                            "clustering" : "A dictionnary to be provided to the clustering method. By default, we used random_projections, but if legacy is set to\
+                            True, one other clustering called circus will be used, similar to the one used in Spyking Circus 1",
                             "matching" : "A dictionnary to specify the matching engine used to recover spikes. The method default is circus-omp-svd, but other engines\
                                           can be used",
                             "apply_preprocessing" : "Boolean to specify is circus 2 should preprocess the recording or not. If yes, then high_pass filtering + common\
-                                                    median reference + z_score",
+                                                    median reference + zscore",
                             "shared_memory" : "Boolean to specify if the code should use, as much as possible, to put internal datastructure in memory (faster)",
+                            "multi_units_only" : "Boolean to get only multi units activity (i.e. one template per electrode)",
                             "job_kwargs" : "A dictionnary to specify how many jobs and which parameters they should used",
                             "debug" : "Boolean to specify if internal datastructure should be kept for debugging"}
 
@@ -105,7 +107,7 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
 
         recording = cls.load_recording_from_folder(sorter_output_folder.parent, with_warnings=False)
 
-        sampling_rate = recording.get_sampling_frequency()
+        sampling_frequency = recording.get_sampling_frequency()
         num_channels = recording.get_num_channels()
 
         ## First, we are filtering the data
@@ -134,121 +136,124 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
         if verbose:
             print("We found %d peaks in total" % len(peaks))
 
-        ## We subselect a subset of all the peaks, by making the distributions os SNRs over all
-        ## channels as flat as possible
-        selection_params = params["selection"]
-        selection_params["n_peaks"] = params["selection"]["n_peaks_per_channel"] * num_channels
-        selection_params["n_peaks"] = max(selection_params["min_n_peaks"], selection_params["n_peaks"])
-
-        selection_params.update({"noise_levels": noise_levels})
-        selected_peaks = select_peaks(
-            peaks, **selection_params
-        )
-
-        if verbose:
-            print("We kept %d peaks for clustering" % len(selected_peaks))
-
-        ## We launch a clustering (using hdbscan) relying on positions and features extracted on
-        ## the fly from the snippets
-        clustering_params = params["clustering"].copy()
-        clustering_params["waveforms"] = params["waveforms"].copy()
-
-        for k in ["ms_before", "ms_after"]:
-            clustering_params["waveforms"][k] = params["general"][k]
-
-        clustering_params.update(dict(shared_memory=params["shared_memory"]))
-        clustering_params["job_kwargs"] = job_kwargs
-        clustering_params["tmp_folder"] = sorter_output_folder / "clustering"
-
-        if "legacy" in clustering_params:
-            legacy = clustering_params.pop("legacy")
+        if params["multi_units_only"]:
+            sorting = NumpySorting.from_peaks(peaks, sampling_frequency)
         else:
-            legacy = False
+            ## We subselect a subset of all the peaks, by making the distributions os SNRs over all
+            ## channels as flat as possible
+            selection_params = params["selection"]
+            selection_params["n_peaks"] = params["selection"]["n_peaks_per_channel"] * num_channels
+            selection_params["n_peaks"] = max(selection_params["min_n_peaks"], selection_params["n_peaks"])
 
-        if legacy:
-            clustering_method = "circus"
-        else:
-            clustering_method = "random_projections"
+            selection_params.update({"noise_levels": noise_levels})
+            selected_peaks = select_peaks(
+                peaks, **selection_params
+            )
 
-        labels, peak_labels = find_cluster_from_peaks(
-            recording_f, selected_peaks, method=clustering_method, method_kwargs=clustering_params
-        )
+            if verbose:
+                print("We kept %d peaks for clustering" % len(selected_peaks))
 
-        ## We get the labels for our peaks
-        mask = peak_labels > -1
-        sorting = NumpySorting.from_times_labels(
-            selected_peaks["sample_index"][mask], peak_labels[mask].astype(int), sampling_rate
-        )
-        clustering_folder = sorter_output_folder / "clustering"
-        clustering_folder.mkdir(parents=True, exist_ok=True)
+            ## We launch a clustering (using hdbscan) relying on positions and features extracted on
+            ## the fly from the snippets
+            clustering_params = params["clustering"].copy()
+            clustering_params["waveforms"] = params["waveforms"].copy()
 
-        if not params["debug"]:
-            shutil.rmtree(clustering_folder)
-        else:
-            np.save(clustering_folder / "labels", labels)
-            np.save(clustering_folder / "peaks", selected_peaks)
+            for k in ["ms_before", "ms_after"]:
+                clustering_params["waveforms"][k] = params["general"][k]
 
-        ## We get the templates our of such a clustering
-        waveforms_params = params["waveforms"].copy()
-        waveforms_params.update(job_kwargs)
+            clustering_params.update(dict(shared_memory=params["shared_memory"]))
+            clustering_params["job_kwargs"] = job_kwargs
+            clustering_params["tmp_folder"] = sorter_output_folder / "clustering"
 
-        for k in ["ms_before", "ms_after"]:
-            waveforms_params[k] = params["general"][k]
+            if "legacy" in clustering_params:
+                legacy = clustering_params.pop("legacy")
+            else:
+                legacy = False
 
-        if params["shared_memory"] and not params["debug"]:
-            mode = "memory"
-            waveforms_folder = None
-        else:
-            sorting = sorting.save(folder=clustering_folder)
-            mode = "folder"
-            waveforms_folder = sorter_output_folder / "waveforms"
+            if legacy:
+                clustering_method = "circus"
+            else:
+                clustering_method = "random_projections"
 
-        we = extract_waveforms(
-            recording_f,
-            sorting,
-            waveforms_folder,
-            return_scaled=False,
-            precompute_template=["median"],
-            mode=mode,
-            **waveforms_params,
-        )
+            labels, peak_labels = find_cluster_from_peaks(
+                recording_f, selected_peaks, method=clustering_method, method_kwargs=clustering_params
+            )
 
-        ## We launch a OMP matching pursuit by full convolution of the templates and the raw traces
+            ## We get the labels for our peaks
+            mask = peak_labels > -1
+            sorting = NumpySorting.from_times_labels(
+                selected_peaks["sample_index"][mask], peak_labels[mask].astype(int), sampling_frequency
+            )
+            clustering_folder = sorter_output_folder / "clustering"
+            clustering_folder.mkdir(parents=True, exist_ok=True)
 
-        matching_method = params['matching']['method']
+            if not params["debug"]:
+                shutil.rmtree(clustering_folder)
+            else:
+                np.save(clustering_folder / "labels", labels)
+                np.save(clustering_folder / "peaks", selected_peaks)
 
-        matching_params = params["matching"]['method_kwargs'].copy()
-        matching_job_params = {}
-        matching_job_params.update(job_kwargs)
-        if matching_method == 'wobble':
-            matching_params['templates'] = we.get_all_templates(mode='median')
-            matching_params['nbefore'] = we.nbefore
-            matching_params['nafter'] = we.nafter
-        else:
-            matching_params["waveform_extractor"] = we
+            ## We get the templates our of such a clustering
+            waveforms_params = params["waveforms"].copy()
+            waveforms_params.update(job_kwargs)
+
+            for k in ["ms_before", "ms_after"]:
+                waveforms_params[k] = params["general"][k]
+
+            if params["shared_memory"] and not params["debug"]:
+                mode = "memory"
+                waveforms_folder = None
+            else:
+                sorting = sorting.save(folder=clustering_folder)
+                mode = "folder"
+                waveforms_folder = sorter_output_folder / "waveforms"
+
+            we = extract_waveforms(
+                recording_f,
+                sorting,
+                waveforms_folder,
+                return_scaled=False,
+                precompute_template=["median"],
+                mode=mode,
+                **waveforms_params,
+            )
+
+            ## We launch a OMP matching pursuit by full convolution of the templates and the raw traces
+
+            matching_method = params['matching']['method']
+
+            matching_params = params["matching"]['method_kwargs'].copy()
+            matching_job_params = {}
+            matching_job_params.update(job_kwargs)
+            if matching_method == 'wobble':
+                matching_params['templates'] = we.get_all_templates(mode='median')
+                matching_params['nbefore'] = we.nbefore
+                matching_params['nafter'] = we.nafter
+            else:
+                matching_params["waveform_extractor"] = we
+            
+            if matching_method == 'circus-omp-svd':
+                for value in ["chunk_size", "chunk_memory", "total_memory", "chunk_duration"]:
+                    if value in matching_job_params:
+                        matching_job_params.pop(value)
+                matching_job_params["chunk_duration"] = "100ms"
+
+            spikes = find_spikes_from_templates(
+                recording_f, matching_method, method_kwargs=matching_params, **matching_job_params
+            )
+
+            if params["debug"]:
+                fitting_folder = sorter_output_folder / "fitting"
+                fitting_folder.mkdir(parents=True, exist_ok=True)
+                np.save(fitting_folder / "spikes", spikes)
+
+            if verbose:
+                print("We found %d spikes" % len(spikes))
+
+            ## And this is it! We have a spyking circus
+            sorting = NumpySorting.from_times_labels(spikes["sample_index"], spikes["cluster_index"], sampling_frequency)
         
-        if matching_method == 'circus-omp-svd':
-            for value in ["chunk_size", "chunk_memory", "total_memory", "chunk_duration"]:
-                if value in matching_job_params:
-                    matching_job_params.pop(value)
-            matching_job_params["chunk_duration"] = "100ms"
-
-        spikes = find_spikes_from_templates(
-            recording_f, matching_method, method_kwargs=matching_params, **matching_job_params
-        )
-
-        if params["debug"]:
-            fitting_folder = sorter_output_folder / "fitting"
-            fitting_folder.mkdir(parents=True, exist_ok=True)
-            np.save(fitting_folder / "spikes", spikes)
-
-        if verbose:
-            print("We found %d spikes" % len(spikes))
-
-        ## And this is it! We have a spyking circus
-        sorting = NumpySorting.from_times_labels(spikes["sample_index"], spikes["cluster_index"], sampling_rate)
         sorting_folder = sorter_output_folder / "sorting"
-
         if sorting_folder.exists():
             shutil.rmtree(sorting_folder)
 
