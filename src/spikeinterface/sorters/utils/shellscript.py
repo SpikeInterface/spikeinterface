28d63892302a7f48d665330a33fc639af3592e1a
Alessio Buccino <alejoe9187@gmail.com>
2023-12-14 17:57:39 +0100

    Close stdout and stderr in ShellScript

--- src/spikeinterface/sorters/utils/shellscript.py
+++ src/spikeinterface/sorters/utils/shellscript.py
@@ -111,6 +111,10 @@ class ShellScript:
         for dirpath in self._dirs_to_remove:
             _rmdir_with_retries(str(dirpath), num_retries=5)
         if self._process is not None:
+            if self._process.stdout:
+                self._process.stdout.close()
+            if self._process.stderr:
+                self._process.stderr.close()
             self._process.kill()
 
     def stop(self) -> None:
