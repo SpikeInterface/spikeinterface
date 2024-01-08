097373e03140365a64d2385b7804c491425234f0
Tom Bugnon <tombugnon@hotmail.com>
2024-01-08 08:00:08 -0600

    Parameterize test_decimate

--- src/spikeinterface/preprocessing/tests/test_decimate.py
+++ src/spikeinterface/preprocessing/tests/test_decimate.py
@@ -8,28 +8,26 @@ from spikeinterface.preprocessing.decimate import DecimateRecording
 import numpy as np
 
 
-def test_decimate():
+@pytest.mark.parametrize("decimation_frame_start", [0, 1, 9, 10, 11, 200])
+@pytest.mark.parametrize("decimation_frame_step", [1, 9, 10, 11, 200])
+@pytest.mark.parametrize("start_frame", [0, 1, 5, 20])
+@pytest.mark.parametrize("end_frame", [None, 1, 5, 20])
+def test_decimate(decimation_frame_start, decimation_frame_step, start_frame, end_frame):
     rec = generate_recording()
 
     N = 101
     rec = NumpyRecording([np.arange(N).reshape(N,1)], 1)
     parent_traces = rec.get_traces()
 
-    for decimation_frame_start, decimation_frame_step in itertools.product(
-        [0, 1, 9, 10, 11, 200],
-        [1, 9, 10, 11, 200]
-    ):
+    decimated_rec = DecimateRecording(rec, decimation_frame_step, decimation_frame_start=decimation_frame_start)
+    decimated_parent_traces = parent_traces[decimation_frame_start::decimation_frame_step]
 
-        decimated_rec = DecimateRecording(rec, decimation_frame_step, decimation_frame_start=decimation_frame_start)
-        decimated_parent_traces = parent_traces[decimation_frame_start::decimation_frame_step]
+    if start_frame is None:
+        start_frame = len(decimated_parent_traces)
 
-        for start_frame, end_frame in itertools.product(
-            [0, 1, 5, 20],
-            [len(decimated_parent_traces), 1, 5, 20],
-        ):
-            assert np.all(
-                decimated_rec.get_traces(0, start_frame, end_frame) == decimated_parent_traces[start_frame:end_frame]
-            )
+    assert np.all(
+        decimated_rec.get_traces(0, start_frame, end_frame) == decimated_parent_traces[start_frame:end_frame]
+    )
 
 
 if __name__ == "__main__":
