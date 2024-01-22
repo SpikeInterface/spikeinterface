a7d4f03544fe6cc11505c6a30ac2b363109badd6
Alessio Buccino <alejoe9187@gmail.com>
2024-01-22 17:04:21 +0100

    Refactor group section and added tests

--- src/spikeinterface/preprocessing/tests/test_common_reference.py
+++ src/spikeinterface/preprocessing/tests/test_common_reference.py
@@ -1,33 +1,33 @@
+import operator
 import pytest
-from pathlib import Path
-import shutil
 
-from spikeinterface import set_global_tmp_folder
 from spikeinterface.core import generate_recording
 
-from spikeinterface.preprocessing import CommonReferenceRecording, common_reference
+from spikeinterface.preprocessing import common_reference
 
 import numpy as np
-
-if hasattr(pytest, "global_test_folder"):
-    cache_folder = pytest.global_test_folder / "preprocessing"
-else:
-    cache_folder = Path("cache_folder") / "preprocessing"
-
-set_global_tmp_folder(cache_folder)
+from tqdm import trange
 
 
-def test_common_reference():
-    rec = generate_recording(durations=[5.0], num_channels=4)
-    rec._main_ids = np.array(["a", "b", "c", "d"])
+def _generate_test_recording():
+    recording = generate_recording(durations=[5.0], num_channels=4)
+    recording = recording.channel_slice(recording.channel_ids, np.array(["a", "b", "c", "d"]))
+    return recording
 
+
+@pytest.fixture
+def recording():
+    return _generate_test_recording()
+
+
+def test_common_reference(recording):
     # Test simple case
-    rec_cmr = common_reference(rec, reference="global", operator="median")
-    rec_car = common_reference(rec, reference="global", operator="average")
-    rec_sin = common_reference(rec, reference="single", ref_channel_ids=["a"])
-    rec_local_car = common_reference(rec, reference="local", local_radius=(20, 65), operator="median")
+    rec_cmr = common_reference(recording, reference="global", operator="median")
+    rec_car = common_reference(recording, reference="global", operator="average")
+    rec_sin = common_reference(recording, reference="single", ref_channel_ids=["a"])
+    rec_local_car = common_reference(recording, reference="local", local_radius=(20, 65), operator="median")
 
-    traces = rec.get_traces()
+    traces = recording.get_traces()
     assert np.allclose(traces, rec_cmr.get_traces() + np.median(traces, axis=1, keepdims=True), atol=0.01)
     assert np.allclose(traces, rec_car.get_traces() + np.mean(traces, axis=1, keepdims=True), atol=0.01)
     assert not np.all(rec_sin.get_traces()[0])
@@ -43,13 +43,11 @@ def test_common_reference():
     rec_local_car.save(verbose=False)
 
 
-def test_common_reference_channel_slicing():
-    recording = generate_recording(durations=[1.0], num_channels=4)
-    recording._main_ids = np.array(["a", "b", "c", "d"])
-
+def test_common_reference_channel_slicing(recording):
     recording_cmr = common_reference(recording, reference="global", operator="median")
     recording_car = common_reference(recording, reference="global", operator="average")
     recording_single_reference = common_reference(recording, reference="single", ref_channel_ids=["a"])
+    recording_local_car = common_reference(recording, reference="local", local_radius=(20, 65), operator="median")
 
     channel_ids = ["a", "b"]
     indices = recording.ids_to_indices(["a", "b"])
@@ -69,6 +67,55 @@ def test_common_reference_channel_slicing():
 
     assert np.allclose(single_reference_trace, expected_trace, atol=0.01)
 
+    # local car
+    local_trace = recording_local_car.get_traces(channel_ids=channel_ids)
+
+
+def test_common_reference_groups(recording):
+    original_traces = recording.get_traces()
+    groups = [["a", "c", "d"], ["b"]]
+
+    # median - global
+    recording_cmr = common_reference(recording, reference="global", operator="median", groups=groups)
+    traces = recording_cmr.get_traces(channel_ids=["c", "b", "d"])
+    # c/d
+    assert np.allclose(
+        traces[:, 0], original_traces[:, 2] - np.median(original_traces[:, [0, 2, 3]], axis=1), atol=0.01
+    )
+    assert np.allclose(
+        traces[:, 2], original_traces[:, 3] - np.median(original_traces[:, [0, 2, 3]], axis=1), atol=0.01
+    )
+    # b will be all zeros
+    assert np.allclose(traces[:, 1], 0)
+
+    traces = recording_cmr.get_traces(channel_ids=["a"])
+    assert np.allclose(
+        traces[:, 0], original_traces[:, 0] - np.median(original_traces[:, [0, 2, 3]], axis=1), atol=0.01
+    )
+
+    # single
+    groups = [["a", "c"], ["b", "d"]]
+    recording_single = common_reference(recording, reference="single", ref_channel_ids=["a", "b"], groups=groups)
+    traces = recording_single.get_traces(channel_ids=["c", "b", "a", "d"])
+    # c - referenced to a
+    assert np.allclose(traces[:, 0], original_traces[:, 2] - original_traces[:, 0], atol=0.01)
+    # b - all zeros
+    assert np.allclose(traces[:, 1], 0)
+    # a - all zeros
+    assert np.allclose(traces[:, 2], 0)
+    # d - referenced to b
+    assert np.allclose(traces[:, 3], original_traces[:, 3] - original_traces[:, 1], atol=0.01)
+
+    # TODO: fix this!
+    traces = recording_single.get_traces(channel_ids=["d", "b"])
+    # d - referenced to b
+    assert np.allclose(traces[:, 0], original_traces[:, 3] - original_traces[:, 1], atol=0.01)
+    # b - all zeros
+    assert np.allclose(traces[:, 1], 0)
+
 
 if __name__ == "__main__":
-    test_common_reference()
+    recording = _generate_test_recording()
+    test_common_reference(recording)
+    test_common_reference_channel_slicing(recording)
+    test_common_reference_groups(recording)
