84b6b3ec0061e934c9d682b3ff896ff37f691881
Samuel Garcia <sam.garcia.die@gmail.com>
2024-01-26 08:33:39 +0100

    wip

--- src/spikeinterface/postprocessing/tests/test_amplitude_scalings.py
+++ src/spikeinterface/postprocessing/tests/test_amplitude_scalings.py
@@ -11,24 +11,28 @@ from spikeinterface.postprocessing import ComputeAmplitudeScalings
 class AmplitudeScalingsExtensionTest(ResultExtensionCommonTestSuite, unittest.TestCase):
     extension_class = ComputeAmplitudeScalings
     extension_function_kwargs_list = [
-        dict(),
-        dict(ms_before=0.5, ms_after=0.5),
+        dict(handle_collisions=True),
+        dict(handle_collisions=False),
     ]
 
     def test_scaling_values(self):
-        key0 = next(iter(self.sorting_results.keys()))
-        sorting_result = self.sorting_results[key0]
+        sorting_result = self.sorting_results["sparseTrue_memory"]
 
         spikes = sorting_result.sorting.to_spike_vector()
 
         ext = sorting_result.get_extension("amplitude_scalings")
-        ext.data["amplitude_scalings"]
+        
         for unit_index, unit_id in enumerate(sorting_result.unit_ids):
             mask = spikes["unit_index"] == unit_index
             scalings = ext.data["amplitude_scalings"][mask]
             median_scaling = np.median(scalings)
-            print(unit_index, median_scaling)
+            # print(unit_index, median_scaling)
             np.testing.assert_array_equal(np.round(median_scaling), 1)
+
+        # import matplotlib.pyplot as plt
+        # fig, ax = plt.subplots()
+        # ax.hist(ext.data["amplitude_scalings"])
+        # plt.show()
 
 
 if __name__ == "__main__":
