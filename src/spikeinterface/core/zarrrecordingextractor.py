71100c447ca9aa0ec4a6dda48d63d97f6c329f81
Alessio Buccino <alejoe9187@gmail.com>
2024-01-31 12:21:49 +0100

    Zarr backcompatibility: map root_path to folder_path

--- src/spikeinterface/core/zarrrecordingextractor.py
+++ src/spikeinterface/core/zarrrecordingextractor.py
@@ -2,4 +2,12 @@
 This file is for backwards compatibility with the old zarr extractor file structure.
 """
 
-from .zarrextractors import ZarrRecordingExtractor
+from __future__ import annotations
+
+from pathlib import Path
+from .zarrextractors import ZarrRecordingExtractor as ZarrRecordingExtractorNew
+
+
+class ZarrRecordingExtractor(ZarrRecordingExtractorNew):
+    def __init__(self, root_path: Path | str, storage_options: dict | None = None):
+        super().__init__(folder_path=root_path, storage_options=storage_options)
