7aed807bd7a4df1062dc4c373c9980ea4d3f3137
Samuel Garcia <sam.garcia.die@gmail.com>
2024-02-23 10:34:05 +0100

    Fix mutually exclusive keys in job_kwargs

--- src/spikeinterface/core/tests/test_job_tools.py
+++ src/spikeinterface/core/tests/test_job_tools.py
@@ -1,7 +1,7 @@
 import pytest
 import os
 
-from spikeinterface.core import generate_recording
+from spikeinterface.core import generate_recording, set_global_job_kwargs
 
 from spikeinterface.core.job_tools import (
     divide_segment_into_chunks,
@@ -189,6 +189,18 @@ def test_fix_job_kwargs():
     with pytest.raises(AssertionError):
         job_kwargs = dict(n_jobs=0, progress_bar=False, chunk_duration="1s", other_param="other")
         fixed_job_kwargs = fix_job_kwargs(job_kwargs)
+    
+    # test mutually exclusive
+    set_global_job_kwargs(chunk_memory="50M")
+    job_kwargs = dict()
+    fixed_job_kwargs = fixed_job_kwargs = fix_job_kwargs(job_kwargs)
+    assert "chunk_memory" in fixed_job_kwargs
+
+    job_kwargs = dict(chunk_duration="300ms")
+    fixed_job_kwargs = fixed_job_kwargs = fix_job_kwargs(job_kwargs)
+    assert "chunk_memory" not in fixed_job_kwargs
+    assert fixed_job_kwargs["chunk_duration"] == "300ms"
+
 
 
 def test_split_job_kwargs():
@@ -204,6 +216,6 @@ if __name__ == "__main__":
     # test_divide_segment_into_chunks()
     # test_ensure_n_jobs()
     # test_ensure_chunk_size()
-    test_ChunkRecordingExecutor()
-    # test_fix_job_kwargs()
+    # test_ChunkRecordingExecutor()
+    test_fix_job_kwargs()
     # test_split_job_kwargs()
