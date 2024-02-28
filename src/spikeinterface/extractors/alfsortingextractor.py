some fixes about the total number of units in ALF sorting object

src/spikeinterface/extractors/alfsortingextractor.py
@@ -46,7 +46,7 @@ class ALFSortingExtractor(BaseSorting):
         self._folder_path = Path(folder_path)
         spikes = alfio.load_object(self._folder_path, "spikes", short_keys=True)
         clusters = alfio.load_object(self._folder_path, "clusters", short_keys=True)
-        total_units = clusters[next(iter(clusters))].size
+        total_units = clusters[next(iter(clusters))].shape[0]
         unit_ids = np.arange(total_units)  # in alf format, spikes.clusters index directly into clusters
         BaseSorting.__init__(self, unit_ids=unit_ids, sampling_frequency=sampling_frequency)
         sorting_segment = ALFSortingSegment(spikes["clusters"], spikes["times"], sampling_frequency)