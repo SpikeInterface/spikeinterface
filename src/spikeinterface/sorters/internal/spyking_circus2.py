32d855e2b6dca889af7869399c8154a9a4e76407
Pierre Yger <pierre.yger@gmail.com>
2024-02-29 09:19:30 +0100

    WIP

--- src/spikeinterface/sorters/internal/spyking_circus2.py
+++ src/spikeinterface/sorters/internal/spyking_circus2.py
@@ -28,7 +28,7 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
     sorter_name = "spykingcircus2"
 
     _default_params = {
-        "general": {"ms_before": 2, "ms_after": 2, "radius_um": 50},
+        "general": {"ms_before": 2, "ms_after": 2, "radius_um": 100},
         "sparsity": {"method": "ptp", "threshold": 0.25},
         "filtering": {"freq_min": 150, "dtype": "float32"},
         "detection": {"peak_sign": "neg", "detect_threshold": 4},
