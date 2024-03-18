2981e07e80c6511d4bd637fdcf3fa28e8bd1831e
Pierre Yger <pierre.yger@gmail.com>
2024-03-18 11:07:27 +0100

    Fix

--- src/spikeinterface/core/numpyextractors.py
+++ src/spikeinterface/core/numpyextractors.py
@@ -457,7 +457,7 @@ class NumpySorting(BaseSorting):
         spikes["sample_index"] = peaks["sample_index"]
         spikes["unit_index"] = peaks["channel_index"]
         spikes["segment_index"] = peaks["segment_index"]
-        order = np.argsort(spikes["sample_index"])
+        order = np.lexsort((spikes["segment_index"], spikes["sample_index"]))
         spikes = spikes[order]
         sorting = NumpySorting(spikes, sampling_frequency, unit_ids)
 
