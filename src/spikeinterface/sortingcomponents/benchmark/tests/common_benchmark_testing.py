b65be0d642e90e2e767c201c6fb49c85bbabdace
Sebastien <pierre.yger@gmail.com>
2024-03-21 12:04:50 +0100

    Some more

--- src/spikeinterface/sortingcomponents/benchmark/tests/common_benchmark_testing.py
+++ src/spikeinterface/sortingcomponents/benchmark/tests/common_benchmark_testing.py
@@ -48,7 +48,7 @@ def make_dataset():
             contact_shape_params={"radius": 6},
         ),
         generate_sorting_kwargs=dict(firing_rates=6.0, refractory_period_ms=4.0),
-        noise_kwargs=dict(noise_level=5.0, strategy="on_the_fly"),
+        noise_kwargs=dict(noise_levels=5.0, strategy="on_the_fly"),
         seed=2205,
     )
     return recording, gt_sorting
@@ -187,7 +187,7 @@ def make_drifting_dataset():
         num_channels=probe.contact_ids.size,
         sampling_frequency=sampling_frequency,
         durations=[duration],
-        noise_level=1.0,
+        noise_levels=1.0,
         dtype="float32",
     )
 
