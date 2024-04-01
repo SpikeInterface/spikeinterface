69a17071da3a55194d5457eb37eb6f23bbf47a5b
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-04-01 10:41:35 +0100

    Add test for channel selection

--- src/spikeinterface/preprocessing/tests/test_common_reference.py
+++ src/spikeinterface/preprocessing/tests/test_common_reference.py
@@ -70,6 +70,15 @@ def test_common_reference_channel_slicing(recording):
     # local car
     local_trace = recording_local_car.get_traces(channel_ids=channel_ids)
 
+def test_common_reference_select_channels(recording):
+
+    recording_cmr = common_reference(recording)
+    recording_segment = recording_cmr._recording_segments[0]
+
+    traces_all = recording_segment.get_traces(start_frame=0, end_frame=10, channel_indices=[0,1,2,3])
+    traces_sub = recording_segment.get_traces(start_frame=0, end_frame=10, channel_indices=[1,3])
+    
+    np.all(traces_all[:,[1,3]] == traces_sub)
 
 def test_common_reference_groups(recording):
     original_traces = recording.get_traces()
