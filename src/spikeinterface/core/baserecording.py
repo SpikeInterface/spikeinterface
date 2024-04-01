f5f97dbda7c54aad783022d9758f6b5f1a8be6d3
Pierre Yger <pierre.yger@gmail.com>
2024-04-01 23:14:32 +0200

    Error with fusion

--- src/spikeinterface/core/baserecording.py
+++ src/spikeinterface/core/baserecording.py
@@ -330,28 +330,6 @@ class BaseRecording(BaseRecordingSnippets):
         if return_scaled:
             traces = self._scale_traces(traces, channel_indices)
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-            if not self.has_scaled():
-                if self._dtype.kind == "f":
-                    # here we do not truely have scale but we assume this is scaled
-                    # this helps a lot for simulated data
-                    pass
-                else:
-                    raise ValueError(
-                        "This recording does not support return_scaled=True (need gain_to_uV and offset_"
-                        "to_uV properties)"
-                    )
-            else:
-                gains = self.get_property("gain_to_uV")
-                offsets = self.get_property("offset_to_uV")
-                gains = gains[channel_indices].astype("float32", copy=False)
-                offsets = offsets[channel_indices].astype("float32", copy=False)
-                traces = traces.astype("float32", copy=False) * gains + offsets
->>>>>>> 225d75faf79a711ddd91f905bfdd74fa7db7bebb
-=======
->>>>>>> a5ead7846fd1520b84dc0036291764d4dc3c816f
         return traces
 
     def has_scaled_traces(self) -> bool:
