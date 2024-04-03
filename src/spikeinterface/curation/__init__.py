aef6ef77c9b070747b68c46cdf092947a9a1b1f9
zm711 <92116279+zm711@users.noreply.github.com>
2024-04-03 09:50:49 -0400

    add functions for curation modules

--- src/spikeinterface/curation/__init__.py
+++ src/spikeinterface/curation/__init__.py
@@ -7,8 +7,8 @@ from .auto_merge import get_potential_auto_merge
 
 
 # manual sorting,
-from .curationsorting import CurationSorting
-from .mergeunitssorting import MergeUnitsSorting
-from .splitunitsorting import SplitUnitSorting
+from .curationsorting import CurationSorting, curation_sorting
+from .mergeunitssorting import MergeUnitsSorting, merge_units_sorting
+from .splitunitsorting import SplitUnitSorting, split_unit_sorting
 
 from .sortingview_curation import apply_sortingview_curation
