01ec8ab1f3f008f0df387059fa3a946603308b2c
Sebastien <pierre.yger@gmail.com>
2024-04-09 15:46:07 +0200

    Imports

--- src/spikeinterface/postprocessing/localization_tools.py
+++ src/spikeinterface/postprocessing/localization_tools.py
@@ -11,9 +11,6 @@ try:
 except ImportError:
     HAVE_NUMBA = False
 
-from ..core.template_tools import get_template_extremum_channel, _get_nbefore, _get_dense_templates_array
-
-
 def make_initial_guess_and_bounds(wf_data, local_contact_locations, max_distance_um, initial_z=20):
     # constant for initial guess and bounds
     ind_max = np.argmax(wf_data)
