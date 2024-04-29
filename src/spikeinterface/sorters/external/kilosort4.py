4dde74759066c52eb87a596e3ac9488a6a5c3913
Alessio Buccino <alejoe9187@gmail.com>
2024-04-29 13:36:34 +0200

    Update src/spikeinterface/sorters/external/kilosort4.py
    Co-authored-by: Zach McKenzie <92116279+zm711@users.noreply.github.com>

--- src/spikeinterface/sorters/external/kilosort4.py
+++ src/spikeinterface/sorters/external/kilosort4.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from pathlib import Path
 from typing import Union
-from packaging.version import parse
 
 from ..basesorter import BaseSorter
 from .kilosortbase import KilosortBase
