0af5db1007d203d5fc677a25b120957b00d6ef5d
Alessio Buccino <alejoe9187@gmail.com>
2024-04-30 19:38:33 +0200

    Update src/spikeinterface/generation/template_database.py

--- src/spikeinterface/generation/template_database.py
+++ src/spikeinterface/generation/template_database.py
@@ -5,7 +5,7 @@ import zarr
 def fetch_templates_from_database(dataset="test_templates") -> Templates:
 
     s3_path = f"s3://spikeinterface-template-database/{dataset}/"
-    zarr_group = zarr.open_consolidated(s3_path, storage_options={"anon": False})
+    zarr_group = zarr.open_consolidated(s3_path, storage_options={"anon": True})
 
     templates_object = Templates.from_zarr_group(zarr_group)
 
