ccf297313f57e978501a910ece785066377d14ab
Samuel Garcia <sam.garcia.die@gmail.com>
2024-04-30 22:33:39 +0200

    remove sortingcomponents form test_imports actions

--- .github/import_test.py
+++ .github/import_test.py
@@ -9,7 +9,8 @@ import_statement_list = [
     "import spikeinterface.preprocessing",
     "import spikeinterface.comparison",
     "import spikeinterface.postprocessing",
-    "import spikeinterface.sortingcomponents",
+    # sorting components has too non core import everywhere
+    #"import spikeinterface.sortingcomponents",
     "import spikeinterface.curation",
     "import spikeinterface.exporters",
     "import spikeinterface.widgets",
