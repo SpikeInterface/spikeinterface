26157caee3e3da614a0babc3266761a943c1aa97
Samuel Garcia <sam.garcia.die@gmail.com>
2024-05-24 19:25:39 +0200

    oups

--- src/spikeinterface/postprocessing/isi.py
+++ src/spikeinterface/postprocessing/isi.py
@@ -56,7 +56,7 @@ class ComputeISIHistograms(AnalyzerExtension):
         new_extension_data = dict(isi_histograms=new_isi_hists, bins=new_bins)
         return new_extension_data
 
-    def _run(self):
+    def _run(self, verbose=False):
         isi_histograms, bins = _compute_isi_histograms(self.sorting_analyzer.sorting, **self.params)
         self.data["isi_histograms"] = isi_histograms
         self.data["bins"] = bins
