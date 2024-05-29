597a094ba99293ba24594cb5871d00caed0b14f8
Charlie Windolf <cwindolf95@gmail.com>
2024-05-29 16:06:30 +0100

    Add grid option to Motion with a test

--- src/spikeinterface/sortingcomponents/tests/test_motion_utils.py
+++ src/spikeinterface/sortingcomponents/tests/test_motion_utils.py
@@ -1,9 +1,9 @@
-import pytest
-import numpy as np
 import pickle
-from pathlib import Path
 import shutil
+from pathlib import Path
 
+import numpy as np
+import pytest
 from spikeinterface.sortingcomponents.motion_utils import Motion
 
 if hasattr(pytest, "global_test_folder"):
@@ -27,22 +27,29 @@ def test_Motion():
 
     # serialize with pickle before interpolation fit
     motion2 = pickle.loads(pickle.dumps(motion))
-    assert motion2.interpolator == None
+    assert motion2.interpolator is None
     # serialize with pickle after interpolation fit
     motion.make_interpolators()
+    assert motion2.interpolator is not None
     motion2 = pickle.loads(pickle.dumps(motion))
+    assert motion2.interpolator is not None
 
     # to/from dict
     motion2 = Motion(**motion.to_dict())
     assert motion == motion2
+    assert motion2.interpolator is None
 
     # do interpolate
-    displacement = motion.get_displacement_at_time_and_depth([2, 4.4, 11, ], [120., 80., 150.])
+    displacement = motion.get_displacement_at_time_and_depth([2, 4.4, 11], [120., 80., 150.])
     # print(displacement)
     assert displacement.shape[0] == 3
     # check clip
     assert displacement[2] == 20.
 
+    # interpolate grid
+    displacement = motion.get_displacement_at_time_and_depth([2, 4.4, 11, 15, 19], [150., 80.], grid=True)
+    assert displacement.shape == (2, 5)
+    assert displacement[0, 2] == 20.
 
     # save/load to folder
     folder = cache_folder / "motion_saved"
@@ -51,7 +58,6 @@ def test_Motion():
     motion.save(folder)
     motion2 = Motion.load(folder)
     assert motion == motion2
-
 
 
 if __name__ == "__main__":
