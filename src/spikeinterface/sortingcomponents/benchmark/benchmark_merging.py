f0d83783d08790c5b186ecd18c9d964fe10e3780
Pierre Yger <pierre.yger@gmail.com>
2024-05-30 12:13:22 +0200

    WIP

--- src/spikeinterface/sortingcomponents/benchmark/benchmark_merging.py
+++ src/spikeinterface/sortingcomponents/benchmark/benchmark_merging.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from spikeinterface.sortingcomponents.matching import find_spikes_from_templates
+from spikeinterface.sortingcomponents.merging import merge_spikes
 from spikeinterface.core import NumpySorting
 from spikeinterface.comparison import CollisionGTComparison, compare_sorter_to_ground_truth
 from spikeinterface.widgets import (
@@ -17,16 +17,18 @@ from spikeinterface.core.basesorting import minimum_spike_dtype
 
 class MergingBenchmark(Benchmark):
 
-    def __init__(self, recording, splitted_sorting, params):
+    def __init__(self, recording, splitted_sorting, params, gt_sorting):
         self.recording = recording
         self.splitted_sorting = splitted_sorting
         self.method = params["method"]
-        self.gt_sorting = params["method_kwargs"]["gt_sorting"]
+        self.gt_sorting = gt_sorting
         self.method_kwargs = params["method_kwargs"]
         self.result = {}
 
     def run(self, **job_kwargs):
-        pass
+        self.result['sorting'] = merge_spikes(
+            self.recording, self.splitted_sorting, method=self.method, method_kwargs=self.method_kwargs
+        )
 
     def compute_result(self, **result_params):
         sorting = self.result["sorting"]
@@ -47,5 +49,19 @@ class MergingStudy(BenchmarkStudy):
         dataset_key = self.cases[key]["dataset"]
         recording, gt_sorting = self.datasets[dataset_key]
         params = self.cases[key]["params"]
-        benchmark = MergingBenchmark(recording, gt_sorting, params)
+        init_kwargs = self.cases[key]["init_kwargs"]
+        benchmark = MergingBenchmark(recording, gt_sorting, params, **init_kwargs)
         return benchmark
+
+    def plot_agreements(self, case_keys=None, figsize=(15, 15)):
+        if case_keys is None:
+            case_keys = list(self.cases.keys())
+
+        fig, axs = plt.subplots(ncols=len(case_keys), nrows=1, figsize=figsize, squeeze=False)
+
+        for count, key in enumerate(case_keys):
+            ax = axs[0, count]
+            ax.set_title(self.cases[key]["label"])
+            plot_agreement_matrix(self.get_result(key)["gt_comparison"], ax=ax)
+
+        return fig
