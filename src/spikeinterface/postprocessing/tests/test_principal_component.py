fa7d9e8cd5a60bfd031d80cbe840beb228917f46
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-05-31 08:27:56 -0600

    fix mac test, enable the rest of tests

--- src/spikeinterface/postprocessing/tests/test_principal_component.py
+++ src/spikeinterface/postprocessing/tests/test_principal_component.py
@@ -109,7 +109,7 @@ class PrincipalComponentsExtensionTest(AnalyzerExtensionCommonTestSuite, unittes
             ext.run_for_all_spikes(pc_file2, chunk_size=10000, n_jobs=2)
             all_pc2 = np.load(pc_file2)
 
-            assert np.array_equal(all_pc1, all_pc2)
+            np.testing.assert_almost_equal(all_pc1, all_pc2)
 
     def test_project_new(self):
         from sklearn.decomposition import IncrementalPCA
