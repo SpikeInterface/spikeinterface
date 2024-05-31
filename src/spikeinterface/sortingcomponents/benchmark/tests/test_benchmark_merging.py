8f7e2a02dd3c368a28eb9faef4a6b00b0755f732
Pierre Yger <pierre.yger@gmail.com>
2024-05-31 10:22:18 +0200

    WIP

--- src/spikeinterface/sortingcomponents/benchmark/tests/test_benchmark_merging.py
+++ src/spikeinterface/sortingcomponents/benchmark/tests/test_benchmark_merging.py
@@ -8,7 +8,7 @@ import shutil
 
 from spikeinterface.sortingcomponents.benchmark.tests.common_benchmark_testing import make_dataset, cache_folder
 from spikeinterface.sortingcomponents.benchmark.benchmark_merging import MergingStudy
-from spikeinterface.core.template_tools import get_template_extremum_channel
+from spikeinterface.generation.drift_tools import split_sorting_by_amplitudes
 
 
 @pytest.mark.skip()
@@ -18,64 +18,53 @@ def test_benchmark_clustering():
 
     recording, gt_sorting, gt_analyzer = make_dataset()
 
-    num_spikes = gt_sorting.to_spike_vector().size
-    spike_indices = np.arange(0, num_spikes, 5)
-
     # create study
     study_folder = cache_folder / "study_clustering"
     # datasets = {"toy": (recording, gt_sorting)}
     datasets = {"toy": gt_analyzer}
 
-    peaks = {}
-    for dataset, gt_analyzer in datasets.items():
-
-        # recording, gt_sorting = datasets[dataset]
-
-        # sorting_analyzer = create_sorting_analyzer(gt_sorting, recording, format="memory", sparse=False)
-        # sorting_analyzer.compute(["random_spikes", "templates"])
-        extremum_channel_inds = get_template_extremum_channel(gt_analyzer, outputs="index")
-        spikes = gt_analyzer.sorting.to_spike_vector(extremum_channel_inds=extremum_channel_inds)
-        peaks[dataset] = spikes
+    gt_analyzer.compute(['random_spikes', 'templates', 'spike_amplitudes'])
+    new_sorting_amp, splitted_cells_amp = split_sorting_by_amplitudes(gt_analyzer)
 
     cases = {}
     for method in ["circus", "lussac"]:
         cases[method] = {
             "label": f"{method} on toy",
             "dataset": "toy",
-            "init_kwargs": {"indices": spike_indices, "peaks": peaks["toy"]},
-            "params": {"method": method, "method_kwargs": {}},
+            "init_kwargs": {"gt_sorting": gt_sorting, "splitted_cells": splitted_cells_amp},
+            "params": {"method": method, "splitted_sorting" : new_sorting_amp, "method_kwargs": {}},
         }
 
     if study_folder.exists():
         shutil.rmtree(study_folder)
-    study = ClusteringStudy.create(study_folder, datasets=datasets, cases=cases)
+    study = MergingStudy.create(study_folder, datasets=datasets, cases=cases)
     print(study)
 
     # this study needs analyzer
     # study.create_sorting_analyzer_gt(**job_kwargs)
     study.compute_metrics()
 
-    study = ClusteringStudy(study_folder)
+    study = MergingStudy(study_folder)
 
     # run and result
     study.run(**job_kwargs)
     study.compute_results()
 
     # load study to check persistency
-    study = ClusteringStudy(study_folder)
+    study = MergingStudy(study_folder)
     print(study)
 
     # plots
-    study.plot_performances_vs_snr()
+    # study.plot_performances_vs_snr()
     study.plot_agreements()
-    study.plot_comparison_clustering()
-    study.plot_error_metrics()
-    study.plot_metrics_vs_snr()
-    study.plot_run_times()
-    study.plot_metrics_vs_snr("cosine")
-    study.homogeneity_score(ignore_noise=False)
+    # study.plot_comparison_clustering()
+    # study.plot_error_metrics()
+    # study.plot_metrics_vs_snr()
+    # study.plot_run_times()
+    # study.plot_metrics_vs_snr("cosine")
+    # study.homogeneity_score(ignore_noise=False)
     plt.show()
 
 
 if __name__ == "__main__":
-    test_benchmark_clustering()
+    test_benchmark_merging()
