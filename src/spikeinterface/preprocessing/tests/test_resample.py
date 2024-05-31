9c98bcb15fc0d978c64a98c5a3b6deb92793ffa2
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-05-31 07:34:17 -0600

    even more removals

--- src/spikeinterface/preprocessing/tests/test_resample.py
+++ src/spikeinterface/preprocessing/tests/test_resample.py
@@ -1,13 +1,12 @@
 import pytest
 from pathlib import Path
 
-from spikeinterface import NumpyRecording, set_global_tmp_folder
-from spikeinterface.core import generate_recording
+
 from spikeinterface.preprocessing import resample
+from spikeinterface.core import NumpyRecording
 
 
 import numpy as np
-from scipy.fft import fft, fftfreq
 
 if hasattr(pytest, "global_test_folder"):
     cache_folder = pytest.global_test_folder / "preprocessing"
@@ -72,6 +71,8 @@ def create_sinusoidal_traces(sampling_frequency=3e4, duration=30, freqs_n=10, ma
 
 
 def get_fft(traces, sampling_frequency):
+    from scipy.fft import fft, fftfreq
+
     # Return the power spectrum of the positive fft
     N = len(traces)
     yf = fft(traces)
