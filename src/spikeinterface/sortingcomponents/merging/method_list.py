48329ebdb8e91ef525d48d5bd3e1c7e4337dd4ab
Pierre Yger <pierre.yger@gmail.com>
2024-05-31 11:56:34 +0200

    Removing lussac imports

--- src/spikeinterface/sortingcomponents/merging/method_list.py
+++ src/spikeinterface/sortingcomponents/merging/method_list.py
@@ -1,19 +1,6 @@
 from __future__ import annotations
 from .circus import CircusMerging
+from .lussac import LussacMerging
 from .drift import DriftMerging
 
-
-merging_methods = {"circus": CircusMerging, "drift": DriftMerging}
-
-
-try:
-    import lussac.utils as utils
-
-    HAVE_LUSSAC = True
-except Exception:
-    HAVE_LUSSAC = False
-
-if HAVE_LUSSAC:
-    from .lussac import LussacMerging
-
-    merging_methods = {"lussac": LussacMerging}
+merging_methods = {"circus": CircusMerging, "drift": DriftMerging, "lussac": LussacMerging}
