0fde0d2eabd992e42a2b4ca4f8455dca89fb3766
Nina Kudryashova <nkudryas@ed.ac.uk>
2024-06-03 11:48:40 +0100

    Run black locally

--- src/spikeinterface/extractors/sinapsrecordingextractor.py
+++ src/spikeinterface/extractors/sinapsrecordingextractor.py
@@ -6,6 +6,7 @@ from probeinterface import get_probe
 from ..core import BinaryRecordingExtractor, ChannelSliceRecording
 from ..core.core_tools import define_function_from_class
 
+
 class SinapsResearchPlatformRecordingExtractor(ChannelSliceRecording):
     extractor_name = "SinapsResearchPlatform"
     mode = "file"
@@ -23,7 +24,7 @@ class SinapsResearchPlatformRecordingExtractor(ChannelSliceRecording):
         num_electrodes = meta["nbElectrodes"]
         sampling_frequency = meta["samplingFreq"]
 
-        probe_type = meta['probeType']
+        probe_type = meta["probeType"]
         # channel_locations = meta["electrodePhysicalPosition"] # will be depricated soon by Sam, switching to probeinterface
         num_shanks = meta["nbShanks"]
         num_electrodes_per_shank = meta["nbElectrodesShank"]
@@ -66,29 +67,30 @@ class SinapsResearchPlatformRecordingExtractor(ChannelSliceRecording):
 
         ChannelSliceRecording.__init__(self, recording, channel_ids=channel_slice, renamed_channel_ids=renamed_channels)
         # if locations is not None:
-            # self.set_channel_locations(locations)
+        # self.set_channel_locations(locations)
         # if groups is not None:
-            # self.set_channel_groups(groups)
-        
+        # self.set_channel_groups(groups)
+
         self.set_channel_gains(gain)
         self.set_channel_offsets(0)
 
-        if (stream_name == 'filt') | (stream_name == 'raw'):
-            if (probe_type == 'p1024s1NHP'):
-                probe = get_probe(manufacturer='sinaps',
-                                probe_name='SiNAPS-p1024s1NHP')
+        if (stream_name == "filt") | (stream_name == "raw"):
+            if probe_type == "p1024s1NHP":
+                probe = get_probe(manufacturer="sinaps", probe_name="SiNAPS-p1024s1NHP")
                 # now wire the probe
                 channel_indices = np.arange(1024)
                 probe.set_device_channel_indices(channel_indices)
-                self.set_probe(probe,in_place=True)
+                self.set_probe(probe, in_place=True)
             else:
                 raise ValueError(f"Unknown probe type: {probe_type}")
-            
+
         self._kwargs = {"file_path": str(file_path.absolute())}
+
 
 read_sinaps_research_platform = define_function_from_class(
     source_class=SinapsResearchPlatformRecordingExtractor, name="read_sinaps_research_platform"
 )
+
 
 def parse_sinaps_meta(meta_file):
     meta_dict = {}
--- src/spikeinterface/extractors/sinapsrecordingh5extractor.py
+++ src/spikeinterface/extractors/sinapsrecordingh5extractor.py
@@ -17,6 +17,7 @@ class SinapsResearchPlatformH5RecordingExtractor_Unsigned(BaseRecording):
 
         try:
             import h5py
+
             self.installed = True
         except ImportError:
             self.installed = False
@@ -47,36 +48,34 @@ class SinapsResearchPlatformH5RecordingExtractor_Unsigned(BaseRecording):
         self.num_bits = sinaps_info["num_bits"]
 
         # set probe
-        if sinaps_info['probe_type'] == 'p1024s1NHP':
-            probe = get_probe(manufacturer='sinaps',
-                            probe_name='SiNAPS-p1024s1NHP')
+        if sinaps_info["probe_type"] == "p1024s1NHP":
+            probe = get_probe(manufacturer="sinaps", probe_name="SiNAPS-p1024s1NHP")
             probe.set_device_channel_indices(np.arange(1024))
             self.set_probe(probe, in_place=True)
         else:
             raise ValueError(f"Unknown probe type: {sinaps_info['probe_type']}")
-    
 
         # set other properties
 
         self._kwargs = {"file_path": str(Path(file_path).absolute())}
 
-
     def __del__(self):
         self._rf.close()
+
 
 class SiNAPSRecordingSegment(BaseRecordingSegment):
     def __init__(self, rf, num_frames, sampling_frequency):
         BaseRecordingSegment.__init__(self, sampling_frequency=sampling_frequency)
         self._rf = rf
         self._num_samples = int(num_frames)
-        self._stream = self._rf.require_group('RealTimeProcessedData')
+        self._stream = self._rf.require_group("RealTimeProcessedData")
 
     def get_num_samples(self):
         return self._num_samples
 
     def get_traces(self, start_frame=None, end_frame=None, channel_indices=None):
         if isinstance(channel_indices, slice):
-            traces = self._stream.get('FilteredData')[channel_indices, start_frame:end_frame].T
+            traces = self._stream.get("FilteredData")[channel_indices, start_frame:end_frame].T
         else:
             # channel_indices is np.ndarray
             if np.array(channel_indices).size > 1 and np.any(np.diff(channel_indices) < 0):
@@ -84,11 +83,12 @@ class SiNAPSRecordingSegment(BaseRecordingSegment):
                 # to be indexed out of order
                 sorted_channel_indices = np.sort(channel_indices)
                 resorted_indices = np.array([list(sorted_channel_indices).index(ch) for ch in channel_indices])
-                recordings = self._stream.get('FilteredData')[sorted_channel_indices, start_frame:end_frame].T
+                recordings = self._stream.get("FilteredData")[sorted_channel_indices, start_frame:end_frame].T
                 traces = recordings[:, resorted_indices]
             else:
-                traces = self._stream.get('FilteredData')[channel_indices, start_frame:end_frame].T
+                traces = self._stream.get("FilteredData")[channel_indices, start_frame:end_frame].T
         return traces
+
 
 class SinapsResearchPlatformH5RecordingExtractor(UnsignedToSignedRecording):
     extractor_name = "SinapsResearchPlatformH5"
@@ -109,25 +109,29 @@ read_sinaps_research_platform_h5 = define_function_from_class(
 
 def openSiNAPSFile(filename):
     """Open an SiNAPS hdf5 file, read and return the recording info."""
-    
+
     import h5py
 
     rf = h5py.File(filename, "r")
 
-    stream = rf.require_group('RealTimeProcessedData')
+    stream = rf.require_group("RealTimeProcessedData")
     data = stream.get("FilteredData")
     dtype = data.dtype
 
-    parameters = rf.require_group('Parameters')
-    gain = parameters.get('VoltageConverter')[0]
+    parameters = rf.require_group("Parameters")
+    gain = parameters.get("VoltageConverter")[0]
     offset = 0
 
     nRecCh, nFrames = data.shape
 
-    samplingRate = parameters.get('SamplingFrequency')[0]
+    samplingRate = parameters.get("SamplingFrequency")[0]
 
-    probe_type = str(rf.require_group('Advanced Recording Parameters').require_group('Probe').get('probeType').asstr()[...])
-    num_bits = int(np.log2(rf.require_group('Advanced Recording Parameters').require_group('DAQ').get('nbADCLevels')[0]))
+    probe_type = str(
+        rf.require_group("Advanced Recording Parameters").require_group("Probe").get("probeType").asstr()[...]
+    )
+    num_bits = int(
+        np.log2(rf.require_group("Advanced Recording Parameters").require_group("DAQ").get("nbADCLevels")[0])
+    )
 
     sinaps_info = {
         "filehandle": rf,
