9d296b49a6bf5576750eb8718c363c80bf786c16
JoeZiminski <Joseph.j.ziminski@gmail.com>
2024-06-06 00:52:08 +0100

    Add docstring and some small improvement to test_principal_component.py

--- src/spikeinterface/postprocessing/tests/test_principal_component.py
+++ src/spikeinterface/postprocessing/tests/test_principal_component.py
@@ -18,7 +18,12 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
         self.run_extension_tests(ComputePrincipalComponents, params=params)
 
     def test_mode_concatenated(self):
-        # this is tested outside "extension_function_params_list" because it do not support sparsity!
+        """
+        Replicate the "extension_function_params_list" test outside of
+        AnalyzerExtensionCommonTestSuite because it does not support sparsity.
+
+        Also, add two additional checks on the dimension and n components of the output.
+        """
         sorting_analyzer = self._prepare_sorting_analyzer(
             format="memory", sparse=False, extension_class=ComputePrincipalComponents
         )
@@ -34,7 +39,13 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
 
     @pytest.mark.parametrize("sparse", [True, False])
     def test_get_projections(self, sparse):
-
+        """
+        Test the shape of output projection score matrices are
+        correct when adjusting sparsity and using the
+        `get_some_projections()` function. We expect them
+        to hold, for each spike and each channel, the loading
+        for each of the specified number of components.
+        """
         sorting_analyzer = self._prepare_sorting_analyzer(
             format="memory", sparse=sparse, extension_class=ComputePrincipalComponents
         )
@@ -44,6 +55,8 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
         sorting_analyzer.compute("principal_components", mode="by_channel_global", n_components=n_components)
         ext = sorting_analyzer.get_extension("principal_components")
 
+        # First, check the created projections have the expected number
+        # of components and the expected number of channels based on sparsity.
         for unit_id in sorting_analyzer.unit_ids:
             if not sparse:
                 one_proj = ext.get_projections_one_unit(unit_id, sparse=False)
@@ -56,13 +69,19 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
 
                 one_proj, chan_inds = ext.get_projections_one_unit(unit_id, sparse=True)
                 assert one_proj.shape[1] == n_components
-                assert one_proj.shape[2] < num_chans
+                num_channels_for_unit = sorting_analyzer.sparsity.unit_id_to_channel_ids[unit_id].size
+                assert one_proj.shape[2] == num_channels_for_unit
                 assert one_proj.shape[2] == chan_inds.size
 
+        # Next, check that the `get_some_projections()` function returns
+        # projections with the expected shapes when selecting subjsets
+        # of channel and unit IDs.
         some_unit_ids = sorting_analyzer.unit_ids[::2]
         some_channel_ids = sorting_analyzer.channel_ids[::2]
 
         random_spikes_indices = sorting_analyzer.get_extension("random_spikes").get_data()
+        all_num_spikes = sorting_analyzer.sorting.get_total_num_spikes()
+        unit_ids_num_spikes = np.sum(all_num_spikes[unit_id] for unit_id in some_unit_ids)
 
         # this should be all spikes all channels
         some_projections, spike_unit_index = ext.get_some_projections(channel_ids=None, unit_ids=None)
@@ -74,7 +93,7 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
         # this should be some spikes all channels
         some_projections, spike_unit_index = ext.get_some_projections(channel_ids=None, unit_ids=some_unit_ids)
         assert some_projections.shape[0] == spike_unit_index.shape[0]
-        assert spike_unit_index.shape[0] < random_spikes_indices.size
+        assert spike_unit_index.shape[0] == unit_ids_num_spikes
         assert some_projections.shape[1] == n_components
         assert some_projections.shape[2] == num_chans
         assert 1 not in spike_unit_index
@@ -84,14 +103,19 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
             channel_ids=some_channel_ids, unit_ids=some_unit_ids
         )
         assert some_projections.shape[0] == spike_unit_index.shape[0]
-        assert spike_unit_index.shape[0] < random_spikes_indices.size
+        assert spike_unit_index.shape[0] == unit_ids_num_spikes
         assert some_projections.shape[1] == n_components
         assert some_projections.shape[2] == some_channel_ids.size
         assert 1 not in spike_unit_index
 
     @pytest.mark.parametrize("sparse", [True, False])
     def test_compute_for_all_spikes(self, sparse):
-
+        """
+        Compute the principal component scores, checking the shape
+        matches the number of spikes as expected. This is re-run
+        with n_jobs=2 and output projection score matrices
+        checked against n_jobs=1.
+        """
         sorting_analyzer = self._prepare_sorting_analyzer(
             format="memory", sparse=sparse, extension_class=ComputePrincipalComponents
         )
@@ -114,7 +138,16 @@ class TestPrincipalComponentsExtension(AnalyzerExtensionCommonTestSuite):
         assert np.array_equal(all_pc1, all_pc2)
 
     def test_project_new(self):
+        """
+        `project_new` projects new (unseen) waveforms onto the PCA components.
+        First compute principal components from existing waveforms. Then,
+        generate a new 'spikes' vector that includes sample_index, unit_index
+        and segment_index alongside some waveforms (the spike vector is required
+        to generate some corresponding unit IDs for the generated waveforms following
+        the API of principal_components.py).
 
+        Then, check that the new projection scores matrix is the expected shape.
+        """
         sorting_analyzer = self._prepare_sorting_analyzer(
             format="memory", sparse=False, extension_class=ComputePrincipalComponents
         )
