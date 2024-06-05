bcf87d4690eee2b86e4e88e063ad912cc7655223
JoeZiminski <Joseph.j.ziminski@gmail.com>
2024-06-05 19:32:11 +0100

    test_isi.py to parameterised method.

--- src/spikeinterface/postprocessing/tests/test_isi.py
+++ src/spikeinterface/postprocessing/tests/test_isi.py
@@ -6,7 +6,7 @@ from typing import List
 from spikeinterface.postprocessing.tests.common_extension_tests import AnalyzerExtensionCommonTestSuite
 from spikeinterface.postprocessing import compute_isi_histograms, ComputeISIHistograms
 from spikeinterface.postprocessing.isi import _compute_isi_histograms
-
+import pytest
 
 try:
     import numba
@@ -17,30 +17,37 @@ except ModuleNotFoundError as err:
 
 
 class TestComputeISIHistograms(AnalyzerExtensionCommonTestSuite):
-    extension_class = ComputeISIHistograms
-    extension_function_params_list = [
-        dict(method="numpy"),
-        dict(method="auto"),
-    ]
-    if HAVE_NUMBA:
-        extension_function_params_list.append(dict(method="numba"))
+
+    @pytest.mark.parametrize(
+        "params",
+        [
+            dict(method="numpy"),
+            dict(method="auto"),
+            pytest.param(dict(method="numba"), marks=pytest.mark.skipif("not HAVE_NUMBA")),
+        ],
+    )
+    def test_extension(self, params):
+        self.run_extension_tests(ComputeISIHistograms, params)
 
     def test_compute_ISI(self):
+        """
+        Requires as list because everything tested against Numpy.
+        But numpy is not tested against anything.
+        """
         methods = ["numpy", "auto"]
         if HAVE_NUMBA:
             methods.append("numba")
 
-        _test_ISI(self.sorting, window_ms=60.0, bin_ms=1.0, methods=methods)
-        _test_ISI(self.sorting, window_ms=43.57, bin_ms=1.6421, methods=methods)
+        self._test_ISI(self.sorting, window_ms=60.0, bin_ms=1.0, methods=methods)
+        self._test_ISI(self.sorting, window_ms=43.57, bin_ms=1.6421, methods=methods)
 
+    def _test_ISI(self, sorting, window_ms: float, bin_ms: float, methods: List[str]):
+        for method in methods:
+            ISI, bins = _compute_isi_histograms(sorting, window_ms=window_ms, bin_ms=bin_ms, method=method)
 
-def _test_ISI(sorting, window_ms: float, bin_ms: float, methods: List[str]):
-    for method in methods:
-        ISI, bins = _compute_isi_histograms(sorting, window_ms=window_ms, bin_ms=bin_ms, method=method)
-
-        if method == "numpy":
-            ref_ISI = ISI
-            ref_bins = bins
-        else:
-            assert np.all(ISI == ref_ISI), f"Failed with method={method}"
-            assert np.allclose(bins, ref_bins, atol=1e-10), f"Failed with method={method}"
+            if method == "numpy":
+                ref_ISI = ISI
+                ref_bins = bins
+            else:
+                assert np.all(ISI == ref_ISI), f"Failed with method={method}"
+                assert np.allclose(bins, ref_bins, atol=1e-10), f"Failed with method={method}"
