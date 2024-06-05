9d8a699324233e451105da30ad48715713375277
Pierre Yger <pierre.yger@gmail.com>
2024-06-05 21:03:30 +0200

    Adding possibility to only split cells given SNR

--- src/spikeinterface/generation/drift_tools.py
+++ src/spikeinterface/generation/drift_tools.py
@@ -516,22 +516,36 @@ class InjectDriftingTemplatesRecordingSegment(BaseRecordingSegment):
         return self.num_samples
 
 
-def split_sorting_by_times(sorting_or_sorting_analyzer, splitting_probability=0.5, partial_split_prob=0.95, seed=None):
-
-    if isinstance(sorting_or_sorting_analyzer, SortingAnalyzer):
-        sorting = sorting_or_sorting_analyzer.sorting
-    else:
-        sorting = sorting_or_sorting_analyzer
-
+def split_sorting_by_times(sorting_analyzer, 
+                           splitting_probability=0.5, 
+                           partial_split_prob=0.95, 
+                           unit_ids=None,
+                           min_snr=None,
+                           seed=None):
+    sa = sorting_analyzer
+    sorting = sa.sorting
     rng = np.random.RandomState(seed)
 
     sorting_split = sorting.select_units(sorting.unit_ids)
     split_units = []
     original_units = []
     nb_splits = int(splitting_probability * len(sorting.unit_ids))
-    to_split_ids = rng.choice(sorting.unit_ids, nb_splits, replace=False)
-    import spikeinterface.curation as scur
+    if unit_ids is None:
+        select_from = sorting.unit_ids
+        if min_snr is not None:
+            if sa.get_extension("noise_levels") is None:
+                sa.compute("noise_levels")
+            if sa.get_extension("quality_metrics") is None:
+                sa.compute('quality_metrics', metric_names=['snr'])
+            
+            snr = sa.get_extension('quality_metrics').get_data()['snr'].values
+            select_from = select_from[snr > min_snr]
+    
+        to_split_ids = rng.choice(select_from, nb_splits, replace=False)
+    else:
+        to_split_ids = unit_ids
 
+    import spikeinterface.curation as scur
     for unit in to_split_ids:
         num_spikes = len(sorting_split.get_unit_spike_train(unit))
         indices = np.zeros(num_spikes, dtype=int)
@@ -545,16 +559,20 @@ def split_sorting_by_times(sorting_or_sorting_analyzer, splitting_probability=0.
     return sorting_split, split_units
 
 
-def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, seed=None):
+def split_sorting_by_amplitudes(sorting_analyzer, 
+                                splitting_probability=0.5, 
+                                unit_ids=None,
+                                min_snr=None,
+                                seed=None):
     """
     Fonction used to split a sorting based on the amplitudes of the units. This
     might be used for benchmarking meta merging step (see components)
     """
 
-    if sorting_analyzer.get_extension("spike_amplitudes") is None:
-        sorting_analyzer.compute("spike_amplitudes")
-
     sa = sorting_analyzer
+    if sa.get_extension("spike_amplitudes") is None:
+        sa.compute("spike_amplitudes")
+
     rng = np.random.RandomState(seed)
 
     from spikeinterface.core.numpyextractors import NumpySorting
@@ -565,7 +583,21 @@ def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, see
     new_spikes = spikes.copy()
     amplitudes = sa.get_extension("spike_amplitudes").get_data()
     nb_splits = int(splitting_probability * len(sa.sorting.unit_ids))
-    to_split_ids = rng.choice(sa.sorting.unit_ids, nb_splits, replace=False)
+
+    if unit_ids is None:
+        select_from = sa.sorting.unit_ids
+        if min_snr is not None:
+            if sa.get_extension("noise_levels") is None:
+                sa.compute("noise_levels")
+            if sa.get_extension("quality_metrics") is None:
+                sa.compute('quality_metrics', metric_names=['snr'])
+            
+            snr = sa.get_extension('quality_metrics').get_data()['snr'].values
+            select_from = select_from[snr > min_snr]
+        to_split_ids = rng.choice(select_from, nb_splits, replace=False)
+    else:
+        to_split_ids = unit_ids
+
     max_index = np.max(spikes["unit_index"])
     new_unit_ids = list(sa.sorting.unit_ids.copy())
     splitted_pairs = []
