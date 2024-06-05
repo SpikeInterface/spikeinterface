c88f470e3ff9239a5716f912e6be02d9b695630b
JoeZiminski <Joseph.j.ziminski@gmail.com>
2024-06-05 20:05:01 +0100

    test_template_metrics.py a parametrized method.

--- src/spikeinterface/postprocessing/tests/test_template_metrics.py
+++ src/spikeinterface/postprocessing/tests/test_template_metrics.py
@@ -3,12 +3,18 @@ import unittest
 
 from spikeinterface.postprocessing.tests.common_extension_tests import AnalyzerExtensionCommonTestSuite
 from spikeinterface.postprocessing import ComputeTemplateMetrics
+import pytest
 
 
 class TestTemplateMetrics(AnalyzerExtensionCommonTestSuite):
-    extension_class = ComputeTemplateMetrics
-    extension_function_params_list = [
-        dict(),
-        dict(upsampling_factor=2),
-        dict(include_multi_channel_metrics=True),
-    ]
+
+    @pytest.mark.parametrize(
+        "params",
+        [
+            dict(),
+            dict(upsampling_factor=2),
+            dict(include_multi_channel_metrics=True),
+        ],
+    )
+    def test_extension(self, params):
+        self.run_extension_tests(ComputeTemplateMetrics, params)
