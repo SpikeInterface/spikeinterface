2ca41e9c34039a92275ed252cc970aa76408fa90
JoeZiminski <Joseph.j.ziminski@gmail.com>
2024-06-05 20:16:34 +0100

    test_template_similarity.py to parametrized method.

--- src/spikeinterface/postprocessing/tests/test_template_similarity.py
+++ src/spikeinterface/postprocessing/tests/test_template_similarity.py
@@ -10,27 +10,23 @@ from spikeinterface.postprocessing import check_equal_template_with_distribution
 
 
 class TestSimilarityExtension(AnalyzerExtensionCommonTestSuite):
-    extension_class = ComputeTemplateSimilarity
-    extension_function_params_list = [
-        dict(method="cosine_similarity"),
-    ]
 
+    def test_extension(self):
+        self.run_extension_tests(ComputeTemplateSimilarity, params=dict(method="cosine_similarity"))
 
-def test_check_equal_template_with_distribution_overlap():
+    def test_check_equal_template_with_distribution_overlap(self):
 
-    recording, sorting = get_dataset()
+        sorting_analyzer = self._prepare_sorting_analyzer("memory", None, ComputeTemplateSimilarity)
+        sorting_analyzer.compute("random_spikes")
+        sorting_analyzer.compute("waveforms")
+        sorting_analyzer.compute("templates")
 
-    sorting_analyzer = get_sorting_analyzer(recording, sorting, sparsity=None)
-    sorting_analyzer.compute("random_spikes")
-    sorting_analyzer.compute("waveforms")
-    sorting_analyzer.compute("templates")
+        wf_ext = sorting_analyzer.get_extension("waveforms")
 
-    wf_ext = sorting_analyzer.get_extension("waveforms")
-
-    for unit_id0 in sorting_analyzer.unit_ids:
-        waveforms0 = wf_ext.get_waveforms_one_unit(unit_id0)
-        for unit_id1 in sorting_analyzer.unit_ids:
-            if unit_id0 == unit_id1:
-                continue
-            waveforms1 = wf_ext.get_waveforms_one_unit(unit_id1)
-            check_equal_template_with_distribution_overlap(waveforms0, waveforms1)
+        for unit_id0 in sorting_analyzer.unit_ids:
+            waveforms0 = wf_ext.get_waveforms_one_unit(unit_id0)
+            for unit_id1 in sorting_analyzer.unit_ids:
+                if unit_id0 == unit_id1:
+                    continue
+                waveforms1 = wf_ext.get_waveforms_one_unit(unit_id1)
+                check_equal_template_with_distribution_overlap(waveforms0, waveforms1)
