476fc31eb2b7d112424e1d6e2104ef0318751080
Pierre Yger <pierre.yger@gmail.com>
2024-06-13 13:15:58 +0200

    Useless imports

--- src/spikeinterface/sortingcomponents/merging/circus.py
+++ src/spikeinterface/sortingcomponents/merging/circus.py
@@ -5,7 +5,6 @@ from .main import BaseMergingEngine
 from spikeinterface.core.sortinganalyzer import create_sorting_analyzer
 from spikeinterface.core.analyzer_extension_core import ComputeTemplates
 from spikeinterface.curation.auto_merge import get_potential_auto_merge
-from spikeinterface.curation.merge_temporal_splits import get_potential_temporal_splits
 from spikeinterface.sortingcomponents.merging.tools import resolve_merging_graph, apply_merges_to_sorting
 
 
