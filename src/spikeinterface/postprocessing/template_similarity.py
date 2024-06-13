5afcf9b0d958e36fca0d5102bb1ca9f230ac8e33
Sebastien <pierre.yger@gmail.com>
2024-06-13 16:02:07 +0200

    union as default

--- src/spikeinterface/postprocessing/template_similarity.py
+++ src/spikeinterface/postprocessing/template_similarity.py
@@ -20,7 +20,7 @@ class ComputeTemplateSimilarity(AnalyzerExtension):
         The method to compute the similarity. Can be in ["cosine", "l2", "l1"]
     max_lag_ms : float, default 0
         If specified, the best distance for all given lag within max_lag_ms is kept, for every template
-    support : str, default "dense"
+    support : str, default "union"
         Support that should be considered to compute the distances between the templates, given their sparsities.
         Can be either ["dense", "union", "intersection"]
 
@@ -45,7 +45,7 @@ class ComputeTemplateSimilarity(AnalyzerExtension):
     def __init__(self, sorting_analyzer):
         AnalyzerExtension.__init__(self, sorting_analyzer)
 
-    def _set_params(self, method="cosine", max_lag_ms=0, support="dense"):
+    def _set_params(self, method="cosine", max_lag_ms=0, support="union"):
         params = dict(method=method, max_lag_ms=max_lag_ms, support=support)
         return params
 
