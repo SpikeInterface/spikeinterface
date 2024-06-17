da99a89c873d216cfb15c760d07ca0b19d4f79dc
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-06-17 16:33:05 -0600

    Update src/spikeinterface/core/core_tools.py
    Co-authored-by: Zach McKenzie <92116279+zm711@users.noreply.github.com>

--- src/spikeinterface/core/core_tools.py
+++ src/spikeinterface/core/core_tools.py
@@ -87,7 +87,7 @@ class SIJsonEncoder(json.JSONEncoder):
         if isinstance(obj, np.dtype):
             return np.dtype(obj).name
 
-        # This will transform to a sring canonical representation of the dtype (e.g. np.int32 -> 'int32')
+        # This will transform to a string canonical representation of the dtype (e.g. np.int32 -> 'int32')
 
         if isinstance(obj, type) and issubclass(obj, np.generic):
             return np.dtype(obj).name
