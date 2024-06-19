3784c3898ee521b057bc3962fb66e2eecc561cfe
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-06-19 15:08:26 -0600

    use the value instead of book

--- src/spikeinterface/core/base.py
+++ src/spikeinterface/core/base.py
@@ -56,7 +56,7 @@ class BaseExtractor:
 
         # "main_ids" will either be channel_ids or units_ids
         # They are used for properties
-        self._main_ids = np.array(main_ids)
+        self._main_ids = np.asarray(main_ids)
         if len(self._main_ids) > 0:
             assert (
                 self._main_ids.dtype.kind in "uiSU"
@@ -128,8 +128,18 @@ class BaseExtractor:
                 indices = np.arange(len(self._main_ids))
         else:
             assert isinstance(ids, (list, np.ndarray, tuple)), "'ids' must be a list, np.ndarray or tuple"
+
+            non_existent_ids = [id for id in ids if id not in self._main_ids]
+            if non_existent_ids:
+                error_msg = (
+                    f"IDs {non_existent_ids} are not channel ids of the extractor. \n"
+                    f"Available ids are {self._main_ids} with dtype {self._main_ids.dtype}"
+                )
+                raise ValueError(error_msg)
+
             _main_ids = self._main_ids.tolist()
-            indices = np.array([_main_ids.index(id) for id in ids], dtype=int)
+            indices = np.array([_main_ids.index(id) for id in ids], dtype=np.int)
+
             if prefer_slice:
                 if np.all(np.diff(indices) == 1):
                     indices = slice(indices[0], indices[-1] + 1)
