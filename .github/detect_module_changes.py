d22344caad4246aea25d50e8592b741765b1d8f7
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-06-25 13:51:53 -0600

    another attempt

--- .github/detect_module_changes.py
+++ .github/detect_module_changes.py
@@ -2,29 +2,29 @@ import os
 import pathlib
 
 changes = {
-    'CORE_CHANGED': ['pyproject.toml', '/core/', '/extractors/neoextractors/neobaseextractor.py'],
-    'EXTRACTORS_CHANGED': ['/extractors/'],
-    'PLEXON2_CHANGED': ['plexon2'],
-    'PREPROCESSING_CHANGED': ['/preprocessing/'],
-    'POSTPROCESSING_CHANGED': ['/postprocessing/'],
-    'QUALITYMETRICS_CHANGED': ['/qualitymetrics/'],
-    'SORTERS_CHANGED': ['/sorters/'],
-    'SORTERS_EXTERNAL_CHANGED': ['/sorters/external'],
-    'SORTERS_INTERNAL_CHANGED': ['/sorters/internal'],
-    'COMPARISON_CHANGED': ['/comparison/'],
-    'CURATION_CHANGED': ['/curation/'],
-    'WIDGETS_CHANGED': ['/widgets/'],
-    'EXPORTERS_CHANGED': ['/exporters/'],
-    'SORTINGCOMPONENTS_CHANGED': ['/sortingcomponents/'],
-    'GENERATION_CHANGED': ['/generation/'],
+    "CORE_CHANGED": ["pyproject.toml", "/core/", "/extractors/neoextractors/neobaseextractor.py"],
+    "EXTRACTORS_CHANGED": ["/extractors/"],
+    "PLEXON2_CHANGED": ["plexon2"],
+    "PREPROCESSING_CHANGED": ["/preprocessing/"],
+    "POSTPROCESSING_CHANGED": ["/postprocessing/"],
+    "QUALITYMETRICS_CHANGED": ["/qualitymetrics/"],
+    "SORTERS_CHANGED": ["/sorters/"],
+    "SORTERS_EXTERNAL_CHANGED": ["/sorters/external"],
+    "SORTERS_INTERNAL_CHANGED": ["/sorters/internal"],
+    "COMPARISON_CHANGED": ["/comparison/"],
+    "CURATION_CHANGED": ["/curation/"],
+    "WIDGETS_CHANGED": ["/widgets/"],
+    "EXPORTERS_CHANGED": ["/exporters/"],
+    "SORTINGCOMPONENTS_CHANGED": ["/sortingcomponents/"],
+    "GENERATION_CHANGED": ["/generation/"],
 }
 
-changed_files = os.getenv('CHANGED_FILES', '').split()
+changed_files = os.getenv("CHANGED_FILES", "").split()
 
 for change, patterns in changes.items():
     for file in changed_files:
         if any(pathlib.PurePath(file).match(pattern) for pattern in patterns):
-            print(f'{change}=true')
-            with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
-                env_file.write(f'{change}=true\n')
+            print(f"{change}=true")
+            with open(os.getenv("GITHUB_OUTPUT"), "a") as env_file:
+                env_file.write(f"{change}=true\n")
             break
