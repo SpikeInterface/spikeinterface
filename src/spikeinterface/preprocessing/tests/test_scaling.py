62485d5ee5c9dbb30085b3eaad9ea07a448d2ace
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-06-27 11:05:50 -0600

    add failing test

--- src/spikeinterface/preprocessing/tests/test_scaling.py
+++ src/spikeinterface/preprocessing/tests/test_scaling.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 from spikeinterface.core.testing_tools import generate_recording
-from spikeinterface.preprocessing import ScaleTouVRecording
+from spikeinterface.preprocessing import ScaleTouVRecording, CenterRecording
 
 
 def test_scale_to_uv():
@@ -34,3 +34,37 @@ def test_scale_to_uv():
     recording.set_channel_gains(None)  # Remove gains to make traces unscaleable
     with pytest.raises(RuntimeError):
         ScaleTouVRecording(recording)
+
+
+def test_scaling_in_preprocessing_chain():
+
+    # Create a sample recording extractor with fake gains and offsets
+    num_channels = 4
+    sampling_frequency = 30_000.0
+    durations = [1.0]  # seconds
+    recording = generate_recording(
+        num_channels=num_channels,
+        durations=durations,
+        sampling_frequency=sampling_frequency,
+    )
+
+    rng = np.random.default_rng(0)
+    gains = rng.random(size=(num_channels)).astype(np.float32)
+    offsets = rng.random(size=(num_channels)).astype(np.float32)
+
+    recording.set_channel_gains(gains)
+    recording.set_channel_offsets(offsets)
+
+    centered_recording = CenterRecording(ScaleTouVRecording(recording=recording))
+    traces_scaled_with_argument = centered_recording.get_traces(return_scaled=True)
+
+    # Chain preprocessors
+    centered_recording_scaled = CenterRecording(ScaleTouVRecording(recording=recording))
+    traces_scaled_with_preprocessor = centered_recording_scaled.get_traces()
+
+    np.testing.assert_allclose(traces_scaled_with_argument, traces_scaled_with_preprocessor)
+
+    # Test if the scaling is not done twice
+    traces_scaled_with_preprocessor_and_argument = centered_recording_scaled.get_traces(return_scaled=True)
+
+    np.testing.assert_allclose(traces_scaled_with_preprocessor, traces_scaled_with_preprocessor_and_argument)
