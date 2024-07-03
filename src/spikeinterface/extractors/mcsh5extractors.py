6391d433d44cf25b698c709da9ebf334ae9862be
Pierre Yger <pierre.yger@gmail.com>
2024-07-03 14:27:29 +0200

    Update src/spikeinterface/extractors/mcsh5extractors.py
    Co-authored-by: Alessio Buccino <alejoe9187@gmail.com>

--- src/spikeinterface/extractors/mcsh5extractors.py
+++ src/spikeinterface/extractors/mcsh5extractors.py
@@ -130,7 +130,7 @@ def openMCSH5File(filename, stream_id):
     exponent = info["Exponent"][0]
     convFact = info["ConversionFactor"][0]
     gain_uV = 1e6 * (convFact.astype(float) * (10.0**exponent))
-    offset_uV = -1e6 * (info["ADZero"].astype(float) * (10.0**exponent))
+    offset_uV = -1e6 * (info["ADZero"].astype(float) * (10.0**exponent)) / gain_uV
 
     nRecCh, nFrames = data.shape
     channel_ids = [f"Ch{ch}" for ch in info["ChannelID"]]
