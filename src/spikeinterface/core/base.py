1064105ebf083f8b18e3e37f3c1e0c4f59949578
Heberto Mayorquin <h.mayorquin@gmail.com>
2024-07-09 09:43:45 -0600

    remove base printing, add more specification about what is expected

--- src/spikeinterface/core/base.py
+++ src/spikeinterface/core/base.py
@@ -799,9 +799,8 @@ class BaseExtractor:
             return extractor
 
         else:
-            error_msg = f"{file_path} is not a file or a folder"
-            if base_folder:
-                error_msg = f"\n The provided base_folder is {base_folder}"
+            error_msg = f"{file_path} is not a file or a folder. It should point out to either a json, pickle file or a
+            folder that is the result of extractor.save(...)"
             raise ValueError(error_msg)
 
     def __reduce__(self):
