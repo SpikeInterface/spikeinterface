d0c8b12916e33b0a663c97123267b20a684ab12b
jakeswann1 <jake.swann.22@ucl.ac.uk>
2024-07-11 15:24:59 +0100

    Set labels & confidence as properties. Allow metrics input

--- src/spikeinterface/curation/model_based_curation.py
+++ src/spikeinterface/curation/model_based_curation.py
@@ -47,9 +47,17 @@ class ModelBasedClassification:
         self.pipeline = pipeline
         self.required_metrics = pipeline.feature_names_in_
 
-    def predict_labels(self, export_to_phy=False, export_to_sortingview=False):
+    def predict_labels(self, input_data=None, export_to_phy=False):
         """
         Predicts the labels for the spike sorting data using the trained model.
+        Populates the sorting object with the predicted labels and probabilities as unit properties
+
+        Parameters
+        ----------
+        input_data : pandas.DataFrame, optional
+            The input data for classification. If not provided, the method will extract metrics stored in the sorting analyzer.
+        export_to_phy : bool, optional
+            Whether to export the classified units to Phy format. Default is False.
 
         Returns
         -------
@@ -57,12 +65,18 @@ class ModelBasedClassification:
             A dictionary containing the classified units and their corresponding predictions and probabilities.
             The dictionary has the format {unit_id: (prediction, probability)}.
         """
+        import pandas as pd
 
         # Get metrics DataFrame for classification
-        input_data = self._get_metrics_for_classification()
-
-        # # Check params match training data - TODO: implement reliably
-        # self._check_params_for_classification()
+        if input_data is None:
+            input_data = self._get_metrics_for_classification()
+        else:
+            if not isinstance(input_data, pd.DataFrame):
+                raise ValueError("Input data must be a pandas DataFrame")
+            try:
+                input_data = input_data[self.required_metrics]
+            except KeyError:
+                print("Input data does not contain all required metrics for classification")
 
         # Prepare input data
         input_data = input_data.map(lambda x: np.nan if np.isinf(x) else x)
@@ -71,6 +85,7 @@ class ModelBasedClassification:
         # Apply classifier
         predictions = self.pipeline.predict(input_data)
         probabilities = self.pipeline.predict_proba(input_data)
+        probabilities = np.max(probabilities, axis=1)
 
         # Make output dict with {unit_id: (prediction, probability)}
         classified_units = {
@@ -78,13 +93,12 @@ class ModelBasedClassification:
             for unit_id, prediction, probability in zip(input_data.index, predictions, probabilities)
         }
 
+        # Set predictions and probability as sorting properties
+        self.sorting_analyzer.sorting.set_property("label_prediction", predictions)
+        self.sorting_analyzer.sorting.set_property("label_confidence", probabilities)
+
         if export_to_phy:
             self._export_to_phy(classified_units)
-
-        if export_to_sortingview:
-            pass
-            # TODO: implement
-            # self._export_to_sortingview(classified_units)
 
         return classified_units
 
@@ -163,9 +177,11 @@ class ModelBasedClassification:
         classified_df.to_csv(f"{sorting_path}/cluster_prediction.tsv", sep="\t", index_label="cluster_id")
 
 
-def auto_label_units(sorting_analyzer: SortingAnalyzer, pipeline, export_to_phy=False, export_to_sortingview=False):
+def auto_label_units(sorting_analyzer: SortingAnalyzer, pipeline, export_to_phy=False):
     """
     Automatically labels units based on a model-based classification.
+
+    This function populates the sorting object with the predicted labels and probabilities as unit properties.
 
     Parameters
     ----------
@@ -173,22 +189,26 @@ def auto_label_units(sorting_analyzer: SortingAnalyzer, pipeline, export_to_phy=
         The sorting analyzer object containing the spike sorting results.
     pipeline : Pipeline
         The pipeline object containing the model-based classification pipeline.
+    export_to_phy : bool, optional
+        Whether to export the results to Phy format. Default is False.
 
     Returns
     -------
     classified_units : dict
         A dictionary containing the classified units, where the keys are the unit IDs and the values are a tuple of labels and confidence.
 
-    """
+    Raises
+    ------
+    ValueError
+        If the pipeline is not an instance of sklearn.pipeline.Pipeline.
 
+    """
     from sklearn.pipeline import Pipeline
 
     if not isinstance(pipeline, Pipeline):
         raise ValueError("The pipeline must be an instance of sklearn.pipeline.Pipeline")
 
-    model_based_classification = ModelBasedClassification(
-        sorting_analyzer, pipeline, export_to_phy, export_to_sortingview
-    )
+    model_based_classification = ModelBasedClassification(sorting_analyzer, pipeline, export_to_phy)
 
     classified_units = model_based_classification.predict_labels()
 
