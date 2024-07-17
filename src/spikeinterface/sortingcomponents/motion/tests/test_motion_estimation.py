1b55388d627d0011acf4fac08d3333b80ad07e13
Alessio Buccino <alejoe9187@gmail.com>
2024-07-17 12:48:26 +0200

    Fix estimate_motion when time_vector is set

--- src/spikeinterface/sortingcomponents/motion/tests/test_motion_estimation.py
+++ src/spikeinterface/sortingcomponents/motion/tests/test_motion_estimation.py
@@ -45,7 +45,10 @@ def setup_dataset_and_peaks(cache_folder):
     peak_location_path = cache_folder / "dataset_peak_locations.npy"
     np.save(peak_location_path, peak_locations)
 
-    return recording, sorting, cache_folder
+    recording_with_times = recording.clone()
+    recording_with_times.set_times(recording.get_times() + 100)
+
+    return recording, recording_with_times, sorting, cache_folder
 
 
 @pytest.fixture(scope="module", name="dataset")
@@ -56,7 +59,7 @@ def dataset_fixture(create_cache_folder):
 
 def test_estimate_motion(dataset):
     # recording, sorting = make_dataset()
-    recording, sorting, cache_folder = dataset
+    recording, recording_with_times, sorting, cache_folder = dataset
 
     peaks = np.load(cache_folder / "dataset_peaks.npy")
     peak_locations = np.load(cache_folder / "dataset_peak_locations.npy")
@@ -146,78 +149,79 @@ def test_estimate_motion(dataset):
         ),
     }
 
-    motions = {}
-    for name, cases_kwargs in all_cases.items():
-        print(name)
+    for rec in [recording, recording_with_times]:
+        motions = {}
+        for name, cases_kwargs in all_cases.items():
+            print(name)
 
-        kwargs = dict(
-            direction="y",
-            bin_s=1.0,
-            bin_um=10.0,
-            margin_um=5,
-            extra_outputs=True,
-        )
-        kwargs.update(cases_kwargs)
+            kwargs = dict(
+                direction="y",
+                bin_s=1.0,
+                bin_um=10.0,
+                margin_um=5,
+                extra_outputs=True,
+            )
+            kwargs.update(cases_kwargs)
 
-        motion, extra = estimate_motion(recording, peaks, peak_locations, **kwargs)
-        motions[name] = motion
+            motion, extra = estimate_motion(rec, peaks, peak_locations, **kwargs)
+            motions[name] = motion
 
-        if cases_kwargs["rigid"]:
-            assert motion.displacement[0].shape[1] == 1
-        else:
-            assert motion.displacement[0].shape[1] > 1
+            if cases_kwargs["rigid"]:
+                assert motion.displacement[0].shape[1] == 1
+            else:
+                assert motion.displacement[0].shape[1] > 1
 
-        # # Test saving to disk
-        # corrected_rec = InterpolateMotionRecording(
-        #     recording, motion, temporal_bins, spatial_bins, border_mode="force_extrapolate"
-        # )
-        # rec_folder = cache_folder / (name.replace("/", "").replace(" ", "_") + "_recording")
-        # if rec_folder.exists():
-        #     shutil.rmtree(rec_folder)
-        # corrected_rec.save(folder=rec_folder)
+            # # Test saving to disk
+            # corrected_rec = InterpolateMotionRecording(
+            #     recording, motion, temporal_bins, spatial_bins, border_mode="force_extrapolate"
+            # )
+            # rec_folder = cache_folder / (name.replace("/", "").replace(" ", "_") + "_recording")
+            # if rec_folder.exists():
+            #     shutil.rmtree(rec_folder)
+            # corrected_rec.save(folder=rec_folder)
 
-        if DEBUG:
-            fig, ax = plt.subplots()
-            seg_index = 0
-            ax.plot(motion.temporal_bins_s[0], motion.displacement[seg_index])
+            if DEBUG:
+                fig, ax = plt.subplots()
+                seg_index = 0
+                ax.plot(motion.temporal_bins_s[0], motion.displacement[seg_index])
 
-            # motion_histogram = extra_check['motion_histogram']
-            # spatial_hist_bins = extra_check['spatial_hist_bin_edges']
-            # fig, ax = plt.subplots()
-            # extent = (temporal_bins[0], temporal_bins[-1], spatial_hist_bins[0], spatial_hist_bins[-1])
-            # im = ax.imshow(motion_histogram.T, interpolation='nearest',
-            #                     origin='lower', aspect='auto', extent=extent)
+                # motion_histogram = extra_check['motion_histogram']
+                # spatial_hist_bins = extra_check['spatial_hist_bin_edges']
+                # fig, ax = plt.subplots()
+                # extent = (temporal_bins[0], temporal_bins[-1], spatial_hist_bins[0], spatial_hist_bins[-1])
+                # im = ax.imshow(motion_histogram.T, interpolation='nearest',
+                #                     origin='lower', aspect='auto', extent=extent)
 
-            # fig, ax = plt.subplots()
-            # pairwise_displacement = extra_check['pairwise_displacement_list'][0]
-            # im = ax.imshow(pairwise_displacement, interpolation='nearest',
-            #                     cmap='PiYG', origin='lower', aspect='auto', extent=None)
-            # im.set_clim(-40, 40)
-            # ax.set_aspect('equal')
-            # fig.colorbar(im)
+                # fig, ax = plt.subplots()
+                # pairwise_displacement = extra_check['pairwise_displacement_list'][0]
+                # im = ax.imshow(pairwise_displacement, interpolation='nearest',
+                #                     cmap='PiYG', origin='lower', aspect='auto', extent=None)
+                # im.set_clim(-40, 40)
+                # ax.set_aspect('equal')
+                # fig.colorbar(im)
 
-            plt.show()
+                plt.show()
 
-    # same params with differents engine should be the same
-    motion0 = motions["rigid / decentralized / torch"]
-    motion1 = motions["rigid / decentralized / numpy"]
-    assert motion0 == motion1
+        # same params with differents engine should be the same
+        motion0 = motions["rigid / decentralized / torch"]
+        motion1 = motions["rigid / decentralized / numpy"]
+        assert motion0 == motion1
 
-    motion0 = motions["rigid / decentralized / torch / time_horizon_s"]
-    motion1 = motions["rigid / decentralized / numpy / time_horizon_s"]
-    np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
+        motion0 = motions["rigid / decentralized / torch / time_horizon_s"]
+        motion1 = motions["rigid / decentralized / numpy / time_horizon_s"]
+        np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
 
-    motion0 = motions["non-rigid / decentralized / torch"]
-    motion1 = motions["non-rigid / decentralized / numpy"]
-    np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
+        motion0 = motions["non-rigid / decentralized / torch"]
+        motion1 = motions["non-rigid / decentralized / numpy"]
+        np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
 
-    motion0 = motions["non-rigid / decentralized / torch / time_horizon_s"]
-    motion1 = motions["non-rigid / decentralized / numpy / time_horizon_s"]
-    np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
+        motion0 = motions["non-rigid / decentralized / torch / time_horizon_s"]
+        motion1 = motions["non-rigid / decentralized / numpy / time_horizon_s"]
+        np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
 
-    motion0 = motions["non-rigid / decentralized / torch / spatial_prior"]
-    motion1 = motions["non-rigid / decentralized / numpy / spatial_prior"]
-    np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
+        motion0 = motions["non-rigid / decentralized / torch / spatial_prior"]
+        motion1 = motions["non-rigid / decentralized / numpy / spatial_prior"]
+        np.testing.assert_array_almost_equal(motion0.displacement, motion1.displacement)
 
 
 if __name__ == "__main__":
