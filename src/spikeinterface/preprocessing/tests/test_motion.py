f4505e5902e6623e034949e79c4b3252c8fe3dda
Garcia Samuel <sam.garcia.die@gmail.com>
2024-07-19 15:44:03 +0200

    Update doc handle drift  + better preset (#3232)

--- src/spikeinterface/preprocessing/tests/test_motion.py
+++ src/spikeinterface/preprocessing/tests/test_motion.py
@@ -1,7 +1,13 @@
 import shutil
 
 from spikeinterface.core import generate_recording
-from spikeinterface.preprocessing import correct_motion, load_motion_info, save_motion_info
+from spikeinterface.preprocessing import (
+    correct_motion,
+    load_motion_info,
+    save_motion_info,
+    get_motion_parameters_preset,
+)
+from spikeinterface.preprocessing.motion import _get_default_motion_params
 
 
 def test_estimate_and_correct_motion(create_cache_folder):
@@ -27,6 +33,19 @@ def test_estimate_and_correct_motion(create_cache_folder):
     assert motion_info_loaded["motion"] == motion_info["motion"]
 
 
+def test_get_motion_parameters_preset():
+    from pprint import pprint
+
+    p = _get_default_motion_params()
+    # pprint(p)
+
+    params = get_motion_parameters_preset("nonrigid_accurate")
+    params = get_motion_parameters_preset("dredge")
+    params = get_motion_parameters_preset("rigid_fast")
+    pprint(params)
+
+
 if __name__ == "__main__":
     # print(correct_motion.__doc__)
-    test_estimate_and_correct_motion()
+    # test_estimate_and_correct_motion()
+    test_get_motion_parameters_preset()
