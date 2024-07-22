fc0743e59412910e07f5c1355a58718ad6f9a4ed
Axoft Server <axoft.software@gmail.com>
2024-07-22 18:29:38 -0400

    fix: improve variable names

--- src/spikeinterface/widgets/unit_summary.py
+++ src/spikeinterface/widgets/unit_summary.py
@@ -51,7 +51,7 @@ class UnitSummaryWidget(BaseWidget):
         unit_colors=None,
         sparsity=None,
         unitlocationswidget_params=None,
-        unitwaveformswidgets_params=None,
+        unitwaveformswidget_params=None,
         unitwaveformdensitymapwidget_params=None,
         autocorrelogramswidget_params=None,
         amplitudeswidget_params=None,
@@ -65,8 +65,8 @@ class UnitSummaryWidget(BaseWidget):
 
         if unitlocationswidget_params is None:
             unitlocationswidget_params = dict()
-        if unitwaveformswidgets_params is None:
-            unitwaveformswidgets_params = dict()
+        if unitwaveformswidget_params is None:
+            unitwaveformswidget_params = dict()
         if unitwaveformdensitymapwidget_params is None:
             unitwaveformdensitymapwidget_params = dict()
         if autocorrelogramswidget_params is None:
@@ -80,7 +80,7 @@ class UnitSummaryWidget(BaseWidget):
             unit_colors=unit_colors,
             sparsity=sparsity,
             unitlocationswidget_params=unitlocationswidget_params,
-            unitwaveformswidgets_params=unitwaveformswidgets_params,
+            unitwaveformswidget_params=unitwaveformswidget_params,
             unitwaveformdensitymapwidget_params=unitwaveformdensitymapwidget_params,
             autocorrelogramswidget_params=autocorrelogramswidget_params,
             amplitudeswidget_params=amplitudeswidget_params
@@ -98,11 +98,11 @@ class UnitSummaryWidget(BaseWidget):
         sorting_analyzer = dp.sorting_analyzer
         unit_colors = dp.unit_colors
         sparsity = dp.sparsity
-        unitlocationswidget = dp.unitlocationswidget_params
-        unitwaveformswidgets = dp.unitwaveformswidgets_params
-        unitwaveformdensitymapwidget = dp.unitwaveformdensitymapwidget_params
-        autocorrelogramswidget = dp.autocorrelogramswidget_params
-        amplitudeswidget = dp.amplitudeswidget_params
+        unitlocationswidget_params = dp.unitlocationswidget_params
+        unitwaveformswidget_params = dp.unitwaveformswidget_params
+        unitwaveformdensitymapwidget_params = dp.unitwaveformdensitymapwidget_params
+        autocorrelogramswidget_params = dp.autocorrelogramswidget_params
+        amplitudeswidget_params = dp.amplitudeswidget_params
 
         # force the figure without axes
         if "figsize" not in backend_kwargs:
@@ -133,7 +133,7 @@ class UnitSummaryWidget(BaseWidget):
                 plot_legend=False,
                 backend="matplotlib",
                 ax=ax1,
-                **unitlocationswidget  
+                **unitlocationswidget_params  
             )
 
             unit_locations = sorting_analyzer.get_extension("unit_locations").get_data(outputs="by_unit")
@@ -156,7 +156,7 @@ class UnitSummaryWidget(BaseWidget):
             sparsity=sparsity,
             backend="matplotlib",
             ax=ax2,
-            **unitwaveformswidgets
+            **unitwaveformswidget_params
         )
 
         ax2.set_title(None)
@@ -170,7 +170,7 @@ class UnitSummaryWidget(BaseWidget):
             same_axis=False,
             backend="matplotlib",
             ax=ax3,
-            **unitwaveformdensitymapwidget
+            **unitwaveformdensitymapwidget_params
         )
         ax3.set_ylabel(None)
 
@@ -182,7 +182,7 @@ class UnitSummaryWidget(BaseWidget):
                 unit_colors=unit_colors,
                 backend="matplotlib",
                 ax=ax4,
-                **autocorrelogramswidget
+                **autocorrelogramswidget_params
             )
 
             ax4.set_title(None)
@@ -200,7 +200,7 @@ class UnitSummaryWidget(BaseWidget):
                 plot_histograms=True,
                 backend="matplotlib",
                 axes=axes,
-                **amplitudeswidget
+                **amplitudeswidget_params
             )
 
         fig.suptitle(f"unit_id: {dp.unit_id}")
