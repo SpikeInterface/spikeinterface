642eea9b2c1242000dd847701eb89dc533def6be
JoeZiminski <joseph.j.ziminski@gmail.com>
2024-08-21 12:55:59 +0100

    Update KS4 versions to test on, add a warning for the next version.

--- .github/scripts/check_kilosort4_releases.py
+++ .github/scripts/check_kilosort4_releases.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import requests
 import json
 from packaging.version import parse
+import spikeinterface
 
 def get_pypi_versions(package_name):
     """
@@ -15,7 +16,13 @@ def get_pypi_versions(package_name):
     response.raise_for_status()
     data = response.json()
     versions = list(sorted(data["releases"].keys()))
-    versions = [ver for ver in versions if parse(ver) >= parse("4.0.5")]
+
+    assert parse(spikeinterface.__version__) < parse("0.101.1"), (
+        "Kilosort 4.0.5-12 are supported in SpikeInterface < 0.101.1."
+        "At version 0.101.1, this should be updated to support newer"
+        "kilosort verrsions."
+    )
+    versions = [ver for ver in versions if parse("4.0.12") >= parse(ver) >= parse("4.0.5")]
     return versions
 
 
@@ -24,4 +31,5 @@ if __name__ == "__main__":
     package_name = "kilosort"
     versions = get_pypi_versions(package_name)
     with open(Path(os.path.realpath(__file__)).parent / "kilosort4-latest-version.json", "w") as f:
+        print(versions)
         json.dump(versions, f)
