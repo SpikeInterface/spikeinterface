f0744d5e45437b93a27f6c252dc615057e316b8e
tabedzki <35670232+tabedzki@users.noreply.github.com>
2024-09-04 10:04:02 -0400

    Update src/spikeinterface/core/sortinganalyzer.py
    Co-authored-by: Alessio Buccino <alejoe9187@gmail.com>

--- src/spikeinterface/core/sortinganalyzer.py
+++ src/spikeinterface/core/sortinganalyzer.py
@@ -1122,7 +1122,7 @@ class SortingAnalyzer:
         return self.sorting.get_num_units()
 
     ## extensions zone
-    def compute(self, input, save=True, extension_params=None, verbose=False, **kwargs) -> "SortingAnalyzer | None":
+    def compute(self, input, save=True, extension_params=None, verbose=False, **kwargs) -> "AnalyzerExtension | None":
         """
         Compute one extension or several extensiosn.
         Internally calls compute_one_extension() or compute_several_extensions() depending on the input type.
