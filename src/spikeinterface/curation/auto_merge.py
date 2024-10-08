484a5f4626c1cd160f40d08cb8f6980ea6f6b8b3
Pierre Yger <pierre.yger@gmail.com>
2024-10-08 10:38:45 +0200

    WIP

--- src/spikeinterface/curation/auto_merge.py
+++ src/spikeinterface/curation/auto_merge.py
@@ -35,26 +35,31 @@ def auto_merges(
     sorting_analyzer: SortingAnalyzer,
     preset: str | None = "similarity_correlograms",
     resolve_graph: bool = False,
-    num_spikes_kwargs={"min_spikes": 100},
-    snr_kwargs={"min_snr": 2},
-    remove_contaminated_kwargs={"contamination_thresh": 0.2, "refractory_period_ms": 1.0, "censored_period_ms": 0.3},
-    unit_locations_kwargs={"max_distance_um": 50},
-    correlogram_kwargs={
-        "corr_diff_thresh": 0.16,
-        "censor_correlograms_ms": 0.3,
-        "sigma_smooth_ms": 0.6,
-        "adaptative_window_thresh": 0.5,
-    },
-    template_similarity_kwargs={"template_diff_thresh": 0.25},
-    presence_distance_kwargs={"presence_distance_thresh": 100},
-    knn_kwargs={"k_nn": 10},
-    cross_contamination_kwargs={
-        "cc_thresh": 0.1,
-        "p_value": 0.2,
-        "refractory_period_ms": 1.0,
-        "censored_period_ms": 0.3,
-    },
-    quality_score_kwargs={"firing_contamination_balance": 2.5, "refractory_period_ms": 1.0, "censored_period_ms": 0.3},
+    steps_params: dict = {"num_spikes" : {"min_spikes": 100},
+                    "snr" : {"min_snr": 2},
+                    "remove_contaminated" : {"contamination_thresh": 0.2, 
+                                             "refractory_period_ms": 1.0, 
+                                             "censored_period_ms": 0.3},
+                    "unit_locations" : {"max_distance_um": 50},
+                    "correlogram" : {
+                            "corr_diff_thresh": 0.16,
+                            "censor_correlograms_ms": 0.3,
+                            "sigma_smooth_ms": 0.6,
+                            "adaptative_window_thresh": 0.5,
+                        },
+                    "template_similarity" : {"template_diff_thresh": 0.25},
+                    "presence_distance" : {"presence_distance_thresh": 100},
+                    "knn" : {"k_nn": 10},
+                    "cross_contamination" : {
+                        "cc_thresh": 0.1,
+                        "p_value": 0.2,
+                        "refractory_period_ms": 1.0,
+                        "censored_period_ms": 0.3,
+                    },
+                    "quality_score" : {"firing_contamination_balance": 2.5, 
+                                       "refractory_period_ms": 1.0, 
+                                       "censored_period_ms": 0.3},
+                },
     compute_needed_extensions: bool = True,
     extra_outputs: bool = False,
     steps: list[str] | None = None,
@@ -115,7 +120,8 @@ def auto_merges(
         Which steps to run, if no preset is used.
         Pontential steps : "num_spikes", "snr", "remove_contaminated", "unit_locations", "correlogram",
         "template_similarity", "presence_distance", "cross_contamination", "knn", "quality_score"
-        Please check steps explanations above!$
+        Please check steps explanations above!
+    steps_params : A dictionary whose keys are the steps, and keys are steps parameters.
     force_copy : boolean, default: True
         When new extensions are computed, the default is to make a copy of the analyzer, to avoid overwriting
         already computed extensions. False if you want to overwrite
@@ -139,11 +145,6 @@ def auto_merges(
 
     sorting = sorting_analyzer.sorting
     unit_ids = sorting.unit_ids
-
-    # to get fast computation we will not analyse pairs when:
-    #    * not enough spikes for one of theses
-    #    * auto correlogram is contaminated
-    #    * to far away one from each other
 
     all_steps = [
         "num_spikes",
@@ -227,11 +228,13 @@ def auto_merges(
     for step in steps:
 
         assert step in all_steps, f"{step} is not a valid step"
+        params = steps_params.get(step, {})
 
         # STEP : remove units with too few spikes
         if step == "num_spikes":
+            
             num_spikes = sorting.count_num_spikes_per_unit(outputs="array")
-            to_remove = num_spikes < num_spikes_kwargs["min_spikes"]
+            to_remove = num_spikes < params["min_spikes"]
             pair_mask[to_remove, :] = False
             pair_mask[:, to_remove] = False
             outs["num_spikes"] = to_remove
@@ -245,7 +248,7 @@ def auto_merges(
                 qm_ext = sorting_analyzer.get_extension("quality_metrics")
 
             snrs = qm_ext.get_data()["snr"].values
-            to_remove = snrs < snr_kwargs["min_snr"]
+            to_remove = snrs < params["min_snr"]
             pair_mask[to_remove, :] = False
             pair_mask[:, to_remove] = False
             outs["snr"] = to_remove
@@ -254,12 +257,12 @@ def auto_merges(
         elif step == "remove_contaminated":
             contaminations, nb_violations = compute_refrac_period_violations(
                 sorting_analyzer,
-                refractory_period_ms=remove_contaminated_kwargs["refractory_period_ms"],
-                censored_period_ms=remove_contaminated_kwargs["censored_period_ms"],
+                refractory_period_ms=params["refractory_period_ms"],
+                censored_period_ms=params["censored_period_ms"],
             )
             nb_violations = np.array(list(nb_violations.values()))
             contaminations = np.array(list(contaminations.values()))
-            to_remove = contaminations > remove_contaminated_kwargs["contamination_thresh"]
+            to_remove = contaminations > params["contamination_thresh"]
             pair_mask[to_remove, :] = False
             pair_mask[:, to_remove] = False
             outs["remove_contaminated"] = to_remove
@@ -270,15 +273,15 @@ def auto_merges(
             unit_locations = location_ext.get_data()[:, :2]
 
             unit_distances = scipy.spatial.distance.cdist(unit_locations, unit_locations, metric="euclidean")
-            pair_mask = pair_mask & (unit_distances <= unit_locations_kwargs["max_distance_um"])
+            pair_mask = pair_mask & (unit_distances <= params["max_distance_um"])
             outs["unit_distances"] = unit_distances
 
         # STEP : potential auto merge by correlogram
         elif step == "correlogram" in steps:
             correlograms_ext = sorting_analyzer.get_extension("correlograms")
             correlograms, bins = correlograms_ext.get_data()
-            censor_ms = correlogram_kwargs["censor_correlograms_ms"]
-            sigma_smooth_ms = correlogram_kwargs["sigma_smooth_ms"]
+            censor_ms = params["censor_correlograms_ms"]
+            sigma_smooth_ms = params["sigma_smooth_ms"]
             mask = (bins[:-1] >= -censor_ms) & (bins[:-1] < censor_ms)
             correlograms[:, :, mask] = 0
             correlograms_smoothed = smooth_correlogram(correlograms, bins, sigma_smooth_ms=sigma_smooth_ms)
@@ -286,7 +289,7 @@ def auto_merges(
             win_sizes = np.zeros(n, dtype=int)
             for unit_ind in range(n):
                 auto_corr = correlograms_smoothed[unit_ind, unit_ind, :]
-                thresh = np.max(auto_corr) * correlogram_kwargs["adaptative_window_thresh"]
+                thresh = np.max(auto_corr) * params["adaptative_window_thresh"]
                 win_size = get_unit_adaptive_window(auto_corr, thresh)
                 win_sizes[unit_ind] = win_size
             correlogram_diff = compute_correlogram_diff(
@@ -296,7 +299,7 @@ def auto_merges(
                 pair_mask=pair_mask,
             )
             # print(correlogram_diff)
-            pair_mask = pair_mask & (correlogram_diff < correlogram_kwargs["corr_diff_thresh"])
+            pair_mask = pair_mask & (correlogram_diff < params["corr_diff_thresh"])
             outs["correlograms"] = correlograms
             outs["bins"] = bins
             outs["correlograms_smoothed"] = correlograms_smoothed
@@ -308,16 +311,16 @@ def auto_merges(
             template_similarity_ext = sorting_analyzer.get_extension("template_similarity")
             templates_similarity = template_similarity_ext.get_data()
             templates_diff = 1 - templates_similarity
-            pair_mask = pair_mask & (templates_diff < template_similarity_kwargs["template_diff_thresh"])
+            pair_mask = pair_mask & (templates_diff < params["template_diff_thresh"])
             outs["templates_diff"] = templates_diff
 
         # STEP : check the vicinity of the spikes
         elif step == "knn" in steps:
-            pair_mask = get_pairs_via_nntree(sorting_analyzer, **knn_kwargs, pair_mask=pair_mask)
+            pair_mask = get_pairs_via_nntree(sorting_analyzer, **params, pair_mask=pair_mask)
 
         # STEP : check how the rates overlap in times
         elif step == "presence_distance" in steps:
-            presence_distance_kwargs = presence_distance_kwargs.copy()
+            presence_distance_kwargs = params.copy()
             presence_distance_thresh = presence_distance_kwargs.pop("presence_distance_thresh")
             num_samples = [
                 sorting_analyzer.get_num_samples(segment_index) for segment_index in range(sorting.get_num_segments())
@@ -331,13 +334,13 @@ def auto_merges(
         # STEP : check if the cross contamination is significant
         elif step == "cross_contamination" in steps:
             refractory = (
-                cross_contamination_kwargs["censored_period_ms"],
-                cross_contamination_kwargs["refractory_period_ms"],
+                params["censored_period_ms"],
+                params["refractory_period_ms"],
             )
             CC, p_values = compute_cross_contaminations(
-                sorting_analyzer, pair_mask, cross_contamination_kwargs["cc_thresh"], refractory, contaminations
+                sorting_analyzer, pair_mask, params["cc_thresh"], refractory, contaminations
             )
-            pair_mask = pair_mask & (p_values > cross_contamination_kwargs["p_value"])
+            pair_mask = pair_mask & (p_values > params["p_value"])
             outs["cross_contaminations"] = CC, p_values
 
         # STEP : validate the potential merges with CC increase the contamination quality metrics
@@ -346,9 +349,9 @@ def auto_merges(
                 sorting_analyzer,
                 pair_mask,
                 contaminations,
-                quality_score_kwargs["firing_contamination_balance"],
-                quality_score_kwargs["refractory_period_ms"],
-                quality_score_kwargs["censored_period_ms"],
+                params["firing_contamination_balance"],
+                params["refractory_period_ms"],
+                params["censored_period_ms"],
             )
             outs["pairs_decreased_score"] = pairs_decreased_score
 
@@ -505,34 +508,34 @@ def get_potential_auto_merge(
         sorting_analyzer,
         preset,
         resolve_graph,
-        num_spikes_kwargs={"min_spikes": min_spikes},
-        snr_kwargs={"min_snr": min_snr},
-        remove_contaminated_kwargs={
-            "contamination_thresh": contamination_thresh,
-            "refractory_period_ms": refractory_period_ms,
-            "censored_period_ms": censored_period_ms,
-        },
-        unit_locations_kwargs={"max_distance_um": max_distance_um},
-        correlogram_kwargs={
-            "corr_diff_thresh": corr_diff_thresh,
-            "censor_correlograms_ms": censor_correlograms_ms,
-            "sigma_smooth_ms": sigma_smooth_ms,
-            "adaptative_window_thresh": adaptative_window_thresh,
-        },
-        template_similarity_kwargs={"template_diff_thresh": template_diff_thresh},
-        presence_distance_kwargs={"presence_distance_thresh": presence_distance_thresh, **presence_distance_kwargs},
-        knn_kwargs={"k_nn": k_nn, **knn_kwargs},
-        cross_contamination_kwargs={
-            "cc_thresh": cc_thresh,
-            "p_value": p_value,
-            "refractory_period_ms": refractory_period_ms,
-            "censored_period_ms": censored_period_ms,
-        },
-        quality_score_kwargs={
-            "firing_contamination_balance": firing_contamination_balance,
-            "refractory_period_ms": refractory_period_ms,
-            "censored_period_ms": censored_period_ms,
-        },
+        step_params={"num_spikes" : {"min_spikes": min_spikes},
+                    "snr_kwargs" : {"min_snr": min_snr},
+                    "remove_contaminated_kwargs" : {
+                        "contamination_thresh": contamination_thresh,
+                        "refractory_period_ms": refractory_period_ms,
+                        "censored_period_ms": censored_period_ms,
+                    },
+                    "unit_locations" : {"max_distance_um": max_distance_um},
+                    "correlogram" : {
+                        "corr_diff_thresh": corr_diff_thresh,
+                        "censor_correlograms_ms": censor_correlograms_ms,
+                        "sigma_smooth_ms": sigma_smooth_ms,
+                        "adaptative_window_thresh": adaptative_window_thresh,
+                    },
+                    "template_similarity":  {"template_diff_thresh": template_diff_thresh},
+                    "presence_distance" : {"presence_distance_thresh": presence_distance_thresh, **presence_distance_kwargs},
+                    "knn" : {"k_nn": k_nn, **knn_kwargs},
+                    "cross_contamination" : {
+                        "cc_thresh": cc_thresh,
+                        "p_value": p_value,
+                        "refractory_period_ms": refractory_period_ms,
+                        "censored_period_ms": censored_period_ms,
+                    },
+                    "quality_score" : {
+                        "firing_contamination_balance": firing_contamination_balance,
+                        "refractory_period_ms": refractory_period_ms,
+                        "censored_period_ms": censored_period_ms,
+                    }},
         compute_needed_extensions=False,
         extra_outputs=extra_outputs,
         steps=steps,
