c88f43bdf71ae31b1bfde46dbe1b4eda5442bf0c
Pierre Yger <pierre.yger@gmail.com>
2024-10-17 10:44:54 +0200

    Harmonization of splitting functions and removing dependencies

--- src/spikeinterface/generation/splitting_tools.py
+++ src/spikeinterface/generation/splitting_tools.py
@@ -1,13 +1,38 @@
+import numpy as np
+from spikeinterface.core.numpyextractors import NumpySorting
+
+
 def split_sorting_by_times(
-    sorting_analyzer, splitting_probability=0.5, partial_split_prob=0.95, unit_ids=None, min_snr=None, seed=None
+    sorting_analyzer, splitting_probability=0.5, partial_split_prob=0.8, unit_ids=None, min_snr=None, seed=None
 ):
+    """
+    Fonction used to split a sorting based on the times of the units. This
+    might be used for benchmarking meta merging step (see components)
+
+    Parameters
+    ----------
+    sorting_analyzer : A sortingAnalyzer object
+        The sortingAnalyzer object whose sorting should be splitted
+    splitting_probability : float, default 0.5
+        probability of being splitted, for any cell in the provided sorting
+    partial_split_prob : float, default 0.95
+        The percentage of spikes that will belong to pre/post splits
+    unit_ids : list of unit_ids, default None
+        The list of unit_ids to be splitted, if prespecified
+    min_snr : float, default=None
+        If specified, only cells with a snr higher than min_snr might be splitted
+    seed : int | None, default: None
+        The seed for random generator.
+
+    Returns
+    -------
+    new_sorting, splitted_pairs : The new splitted sorting, and the pairs that have been splitted
+    """
+
     sa = sorting_analyzer
     sorting = sa.sorting
     rng = np.random.RandomState(seed)
 
-    sorting_split = sorting.select_units(sorting.unit_ids)
-    split_units = []
-    original_units = []
     nb_splits = int(splitting_probability * len(sorting.unit_ids))
     if unit_ids is None:
         select_from = sorting.unit_ids
@@ -24,25 +49,48 @@ def split_sorting_by_times(
     else:
         to_split_ids = unit_ids
 
-    import spikeinterface.curation as scur
+    spikes = sa.sorting.to_spike_vector()
+    new_spikes = spikes.copy()
+    max_index = np.max(spikes["unit_index"])
+    new_unit_ids = list(sa.sorting.unit_ids.copy())
+    splitted_pairs = []
+    for unit_id in to_split_ids:
+        ind_mask = spikes["unit_index"] == sa.sorting.id_to_index(unit_id)
+        m = np.median(spikes[ind_mask]["sample_index"])
+        time_mask = spikes[ind_mask]["sample_index"] > m
+        mask = time_mask & (rng.rand(np.sum(ind_mask)) > partial_split_prob).astype(bool)
+        new_spikes[ind_mask]["unit_index"][mask] = max_index + 1
+        max_index += 1
+        new_unit_ids += [max(new_unit_ids) + 1]
+        splitted_pairs += [(unit_id, new_unit_ids[-1])]
 
-    for unit in to_split_ids:
-        num_spikes = len(sorting_split.get_unit_spike_train(unit))
-        indices = np.zeros(num_spikes, dtype=int)
-        indices[: num_spikes // 2] = (rng.rand(num_spikes // 2) < partial_split_prob).astype(int)
-        indices[num_spikes // 2 :] = (rng.rand(num_spikes - num_spikes // 2) < 1 - partial_split_prob).astype(int)
-        sorting_split = scur.split_unit_sorting(
-            sorting_split, split_unit_id=unit, indices_list=indices, properties_policy="remove"
-        )
-        split_units.append(sorting_split.unit_ids[-2:])
-        original_units.append(unit)
-    return sorting_split, split_units
+    new_sorting = NumpySorting(new_spikes, sampling_frequency=sa.sampling_frequency, unit_ids=new_unit_ids)
+    return new_sorting, splitted_pairs
 
 
-def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, unit_ids=None, min_snr=None, seed=None):
+def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, partial_split_prob=0.8, unit_ids=None, min_snr=None, seed=None):
     """
     Fonction used to split a sorting based on the amplitudes of the units. This
     might be used for benchmarking meta merging step (see components)
+
+    Parameters
+    ----------
+    sorting_analyzer : A sortingAnalyzer object
+        The sortingAnalyzer object whose sorting should be splitted
+    splitting_probability : float, default 0.5
+        probability of being splitted, for any cell in the provided sorting
+    partial_split_prob : float, default 0.95
+        The percentage of spikes that will belong to pre/post splits
+    unit_ids : list of unit_ids, default None
+        The list of unit_ids to be splitted, if prespecified
+    min_snr : float, default=None
+        If specified, only cells with a snr higher than min_snr might be splitted
+    seed : int | None, default: None
+        The seed for random generator.
+
+    Returns
+    -------
+    new_sorting, splitted_pairs : The new splitted sorting, and the pairs that have been splitted
     """
 
     sa = sorting_analyzer
@@ -50,8 +98,6 @@ def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, uni
         sa.compute("spike_amplitudes")
 
     rng = np.random.RandomState(seed)
-
-    from spikeinterface.core.numpyextractors import NumpySorting
     from spikeinterface.core.template_tools import get_template_extremum_channel
 
     extremum_channel_inds = get_template_extremum_channel(sa, outputs="index")
@@ -79,18 +125,10 @@ def split_sorting_by_amplitudes(sorting_analyzer, splitting_probability=0.5, uni
     splitted_pairs = []
     for unit_id in to_split_ids:
         ind_mask = spikes["unit_index"] == sa.sorting.id_to_index(unit_id)
-
         m = amplitudes[ind_mask].mean()
-        s = amplitudes[ind_mask].std()
-        thresh = m + 0.2 * s
-
-        amplitude_mask = amplitudes > thresh
-        mask = ind_mask & amplitude_mask
-        new_spikes["unit_index"][mask] = max_index + 1
-
-        amplitude_mask = (amplitudes > m) * (amplitudes < thresh)
-        mask = ind_mask & amplitude_mask
-        new_spikes["unit_index"][mask] = (max_index + 1) * rng.rand(np.sum(mask)) > 0.5
+        amplitude_mask = amplitudes[ind_mask] > m
+        mask = amplitude_mask & (rng.rand(np.sum(ind_mask)) > partial_split_prob).astype(bool)
+        new_spikes[ind_mask]["unit_index"][mask] = max_index + 1
         max_index += 1
         new_unit_ids += [max(new_unit_ids) + 1]
         splitted_pairs += [(unit_id, new_unit_ids[-1])]
