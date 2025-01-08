133cb2a32093198e376a1b356e9a601caba0b369
Samuel Garcia <sam.garcia.die@gmail.com>
2025-01-08 09:54:40 +0100

    Update with medine API changes + wrap all params.

--- src/spikeinterface/sortingcomponents/motion/medecine.py
+++ src/spikeinterface/sortingcomponents/motion/medecine.py
@@ -33,13 +33,20 @@ class MedecineRegistration:
         verbose,
         progress_bar,
         extra,
-        # bin_um=5.0,
-        # hist_margin_um=20.0,
         bin_s=1.0,
-        time_kernel_width=30.0,
+
+        ## medecine specific kwargs propagated to the lib
+        motion_bound=800,
+        time_kernel_width=30,
+        activity_network_hidden_features=(256, 256),
         amplitude_threshold_quantile=0.0,
-        ####
+        batch_size=4096,
         training_steps=10_000,
+        initial_motion_noise=0.1,
+        motion_noise_steps=2000,
+        optimizer=None,
+        learning_rate=0.0005,
+        epsilon=1e-3,
     ):
 
         from medicine import run_medicine
@@ -61,33 +68,37 @@ class MedecineRegistration:
             num_depth_bins = max(int(np.round(deph_range / win_scale_um)), 1)
             print("num_depth_bins", num_depth_bins)
 
-        trainer, motion = run_medicine(
+        if optimizer is None:
+            import torch
+            optimizer = torch.optim.Adam
+
+        trainer, time_bins, depth_bins, pred_motion = run_medicine(
             peak_amplitudes=peaks["amplitude"],
             peak_depths=peak_locations[direction],
             peak_times=peaks["sample_index"] / recording.get_sampling_frequency(),
             time_bin_size=bin_s,
             num_depth_bins=num_depth_bins,
-            training_steps=training_steps,
-            time_kernel_width=time_kernel_width,
-            amplitude_threshold_quantile=amplitude_threshold_quantile,
+
             output_dir=None,
             plot_figures=False,
-            return_motion=True,
+
+            motion_bound=motion_bound,
+            time_kernel_width=time_kernel_width,
+            activity_network_hidden_features=activity_network_hidden_features,
+            amplitude_threshold_quantile=amplitude_threshold_quantile,
+            batch_size=batch_size,
+            training_steps=training_steps,
+            initial_motion_noise=initial_motion_noise,
+            motion_noise_steps=motion_noise_steps,
+            optimizer=optimizer,
+            learning_rate=learning_rate,
+            epsilon=epsilon,
         )
 
-        # Load motion estimated by MEDiCINe
-        # motion_array = np.load(folder / 'motion.npy')
-        # time_bins = np.load(folder / 'time_bins.npy')
-        # depth_bins = np.load(folder / 'depth_bins.npy')
-
-        # # Use interpolation to correct for motion estimated by MEDiCINe
-        # motion = Motion(
-        #     displacement=[motion_array],
-        #     temporal_bins_s=[time_bins],
-        #     spatial_bins_um=depth_bins,
-        # )
-
-        # TODO check why not working
-        # shutil.rmtree(folder)
+        motion = Motion(
+            displacement=[np.array(pred_motion)],
+            temporal_bins_s=[np.array(time_bins)],
+            spatial_bins_um=np.array(depth_bins),
+        )
 
         return motion
