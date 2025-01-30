ccdff24db3c8e178749ee002f1363f0e4903562e
Samuel Garcia <sam.garcia.die@gmail.com>
2025-01-30 22:03:28 +0100

    refactor load() for more general purpose

--- src/spikeinterface/core/loading.py
+++ src/spikeinterface/core/loading.py
@@ -1,14 +1,24 @@
 import warnings
 from pathlib import Path
 
+import json
+import pickle
 
 from .base import BaseExtractor
 from .core_tools import is_path_remote
 
 
+
+
+_error_msg = (
+    "{file_path} is not a file or a folder. It should point to either a json, pickle file or a "
+    "folder that is the result of extractor.save(...) or sortinganalyzer.save_as(...)"
+)
+
 def load(
-    file_or_folder_or_dict, base_folder=None, load_extensions=True, backend_options=None
-) -> "BaseExtractor | SortingAnalyzer":
+    file_or_folder_or_dict, base_folder=None, **kwargs
+    # load_extensions=True, backend_options=None
+) -> "BaseExtractor | SortingAnalyzer | Motion | Template":
     """
     General load function to load a SpikeInterface object.
 
@@ -24,7 +34,13 @@ def load(
             * binary folder
             * zarr folder
             * remote zarr folder
-            * WaveformExtractor folder
+            * WaveformExtractor folder (backward compatibility for v<0.101)
+        - a `Motion` after
+           * Motion.save(folder)
+           * Motion.to_dict()
+        - a `Templates` after
+           * Templates.add_templates_to_zarr_group()
+           * Templates.to_dict()
 
     Parameters
     ----------
@@ -33,6 +49,9 @@ def load(
     base_folder : str | Path | bool | None, default: None
         The base folder to make relative paths absolute. Only used to load Recording/Sorting objects.
         If True and file_or_folder_or_dict is a file, the parent folder of the file is used.
+
+
+    # TODO discuss this kwargs or expicit ????
     load_extensions : bool, default: True
         If True, the SortingAnalyzer extensions are loaded. Only used to load SortingAnalyzer objects.
     backend_options : dict | None, default: None
@@ -43,137 +62,229 @@ def load(
 
     Returns
     -------
-    spikeinterface object: Recording or Sorting or SortingAnalyzer
+    spikeinterface object: Recording or Sorting or SortingAnalyzer or Motion or Templates
         The loaded spikeinterface object
     """
     if isinstance(file_or_folder_or_dict, dict):
-        assert not isinstance(base_folder, bool), "`base_folder` must be a string or Path when loading from dict"
-        return BaseExtractor.from_dict(file_or_folder_or_dict, base_folder=base_folder)
-    else:
-        file_path = file_or_folder_or_dict
-        error_msg = (
-            f"{file_path} is not a file or a folder. It should point to either a json, pickle file or a "
-            "folder that is the result of extractor.save(...) or sortinganalyzer.save_as(...)"
-        )
-        if not is_path_remote(file_path):
-            file_path = Path(file_path)
+        # dict can be only Recording Sorting Motion Templates
+        d = file_or_folder_or_dict
+        object_type = _guess_object_from_dict(d)
+        if object_type is None:
+            raise ValueError(_error_msg.format(file_path=file_path))
+        return _load_object_from_dict(d, object_type, base_folder=base_folder)
 
-            if base_folder is True:
-                base_folder = file_path.parent
-
-            if file_path.is_file():
-                # standard case based on a file (json or pickle)
-                if str(file_path).endswith(".json"):
-                    import json
-
-                    with open(file_path, "r") as f:
-                        d = json.load(f)
-                elif str(file_path).endswith(".pkl") or str(file_path).endswith(".pickle"):
-                    import pickle
-
-                    with open(file_path, "rb") as f:
-                        d = pickle.load(f)
-                else:
-                    raise ValueError(error_msg)
-
-                # this is for back-compatibility since now unserializable objects will not
-                # be saved to file
-                if "warning" in d:
-                    print("The extractor was not serializable to file")
-                    return None
-
-                si_object = BaseExtractor.from_dict(d, base_folder=base_folder)
-
-            elif file_path.is_dir():
-                # this can be and extractor, SortingAnalyzer, or WaveformExtractor
-                folder = file_path
-                file = None
-
-                if folder.suffix == ".zarr":
-                    # here it can be a zarr folder for extractor or SortingAnalyzer
-                    import zarr
-
-                    zarr_root = zarr.open(str(folder), mode="r")
-
-                    if "spikeinterface_info" in zarr_root.attrs:
-                        # SortingAnalyzer case
-                        from .sortinganalyzer import load_sorting_analyzer
-
-                        si_object = load_sorting_analyzer(folder, load_extensions=load_extensions)
-                    else:
-                        # Extractor case
-                        from .zarrextractors import read_zarr
-
-                        si_object = read_zarr(folder)
-                else:
-                    if (folder / "spikeinterface_info.json").is_file():
-                        # SortingAnalyzer case
-                        from .sortinganalyzer import load_sorting_analyzer
-
-                        si_object = load_sorting_analyzer(folder, load_extensions=load_extensions)
-                    elif (folder / "waveforms").is_dir():
-                        # WaveformExtractor case
-                        from .waveforms_extractor_backwards_compatibility import load_waveforms
-
-                        si_object = load_waveforms(folder, output="SortingAnalyzer")
-                    else:
-                        # Extractor case
-
-                        # For backward compatibility (v<=0.94) we check for the cached.json/pkl/pickle files
-                        # In later versions (v>0.94) we use the si_folder.json file
-                        for dump_ext in ("json", "pkl", "pickle"):
-                            f = folder / f"cached.{dump_ext}"
-                            if f.is_file():
-                                file = f
-
-                        f = folder / f"si_folder.json"
-                        if f.is_file():
-                            file = f
-
-                        if file is None:
-                            raise ValueError(error_msg)
-                        si_object = BaseExtractor.load(file, base_folder=folder)
-
-            else:
-                raise ValueError(error_msg)
+    is_local = not is_path_remote(file_or_folder_or_dict)
+    if is_local:
+        file_path = Path(file_or_folder_or_dict)
+    
+    if is_local and file_path.is_file():
+        # Standard case based on a file (json or pickle) after a Base.dump(json/pickle)
+        if base_folder is True:
+            base_folder = file_path.parent
+        
+        if str(file_path).endswith(".json"):
+            import json
+            with open(file_path, "r") as f:
+                d = json.load(f)
+        elif str(file_path).endswith(".pkl") or str(file_path).endswith(".pickle"):
+            import pickle
+            with open(file_path, "rb") as f:
+                d = pickle.load(f)
         else:
-            # remote case - currently, only zarr is supported
-            if str(file_path).endswith((".zarr", ".zarr/")):
-                # here we can have a zarr folder for extractor or SortingAnalyzer
-                # since the data is remote, we need to try and see if it is an extractor or a SortingAnalyzer
-                from .zarrextractors import super_zarr_open
+            raise ValueError(_error_msg.format(file_path=file_path))
+        
+        object_type = _guess_object_from_dict(d)
+        if object_type is None:
+            raise ValueError(_error_msg.format(file_path=file_path))
+        return _load_object_from_dict(d, object_type, base_folder=base_folder)
+        
+    elif is_local and file_path.is_dir():
+        
+        folder = file_path
+        if folder.suffix == ".zarr":
+            # Local zarr can be
+            # Sorting Recording SortingAnalyzer Template
+            object_type = _guess_object_from_zarr(folder)
+            if object_type is None:
+                raise ValueError(_error_msg.format(file_path=file_path))
+            loaded_object = _load_object_from_zarr(folder, object_type,  **kwargs)
+            return loaded_object
 
-                storage_options = backend_options.get("storage_options", None) if backend_options is not None else None
-                zarr_root = super_zarr_open(file_path, mode="r", storage_options=storage_options)
-                attrs = zarr_root.attrs.asdict()
-                spikeinterface_info = attrs.get("spikeinterface_info", None)
-                if spikeinterface_info and spikeinterface_info.get("object", None) == "SortingAnalyzer":
-                    from .sortinganalyzer import load_sorting_analyzer
-
-                    si_object = load_sorting_analyzer(
-                        file_path, load_extensions=load_extensions, backend_options=backend_options
-                    )
-                else:
-                    from .zarrextractors import read_zarr
-
-                    storage_options = (
-                        backend_options.get("storage_options", None) if backend_options is not None else None
-                    )
-                    si_object = read_zarr(file_path, storage_options=storage_options)
-            else:
-                raise NotImplementedError(
-                    "Only zarr format is supported for remote files and you should provide a path to a .zarr "
-                    "remote path. You can save to a valid zarr folder using: "
-                    "`extractor.save(folder='path/to/folder', format='zarr')` or `analyzer.save_as(folder='path/to/folder', format='zarr')`"
-                )
-
-        return si_object
+        else:
+            # Local folder can be
+            # Sorting Recording SortingAnalyzer Motion
+            object_type = _guess_object_from_local_folder(folder)
+            if object_type is None:
+                raise ValueError(_error_msg.format(file_path=file_path))
+            loaded_object = _load_object_from_folder(folder, object_type,  **kwargs)
+            return loaded_object
+    else:
+        # remote zarr can be
+        # Sorting Recording SortingAnalyzer Template
+        url = file_or_folder_or_dict
+        object_type = _guess_object_from_zarr(url)
+        loaded_object = _load_object_from_zarr(url, object_type, **kwargs)
+        return loaded_object
+                
 
 
-def load_extractor(file_or_folder_or_dict, base_folder=None) -> "BaseExtractor | SortingAnalyzer":
+def load_extractor(file_or_folder_or_dict, base_folder=None) -> "BaseExtractor":
     warnings.warn(
         "load_extractor() is deprecated and will be removed in the future. Please use load() instead.",
         DeprecationWarning,
         stacklevel=2,
     )
     return load(file_or_folder_or_dict, base_folder=base_folder)
+
+
+def _guess_object_from_dict(d):
+    """
+    When an object is read from json or pickle or zarr attr we can guess which object it is
+    """
+
+    # SortingAnalyzer and Motion
+    if d.get("object") is not None:
+        # the case is explicit.
+        # SortingAnalyzer and Motion used to implement this from the start of implementing
+        return d["object"]
+    
+    # Template
+    is_template = True
+    for k in ("templates_array", "sparsity_mask", "channel_ids", "unit_ids"):
+        if k not in d:
+            is_template = False
+            break
+    if is_template:
+        return "Templates"
+
+    # Recording | Sorting (A BaseExtractor)
+    is_sorting_or_recording = True
+    for k in ("kwargs", "class", "version", "annotations"):
+        if k not in d:
+            is_sorting_or_recording = False
+            break
+    if is_sorting_or_recording:
+        return "Recording|Sorting"
+    
+    # Unknow
+    return None
+
+def _load_object_from_dict(d, object_type, base_folder=None):
+    if object_type in ("Recording", "Sorting", "Recording|Sorting"):
+        return BaseExtractor.from_dict(d, base_folder=base_folder)
+    
+    elif object_type == "Templates":
+        from spikeinterface.core import Templates
+        return Templates.from_dict(d)
+
+    # elif object_type == "Motion":
+    # TODO to be implemented in Motion.from_dict
+
+
+
+
+def _guess_object_from_local_folder(folder):
+    folder = Path(folder)
+
+    if folder.suffix == ".zarr":
+        return _guess_object_from_zarr(folder)
+
+    if (folder / "spikeinterface_info.json").is_file():
+        # can be SortingAnalyzer | Motion
+        with open(folder / "spikeinterface_info.json", "r") as f:
+            spikeinterface_info = json.load(f)
+            return _guess_object_from_dict(spikeinterface_info)
+    elif (folder / "waveforms").is_dir():
+        # before the SortingAnlazer, it was WaveformsExtractor (v<0.101)
+        return "WaveformsExtractor"
+    elif (folder / f"si_folder.json").is_file():
+        # In later versions (0.94<v<0.102) we use the si_folder.json file
+        # This should be Recording | Sorting
+        return "Recording|Sorting"
+    else:
+        # For backward compatibility (v<=0.94) we check for the cached.json/pkl/pickle files
+        for dump_ext in ("json", "pkl", "pickle"):
+            f = folder / f"cached.{dump_ext}"
+            if f.is_file():
+                file = f
+        return "Recording|Sorting"
+
+
+def _load_object_from_folder(folder, object_type, **kwargs):
+    if object_type == "SortingAnalyzer":
+        from .sortinganalyzer import load_sorting_analyzer
+        analyzer = load_sorting_analyzer(folder, **kwargs)
+        return analyzer
+    
+    elif object_type == "Motion":
+        from spikeinterface.sortingcomponents.motion import Motion
+        motion = Motion.load(folder)
+        return motion
+
+    elif object_type == "Waveforms":
+        from .waveforms_extractor_backwards_compatibility import load_waveforms
+        analyzer = load_waveforms(folder, output="SortingAnalyzer")
+        return analyzer
+
+    elif object_type in ("Recording", "Sorting", "Recording|Sorting"):
+        if (folder / f"si_folder.json").is_file():
+            # In later versions (0.94<v<0.102) we use the si_folder.json file
+            # This should be Recording | Sorting
+            si_file = folder / f"si_folder.json"
+        else:
+            # For backward compatibility (v<=0.94) we check for the cached.json/pkl/pickle files
+            for dump_ext in ("json", "pkl", "pickle"):
+                f = folder / f"cached.{dump_ext}"
+                if f.is_file():
+                    si_file = f
+        return BaseExtractor.load(si_file, base_folder=folder)
+
+
+def _guess_object_from_zarr(zarr_folder):
+    # here it can be a zarr folder for Recording|Sorting|SortingAnalyzer|Template
+    from .zarrextractors import super_zarr_open
+    zarr_root = super_zarr_open(zarr_folder, mode="r")
+
+    # can be SortingAnalyzer
+    spikeinterface_info = zarr_root.attrs.get("spikeinterface_info")
+    if spikeinterface_info is not None:
+        return _guess_object_from_dict(spikeinterface_info)
+    
+    # here it is the old fashion and a bit ambiguous
+    if "channel_ids" in zarr_root.keys() and "unit_ids" in zarr_root.keys() and "nbefore" in zarr_root.keys() :
+        return "Templates"
+    elif "channel_ids" in zarr_root.keys() and "unit_ids" not in zarr_root.keys():
+        return "Recording"
+    elif "unit_ids" in zarr_root.keys() and "channel_ids" not in zarr_root.keys():
+        return "Sorting"
+
+
+def _load_object_from_zarr(folder_or_url, object_type,  **kwargs):
+
+    storage_options = kwargs.get("storage_options", None)
+
+    if object_type == "SortingAnalyzer":
+        from .sortinganalyzer import load_sorting_analyzer
+        analyzer = load_sorting_analyzer(folder_or_url, **kwargs)
+        return analyzer
+    
+    # elif object_type == "Motion":
+    #     No Motion in zarr
+
+    # elif object_type == "Waveforms":
+    #     No Waveforms in zarr
+
+    elif object_type == "Recording":
+        from .zarrextractors import read_zarr_recording
+        recording = read_zarr_recording(folder_or_url, storage_options=storage_options)
+        return recording
+    elif object_type == "Sorting":
+        from .zarrextractors import read_zarr_sorting
+        sorting = read_zarr_sorting(folder_or_url, storage_options=storage_options)
+        return sorting
+    elif object_type == "Recording|Sorting":
+        # This case shoudl deprecated soon because the read_zarr is ultra ambiguous
+        # just testing if the zarrot contains unit_ids or channel_ids but many object also contains it (see template)!!!!
+        from .zarrextractors import read_zarr
+        rec_or_sorting = read_zarr(folder_or_url, storage_options=storage_options)
+        return rec_or_sorting
