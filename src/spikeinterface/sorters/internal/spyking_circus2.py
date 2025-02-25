72555308365fe4f41a3b62019d60a31a837844ca
Sebastien <pierre.yger@gmail.com>
2025-02-25 10:56:46 +0100

    Default

--- src/spikeinterface/sorters/internal/spyking_circus2.py
+++ src/spikeinterface/sorters/internal/spyking_circus2.py
@@ -56,9 +56,9 @@ class Spykingcircus2Sorter(ComponentsBasedSorter):
         "apply_preprocessing": True,
         "matched_filtering": True,
         "cache_preprocessing": {"mode": "memory", "memory_limit": 0.5, "delete_cache": True},
-        "chunk_preprocessing": {"memory_limit": 0.5},
+        "chunk_preprocessing": {"memory_limit": 0.01},
         "multi_units_only": False,
-        "job_kwargs": {"n_jobs": 0.01},
+        "job_kwargs": {"n_jobs": 0.75},
         "seed": 42,
         "debug": False,
     }
