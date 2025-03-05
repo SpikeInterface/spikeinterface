12b5edb4aa06bbb335e630e6c11dd87ee5c5ff84
Sebastien <pierre.yger@gmail.com>
2025-03-05 11:19:55 +0100

    Reverting

--- src/spikeinterface/sorters/external/tridesclous.py
+++ src/spikeinterface/sorters/external/tridesclous.py
@@ -64,7 +64,9 @@ class TridesclousSorter(BaseSorter):
         except ImportError:
             HAVE_TDC = False
         except:
-            print("tridesclous is installed, but it has some dependency problems, check numba install!")
+            print(
+                "tridesclous is installed, but it has some dependency problems, check numba or hdbscan installations!"
+            )
             HAVE_TDC = False
         return HAVE_TDC
 
