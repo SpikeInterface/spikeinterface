4d52f77f1f1dcc7d2b838cccde4344b1e4615137
Alessio Buccino <alejoe9187@gmail.com>
2025-03-06 10:33:03 +0100

    Add si icon and logo

--- doc/conf.py
+++ doc/conf.py
@@ -109,9 +109,9 @@ except ImportError:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-html_favicon = "images/favicon-32x32.png"
+html_static_path = ['_static']
+html_favicon = "_static/spikeinterface.ico"
+html_logo = "_static/spikeinterface.png"
 
 
 from sphinx_gallery.sorting import ExplicitOrder
