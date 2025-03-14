546e336b66078cbc1fbb5b366e822c65d54acae3
Samuel Garcia <sam.garcia.die@gmail.com>
2025-03-14 18:59:11 +0100

    graph new option : loop over channels or vertical_bins

--- src/spikeinterface/sortingcomponents/clustering/graph_clustering.py
+++ src/spikeinterface/sortingcomponents/clustering/graph_clustering.py
@@ -16,32 +16,83 @@ class GraphClustering:
     Then a classic algorithm like louvain or hdbscan is used.
     """
 
+    # _default_params = {
+    #     "radius_um": 180.,
+    #     "bin_um": 60.,
+    #     "motion": None,
+    #     "seed": None,
+    #     "n_neighbors": 15,
+    #     # "clustering_method": "leidenalg",
+    #     "clustering_method": "sknetwork-leiden",
+    # }
+
+
     _default_params = {
-        "radius_um": 180.,
-        "bin_um": 60.,
+        "radius_um": 140.,
+        # "bin_um": 30.,
+
+        # "radius_um": 180.,
+        # "bin_um": 60.,
+
+
+        "ms_before" : 1.,
+        "ms_after" : 2,
         "motion": None,
         "seed": None,
-        "n_neighbors": 100,
-        # "clustering_method": "leidenalg",
-        "clustering_method": "sknetwork-leiden",
+        "graph_kwargs" : dict(
+            bin_mode="channels",
+            neighbors_radius_um=60.,
+
+            # bin_mode="vertical_bins",
+            # bin_um=30.,
+            # direction="y",
+
+            normed_distances=True,
+            # n_neighbors=15,
+            n_neighbors=25,
+            # n_components=0.8,
+            n_components=10,
+            sparse_mode="knn",
+            # sparse_mode="connected_to_all_neighbors"
+            apply_local_svd=True,
+            enforce_diagonal_to_zero=True,
+        ),
+        "clustering_method": "hdbscan",
+        "clustering_kwargs" : dict(
+            min_samples=1,
+            # n_jobs=-1,
+            core_dist_n_jobs=-1,
+            min_cluster_size=50,
+            # cluster_selection_method='leaf',
+            cluster_selection_method='eom',
+            allow_single_cluster=True
+        ),
+        "peak_locations" : None,
+        # "extract_peaks_svd_kwargs" : dict()
     }
+
+
+
 
     @classmethod
     def main_function(cls, recording, peaks, params, job_kwargs=dict()):
 
         radius_um = params["radius_um"]
-        bin_um = params["bin_um"]
+        # bin_um = params["bin_um"]
         motion = params["motion"]
         seed = params["seed"]
-        n_neighbors = params["n_neighbors"]
         clustering_method = params["clustering_method"]
-        normed_distances = params["normed_distances"]
+        clustering_kwargs = params["clustering_kwargs"]
+        graph_kwargs  = params["graph_kwargs"]
 
         motion_aware = motion is not None
 
-        assert radius_um >= bin_um * 3
 
-        
+        if graph_kwargs["bin_mode"] == "channels":
+            assert radius_um >= graph_kwargs["neighbors_radius_um"] * 2
+        elif graph_kwargs["bin_mode"] == "vertical_bins":
+            assert radius_um >= graph_kwargs["bin_um"] * 3
+
 
         peaks_svd, sparse_mask, _ = extract_peaks_svd(
             recording, peaks,
@@ -63,6 +114,9 @@ class GraphClustering:
         ordered_peaks_svd = peaks_svd[order]
 
         # TODO : try to use real peak location
+        
+        # some method need a symetric matrix
+        ensure_symetric = clustering_method in ("hdbscan", )
 
         distances = create_graph_from_peak_features(
             recording,
@@ -70,30 +124,24 @@ class GraphClustering:
             ordered_peaks_svd,
             sparse_mask,
             peak_locations=None,
-            bin_um=bin_um,
-            dim=1,
-            # mode="full_connected_bin",
-            mode="knn",
-            direction="y",
-            n_neighbors=n_neighbors,
-            apply_local_svd=True,
-            # apply_local_svd=False,
-            n_components=8,
-            normed_distances=True,
+            # bin_um=bin_um,
+            ensure_symetric=ensure_symetric,
+            **graph_kwargs
         )
 
         # print(distances)
         # print(distances.shape)
         # print("sparsity: ", distances.indices.size / (distances.shape[0]**2))        
 
-        distances_bool = distances.copy()
-        distances_bool.data[:] = 1
 
         print("clustering_method", clustering_method)
 
         if clustering_method == "networkx-louvain":
             # using networkx : very slow (possible backend with cude  backend="cugraph",)
             import networkx as nx
+
+            distances_bool = distances.copy()
+            distances_bool.data[:] = 1
             G = nx.Graph(distances_bool)
             communities = nx.community.louvain_communities(G, seed=seed)
             peak_labels = np.zeros(ordered_peaks.size, dtype=int)
@@ -108,19 +156,25 @@ class GraphClustering:
         elif clustering_method == "sknetwork-louvain":
             from sknetwork.clustering import Louvain
             classifier = Louvain()
+            distances_bool = distances.copy()
+            distances_bool.data[:] = 1
             peak_labels = classifier.fit_predict(distances_bool)
             _remove_small_cluster(peak_labels, min_size=1)
 
         elif clustering_method == "sknetwork-leiden":
             from sknetwork.clustering import Leiden
             classifier = Leiden()
+            distances_bool = distances.copy()
+            distances_bool.data[:] = 1
             peak_labels = classifier.fit_predict(distances_bool)
             _remove_small_cluster(peak_labels, min_size=1)
 
         elif clustering_method == "leidenalg":
             import leidenalg
             import igraph
-            graph = igraph.Graph.Weighted_Adjacency(distances_bool.tocoo(), mode='directed',)
+            adjacency = distances.copy()
+            adjacency.data = 1. - adjacency.data  
+            graph = igraph.Graph.Weighted_Adjacency(adjacency.tocoo(), mode='directed',)
             clusters = leidenalg.find_partition(graph, leidenalg.ModularityVertexPartition)
             peak_labels = np.array(clusters.membership)
             _remove_small_cluster(peak_labels, min_size=1)
@@ -128,12 +182,13 @@ class GraphClustering:
         elif clustering_method == "hdbscan":
             from hdbscan import HDBSCAN
             # from fast_hdbscan import HDBSCAN
+            # from sklearn.cluster import HDBSCAN
 
             import scipy.sparse
 
             
             # need to make subgraph
-            n_graph, connected_labels = scipy.sparse.csgraph.connected_components(distances)
+            n_graph, connected_labels = scipy.sparse.csgraph.connected_components(distances, directed=False)
 
             # print(np.unique(connected_labels))
             # print("n_graph", n_graph)
@@ -152,29 +207,23 @@ class GraphClustering:
                 has_neibhor = has_neibhor[:, 0]
 
                 rows = rows[has_neibhor]
-                local_dist = distances[rows, :][:, rows]
+                local_dist = distances[rows, :].tocsc()[:, rows].tocsr()
 
-                # make symetric
-                local_dist = local_dist.maximum(local_dist.T)
 
-                # # print(local_dist)
-                # res = hdbscan.hdbscan(local_dist, metric="precomputed")
-                # print(res[0])
-                # fig, ax = plt.subplots()
-                # ax.matshow(local_dist)
                 max_dist=(local_dist.max() - local_dist.min()) * 1000
 
+
+                import time
+                t0 = time.perf_counter()
                 clusterer = HDBSCAN(
                     metric="precomputed",
-                    min_cluster_size=50,
-                    cluster_selection_method= "eom",
-                    allow_single_cluster= True,
-                    metric_params=dict(
-                        max_distance=max_dist,
-                    ),
-                    core_dist_n_jobs=-1,
+                    **clustering_kwargs
                 )
                 local_labels = clusterer.fit_predict(local_dist)
+                t1 = time.perf_counter()
+                print("hdbscan", t1-t0)
+
+
 
                 mask = local_labels>=0
                 if np.sum(mask):
--- src/spikeinterface/sortingcomponents/clustering/graph_tools.py
+++ src/spikeinterface/sortingcomponents/clustering/graph_tools.py
@@ -13,14 +13,19 @@ def create_graph_from_peak_features(
     peak_features,
     sparse_mask,
     peak_locations=None,
+    bin_mode="channel",
+    neighbors_radius_um=60.,
+    # bin_mode="vertical_bins",
     bin_um=20.,
-    dim=1,
-    mode="full_connected_bin",
+    direction="y",
+    # sparse_mode="connected_to_all_neighbors",
+    sparse_mode="knn",
     apply_local_svd=False,
     n_components=10,
     normed_distances=False,
-    direction="y",
+    
     n_neighbors=20,
+    ensure_symetric=False,
     enforce_diagonal_to_zero=True,
     progress_bar=True,
 ):
@@ -46,69 +51,87 @@ def create_graph_from_peak_features(
 
     import scipy.sparse
     from scipy.spatial.distance import cdist
-    if mode == "knn":
+    if sparse_mode == "knn":
         from sklearn.neighbors import NearestNeighbors
     if apply_local_svd:
         from sklearn.decomposition import TruncatedSVD    
 
-    dim = "xyz".index(direction)
+
     channel_locations = recording.get_channel_locations()
-    channel_depth = channel_locations[:, dim]
+    channel_distances = np.linalg.norm(channel_locations[:, np.newaxis] - channel_locations[np.newaxis, :], axis=2)
 
-    if peak_locations is None:
-        # we use the max channel location instead
-        peak_depths = channel_depth[peaks["channel_index"]]
+    if bin_mode == "channels":
+        
+        loop = []
+        for channel_ind in range(channel_locations.shape[0]):
+            local_chans = np.flatnonzero(channel_distances[channel_ind, :]<=neighbors_radius_um)
+            neighbors_indices = np.flatnonzero(np.isin(peaks["channel_index"], local_chans))
+            target_local_inds = np.flatnonzero(peaks["channel_index"][neighbors_indices] == channel_ind)
+            # print(local_chans, neighbors_indices.size, target_local_inds.size)
+            loop.append((local_chans, neighbors_indices, target_local_inds))
+
+    
+    elif bin_mode == "vertical_bins":
+
+        dim = "xyz".index(direction)
+        channel_depth = channel_locations[:, dim]
+
+        if peak_locations is None:
+            # we use the max channel location instead
+            peak_depths = channel_depth[peaks["channel_index"]]
+        else:
+            peak_depths = peak_locations[direction]
+
+
+        loc0 = min(channel_depth)
+        loc1 = max(channel_depth)
+        eps = bin_um/10.
+        bins = np.arange(loc0-bin_um/2, loc1+bin_um/2+eps, bin_um)
+
+        bins[0] = -np.inf
+        bins[-1] = np.inf
+
+        loop = []
+        for b in range(bins.size-1):
+            
+            # limits for peaks
+            l0, l1 = bins[b], bins[b+1]
+            # mask = (peak_depths> l0) & (peak_depths<= l1)
+
+            # limits for features
+            b0, b1 = l0 - bin_um, l1 + bin_um
+            local_chans = np.flatnonzero((channel_locations[:, dim] >= (b0 )) & (channel_locations[:, dim] <= (b1)))
+
+            # print(l0, l1, b0, b1)
+
+            mask = (peak_depths>= b0) & (peak_depths< b1)
+            neighbors_indices = np.flatnonzero(mask)
+            # print(neighbors_indices.size)
+
+            local_depths = peak_depths[neighbors_indices]
+
+            target_mask = (local_depths >= l0) & (local_depths < l1)
+            target_local_inds = np.flatnonzero(target_mask)
+            # target_indices = neighbors_indices[target_local_inds]
+
+            loop.append((local_chans, neighbors_indices, target_local_inds))
+
+
+
     else:
-        peak_depths = peak_locations[direction]
+        raise ValueError("create_graph_from_peak_features : wrong bin_mode")
 
-    # todo center this bons like in peak localization
-    loc0 = min(channel_depth)
-    loc1 = max(channel_depth)
-    eps = bin_um/10.
-    bins = np.arange(loc0-bin_um/2, loc1+bin_um/2+eps, bin_um)
-
-    bins[0] = -np.inf
-    bins[-1] = np.inf
-
-    loop = range(bins.size-1)
     if progress_bar:
-        loop = tqdm(loop, desc="Construct distance graph")
+        loop = tqdm(loop, desc=f"Construct distance graph looping over {bin_mode}")
 
-    # indices0 = []
-    # indices1 = []
-    # data = []
     local_graphs = []
     row_indices = []
-    for b in loop:
-        
-        # limits for peaks
-        l0, l1 = bins[b], bins[b+1]
-        # mask = (peak_depths> l0) & (peak_depths<= l1)
-
-        # limits for features
-        b0, b1 = l0 - bin_um, l1 + bin_um
-        local_chans = np.flatnonzero((channel_locations[:, dim] >= (b0 )) & (channel_locations[:, dim] <= (b1)))
-
-        
-
-
-        # print(l0, l1, b0, b1)
-
-        mask = (peak_depths>= b0) & (peak_depths< b1)
-        peak_indices = np.flatnonzero(mask)
-        # print(peak_indices.size)
-
-        local_depths = peak_depths[peak_indices]
-
-        target_mask = (local_depths >= l0) & (local_depths < l1)
-        target_local_inds = np.flatnonzero(target_mask)
-        target_indices = peak_indices[target_local_inds]
-
-
+    for local_chans, neighbors_indices, target_local_inds in loop:
+        target_indices = neighbors_indices[target_local_inds]
         row_indices.append(target_indices)
 
         # print()
-        # print(target_indices.size, peak_indices.size)
+        # print(target_indices.size, neighbors_indices.size)
 
         # print(local_chans, sparse_mask.shape, peak_features.shape)
 
@@ -118,49 +141,51 @@ def create_graph_from_peak_features(
         
         # import time
         # t0 = time.perf_counter()
-        local_feats, dont_have_channels = aggregate_sparse_features(peaks, peak_indices,
+        local_feats, dont_have_channels = aggregate_sparse_features(peaks, neighbors_indices,
                                                                  peak_features, sparse_mask, local_chans)
         # t1 = time.perf_counter()
         # print("aggregate", t1-t0)
         # print(b)
         if np.sum(dont_have_channels) > 0:
-            print("dont_have_channels", np.sum(dont_have_channels), "for n=", peak_indices.size, "bin", b0, b1)
-        dont_have_channels_target = dont_have_channels[target_mask]
+            print("dont_have_channels", np.sum(dont_have_channels), "for n=", neighbors_indices.size, "bin", b0, b1)
+        # dont_have_channels_target = dont_have_channels[target_local_inds]
 
         flatten_feat = local_feats.reshape(local_feats.shape[0], -1)
 
 
         if apply_local_svd:
-            n_components = min(n_components, flatten_feat.shape[1])
-            tsvd = TruncatedSVD(n_components)
-            flatten_feat = tsvd.fit_transform(flatten_feat)
+            if isinstance(n_components, int):
+                n_components = min(n_components, flatten_feat.shape[1])
+                tsvd = TruncatedSVD(n_components)
+                flatten_feat = tsvd.fit_transform(flatten_feat)
 
-        if mode == "full_connected_bin":
-            # t0 = time.perf_counter()
-            local_dists = cdist(flatten_feat[target_mask], flatten_feat)
+                n_components = min(n_components, flatten_feat.shape[1])
+                tsvd = TruncatedSVD(n_components)
+                flatten_feat = tsvd.fit_transform(flatten_feat)
+            elif isinstance(n_components, float):
+                assert 0 < n_components < 1, "n_components should be in ]0, 1["
+                tsvd = TruncatedSVD(flatten_feat.shape[1])
+                flatten_feat = tsvd.fit_transform(flatten_feat)
+                n_explain = np.sum(np.cumsum(tsvd.explained_variance_ratio_) <= n_components) + 1
+                flatten_feat = flatten_feat[:, :n_explain]
+
+        if sparse_mode == "connected_to_all_neighbors":
+            local_dists = cdist(flatten_feat[target_local_inds], flatten_feat)
 
             if normed_distances:
                 norm = np.linalg.norm(flatten_feat, axis=1)
-                local_dists /= (norm[target_mask, None] + norm[None, :])
+                local_dists /= (norm[target_local_inds, None] + norm[None, :])
 
             data = local_dists.flatten().astype("float32")
             indptr = np.arange(0, local_dists.size + 1, local_dists.shape[1])
-            indices = np.concatenate([peak_indices] * target_indices.size )
+            indices = np.concatenate([neighbors_indices] * target_indices.size )
             local_graph = scipy.sparse.csr_matrix((data, indices, indptr), shape=(target_indices.size, peaks.size))
-            # t1 = time.perf_counter()
-            # print("make sparse", t1-t0)
-
             local_graphs.append(local_graph)
 
-        elif mode == "knn":
-
-
-            # t0 = time.perf_counter()
-            # from sklearn.neighbors import NearestNeighbors
-
+        elif sparse_mode == "knn":
             nn_tree = NearestNeighbors(n_neighbors=min(n_neighbors, target_local_inds.size), metric="minkowski", p=2) # euclidean
             nn_tree.fit(flatten_feat)
-            local_sparse_dist = nn_tree.kneighbors_graph(flatten_feat[target_mask], mode='distance')
+            local_sparse_dist = nn_tree.kneighbors_graph(flatten_feat[target_local_inds], mode='distance')
 
 
             # t1 = time.perf_counter()
@@ -179,7 +204,7 @@ def create_graph_from_peak_features(
                     tgt = flatten_feat[local_sparse_dist.indices[a:b]]
                     norm = (np.linalg.norm(src, 1) + np.linalg.norm(tgt, 1, axis=1))
                     data[a:b] /= norm
-            indices = peak_indices[local_sparse_dist.indices]
+            indices = neighbors_indices[local_sparse_dist.indices]
             local_graph = scipy.sparse.csr_matrix((data, indices, indptr), shape=(target_indices.size, peaks.size))
 
 
@@ -204,7 +229,7 @@ def create_graph_from_peak_features(
         # t1 = time.perf_counter()
         # print("row_order", t1 - t0)
 
-        if mode == "knn":
+        if sparse_mode == "knn":
             # t0 = time.perf_counter()
             distances = scipy.sparse.csr_matrix(distances)
             # t1 = time.perf_counter()
@@ -216,5 +241,10 @@ def create_graph_from_peak_features(
 
     else:
         distances = scipy.sparse.csr_matrix(([], ([], [])), shape=(peaks.size, peaks.size), dtype="float32")
+    
+    if ensure_symetric:
+        # because of the way the graph is done the distance matrix could be not symetric
+        # this trick force the symetry
+        distances = distances.maximum(distances.T)
 
     return distances
