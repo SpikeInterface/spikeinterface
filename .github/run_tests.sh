1a983a6c2e9976f277d182b0960b927fd5b2bf16
JoeZiminski <joseph.j.ziminski@gmail.com>
2025-03-14 22:55:58 +0000

    Try pytest-xdist.

--- .github/run_tests.sh
+++ .github/run_tests.sh
@@ -8,7 +8,7 @@ if [ -z "$NOVIRTUALENV" ] || [ "$NOVIRTUALENV" != "--no-virtual-env" ]; then
   source $GITHUB_WORKSPACE/test_env/bin/activate
 fi
 
-pytest -m "$MARKER" -vv -ra --durations=0 --durations-min=0.001 | tee report.txt; test ${PIPESTATUS[0]} -eq 0 || exit 1
+pytest -m "$MARKER" -n auto  --dist loadfile -vv -ra --durations=0 --durations-min=0.001 | tee report.txt; test ${PIPESTATUS[0]} -eq 0 || exit 1
 echo "# Timing profile of ${MARKER}" >> $GITHUB_STEP_SUMMARY
 python $GITHUB_WORKSPACE/.github/scripts/build_job_summary.py report.txt >> $GITHUB_STEP_SUMMARY
 rm report.txt
