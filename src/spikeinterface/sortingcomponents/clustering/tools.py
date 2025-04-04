908be70ea846ca2fcee45900798e2c1819e6db20
Sebastien <pierre.yger@gmail.com>
2025-04-04 16:00:52 +0200

    WIP

--- src/spikeinterface/sortingcomponents/clustering/tools.py
+++ src/spikeinterface/sortingcomponents/clustering/tools.py
@@ -315,7 +315,7 @@ def get_templates_from_peaks_and_svd(recording,
                 data = np.mean(local_svd[sub_mask, :, count], 0)
             elif operator == 'median':
                 data = np.median(local_svd[sub_mask, :, count], 0)
-            templates_array[unit_ind, :, i] = svd_model.inverse_transform(data)
+            templates_array[unit_ind, :, i] = svd_model.inverse_transform(data.reshape(1,-1))
 
     templates = Templates(
         templates_array=templates_array,
