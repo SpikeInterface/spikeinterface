b312c105b85b36fa37e971c08b6ed7e87871daa1
luiz <luiz_tauffer@hotmail.com>
2025-04-14 10:31:31 +0200

    remove method, add nsx_to_load kwarg

--- src/spikeinterface/extractors/neoextractors/blackrock.py
+++ src/spikeinterface/extractors/neoextractors/blackrock.py
@@ -92,17 +92,18 @@ class BlackrockSortingExtractor(NeoBaseSortingExtractor):
     def __init__(
         self,
         file_path,
-        sampling_frequency: Optional[float] = None,
         stream_id: Optional[str] = None,
         stream_name: Optional[str] = None,
+        sampling_frequency: Optional[float] = None,
+        nsx_to_load: Optional[int | list | str] = None,
     ):
         neo_kwargs = self.map_to_neo_kwargs(file_path)
-        neo_kwargs["nsx_to_load"] = []  # Tell neo to not load the nsx files
         NeoBaseSortingExtractor.__init__(
             self,
-            sampling_frequency=sampling_frequency,
             stream_id=stream_id,
             stream_name=stream_name,
+            sampling_frequency=sampling_frequency,
+            nsx_to_load=nsx_to_load,
             **neo_kwargs,
         )
 
@@ -117,33 +118,6 @@ class BlackrockSortingExtractor(NeoBaseSortingExtractor):
     def map_to_neo_kwargs(cls, file_path):
         neo_kwargs = {"filename": str(file_path)}
         return neo_kwargs
-
-    def build_stream_id_to_sampling_frequency_dict(self) -> Dict[str, float]:
-        """
-        Build a mapping from stream_id to sampling frequencies.
-
-        This function creates a dictionary mapping each stream_id to its corresponding sampling
-        frequency, as extracted from the signal channels in the Neo header.
-
-        Returns
-        -------
-        dict of {str: float}
-            Dictionary mapping stream_ids to their corresponding sampling frequencies.
-
-        Raises
-        ------
-        AssertionError
-            If there are no signal streams available from which to extract the sampling frequencies.
-        """
-        neo_header = self.neo_reader.header
-        if "spike_channels" in neo_header and neo_header["spike_channels"].size > 0:
-            channels = neo_header["spike_channels"]
-            channel_sampling_frequencies = channels["wf_sampling_rate"]
-            stream_to_sampling_frequencies = {0: float(np.unique(channel_sampling_frequencies)[0])}
-        else:
-            raise AssertionError("No signal or spike streams to infer the sampling frequency. Set it manually")
-
-        return stream_to_sampling_frequencies
 
 
 read_blackrock = define_function_from_class(source_class=BlackrockRecordingExtractor, name="read_blackrock")
