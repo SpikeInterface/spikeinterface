c43fa7c13943236150bfd761d4ba2b023e172f51
Jake Swann <jswann@axoft.us>
2025-05-12 11:56:31 -0400

    add test for validate_segment_indices

--- src/spikeinterface/widgets/tests/test_widgets_utils.py
+++ src/spikeinterface/widgets/tests/test_widgets_utils.py
@@ -1,4 +1,7 @@
-from spikeinterface.widgets.utils import get_some_colors
+import pytest
+
+from spikeinterface import generate_sorting
+from spikeinterface.widgets.utils import get_some_colors, validate_segment_indices
 
 
 def test_get_some_colors():
@@ -19,5 +22,34 @@ def test_get_some_colors():
     # print(colors)
 
 
+def test_validate_segment_indices():
+    # Setup
+    sorting_single = generate_sorting(durations=[5])  # 1 segment
+    sorting_multiple = generate_sorting(durations=[5, 10, 15, 20, 25])  # 5 segments
+    
+    # Test None with single segment
+    assert validate_segment_indices(None, sorting_single) == [0]
+    
+    # Test None with multiple segments
+    with pytest.warns(UserWarning):
+        assert validate_segment_indices(None, sorting_multiple) == [0]
+    
+    # Test valid indices
+    assert validate_segment_indices([0], sorting_single) == [0]
+    assert validate_segment_indices([0, 1, 4], sorting_multiple) == [0, 1, 4]
+    
+    # Test invalid type
+    with pytest.raises(TypeError):
+        validate_segment_indices(0, sorting_multiple)
+    
+    # Test invalid index type
+    with pytest.raises(ValueError):
+        validate_segment_indices([0, "1"], sorting_multiple)
+    
+    # Test out of range
+    with pytest.raises(ValueError):
+        validate_segment_indices([5], sorting_multiple)
+
 if __name__ == "__main__":
     test_get_some_colors()
+    test_validate_segment_indices()
