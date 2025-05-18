d6aefee614244b4c39d2cbf96d12be7c21d70c2c
chrishalcrow <chrishalcrow@gmail.com>
2025-05-18 14:38:49 +0100

    Move recording type difference

--- src/spikeinterface/preprocessing/tests/test_motion.py
+++ src/spikeinterface/preprocessing/tests/test_motion.py
@@ -32,6 +32,13 @@ def test_estimate_and_correct_motion(create_cache_folder):
     assert motion_info_loaded["motion"] == motion_info["motion"]
 
 
+def test_estimate_and_correct_motion_int(create_cache_folder):
+
+    rec = generate_recording(durations=[30.0], num_channels=12).astype(int)
+    rec_corrected = correct_motion(rec, estimate_motion_kwargs={"win_step_um": 50, "win_scale_um": 100})
+    assert rec_corrected.get_dtype().kind == "f"
+
+
 def test_get_motion_parameters_preset():
     from pprint import pprint
 
