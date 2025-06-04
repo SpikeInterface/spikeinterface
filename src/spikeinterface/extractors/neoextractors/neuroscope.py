1313c55f0aee736c5d1d344e7f6cf7dc07f167e4
theodchrn <48409582+theodchrn@users.noreply.github.com>
2025-06-04 17:10:23 +0200

    fix: - change the channel and color property name to neuroscope_groups - method now need to be called by the user

--- src/spikeinterface/extractors/neoextractors/neuroscope.py
+++ src/spikeinterface/extractors/neoextractors/neuroscope.py
@@ -66,7 +66,6 @@ class NeuroScopeRecordingExtractor(NeoBaseRecordingExtractor):
             xml_file_path = str(Path(xml_file_path).absolute())
         self._kwargs.update(dict(file_path=str(Path(file_path).absolute()), xml_file_path=xml_file_path))
         self.xml_file_path = xml_file_path if xml_file_path is not None else Path(file_path).with_suffix(".xml")
-        self._set_groups()
 
     @classmethod
     def map_to_neo_kwargs(cls, file_path, xml_file_path=None):
@@ -121,7 +120,7 @@ class NeuroScopeRecordingExtractor(NeoBaseRecordingExtractor):
 
         return channel_groups, kept_channels, discarded_channels, anatomycolors
 
-    def _set_groups(self):
+    def _set_neuroscope_groups(self):
         """
         Set the group ids and colors based on the xml file.
         These group ids are usually different brain/body anatomical areas, or shanks from multi-shank probes.
@@ -138,6 +137,13 @@ class NeuroScopeRecordingExtractor(NeoBaseRecordingExtractor):
         discarded_ppty[discarded_channels] = True
         self.set_property("discarded_channels", discarded_ppty)
         self.set_property("colors", values=list(colors.values()), ids=list(colors.keys()))
+
+    def prepare_neuroscope_for_ephyviewer(self):
+        """
+        Prepare the recording extractor for ephyviewer by setting the group ids and colors.
+        This function is not called when the extractor is initialized, and the user must call it manually.
+        """
+        self._set_neuroscope_groups()
 
 
 class NeuroScopeSortingExtractor(BaseSorting):
