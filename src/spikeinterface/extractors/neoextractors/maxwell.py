df735f050fdd3c3b7e2261b2a959dfca91d498c2
cmccrimmon <103812790+cmccrimmon@users.noreply.github.com>
2025-06-30 10:11:55 -0700

    Update maxwell.py

--- src/spikeinterface/extractors/neoextractors/maxwell.py
+++ src/spikeinterface/extractors/neoextractors/maxwell.py
@@ -52,7 +52,7 @@ class MaxwellRecordingExtractor(NeoBaseRecordingExtractor):
         block_index=None,
         all_annotations=False,
         rec_name=None,
-        install_maxwell_plugin=False,
+        install_maxwell_plugin=True,
         use_names_as_ids: bool = False,
     ):
         if install_maxwell_plugin:
