8ab3e534fda28b6d2ccef71365639eb2f4015627
chrishalcrow <chrishalcrow@gmail.com>
2025-07-15 07:03:09 -0400

    respond to alessio

--- examples/tutorials/forhowto/plot_1_working_with_tetrodes.py
+++ examples/tutorials/forhowto/plot_1_working_with_tetrodes.py
@@ -29,14 +29,14 @@ recording, _ = generate_ground_truth_recording(
 
 ##############################################################################
 # We now need to define the probe. This will tell the recording which channels came from
-# which tetrode. To do this, we will use the `generate_tetrode` function from `ProbeInterface`
+# which tetrode. To do this, we will use the :code:`generate_tetrode` function from :code:`ProbeInterface`
 # to generate two 4-channel probes (representing one tetrode each). In our case, since we
 # don't know the relative distances between the tetrodes, we will move the second
 # tetrode away from the first by 100 microns. This is just so we can visualize the
 # results more easily. Eventually, we will sort each tetrode separately, so their
 # relative distance won't affect the results.
 
-# Technically, we will add each tetrode to a `ProbeGroup`. Read more in the ProbeInterface
+# Technically, we will add each tetrode to a :code:`ProbeGroup`. Read more in the ProbeInterface
 # docs.
 
 # Create each individual tetrode
@@ -59,49 +59,53 @@ tetrode_group.add_probe(tetrode_2)
 tetrode_group.set_global_device_channel_indices(range(8))
 
 ##############################################################################
-# We can now attach the `tetrode_group` to our recording. To check this worked, we'll
+# We can now attach the :code:`tetrode_group` to our recording. To check this worked, we'll
 # plot the probe map
 
 recording_with_probe = recording.set_probegroup(tetrode_group)
 plot_probe_map(recording_with_probe)
 
 ##############################################################################
-# Looks good! Many people preprocess and sort their tetrode data separately for
-# each tetrode. When we use `set_probegroup`, the channels are automatically
-# labelled by which probe in the probe group they belong to. We can access
-# this labeling using the "group" property.
+# Looks good! Now that the recording is aware of the probe geometry, we can
+# begin a standard spike sorting pipeline. First, we can apply preprocessing.
+# Note that we apply this preprocessing on the entire bundle of tetrodes.
+# This is appropriate for a common reference filter because the tetrodes
+# share the same background noise.
 
-print(recording_with_probe.get_property("group"))
-
-# We can then use this information to split the recording by the group property:
-
-grouped_recordings = recording_with_probe.split_by('group')
-print(grouped_recordings)
-
-##############################################################################
-# To read more about using grouped recordings and sortings, see :ref:`recording-by-channel-group`.
-
-# Tetrodes often have dead channels, so it is advised to try and detect
-# and remove these
-
-recording_good_channels = spre.detect_and_remove_bad_channels(recording_with_probe)
-
-##############################################################################
-# Now that we have a grouped recording with only good channels, we can start a
-# standard spike sorting pipeline. For instance, we can do some preprocessing
-# and plot the traces.
-
-preprocessed_recording = spre.common_reference(
-    spre.bandpass_filter(
-        grouped_recordings
+preprocessed_recording = spre.bandpass_filter(
+    spre.common_reference(
+        recording_with_probe
     )
 )
 
-print(preprocessed_recording)
-plot_traces(preprocessed_recording[0])
+##############################################################################
+# Tetrodes often have dead channels, so it is advised to try and detect
+# and remove these
+
+recording_good_channels = spre.detect_and_remove_bad_channels(preprocessed_recording)
 
 ##############################################################################
-# We are now ready to sort. To read more about sorting by group, see
+# It can be a good idea to sort your tetrode data separately for each tetrode.
+# When we use :code:`set_probegroup`, the channels are automatically
+# labelled by which probe in the probe group they belong to. We can access
+# this labeling using the "group" property.
+
+print(recording_good_channels.get_property("group"))
+
+##############################################################################
+# We can then use this information to split the recording by the group property:
+
+grouped_recordings = recording_good_channels.split_by('group')
+print(grouped_recordings)
+
+##############################################################################
+# Now that we've got preprocess, clean data. Let's take a look at a
+# snippet of data from the first group:
+
+plot_traces(grouped_recordings[0])
+
+##############################################################################
+# Beautiful! We are now ready to sort. To read more about sorting by group, see
 # :ref:`sorting-by-channel-group`. Note that many modern sorters are designed
 # to sort data from high-density probes and will fail for tetrodes. Please read
 # each spike sorter's documentation to find out if it is appropriate for tetrodes.
