12351ffe5355ee39112b97295f5f05a96b556176
chrishalcrow <chrishalcrow@gmail.com>
2025-07-25 17:25:42 +0100

    remove metadata from tests

--- src/spikeinterface/exporters/tests/test_export_to_pynapple.py
+++ src/spikeinterface/exporters/tests/test_export_to_pynapple.py
@@ -8,11 +8,8 @@ from spikeinterface.exporters import to_pynapple_tsgroup
 
 def test_export_analyzer_to_pynapple():
     """
-    Checks to see `to_pynapple_tsgroup` works using a generated sorting analyzer
-    with and without metadata.
+    Checks to see `to_pynapple_tsgroup` works using a generated sorting analyzer.
     Then checks that it works when units are not simply 0,1,2,3... .
-    Then tests if error is raised when there is a key mismatch between
-    metadata and sorting analyzer.
     """
 
     # import inside as pandas not a core dependency
@@ -26,12 +23,6 @@ def test_export_analyzer_to_pynapple():
 
     a_TsGroup = to_pynapple_tsgroup(analyzer)
 
-    unit_labels = ["a", "a", "b", "b", "b", "a"]
-    full_metadata = pd.DataFrame(unit_labels, columns=["unit_label"])
-    a_TsGroup_with_metadata = to_pynapple_tsgroup(analyzer, metadata=full_metadata)
-
-    assert np.all(a_TsGroup_with_metadata["unit_label"].values == unit_labels)
-
     assert np.all(a_TsGroup.index == int_unit_ids)
 
     subset_of_unit_ids = analyzer.unit_ids[[1, 3, 5]]
@@ -41,9 +32,6 @@ def test_export_analyzer_to_pynapple():
     a_sub_TsGroup = to_pynapple_tsgroup(subset_analyzer)
 
     assert np.all(a_sub_TsGroup.index == int_subset_of_unit_ids)
-
-    with pytest.raises(ValueError):
-        to_pynapple_tsgroup(subset_analyzer, metadata=full_metadata)
 
     # now test automatic metadata
     subset_analyzer.compute(["random_spikes", "templates", "unit_locations"])
