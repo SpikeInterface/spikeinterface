04450b22aa9882324732a3c008b1d20035f6392b
Samuel Garcia <sam.garcia.die@gmail.com>
2025-10-24 10:05:52 +0200

    oups

--- src/spikeinterface/generation/splitting_tools.py
+++ src/spikeinterface/generation/splitting_tools.py
@@ -60,7 +60,7 @@ def split_sorting_by_times(
         ind_mask = spike_indices[0][unit_id]
         m = np.median(spikes[0][ind_mask]["sample_index"])
         time_mask = spikes[0][ind_mask]["sample_index"] > m
-        mask = time_mask & (rng.rand(len(ind_mask)) <= partial_split_prob).astype(bool)
+        mask = time_mask & (rng.uniform(size=len(ind_mask)) <= partial_split_prob).astype(bool)
         new_index = int(unit_id) * np.ones(len(mask), dtype=bool)
         new_index[mask] = max_index + 1
         new_spikes["unit_index"][ind_mask] = new_index
@@ -135,7 +135,7 @@ def split_sorting_by_amplitudes(
         ind_mask = spike_indices[0][unit_id]
         thresh = np.median(amplitudes[ind_mask])
         amplitude_mask = amplitudes[ind_mask] > thresh
-        mask = amplitude_mask & (rng.rand(len(ind_mask)) <= partial_split_prob).astype(bool)
+        mask = amplitude_mask & (rng.uniform(size=len(ind_mask)) <= partial_split_prob).astype(bool)
         new_index = int(unit_id) * np.ones(len(mask))
         new_index[mask] = max_index + 1
         new_spikes["unit_index"][ind_mask] = new_index
