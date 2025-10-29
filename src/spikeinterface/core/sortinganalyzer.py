6cfed6227ee15787316f33eeb8cdd11925881aa4
ZhangYiqin <312065559@qq.com>
2025-10-29 21:44:55 +0800

    Improve error message for compute_several_extensions

--- src/spikeinterface/core/sortinganalyzer.py
+++ src/spikeinterface/core/sortinganalyzer.py
@@ -1641,7 +1641,8 @@ extension_params={"waveforms":{"ms_before":1.5, "ms_after": "2.5"}}\
         elif isinstance(input, list):
             params_, job_kwargs = split_job_kwargs(kwargs)
             assert len(params_) == 0, (
-                "Too many arguments for SortingAnalyzer.compute_several_extensions(), " f"they are: {set(params_)}"
+                "Too many arguments for SortingAnalyzer.compute_several_extensions(), "
+                f"please remove the arguments {set(params_)} from the compute function."
             )
             extensions = {k: {} for k in input}
             if extension_params is not None:
