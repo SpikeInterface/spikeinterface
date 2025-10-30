6adef7e71a73695c39f3e559553fab2e4486b8cc
Alessio Buccino <alejoe9187@gmail.com>
2025-10-30 16:52:05 +0100

    expose seed param for nn_advanced

--- src/spikeinterface/metrics/quality/pca_metrics.py
+++ src/spikeinterface/metrics/quality/pca_metrics.py
@@ -230,7 +230,7 @@ def _nn_advanced_metric_function(sorting_analyzer, unit_ids, tmp_data, job_kwarg
     n_jobs = job_kwargs.get("n_jobs", 1)
     progress_bar = False
     mp_context = job_kwargs.get("mp_context", None)
-    seed = job_kwargs.get("seed", None)
+    seed = metric_params.get("seed", None)
 
     if sorting_analyzer.format == "memory" and n_jobs > 1:
         warnings.warn(
@@ -299,6 +299,7 @@ class NearestNeighborAdvancedMetrics(BaseMetric):
         "radius_um": 100,
         "peak_sign": "neg",
         "min_spatial_overlap": 0.5,
+        "seed": None,
     }
     metric_columns = {"nn_isolation": float, "nn_noise_overlap": float}
     depend_on = ["principal_components", "waveforms", "templates"]
