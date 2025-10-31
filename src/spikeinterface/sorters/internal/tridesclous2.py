85eabb371dba79f5db35a7e1d60f0c0cd3094ff6
Samuel Garcia <sam.garcia.die@gmail.com>
2025-10-31 11:29:48 +0100

    tdc sc versions

--- src/spikeinterface/sorters/internal/tridesclous2.py
+++ src/spikeinterface/sorters/internal/tridesclous2.py
@@ -84,7 +84,7 @@ class Tridesclous2Sorter(ComponentsBasedSorter):
 
     @classmethod
     def get_sorter_version(cls):
-        return "2025.09"
+        return "2025.10"
 
     @classmethod
     def _run_from_folder(cls, sorter_output_folder, params, verbose):
