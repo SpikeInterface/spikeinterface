8612117f10514eb60756141f035609c60aec32e6
Théo H. <62891573+tayheau@users.noreply.github.com>
2025-11-26 10:09:47 +0100

    Rename channel_filter to channel_filters for clarity
    Use of plural data only for better intuitivity - removed str

--- src/spikeinterface/preprocessing/detect_bad_channels.py
+++ src/spikeinterface/preprocessing/detect_bad_channels.py
@@ -72,10 +72,10 @@ neighborhood_r2_threshold : float, default: 0.95
     R^2 threshold for the neighborhood_r2 method.
 neighborhood_r2_radius_um : float, default: 30
     Spatial radius below which two channels are considered neighbors in the neighborhood_r2 method.
-seed : int or None, default: None
+seed : int | None, default: None
     The random seed to extract chunks
-channel_filter : str or set or None, default: None
-    For coherence+psd - filter the ids of the returned channels based on their labels.
+channel_filters : set | None, default: None
+    For coherence+psd - only return `bad_channel_ids` whose labels are in the set `channel_filter`.
 """
 
 
@@ -173,7 +173,7 @@ def detect_bad_channels(
     neighborhood_r2_threshold: float = 0.9,
     neighborhood_r2_radius_um: float = 30.0,
     seed: int | None = None,
-    channel_filter: str | set | None = None,
+    channel_filters: set | None  = None,
 ):
     """
     Perform bad channel detection.
@@ -306,23 +306,20 @@ def detect_bad_channels(
 
         allowed_filters = {"dead", "noise", "out"}
 
-        if channel_filter is None:
-            channel_filter = allowed_filters
+        if channel_filters is None:
+            channel_filters = allowed_filters
 
-        if not isinstance(channel_filter, set):
-            if isinstance(channel_filter, (str)):
-                channel_filter = {channel_filter}
-            else:
-                raise ValueError(
-                    f"The channel_filter must be None, str or a set of the following values : {allowed_filters} "
-                )
-
-        if not channel_filter.issubset(allowed_filters):
+        if not isinstance(channel_filters, set):
             raise ValueError(
-                f"The following channel_filters do not exists : {channel_filter.difference(allowed_filters)}. Please use one of the following instead {allowed_filters}."
+                f"channel_filters must be None or a set of the following values : {allowed_filters} "
             )
 
-        filtered_bad_channel_mask = np.isin(channel_labels, list(channel_filter))
+        if not channel_filters.issubset(allowed_filters):
+            raise ValueError(
+                f"{channel_filter.difference(allowed_filters)} is not a valid argument of channel_filters. Please use one of the following instead {allowed_filters}."
+            )
+
+        filtered_bad_channel_mask = np.isin(channel_labels, list(channel_filters))
         bad_channel_ids = recording.channel_ids[filtered_bad_channel_mask]
 
     elif method == "neighborhood_r2":
