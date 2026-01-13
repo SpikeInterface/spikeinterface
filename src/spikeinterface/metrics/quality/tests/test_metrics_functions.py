3f93f97618930203aadb724dda7a50a53e57b4b6
Alessio Buccino <alejoe9187@gmail.com>
2026-01-13 16:42:16 +0100

    Implement select_segment_periods in core

--- src/spikeinterface/metrics/quality/tests/test_metrics_functions.py
+++ src/spikeinterface/metrics/quality/tests/test_metrics_functions.py
@@ -12,8 +12,11 @@ from spikeinterface.core import (
     synthesize_random_firings,
 )
 
-from spikeinterface.metrics.utils import create_ground_truth_pc_distributions
+from spikeinterface.metrics.quality.utils import create_ground_truth_pc_distributions
 
+# from spikeinterface.metrics.quality_metric_list import (
+#     _misc_metric_name_to_func,
+# )
 
 from spikeinterface.metrics.quality import (
     get_quality_metric_list,
