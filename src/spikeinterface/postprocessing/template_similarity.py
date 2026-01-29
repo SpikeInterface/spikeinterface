3a6e9549da9358364720ac40b627dd957ad9e66a
Pierre Yger <pierre.yger@gmail.com>
2026-01-29 08:51:38 +0100

    WIP

--- src/spikeinterface/postprocessing/template_similarity.py
+++ src/spikeinterface/postprocessing/template_similarity.py
@@ -55,6 +55,8 @@ class ComputeTemplateSimilarity(AnalyzerExtension):
             # make compatible analyzer created between february 24 and july 24
             self.params["max_lag_ms"] = 0.0
             self.params["support"] = "union"
+        if "lags" not in self.data:
+            self.data["lags"] = np.zeros_like(self.data["similarity"], dtype=np.int32)
 
     def _set_params(self, method="cosine", max_lag_ms=0, support="union"):
         if method == "cosine_similarity":
@@ -180,7 +182,7 @@ class ComputeTemplateSimilarity(AnalyzerExtension):
             s = self.data["similarity"][old_ind1, old_units_inds]
             similarity[unit_ind1, sub_units_inds] = s
             similarity[sub_units_inds, unit_ind1] = s
-
+            
             l = self.data["lags"][old_ind1, old_units_inds]
             lags[unit_ind1, sub_units_inds] = l
             lags[sub_units_inds, unit_ind1] = l
@@ -258,8 +260,8 @@ def _compute_similarity_matrix_numpy(
             tgt_templates = tgt_sliced_templates[overlapping_templates]
             for gcount, j in enumerate(overlapping_templates):
                 # symmetric values are handled later
-                # if same_array and j < i:
-                # no need exhaustive looping when same template
+                #if same_array and j < i:
+                    # no need exhaustive looping when same template
                 #   continue
                 src = src_template[:, local_mask[j]].reshape(1, -1)
                 tgt = (tgt_templates[gcount][:, local_mask[j]]).reshape(1, -1)
@@ -283,7 +285,7 @@ def _compute_similarity_matrix_numpy(
 
                 if same_array:
                     distances[num_shifts_both_sides - count - 1, j, i] = distances[count, i, j]
-
+        
     return distances
 
 
@@ -353,8 +355,8 @@ if HAVE_NUMBA:
 
                     j = overlapping_templates[gcount]
                     # symmetric values are handled later
-                    # if same_array and j < i:
-                    # no need exhaustive looping when same template
+                    #if same_array and j < i:
+                        # no need exhaustive looping when same template
                     #    continue
                     src = src_template[:, local_mask[j]].flatten()
                     tgt = (tgt_templates[gcount][:, local_mask[j]]).flatten()
@@ -393,7 +395,7 @@ if HAVE_NUMBA:
                     if same_array:
                         distances[num_shifts_both_sides - count - 1, j, i] = distances[count, i, j]
 
-            # if same_array and num_shifts != 0:
+            #if same_array and num_shifts != 0:
             #    distances[num_shifts_both_sides - count - 1] = distances[count].T
 
         return distances
