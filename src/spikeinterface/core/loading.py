1a95c6e1788692c9e7b916e102318230b1fa6526
Alessio Buccino <alejoe9187@gmail.com>
2026-02-11 12:27:13 +0100

    Apply suggestion from @alejoe91

--- src/spikeinterface/core/loading.py
+++ src/spikeinterface/core/loading.py
@@ -14,7 +14,6 @@ _error_msg = (
 
 def load(
     file_or_folder_or_dict,
-    ignore_warnings=False,
     **kwargs,
     # load_extensions=True, backend_options=None
 ) -> "BaseExtractor | SortingAnalyzer | Motion | Template":
