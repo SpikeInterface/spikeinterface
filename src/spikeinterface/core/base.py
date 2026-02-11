fc60df434f0733c9d812d0417445edfae317ab1a
chrishalcrow <chrishalcrow@gmail.com>
2026-02-11 10:09:14 +0000

    use futures for BaseExtractor

--- src/spikeinterface/core/base.py
+++ src/spikeinterface/core/base.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pathlib import Path
 import shutil
 from typing import Any
@@ -144,7 +146,7 @@ class BaseExtractor:
         # This is implemented in BaseRecording or BaseSorting
         raise NotImplementedError
 
-    def get_parent(self) -> "BaseExtractor" | None:
+    def get_parent(self) -> BaseExtractor | None:
         """Returns parent object if it exists, otherwise None"""
         return getattr(self, "_parent", None)
 
@@ -379,7 +381,7 @@ class BaseExtractor:
 
     def copy_metadata(
         self,
-        other: "BaseExtractor",
+        other: BaseExtractor,
         only_main: bool = False,
         ids: Iterable | slice | None = None,
         skip_properties: Iterable[str] | None = None,
@@ -568,7 +570,7 @@ class BaseExtractor:
         return dump_dict
 
     @staticmethod
-    def from_dict(dictionary: dict, base_folder: Path | str | None = None) -> "BaseExtractor":
+    def from_dict(dictionary: dict, base_folder: Path | str | None = None) -> BaseExtractor:
         """
         Instantiate extractor from dictionary
 
@@ -622,7 +624,7 @@ class BaseExtractor:
             values = self.get_property(key)
             np.save(prop_folder / (key + ".npy"), values)
 
-    def clone(self) -> "BaseExtractor":
+    def clone(self) -> BaseExtractor:
         """
         Clones an existing extractor into a new instance.
         """
@@ -814,7 +816,7 @@ class BaseExtractor:
         file_path.write_bytes(pickle.dumps(dump_dict))
 
     @staticmethod
-    def load(file_or_folder_path: str | Path, base_folder: Path | str | bool | None = None) -> "BaseExtractor":
+    def load(file_or_folder_path: str | Path, base_folder: Path | str | bool | None = None) -> BaseExtractor:
         """
         Load extractor from file path (.json or .pkl)
 
@@ -837,7 +839,7 @@ class BaseExtractor:
         return (instance_constructor, intialization_args)
 
     @staticmethod
-    def load_from_folder(folder) -> "BaseExtractor":
+    def load_from_folder(folder) -> BaseExtractor:
         return BaseExtractor.load(folder)
 
     def _save(self, folder, **save_kwargs):
@@ -853,7 +855,7 @@ class BaseExtractor:
         # This implemented in BaseRecording for probe
         pass
 
-    def save(self, **kwargs) -> "BaseExtractor":
+    def save(self, **kwargs) -> BaseExtractor:
         """
         Save a SpikeInterface object.
 
@@ -889,7 +891,7 @@ class BaseExtractor:
 
     save.__doc__ = save.__doc__.format(_shared_job_kwargs_doc)
 
-    def save_to_memory(self, sharedmem=True, **save_kwargs) -> "BaseExtractor":
+    def save_to_memory(self, sharedmem=True, **save_kwargs) -> BaseExtractor:
         save_kwargs.pop("format", None)
 
         cached = self._save(format="memory", sharedmem=sharedmem, **save_kwargs)
@@ -1090,7 +1092,7 @@ class BaseExtractor:
         return cached
 
 
-def _load_extractor_from_dict(dic) -> "BaseExtractor":
+def _load_extractor_from_dict(dic) -> BaseExtractor:
     """
     Convert a dictionary into an instance of BaseExtractor or its subclass.
 
