c60b6a6d142e72e23fde25f6198c52968fe5ca43
Samuel Garcia <sam.garcia.die@gmail.com>
2026-02-12 12:01:24 +0100

    wip dartsort wrapper

--- src/spikeinterface/sorters/external/dartsort.py
+++ src/spikeinterface/sorters/external/dartsort.py
@@ -5,13 +5,18 @@ from ..basesorter import BaseSorter
 from ...core import NumpyFolderSorting
 
 class DartsortSorter(BaseSorter):
-    """Dasrtsort wrapper"""
+    """Dartsort wrapper"""
 
     sorter_name = "dartsort"
     requires_locations = False
     compatible_with_parallel = {"loky": False, "multiprocessing": False, "threading": False}
-
     sorter_description = "Dartsort is the Columbia university sorter made with love by Charlie Windolf and Liam Paninski's team."
+    installation_mesg = """\nTo use dartsort run:\n
+       >>> pip install dartsort
+
+    More information on mountainsort5 at:
+      * https://github.com/cwindolf/dartsort
+    """
 
     _default_params = {
     }
@@ -21,31 +26,25 @@ class DartsortSorter(BaseSorter):
 
     @classmethod
     def _dynamic_params(cls):
-        from darsort import DARTsortUserConfig
+        from dartsort import DARTsortUserConfig
         from pydantic import RootModel
         # the trick is to transform the DARTsortUserConfig  (a pydantic.dataclass) into a pydantic model
-        model = RootModel[DARTsortUserConfig](DARTsortUserConfig())
-        default_params = model.model_dump(mode='python')
-        # default_params_descriptions = 
+        Model = RootModel[DARTsortUserConfig]
+        # so we can dump to dict
+        cfg = Model(DARTsortUserConfig())
+        default_params = cfg.model_dump(mode='python')
+        # and retrieve properties
+        schema = Model.model_json_schema()
+        default_params_descriptions = {}
+        for k, props in schema['$defs']['DARTsortUserConfig']['properties'].items():
+            default_params_descriptions[k] = props['title']
 
         return default_params, default_params_descriptions
-
-
-
-    
-
-    installation_mesg = """\nTo use dartsort run:\n
-       >>> pip install dartsort
-
-    More information on mountainsort5 at:
-      * https://github.com/cwindolf/dartsort
-    """
 
     @classmethod
     def is_installed(cls):
         try:
             import dartsort
-
             HAVE_DARTSORT = True
         except ImportError:
             HAVE_DARTSORT = False
@@ -55,7 +54,6 @@ class DartsortSorter(BaseSorter):
     @staticmethod
     def get_sorter_version():
         import dartsort
-
         if hasattr(dartsort, "__version__"):
             return dartsort.__version__
         return "unknown"
@@ -66,38 +64,26 @@ class DartsortSorter(BaseSorter):
 
     @classmethod
     def _run_from_folder(cls, sorter_output_folder, params, verbose):
-        from dartsort.main import dartsort as dartsort_main
-        from dartsort.config import WaveformConfig, FeaturizationConfig, SubtractionConfig, TemplateConfig, MatchingConfig
+        from dartsort import dartsort as dartsort_main
+        from dartsort import DARTsortUserConfig
 
         recording = cls.load_recording_from_folder(sorter_output_folder.parent, with_warnings=False)
 
         # dartsort config are set using dataclass we need to map this
+        print(params)
+        cfg = DARTsortUserConfig(**params)
+        print(cfg)
         
-        waveform_config = WaveformConfig(**p["featurization"])
-        trough_offset_samples = waveform_config.trough_offset_samples
-        featurization_config = FeaturizationConfig(**p["featurization"])
-        subtraction_config = SubtractionConfig(trough_offset_samples=trough_offset_samples, **p["subtraction"])
-        template_config = TemplateConfig(trough_offset_samples=trough_offset_samples, **p["template"])
-        matching_config = MatchingConfig(trough_offset_samples=trough_offset_samples, **p["matching"])
-
-        sorting= dartsort_main(
+        sorting = dartsort_main(
             recording,
             sorter_output_folder,
-
-            featurization_config=featurization_config,
-            subtraction_config=subtraction_config,
-            
-
-            n_jobs=p["n_jobs"],
-            overwrite=False,
-            show_progress=verbose,
-            device=p["device"],
+            cfg,
         )
         
-        NumpyFolderSorting.write_sorting(sorting, sorter_output_folder / "final_sorting")
+        NumpyFolderSorting.write_sorting(sorting, sorter_output_folder / "final_darsort_sorting")
 
     @classmethod
     def _get_result_from_folder(cls, sorter_output_folder):
         sorter_output_folder = Path(sorter_output_folder)
-        sorting = NumpyFolderSorting(sorter_output_folder / "final_sorting")
+        sorting = NumpyFolderSorting(sorter_output_folder / "final_darsort_sorting")
         return sorting
