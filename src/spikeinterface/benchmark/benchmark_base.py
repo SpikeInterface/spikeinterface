b5c26a5280f34a9f749de7be608b165e7462dd0c
Pierre Yger <pierre.yger@gmail.com>
2026-03-23 11:53:01 +0100

    Refactoring

--- src/spikeinterface/benchmark/benchmark_base.py
+++ src/spikeinterface/benchmark/benchmark_base.py
@@ -308,7 +308,7 @@ class BenchmarkStudy:
             benchmark.save_run(bench_folder)
             benchmark.save_main(bench_folder)
 
-    def run(self, case_keys=None, keep=True, verbose=False, precomputed_results=None, **job_kwargs):
+    def run(self, case_keys=None, keep=True, verbose=False, **job_kwargs):
         if case_keys is None:
             case_keys = list(self.cases.keys())
 
@@ -329,21 +329,13 @@ class BenchmarkStudy:
             if verbose:
                 print("### Run benchmark", key, "###")
             t0 = time.perf_counter()
-            if key not in precomputed_results:
-                benchmark.run(**job_kwargs)
-            else:
-                for k, v in precomputed_results[key].items():
-                    benchmark.result[k] = v
+            benchmark.run(**job_kwargs)
             t1 = time.perf_counter()
             self.benchmarks[key] = benchmark
             bench_folder = self.folder / "results" / self.key_to_str(key)
             bench_folder.mkdir(exist_ok=True)
             benchmark.save_run(bench_folder)
-            if key not in precomputed_results:
-                benchmark.result["run_time"] = float(t1 - t0)
-            elif "run_time" in precomputed_results[key]:
-                benchmark.result["run_time"] = precomputed_results[key]["run_time"]
-
+            benchmark.result["run_time"] = float(t1 - t0)
             benchmark.save_main(bench_folder)
 
     def set_colors(self, colors=None, map_name="tab10", levels_to_group_by=None):
