6f8ba0f915b2a8dbc58797efe607b2a1f488a836
Alessio Buccino <alejoe9187@gmail.com>
2026-04-21 10:19:19 +0200

    add comment

--- src/spikeinterface/core/baserecording.py
+++ src/spikeinterface/core/baserecording.py
@@ -172,10 +172,9 @@ class BaseRecording(BaseRecordingSnippets, ChunkableMixin):
         """List of recording segments."""
         return self._segments
 
-    # For backward compatibility, we keep _recording_segments
     @property
     def _recording_segments(self) -> list["BaseRecordingSegment"]:
-        """List of recording segments."""
+        """For backward compatibility, we keep _recording_segments."""
         return self._segments
 
     def add_recording_segment(self, recording_segment: "BaseRecordingSegment") -> None:
