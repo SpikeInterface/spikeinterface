Fix file size calculation in `write_binary()`
`write_binary()` was determining the file size and seek offests based on
the source dtype's itemsize, but the data is cast to the target dtype before
writing. This means that if the target dtype has a different itemsize than
the source dtype, a corrupted file of the wrong size and layout was produced.

The solution was just just use the target dtype's itemsize for all
file size and seek offset calculations.

src/spikeinterface/core/time_series_tools.py
@@ -64,7 +64,7 @@ def write_binary(
 
     dtype = dtype if dtype is not None else time_series.get_dtype()
 
-    sample_size_bytes = time_series.get_sample_size_in_bytes()
+    sample_size_bytes = np.dtype(dtype).itemsize * time_series.get_num_channels()
 
     file_path_dict = {segment_index: file_path for segment_index, file_path in enumerate(file_path_list)}
     if file_timestamps_paths is not None:
@@ -125,7 +125,7 @@ def _write_binary_chunk(segment_index, start_frame, end_frame, worker_ctx):
     byte_offset = worker_ctx["byte_offset"]
     file = worker_ctx["file_dict"][segment_index]
     file_timestamps_dict = worker_ctx["file_timestamps_dict"]
-    sample_size_bytes = time_series.get_sample_size_in_bytes()
+    sample_size_bytes = dtype.itemsize * time_series.get_num_channels()
 
     # Calculate byte offsets for the start frames relative to the entire recording
     start_byte = byte_offset + start_frame * sample_size_bytes