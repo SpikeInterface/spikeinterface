2df4187212eab8ab785fe02ff400e91ae9e9490d
chrishalcrow <chrishalcrow@gmail.com>
2026-06-24 12:08:49 +0100

    change main_channel_peak_sign to peak_sign

--- src/spikeinterface/sortingcomponents/matching/tdc_peeler.py
+++ src/spikeinterface/sortingcomponents/matching/tdc_peeler.py
@@ -222,9 +222,7 @@ class TridesclousPeeler(BaseTemplateMatching):
             self.dtype = self.sparse_templates_array_static.dtype
 
         # as numpy vector
-        self.main_channels = templates.get_main_channels(
-            main_channel_peak_sign=peak_sign, outputs="index", with_dict=False
-        )
+        self.main_channels = templates.get_main_channels(peak_sign=peak_sign, outputs="index", with_dict=False)
 
         channel_locations = templates.probe.contact_positions
         unit_locations = channel_locations[self.main_channels]
