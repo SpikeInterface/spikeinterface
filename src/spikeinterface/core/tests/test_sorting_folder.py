d4c177839b80d8178cc49a85c4717fd0a89f1027
Samuel Garcia <sam.garcia.die@gmail.com>
2026-06-29 13:59:10 +0200

    go back to previous test_NpzFolderSorting

--- src/spikeinterface/core/tests/test_sorting_folder.py
+++ src/spikeinterface/core/tests/test_sorting_folder.py
@@ -38,18 +38,13 @@ def test_NpzFolderSorting(create_cache_folder):
     NpzFolderSorting.write_sorting(sorting, folder)
 
     sorting_loaded = NpzFolderSorting(folder)
-    # the NpzFolderSorting is a by unit storage and te lexsort is not maintain always so check_exact_lexsort=False
-    # check_sortings_equal(sorting_loaded, sorting, check_exact_lexsort=False)
-    check_sortings_equal(sorting_loaded, sorting, check_exact_lexsort=True)
-    assert np.array_equal(sorting_loaded.unit_ids, sorting.unit_ids)
+    check_sortings_equal(sorting_loaded, sorting)
 
-    # Note changing the class do not necessarily maintain the internal internal.
-    # but the vectors should be the same after lexsort
-    s1 = sorting_loaded.to_spike_vector()
-    s2 = sorting.to_spike_vector()
-    s1 = s1[np.lexsort((s1["unit_index"], s1["sample_index"], s1["segment_index"]))]
-    s2 = s2[np.lexsort((s2["unit_index"], s2["sample_index"], s2["segment_index"]))]
-    assert np.array_equal(s1, s2)
+    assert np.array_equal(sorting_loaded.unit_ids, sorting.unit_ids)
+    assert np.array_equal(
+        sorting_loaded.to_spike_vector(),
+        sorting.to_spike_vector(),
+    )
 
 
 if __name__ == "__main__":
