d1033c0cc31e036791232c4d937b49cf0517a04d
Garcia Samuel <sam.garcia.die@gmail.com>
2026-09-08 14:07:12 +0200

    Update src/spikeinterface/sorters/external/dartsort.py
    Co-authored-by: Chris Halcrow <57948917+chrishalcrow@users.noreply.github.com>

--- src/spikeinterface/sorters/external/dartsort.py
+++ src/spikeinterface/sorters/external/dartsort.py
@@ -10,7 +10,7 @@ class DartsortSorter(BaseSorter):
     sorter_name = "dartsort"
     requires_locations = False
     compatible_with_parallel = {"loky": False, "multiprocessing": False, "threading": False}
-    sorter_description = "Dartsort is the Columbia university sorter made with love by Charlie Windolf and Liam Paninski's team."
+    sorter_description = """Dartsort is a Drift Aware Registration and Tracking spike sorter, developed in the Paninski lab. Read the preprint "" by Windolf et. al. on biorxiv: https://www.biorxiv.org/content/10.1101/2023.08.11.553023v1 or find the code  on the GitHub repo: https://github.com/cwindolf/dartsort ."""
     installation_mesg = """\nTo use dartsort run:\n
        >>> pip install dartsort
 
